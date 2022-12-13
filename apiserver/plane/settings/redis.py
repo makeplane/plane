4414a71331754cacf44c651e85b8b3683efdfdb6
pablohashescobar <nikhilschacko@gmail.com>
2022-12-13 20:47:49 +0530

    build: updated config for redis and wait_for_db command

--- apiserver/plane/settings/redis.py
+++ apiserver/plane/settings/redis.py
@@ -6,17 +6,7 @@ from urllib.parse import urlparse
 
 def redis_instance():
     if settings.REDIS_URL:
-        tls_url = os.environ.get("REDIS_TLS_URL", False)
-        url = urlparse(settings.REDIS_URL)
-        if tls_url:
-            url = urlparse(tls_url)
-        ri = redis.Redis(
-            host=url.hostname,
-            port=url.port,
-            password=url.password,
-            ssl=True,
-            ssl_cert_reqs=None,
-        )
+        ri = redis.from_url(settings.REDIS_URL, db=0)
     else:
         ri = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=0)
 
