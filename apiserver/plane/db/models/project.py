78d4aa54678c8c243fbaa3325cee5630a31d0664
pablohashescobar <nikhilschacko@gmail.com>
2022-12-16 02:59:17 +0530

    feat: add icon for project

--- apiserver/plane/db/models/project.py
+++ apiserver/plane/db/models/project.py
@@ -53,6 +53,7 @@ class Project(BaseModel):
         null=True,
         blank=True,
     )
+    icon = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         """Return name of the project"""
