378199c663bb33e02704fe6c75e97bae9dd3ebbd
vamsi <vamsi.kurama@gmail.com>
2022-12-17 01:10:10 +0530

    dev: imports errors on casing issues

--- apps/app/layouts/DefaultLayout.tsx
+++ apps/app/layouts/DefaultLayout.tsx
@@ -2,7 +2,6 @@ import React from "react";
 
 // layouts
 import Container from "layouts/container";
-import DefaultTopBar from "layouts/navbar/DefaultTopBar";
 
 // types
 import type { Props } from "./types";
@@ -11,7 +10,6 @@ const DefaultLayout: React.FC<Props> = ({ meta, children }) => {
   return (
     <Container meta={meta}>
       <div className="w-full h-screen overflow-auto bg-gray-50">
-        {/* <DefaultTopBar /> */}
         <>{children}</>
       </div>
     </Container>
