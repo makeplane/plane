18ff810a145ef599377714372009824943181142
pablohashescobar <nikhilschacko@gmail.com>
2022-12-22 00:33:32 +0530

    feat: add plane docs to turbo

---
+++ apps/docs/src/pages/_app.jsx
@@ -0,0 +1,40 @@
+import Head from 'next/head'
+import { Router, useRouter } from 'next/router'
+import { MDXProvider } from '@mdx-js/react'
+
+import { Layout } from '@/components/Layout'
+import * as mdxComponents from '@/components/mdx'
+import { useMobileNavigationStore } from '@/components/MobileNavigation'
+
+import '@/styles/tailwind.css'
+import 'focus-visible'
+
+function onRouteChange() {
+  useMobileNavigationStore.getState().close()
+}
+
+Router.events.on('hashChangeStart', onRouteChange)
+Router.events.on('routeChangeComplete', onRouteChange)
+Router.events.on('routeChangeError', onRouteChange)
+
+export default function App({ Component, pageProps }) {
+  let router = useRouter()
+
+  return (
+    <>
+      <Head>
+        {router.pathname === '/' ? (
+          <title>Plane Documentation</title>
+        ) : (
+          <title>{`${pageProps.title} - Plane Documentation`}</title>
+        )}
+        <meta name="description" content={pageProps.description} />
+      </Head>
+      <MDXProvider components={mdxComponents}>
+        <Layout {...pageProps}>
+          <Component {...pageProps} />
+        </Layout>
+      </MDXProvider>
+    </>
+  )
+}
