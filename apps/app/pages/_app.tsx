3e13035eeb19baafef31423125428c96abb4e4c0
Dakshesh Jain <dakshesh.jain14@gmail.com>
2022-12-30 13:07:40 +0530

    refractor: moved all context provider to _app for more clarity

--- apps/app/pages/_app.tsx
+++ apps/app/pages/_app.tsx
@@ -2,18 +2,23 @@ import "../styles/globals.css";
 import "styles/editor.css";
 import type { AppProps } from "next/app";
 
-import GlobalContextProvider from "contexts/globalContextProvider";
+// context
+import { UserProvider } from "contexts/user.context";
+import { ToastContextProvider } from "contexts/toast.context";
+import { ThemeContextProvider } from "contexts/theme.context";
 
 import CommandPalette from "components/command-palette";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <GlobalContextProvider>
-      <>
-        <CommandPalette />
-        <Component {...pageProps} />
-      </>
-    </GlobalContextProvider>
+    <UserProvider>
+      <ToastContextProvider>
+        <ThemeContextProvider>
+          <CommandPalette />
+          <Component {...pageProps} />
+        </ThemeContextProvider>
+      </ToastContextProvider>
+    </UserProvider>
   );
 }
 
