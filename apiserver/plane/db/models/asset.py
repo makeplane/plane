87847a24091d3c2421046b74c7fffba17853ec2a
pablohashescobar <nikhilschacko@gmail.com>
2023-01-07 16:08:57 +0530

    feat: file size validator

--- apiserver/plane/db/models/asset.py
+++ apiserver/plane/db/models/asset.py
@@ -1,5 +1,6 @@
 # Django import
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Module import
 from . import BaseModel
@@ -9,14 +10,26 @@ def get_upload_path(instance, filename):
     return f"{instance.workspace.id}/{filename}"
 
 
+def file_size(value):
+    limit = 5 * 1024 * 1024
+    if value.size > limit:
+        raise ValidationError("File too large. Size should not exceed 5 MB.")
+
 class FileAsset(BaseModel):
     """
     A file asset.
     """
 
     attributes = models.JSONField(default=dict)
-    asset = models.FileField(upload_to=get_upload_path)
-    workspace = models.ForeignKey("db.Workspace", on_delete=models.CASCADE, null=True, related_name="assets")
+    asset = models.FileField(
+        upload_to=get_upload_path,
+        validators=[
+            file_size,
+        ],
+    )
+    workspace = models.ForeignKey(
+        "db.Workspace", on_delete=models.CASCADE, null=True, related_name="assets"
+    )
 
     class Meta:
         verbose_name = "File Asset"
