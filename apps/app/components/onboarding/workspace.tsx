56fdd2785ff4491692d1441b8cf17bc02858680b
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2023-01-10 22:27:17 +0530

    style: updated onboarding and create workspace page design

--- apps/app/components/onboarding/workspace.tsx
+++ apps/app/components/onboarding/workspace.tsx
@@ -138,7 +138,7 @@ const Workspace: React.FC<Props> = ({ setStep, setWorkspace }) => {
         <Tab.Panels>
           <Tab.Panel>
             <form className="mt-4 space-y-8" onSubmit={handleSubmit(handleCreateWorkspace)}>
-              <div className="w-full space-y-4 bg-white">
+              <div className="w-full space-y-4">
                 <div className="grid grid-cols-1 gap-4">
                   <div>
                     <Input
@@ -156,21 +156,24 @@ const Workspace: React.FC<Props> = ({ setStep, setWorkspace }) => {
                       error={errors.name}
                     />
                   </div>
-                  <div className="flex items-center rounded-md border px-3">
-                    <span className="text-sm text-slate-600">{"https://app.plane.so/"}</span>
-                    <Input
-                      name="slug"
-                      mode="transparent"
-                      autoComplete="off"
-                      register={register}
-                      className="block w-full rounded-md bg-transparent py-2 px-0 text-sm  focus:outline-none focus:ring-0"
-                    />
+                  <div>
+                    <h6 className="text-gray-500">Workspace slug</h6>
+                    <div className="flex items-center rounded-md border border-gray-300 px-3">
+                      <span className="text-sm text-slate-600">{"https://app.plane.so/"}</span>
+                      <Input
+                        name="slug"
+                        mode="transparent"
+                        autoComplete="off"
+                        register={register}
+                        className="block w-full rounded-md bg-transparent py-2 px-0 text-sm  focus:outline-none focus:ring-0"
+                      />
+                    </div>
+                    {slugError && (
+                      <span className="-mt-3 text-sm text-red-500">
+                        Workspace URL is already taken!
+                      </span>
+                    )}
                   </div>
-                  {slugError && (
-                    <span className="-mt-3 text-sm text-red-500">
-                      Workspace URL is already taken!
-                    </span>
-                  )}
                   <div>
                     <Controller
                       name="company_size"
@@ -269,7 +272,6 @@ const Workspace: React.FC<Props> = ({ setStep, setWorkspace }) => {
                   </div>
                 )}
               </div>
-
               <div className="mx-auto h-1/4 lg:w-1/2">
                 <button
                   type="submit"
--- apps/app/pages/create-workspace.tsx
+++ apps/app/pages/create-workspace.tsx
@@ -2,14 +2,13 @@ import React, { useEffect, useState } from "react";
 
 import type { NextPage, NextPageContext } from "next";
 import { useRouter } from "next/router";
+import Image from "next/image";
 
 import { mutate } from "swr";
 
 import { Controller, useForm } from "react-hook-form";
 // services
 import workspaceService from "lib/services/workspace.service";
-// hooks
-import useUser from "lib/hooks/useUser";
 // constants
 import { requiredAuth } from "lib/auth";
 import { USER_WORKSPACES } from "constants/fetch-keys";
@@ -17,6 +16,8 @@ import { USER_WORKSPACES } from "constants/fetch-keys";
 import DefaultLayout from "layouts/DefaultLayout";
 // ui
 import { CustomSelect, Input } from "ui";
+// images
+import Logo from "public/onboarding/logo.svg";
 // types
 import type { IWorkspace } from "types";
 // constants
@@ -42,8 +43,6 @@ const CreateWorkspace: NextPage = () => {
   } = useForm<IWorkspace>({ defaultValues });
 
   const router = useRouter();
-
-  const { user } = useUser();
 
   const onSubmit = async (formData: IWorkspace) => {
     await workspaceService
@@ -78,84 +77,88 @@ const CreateWorkspace: NextPage = () => {
 
   return (
     <DefaultLayout>
-      <div className="flex h-full w-full flex-col items-center justify-center px-4">
-        {user && (
-          <div className="mb-10 w-96 rounded-lg bg-indigo-100 p-2 text-theme lg:mb-20">
-            <p className="text-center text-sm">logged in as {user.email}</p>
+      <div className="grid h-full place-items-center p-5">
+        <div className="w-full space-y-4">
+          <div className="text-center">
+            <Image src={Logo} height="40" alt="Plane Logo" />
           </div>
-        )}
-        <div className="flex w-full flex-col justify-between space-y-4 rounded border bg-white p-4 px-6 md:w-2/3 lg:w-1/3">
-          <h2 className="mb-4 text-center text-2xl font-medium">Create a new workspace</h2>
-          <form className="mt-4 space-y-8" onSubmit={handleSubmit(onSubmit)}>
-            <div className="w-full space-y-4 bg-white">
-              <div className="grid grid-cols-1 gap-4">
-                <div>
-                  <Input
-                    label="Workspace name"
-                    name="name"
-                    placeholder="Enter name"
-                    autoComplete="off"
-                    register={register}
-                    onChange={(e) =>
-                      setValue("slug", e.target.value.toLocaleLowerCase().replace(/ /g, "-"))
-                    }
-                    validations={{
-                      required: "Workspace name is required",
-                    }}
-                    error={errors.name}
-                  />
+          <div className="grid w-full place-items-center">
+            <div className="w-full rounded-lg bg-white p-8 md:w-2/5">
+              <form className="space-y-8" onSubmit={handleSubmit(onSubmit)}>
+                <div className="w-full space-y-4 bg-white">
+                  <div className="grid grid-cols-1 gap-4">
+                    <div>
+                      <Input
+                        label="Workspace name"
+                        name="name"
+                        placeholder="Enter name"
+                        autoComplete="off"
+                        register={register}
+                        onChange={(e) =>
+                          setValue("slug", e.target.value.toLocaleLowerCase().replace(/ /g, "-"))
+                        }
+                        validations={{
+                          required: "Workspace name is required",
+                        }}
+                        error={errors.name}
+                      />
+                    </div>
+                    <div>
+                      <h6 className="text-gray-500">Workspace slug</h6>
+                      <div className="flex items-center rounded-md border border-gray-300 px-3">
+                        <span className="text-sm text-slate-600">{"https://app.plane.so/"}</span>
+                        <Input
+                          name="slug"
+                          mode="transparent"
+                          autoComplete="off"
+                          register={register}
+                          className="block w-full rounded-md bg-transparent py-2 px-0 text-sm  focus:outline-none focus:ring-0"
+                        />
+                      </div>
+                      {slugError && (
+                        <span className="-mt-3 text-sm text-red-500">
+                          Workspace URL is already taken!
+                        </span>
+                      )}
+                    </div>
+                    <div>
+                      <Controller
+                        name="company_size"
+                        control={control}
+                        rules={{ required: "This field is required" }}
+                        render={({ field: { value, onChange } }) => (
+                          <CustomSelect
+                            value={value}
+                            onChange={onChange}
+                            label={value ? value.toString() : "Select company size"}
+                            input
+                          >
+                            {companySize?.map((item) => (
+                              <CustomSelect.Option key={item.value} value={item.value}>
+                                {item.label}
+                              </CustomSelect.Option>
+                            ))}
+                          </CustomSelect>
+                        )}
+                      />
+                      {errors.company_size && (
+                        <span className="text-sm text-red-500">{errors.company_size.message}</span>
+                      )}
+                    </div>
+                  </div>
                 </div>
-                <div className="flex items-center rounded-md border px-3">
-                  <span className="text-sm text-slate-600">{"https://app.plane.so/"}</span>
-                  <Input
-                    name="slug"
-                    mode="transparent"
-                    autoComplete="off"
-                    register={register}
-                    className="block w-full rounded-md bg-transparent py-2 px-0 text-sm  focus:outline-none focus:ring-0"
-                  />
+                <div className="mx-auto h-1/4 lg:w-1/2">
+                  <button
+                    type="submit"
+                    className="w-full rounded-md bg-gray-200 px-4 py-2 text-sm"
+                    disabled={isSubmitting}
+                  >
+                    {isSubmitting ? "Creating..." : "Continue"}
+                  </button>
                 </div>
-                {slugError && (
-                  <span className="-mt-3 text-sm text-red-500">
-                    Workspace URL is already taken!
-                  </span>
-                )}
-                <div>
-                  <Controller
-                    name="company_size"
-                    control={control}
-                    rules={{ required: "This field is required" }}
-                    render={({ field: { value, onChange } }) => (
-                      <CustomSelect
-                        value={value}
-                        onChange={onChange}
-                        label={value ? value.toString() : "Select company size"}
-                        input
-                      >
-                        {companySize?.map((item) => (
-                          <CustomSelect.Option key={item.value} value={item.value}>
-                            {item.label}
-                          </CustomSelect.Option>
-                        ))}
-                      </CustomSelect>
-                    )}
-                  />
-                  {errors.company_size && (
-                    <span className="text-sm text-red-500">{errors.company_size.message}</span>
-                  )}
-                </div>
-              </div>
+              </form>
             </div>
-            <div className="mx-auto h-1/4 lg:w-1/2">
-              <button
-                type="submit"
-                className="w-full rounded-md bg-gray-200 px-4 py-2 text-sm"
-                disabled={isSubmitting}
-              >
-                {isSubmitting ? "Creating..." : "Continue"}
-              </button>
-            </div>
-          </form>
+          </div>
         </div>
       </div>
     </DefaultLayout>
