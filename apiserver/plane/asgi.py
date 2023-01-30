45a52116a7fb264eba04bed358fd8a43c5b50581
pablohashescobar <118773738+pablohashescobar@users.noreply.github.com>
2023-01-31 00:46:59 +0530

    refactor: move issue activity tracking from django-channels to rqworker (#207)
    * refactor: move issue activity tracking from django channels to rqworker
    
    * refactor: update old value and new value from character type to text type

--- apiserver/plane/asgi.py
+++ apiserver/plane/asgi.py
@@ -1,6 +1,6 @@
 import os
 
-from channels.routing import ProtocolTypeRouter, ChannelNameRouter
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
 django_asgi_app = get_asgi_application()
@@ -10,15 +10,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "plane.settings.production")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 
-from plane.api.consumers import IssueConsumer
 
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        "channel": ChannelNameRouter(
-            {
-                "issue-activites": IssueConsumer.as_asgi(),
-            }
-        ),
     }
 )
