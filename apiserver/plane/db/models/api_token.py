cd41a050228df74321a87f2f63ddaabc909a03b1
pablohashescobar <nikhilschacko@gmail.com>
2023-02-06 23:20:21 +0530

    refactor: update model association to cascade on delete

--- apiserver/plane/db/models/api_token.py
+++ apiserver/plane/db/models/api_token.py
@@ -28,7 +28,7 @@ class APIToken(BaseModel):
         choices=((0, "Human"), (1, "Bot")), default=0
     )
     workspace = models.ForeignKey(
-        "db.workspace", related_name="api_tokens", on_delete=models.SET_NULL, null=True
+        "db.Workspace", related_name="api_tokens", on_delete=models.CASCADE, null=True
     )
 
     class Meta:
