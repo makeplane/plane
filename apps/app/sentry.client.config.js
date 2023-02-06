42e66a4b3732d9588d0d2694cf47ef04162aab64
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2023-02-06 21:30:29 +0530

    fix: removed unnecessary env variable

--- apps/app/sentry.client.config.js
+++ apps/app/sentry.client.config.js
@@ -4,7 +4,7 @@
 
 import * as Sentry from "@sentry/nextjs";
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
   dsn: SENTRY_DSN,
