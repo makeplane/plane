f71cb7f83661950bda2554cc92205a4db2ed2275
sriramveeraghanta <veeraghanta.sriram@gmail.com>
2023-02-08 15:23:00 +0530

    restructing turbo repo

--- apps/docs/mdx/remark.mjs
+++
@@ -1,3 +0,0 @@
-import { mdxAnnotations } from 'mdx-annotations'
-
-export const remarkPlugins = [mdxAnnotations.remark]
--- apps/docs/src/lib/remToPx.js
+++
@@ -1,8 +0,0 @@
-export function remToPx(remValue) {
-  const rootFontSize =
-    typeof window === 'undefined'
-      ? 16
-      : parseFloat(window.getComputedStyle(document.documentElement).fontSize)
-
-  return parseFloat(remValue) * rootFontSize
-}
