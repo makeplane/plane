7c06be19fca89208f5c8f6a0e08d94798482cb4d
Anmol Singh Bhatia <121005188+anmolsinghbhatia@users.noreply.github.com>
2023-02-09 17:54:47 +0530

    feat: cycle status (#265)
    * feat: cycle status and dates added in sidebar
    
    * feat: update status added
    
    ---------
    
    Co-authored-by: Anmol Singh Bhatia <anmolsinghbhatia1001@gmail.com>

---
+++ apps/app/constants/cycle.ts
@@ -0,0 +1,5 @@
+export const CYCLE_STATUS = [
+    { label: "Started", value: "started", color: "#5e6ad2" },
+    { label: "Completed", value: "completed", color: "#eb5757" },
+    { label: "Draft", value: "draft", color: "#f2c94c" },
+  ];
