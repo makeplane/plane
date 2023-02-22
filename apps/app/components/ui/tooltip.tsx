d29f34566c63bf75dd550133b5bdd6cfefeaa12f
Anmol Singh Bhatia <121005188+anmolsinghbhatia@users.noreply.github.com>
2023-02-22 13:24:59 +0530

    fix : tooltip fix (#318)
    * fix: ellipsis added to issue title
    
    * feat: toolttip added
    
    * feat: assignees tooltip added
    
    * fix: build fix
    
    * fix: build fix
    
    * fix: build error
    
    ---------
    
    Co-authored-by: Aaryan Khandelwal <aaryankhandu123@gmail.com>

--- apps/app/components/ui/tooltip.tsx
+++ apps/app/components/ui/tooltip.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+
 import { Tooltip2 } from "@blueprintjs/popover2";
 
-export type Props = {
+type Props = {
   tooltipHeading?: string;
   tooltipContent: string;
   position?: "top" | "right" | "bottom" | "left";
@@ -15,26 +16,24 @@ export const Tooltip: React.FC<Props> = ({
   position = "top",
   children,
   disabled = false,
-}) => {
-  return (
-    <Tooltip2
-      disabled={disabled}
-      content={
-        <div className="flex flex-col justify-center items-start gap-1 max-w-[600px] text-xs rounded-md bg-white p-2 shadow-md capitalize text-left">
-          {tooltipHeading ? (
-            <>
-              <h5 className="font-medium">{tooltipHeading}</h5>
-              <p className="text-gray-700">{tooltipContent}</p>
-            </>
-          ) : (
+}) => (
+  <Tooltip2
+    disabled={disabled}
+    content={
+      <div className="flex flex-col justify-center items-start gap-1 max-w-[600px] text-xs rounded-md bg-white p-2 shadow-md capitalize text-left">
+        {tooltipHeading ? (
+          <>
+            <h5 className="font-medium">{tooltipHeading}</h5>
             <p className="text-gray-700">{tooltipContent}</p>
-          )}
-        </div>
-      }
-      position={position}
-      renderTarget={({ isOpen: isTooltipOpen, ref: eleRefernce, ...tooltipProps }) =>
-        React.cloneElement(children, { ref: eleRefernce, ...tooltipProps, ...children.props })
-      }
-    />
-  );
-};
+          </>
+        ) : (
+          <p className="text-gray-700">{tooltipContent}</p>
+        )}
+      </div>
+    }
+    position={position}
+    renderTarget={({ isOpen: isTooltipOpen, ref: eleRefernce, ...tooltipProps }) =>
+      React.cloneElement(children, { ref: eleRefernce, ...tooltipProps, ...children.props })
+    }
+  />
+);
