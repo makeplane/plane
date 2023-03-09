f0bae8f9eb36b7d9d7ed78b31c9952acb08c2980
pablohashescobar <nikhilschacko@gmail.com>
2023-03-09 17:04:34 +0530

    dev: update script to get the instance IP

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ cp ./apiserver/.env.example ./apiserver/.env
 # Generating App environmental variables
 cp ./apps/app/.env.example ./apps/app/.env
 
-echo NEXT_PUBLIC_API_BASE_URL = http://$1  >> ./apps/app/.env
+echo -e "\nNEXT_PUBLIC_API_BASE_URL = http://$1"  >> ./apps/app/.env
