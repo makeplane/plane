58cc3b7e596e8300a5f695da850f964e046a3964
pablohashescobar <nikhilschacko@gmail.com>
2023-03-09 15:47:21 +0530

    dev: updates in self hosting setup

--- apiserver/plane/settings/production.py
+++ apiserver/plane/settings/production.py
@@ -43,24 +43,24 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 
 CORS_ALLOW_METHODS = [
-'DELETE',
-'GET',
-'OPTIONS',
-'PATCH',
-'POST',
-'PUT',
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
 ]
 
 CORS_ALLOW_HEADERS = [
-'accept',
-'accept-encoding',
-'authorization',
-'content-type',
-'dnt',
-'origin',
-'user-agent',
-'x-csrftoken',
-'x-requested-with',
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
-# Generating API Server environmental variables
+#!/bin/bash
 cp ./apiserver/.env.example ./apiserver/.env
 # Generating App environmental variables
 cp ./apps/app/.env.example ./apps/app/.env
+
+echo NEXT_PUBLIC_API_BASE_URL = http://$1  >> ./apps/app/.env
