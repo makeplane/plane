d0a014859b386d25b5501c9866604c1963f87845
Anmol Singh Bhatia <anmolsinghbhatia@caravel.tech>
2023-03-18 16:55:12 +0530

    style: sign with code and password

--- apps/app/components/account/email-signin-form.tsx
+++ apps/app/components/account/email-signin-form.tsx
@@ -19,16 +19,7 @@ export const EmailSignInForm: FC<EmailSignInFormProps> = (props) => {
       ) : (
         <EmailPasswordForm onSuccess={handleSuccess} />
       )}
-      <div className="mt-6">
-        <div className="relative">
-          <div className="absolute inset-0 flex items-center">
-            <div className="w-full border-t border-gray-300" />
-          </div>
-          <div className="relative flex justify-center text-sm">
-            <span className="bg-white px-2 text-gray-500">or</span>
-          </div>
-        </div>
-        {/* <div className="mt-6 flex w-full flex-col items-stretch gap-y-2">
+      {/* <div className="mt-6 flex w-full flex-col items-stretch gap-y-2">
           <button
             type="button"
             className="flex w-full items-center rounded border border-gray-300 px-3 py-2 text-sm duration-300 hover:bg-gray-100"
@@ -40,7 +31,6 @@ export const EmailSignInForm: FC<EmailSignInFormProps> = (props) => {
             </span>
           </button>
         </div> */}
-      </div>
     </>
   );
 };
