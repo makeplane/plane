9a7186f0ecff1e9b5ebb4873198b51e86d71b051
Anmol Singh Bhatia <anmolsinghbhatia@caravel.tech>
2023-03-24 06:32:58 +0530

    feat: start and last day of week helper function

--- apps/app/helpers/calendar.helper.ts
+++ apps/app/helpers/calendar.helper.ts
@@ -7,3 +7,20 @@ export const lastDayOfMonth = (date: Date) => {
   const lastDayOfPreviousMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0);
   return lastDayOfPreviousMonth;
 };
+
+export const startOfWeek = (date: Date) => {
+    const dayOfWeek = date.getDay() % 7;
+    const startOfWeekDate = new Date(date.getFullYear(), date.getMonth(), date.getDate() - dayOfWeek);
+    return startOfWeekDate;
+  };
+  
+  export const lastDayOfWeek = (date: Date) => {
+    const dayOfWeek = date.getDay() % 7;
+    const daysUntilEndOfWeek = 6 - dayOfWeek;
+    const lastDayOfWeekDate = new Date(
+      date.getFullYear(),
+      date.getMonth(),
+      date.getDate() + daysUntilEndOfWeek
+    );
+    return lastDayOfWeekDate;
+  };
