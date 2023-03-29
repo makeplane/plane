7337707a4ed2561db3d2e30dea44bf47470be94a
Saheb Giri <47132373+iamsahebgiri@users.noreply.github.com>
2023-03-29 19:19:37 +0530

    style: redesign issue activity (#579)
    * style: redesign the activity feed and refactor the code
    
    * chore: remove console logs
    
    * revert: back to remirror rich text editor
    
    * style: make icons, text smaller and reduce the spacing between logs

--- apps/app/components/issues/comment/comment-card.tsx
+++ apps/app/components/issues/comment/comment-card.tsx
@@ -6,7 +6,7 @@ import dynamic from "next/dynamic";
 // react-hook-form
 import { useForm } from "react-hook-form";
 // icons
-import { CheckIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { ChatBubbleLeftEllipsisIcon, CheckIcon, XMarkIcon } from "@heroicons/react/24/outline";
 // hooks
 import useUser from "hooks/use-user";
 // ui
@@ -49,71 +49,77 @@ export const CommentCard: React.FC<Props> = ({ comment, onSubmit, handleCommentD
   }, [isEditing, setFocus]);
 
   return (
-    <div className="-ml-1 flex h-full w-full justify-between">
-      <div className="flex w-full gap-x-4">
-        <div className="flex-shrink-0">
-          {comment.actor_detail.avatar && comment.actor_detail.avatar !== "" ? (
-            <Image
-              src={comment.actor_detail.avatar}
-              alt={comment.actor_detail.first_name}
-              height={30}
-              width={30}
-              className="rounded-full"
-            />
-          ) : (
-            <div
-              className={`grid h-8 w-8 place-items-center rounded-full border-2 border-white bg-gray-500 text-white`}
-            >
-              {comment.actor_detail.first_name.charAt(0)}
-            </div>
-          )}
-        </div>
-        <div className="w-full space-y-1">
-          <p className="flex items-center gap-2 text-xs text-gray-500">
-            <span>
-              {comment.actor_detail.first_name}
-              {comment.actor_detail.is_bot ? "Bot" : " " + comment.actor_detail.last_name}
-            </span>
-            <span>{timeAgo(comment.created_at)}</span>
-          </p>
-          <div className="issue-comments-section">
-            {isEditing ? (
-              <form className="flex flex-col gap-2" onSubmit={handleSubmit(onEnter)}>
-                <RemirrorRichTextEditor
-                  value={comment.comment_html}
-                  onBlur={(jsonValue, htmlValue) => {
-                    setValue("comment_json", jsonValue);
-                    setValue("comment_html", htmlValue);
-                  }}
-                  placeholder="Enter Your comment..."
-                />
-                <div className="flex gap-1 self-end">
-                  <button
-                    type="submit"
-                    disabled={isSubmitting}
-                    className="group rounded border border-green-500 bg-green-100 p-2 shadow-md duration-300 hover:bg-green-500"
-                  >
-                    <CheckIcon className="h-3 w-3 text-green-500 duration-300 group-hover:text-white" />
-                  </button>
-                  <button
-                    type="button"
-                    className="group rounded border border-red-500 bg-red-100 p-2 shadow-md duration-300 hover:bg-red-500"
-                    onClick={() => setIsEditing(false)}
-                  >
-                    <XMarkIcon className="h-3 w-3 text-red-500 duration-300 group-hover:text-white" />
-                  </button>
-                </div>
-              </form>
-            ) : (
-              <>
-                <RemirrorRichTextEditor
-                  value={comment.comment_html}
-                  editable={false}
-                  onBlur={() => ({})}
-                />
-              </>
-            )}
+    <div className="relative flex items-start space-x-3">
+      <div className="relative px-1">
+        {comment.actor_detail.avatar && comment.actor_detail.avatar !== "" ? (
+          <Image
+            src={comment.actor_detail.avatar}
+            alt={comment.actor_detail.first_name}
+            height={30}
+            width={30}
+            className="grid h-7 w-7 place-items-center rounded-full border-2 border-white bg-gray-500 text-white"
+          />
+        ) : (
+          <div
+            className={`grid h-7 w-7 place-items-center rounded-full border-2 border-white bg-gray-500 text-white`}
+          >
+            {comment.actor_detail.first_name.charAt(0)}
           </div>
+        )}
+
+        <span className="absolute -bottom-0.5 -right-1 rounded-tl bg-white px-0.5 py-px">
+          <ChatBubbleLeftEllipsisIcon className="h-3.5 w-3.5 text-gray-400" aria-hidden="true" />
+        </span>
+      </div>
+      <div className="min-w-0 flex-1">
+        <div>
+          <div className="text-xs">
+            {comment.actor_detail.first_name}
+            {comment.actor_detail.is_bot ? "Bot" : " " + comment.actor_detail.last_name}
+          </div>
+          <p className="mt-0.5 text-xs text-gray-500">Commented {timeAgo(comment.created_at)}</p>
+        </div>
+        <div className="issue-comments-section p-0">
+          {isEditing ? (
+            <form className="flex flex-col gap-2" onSubmit={handleSubmit(onEnter)}>
+              <RemirrorRichTextEditor
+                value={comment.comment_html}
+                onBlur={(jsonValue, htmlValue) => {
+                  setValue("comment_json", jsonValue);
+                  setValue("comment_html", htmlValue);
+                }}
+                placeholder="Enter Your comment..."
+              />
+              <div className="flex gap-1 self-end">
+                <button
+                  type="submit"
+                  disabled={isSubmitting}
+                  className="group rounded border border-green-500 bg-green-100 p-2 shadow-md duration-300 hover:bg-green-500"
+                >
+                  <CheckIcon className="h-3 w-3 text-green-500 duration-300 group-hover:text-white" />
+                </button>
+                <button
+                  type="button"
+                  className="group rounded border border-red-500 bg-red-100 p-2 shadow-md duration-300 hover:bg-red-500"
+                  onClick={() => setIsEditing(false)}
+                >
+                  <XMarkIcon className="h-3 w-3 text-red-500 duration-300 group-hover:text-white" />
+                </button>
+              </div>
+            </form>
+          ) : (
+            // <div
+            //   className="mt-2 mb-6 text-sm text-gray-700"
+            //   dangerouslySetInnerHTML={{ __html: comment.comment_html }}
+            // />
+            <RemirrorRichTextEditor
+              value={comment.comment_html}
+              editable={false}
+              onBlur={() => ({})}
+              noBorder
+              customClassName="text-xs bg-gray-100"
+            />
+          )}
         </div>
       </div>
       {user?.id === comment.actor && (
