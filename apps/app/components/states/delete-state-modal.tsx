47613c625cc252a6aac3eec3376ba5d298a7273f
kunal_17 <kunalvish17360@gmail.com>
2023-03-31 16:03:06 +0530

    fix: viewlist link

--- apps/app/components/states/delete-state-modal.tsx
+++ apps/app/components/states/delete-state-modal.tsx
@@ -104,7 +104,7 @@ export const DeleteStateModal: React.FC<Props> = ({ isOpen, onClose, data }) => 
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-[40rem]">
                 <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
                     <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
@@ -119,9 +119,9 @@ export const DeleteStateModal: React.FC<Props> = ({ isOpen, onClose, data }) => 
                       </Dialog.Title>
                       <div className="mt-2">
                         <p className="text-sm text-gray-500">
-                          Are you sure you want to delete state - {`"`}
+                          Are you sure you want to delete state- {" "}
                           <span className="italic">{data?.name}</span>
-                          {`"`} ? All of the data related to the state will be permanently removed.
+                          ? All of the data related to the state will be permanently removed.
                           This action cannot be undone.
                         </p>
                       </div>
