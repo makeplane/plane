aa1d921ca41adc19f59445b74476644424ad3545
Anmol Singh Bhatia <anmolsinghbhatia@caravel.tech>
2023-04-04 16:25:31 +0530

    style: responsiveness added

--- apps/app/pages/[workspaceSlug]/projects/[projectId]/issues/[issueId].tsx
+++ apps/app/pages/[workspaceSlug]/projects/[projectId]/issues/[issueId].tsx
@@ -202,7 +202,7 @@ const IssueDetailsPage: NextPage<UserAuth> = (props) => {
             </div>
             <div className="flex flex-col gap-3 py-3">
               <h3 className="text-lg">Attachments</h3>
-              <div className="grid grid-cols-4 gap-3">
+              <div className="grid  gap-3 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
               <IssueAttachmentUpload/>
               <IssueAttachments />
               </div>
