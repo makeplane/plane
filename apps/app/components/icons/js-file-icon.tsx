575e9f65cc103a304860f11bd9be58775366dbc8
Anmol Singh Bhatia <anmolsinghbhatia@caravel.tech>
2023-04-05 18:56:58 +0530

    fix: js icon import fix

--- apps/app/components/icons/js-file-icon.tsx
+++ apps/app/components/icons/js-file-icon.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 
 import type { Props } from "./types";
-import JsFileIcon from "public/attachment/excel-icon.png";
+import JsFileIcon from "public/attachment/js-icon.png";
 
 export const JavaScriptIcon: React.FC<Props> = ({ width, height  }) => (
   <Image src={JsFileIcon} height={height} width={width} alt="JsFileIcon" />
