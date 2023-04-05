39f151ce731871d0959259e612fb51b1b3e8df69
sriram veeraghanta <veeraghanta.sriram@gmail.com>
2023-04-05 14:40:36 -0400

    index page redirection

--- apps/app/pages/index.tsx
+++ apps/app/pages/index.tsx
@@ -1,13 +1,15 @@
-// lib
+import { useEffect } from "react";
 import { homePageRedirect } from "lib/auth";
 // types
 import type { NextPage, NextPageContext } from "next";
+import { useRouter } from "next/router";
 
-const Home: NextPage = () => null;
+const Home: NextPage = () => {
+  const router = useRouter();
 
-export const getServerSideProps = (ctx: NextPageContext) => {
-  const cookies = ctx.req?.headers.cookie;
-  return homePageRedirect(cookies);
+  useEffect(() => {
+    router.push("/onboarding");
+  }, []);
+  return <>loading</>;
 };
-
 export default Home;
