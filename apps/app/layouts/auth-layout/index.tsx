c86c568c7f14b3b78f7e68672edb084544814e45
Dakshesh Jain <dakshesh.jain14@gmail.com>
2023-04-07 16:07:02 +0530

    chore: made workspace authorization wrapper component

---
+++ apps/app/layouts/auth-layout/index.tsx
@@ -0,0 +1,68 @@
+import useUser from "hooks/use-user";
+
+// hooks
+import { useRouter } from "next/router";
+
+// swr
+import useSWR from "swr";
+
+// services
+import workspaceServices from "services/workspace.service";
+import { WORKSPACE_MEMBERS_ME } from "constants/fetch-keys";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const WorkspaceAuthorizationLayout: React.FC<Props> = (props) => {
+  const { children } = props;
+
+  const router = useRouter();
+
+  const user = useUser();
+  const { workspaceSlug } = router.query;
+
+  const { data: workspaceMemberMe, error } = useSWR(
+    workspaceSlug ? WORKSPACE_MEMBERS_ME(workspaceSlug as string) : null,
+    workspaceSlug ? () => workspaceServices.workspaceMemberMe(workspaceSlug.toString()) : null,
+    {
+      onErrorRetry(err, key, config, revalidate, revalidateOpts) {
+        if (err.status === 401 || err.status === 403) return;
+        revalidateOpts.retryCount = 5;
+      },
+    }
+  );
+
+  if (!workspaceMemberMe && !error)
+    // TODO: show good loading UI
+    return (
+      <div className="container h-screen flex justify-center items-center p-4 text-2xl font-semibold">
+        <p>Loading...</p>
+      </div>
+    );
+
+  if (error?.status === 401 || error?.status === 403) {
+    // TODO:
+    // [ ] - show proper UI with a button to redirect to get back to the workspace they have access to
+    // [ ] - show proper UI with a button to redirect to create a new workspace
+    // we may not want to show layout if user is not authorized in workspace level
+    return (
+      <div className="container h-screen flex justify-center items-center">
+        <p className="text-2xl font-semibold">You are not authorized to access this workspace.</p>
+      </div>
+    );
+  }
+
+  // FIXME: show 404 for workspace not workspace member
+  if (error?.status === 404) {
+    return (
+      <div className="container h-screen flex justify-center items-center">
+        <p className="text-2xl font-semibold">No such workspace exist. Create one?</p>
+      </div>
+    );
+  }
+
+  return <>{children}</>;
+};
+
+export default WorkspaceAuthorizationLayout;
--- apps/app/pages/[workspaceSlug]/index.tsx
+++ apps/app/pages/[workspaceSlug]/index.tsx
@@ -8,6 +8,7 @@ import useSWR, { mutate } from "swr";
 import { requiredAuth } from "lib/auth";
 // layouts
 import AppLayout from "layouts/app-layout";
+import WorkspaceAuthorizationLayout from "layouts/auth-layout";
 // services
 import userService from "services/user.service";
 // components
@@ -38,42 +39,46 @@ const WorkspacePage: NextPage = () => {
   }, [month, workspaceSlug]);
 
   return (
-    <AppLayout noHeader={true}>
-      <div className="h-full w-full">
-        <div className="flex flex-col gap-8">
-          <div
-            className="flex flex-col bg-white justify-between gap-x-2 gap-y-6 rounded-lg px-8 py-6 text-black md:flex-row md:items-center md:py-3"
-            // style={{ background: "linear-gradient(90deg, #8e2de2 0%, #4a00e0 100%)" }}
-          >
-            <p className="font-semibold">Plane is a open source application, to support us you can star us on GitHub!</p>
-            <div className="flex items-center gap-2">
-              {/* <a href="#" target="_blank" rel="noopener noreferrer">
+    <WorkspaceAuthorizationLayout>
+      <AppLayout noHeader={true}>
+        <div className="h-full w-full">
+          <div className="flex flex-col gap-8">
+            <div
+              className="flex flex-col bg-white justify-between gap-x-2 gap-y-6 rounded-lg px-8 py-6 text-black md:flex-row md:items-center md:py-3"
+              // style={{ background: "linear-gradient(90deg, #8e2de2 0%, #4a00e0 100%)" }}
+            >
+              <p className="font-semibold">
+                Plane is a open source application, to support us you can star us on GitHub!
+              </p>
+              <div className="flex items-center gap-2">
+                {/* <a href="#" target="_blank" rel="noopener noreferrer">
                 View roadmap
               </a> */}
-              <a
-                href="https://github.com/makeplane/plane"
-                target="_blank"
-                className="rounded-md border-2 border-black font-medium px-3 py-1.5 text-sm duration-300"
-                rel="noopener noreferrer"
-              >
-                Star us on GitHub
-              </a>
+                <a
+                  href="https://github.com/makeplane/plane"
+                  target="_blank"
+                  className="rounded-md border-2 border-black font-medium px-3 py-1.5 text-sm duration-300"
+                  rel="noopener noreferrer"
+                >
+                  Star us on GitHub
+                </a>
+              </div>
+            </div>
+            <IssuesStats data={workspaceDashboardData} />
+            <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
+              <IssuesList issues={workspaceDashboardData?.overdue_issues} type="overdue" />
+              <IssuesList issues={workspaceDashboardData?.upcoming_issues} type="upcoming" />
+              <IssuesPieChart groupedIssues={workspaceDashboardData?.state_distribution} />
+              <CompletedIssuesGraph
+                issues={workspaceDashboardData?.completed_issues}
+                month={month}
+                setMonth={setMonth}
+              />
             </div>
           </div>
-          <IssuesStats data={workspaceDashboardData} />
-          <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
-            <IssuesList issues={workspaceDashboardData?.overdue_issues} type="overdue" />
-            <IssuesList issues={workspaceDashboardData?.upcoming_issues} type="upcoming" />
-            <IssuesPieChart groupedIssues={workspaceDashboardData?.state_distribution} />
-            <CompletedIssuesGraph
-              issues={workspaceDashboardData?.completed_issues}
-              month={month}
-              setMonth={setMonth}
-            />
-          </div>
         </div>
-      </div>
-    </AppLayout>
+      </AppLayout>
+    </WorkspaceAuthorizationLayout>
   );
 };
 
