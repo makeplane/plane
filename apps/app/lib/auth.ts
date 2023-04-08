c50840020cab482397671217ddd122ab7aeec6ab
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2023-04-09 00:01:35 +0530

    chore: add homePageRedirect function back

--- apps/app/lib/auth.ts
+++ apps/app/lib/auth.ts
@@ -97,3 +97,107 @@ export const requiredWorkspaceAdmin = async (workspaceSlug: string, cookie?: str
 
   return memberDetail || null;
 };
+
+export const homePageRedirect = async (cookie?: string) => {
+  const user = await requiredAuth(cookie);
+
+  if (!user)
+    return {
+      redirect: {
+        destination: "/signin",
+        permanent: false,
+      },
+    };
+
+  if (!user.is_onboarded)
+    return {
+      redirect: {
+        destination: "/onboarding",
+        permanent: false,
+      },
+    };
+
+  let workspaces: IWorkspace[] = [];
+
+  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || "https://api.plane.so";
+
+  const cookies = convertCookieStringToObject(cookie);
+  const token = cookies?.accessToken;
+
+  try {
+    const data = await fetch(`${baseUrl}/api/users/me/workspaces/`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => data);
+
+    workspaces = data;
+  } catch (e) {
+    console.error(e);
+    return {
+      redirect: {
+        destination: "/error",
+        permanent: false,
+      },
+    };
+  }
+
+  const lastActiveWorkspace = workspaces.find(
+    (workspace) => workspace.id === user.last_workspace_id
+  );
+
+  if (lastActiveWorkspace) {
+    return {
+      redirect: {
+        destination: `/${lastActiveWorkspace.slug}`,
+        permanent: false,
+      },
+    };
+  } else if (workspaces.length > 0) {
+    return {
+      redirect: {
+        destination: `/${workspaces[0].slug}`,
+        permanent: false,
+      },
+    };
+  }
+
+  const invitations = await fetch(`${baseUrl}/api/users/me/invitations/workspaces/`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch((e) => {
+      console.error(e);
+      return {
+        redirect: {
+          destination: "/error",
+          permanent: false,
+        },
+      };
+    });
+
+  if (invitations.length > 0)
+    return {
+      redirect: {
+        destination: "/invitations",
+        permanent: false,
+      },
+    };
+  else {
+    return {
+      redirect: {
+        destination: "/create-workspace",
+        permanent: false,
+      },
+    };
+  }
+};
