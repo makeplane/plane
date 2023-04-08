b479e86ad9b51228c3aa8a550bc725ba67685103
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2023-04-08 17:55:31 +0530

    chore: user auth layer

--- apps/app/hooks/use-user.tsx
+++ apps/app/hooks/use-user.tsx
@@ -1,40 +1,11 @@
-import { useContext, useEffect } from "react";
-import { useRouter } from "next/router";
+import { useContext } from "react";
+
 // context
 import { UserContext } from "contexts/user.context";
 
-interface useUserOptions {
-  redirectTo?: string;
-}
-
-const useUser = (options: useUserOptions = {}) => {
-  // props
-  const { redirectTo = null } = options;
+const useUser = () => {
   // context
   const contextData = useContext(UserContext);
-  // router
-  const router = useRouter();
-
-  /**
-   * Checks for redirect url and user details from the API.
-   * if the user is not authenticated, user will be redirected
-   * to the provided redirectTo route.
-   */
-  useEffect(() => {
-    if (!contextData?.user || !redirectTo) return;
-
-    if (!contextData?.user) {
-      if (redirectTo) {
-        router?.pathname !== redirectTo && router.push(redirectTo);
-      }
-      router?.pathname !== "/signin" && router.push("/signin");
-    }
-    if (contextData?.user) {
-      if (redirectTo) {
-        router?.pathname !== redirectTo && router.push(redirectTo);
-      }
-    }
-  }, [contextData?.user, redirectTo, router]);
 
   return { ...contextData };
 };
--- apps/app/layouts/auth-layout/project-authorization-wrapper.tsx
+++ apps/app/layouts/auth-layout/project-authorization-wrapper.tsx
@@ -5,13 +5,12 @@ import { useRouter } from "next/router";
 
 // contexts
 import { useProjectMyMembership, ProjectMemberProvider } from "contexts/project-member.context";
-// hooks
-import useUser from "hooks/use-user";
 // layouts
 import Container from "layouts/container";
 import AppHeader from "layouts/app-layout/app-header";
 import AppSidebar from "layouts/app-layout/app-sidebar";
 import SettingsNavbar from "layouts/settings-navbar";
+import { WorkspaceAuthorizationLayout } from "./workspace-authorization-wrapper";
 // components
 import { NotAuthorizedView, JoinProject } from "components/auth-screens";
 import { CommandPalette } from "components/command-palette";
@@ -59,83 +58,87 @@ const ProjectAuthorizationWrapped: React.FC<Props> = ({
   const router = useRouter();
   const { workspaceSlug, projectId } = router.query;
 
-  const user = useUser();
-
   const { loading, error, memberRole: memberType } = useProjectMyMembership();
 
   const settingsLayout = router.pathname.includes("/settings");
 
   return (
-    <Container meta={meta}>
-      <CommandPalette />
-      <div className="flex h-screen w-full overflow-x-hidden">
-        <AppSidebar toggleSidebar={toggleSidebar} setToggleSidebar={setToggleSidebar} />
-        {loading ? (
-          <div className="container h-screen flex justify-center items-center p-4 text-2xl font-semibold">
-            <p>Loading...</p>
-          </div>
-        ) : error?.status === 401 || error?.status === 403 ? (
-          <JoinProject />
-        ) : error?.status === 404 ? (
-          <div className="container h-screen grid place-items-center">
-            <div className="text-center space-y-4">
-              <p className="text-2xl font-semibold">No such project exist. Create one?</p>
-              <PrimaryButton
-                onClick={() => {
-                  const e = new KeyboardEvent("keydown", { key: "p" });
-                  document.dispatchEvent(e);
-                }}
-              >
-                Create project
-              </PrimaryButton>
+    <WorkspaceAuthorizationLayout>
+      <Container meta={meta}>
+        <CommandPalette />
+        <div className="flex h-screen w-full overflow-x-hidden">
+          <AppSidebar toggleSidebar={toggleSidebar} setToggleSidebar={setToggleSidebar} />
+          {loading ? (
+            <div className="container h-screen flex justify-center items-center p-4 text-2xl font-semibold">
+              <p>Loading...</p>
             </div>
-          </div>
-        ) : settingsLayout && (memberType?.isGuest || memberType?.isViewer) ? (
-          <NotAuthorizedView
-            actionButton={
-              <Link href={`/${workspaceSlug}/projects/${projectId}/issues`}>
-                <a>
-                  <PrimaryButton className="flex items-center gap-1">
-                    <LayerDiagonalIcon height={16} width={16} color="white" /> Go to issues
-                  </PrimaryButton>
-                </a>
-              </Link>
-            }
-            type="project"
-          />
-        ) : (
-          <main className="flex h-screen w-full min-w-0 flex-col overflow-y-auto">
-            {!noHeader && (
-              <AppHeader
-                breadcrumbs={breadcrumbs}
-                left={left}
-                right={right}
-                setToggleSidebar={setToggleSidebar}
-              />
-            )}
-            <div
-              className={`flex w-full flex-grow flex-col ${
-                noPadding ? "" : settingsLayout ? "p-8 lg:px-28" : "p-8"
-              } ${
-                bg === "primary" ? "bg-primary" : bg === "secondary" ? "bg-secondary" : "bg-primary"
-              }`}
-            >
-              {settingsLayout && (
-                <div className="mb-12 space-y-6">
-                  <div>
-                    <h3 className="text-3xl font-semibold">Project Settings</h3>
-                    <p className="mt-1 text-gray-600">
-                      This information will be displayed to every member of the project.
-                    </p>
-                  </div>
-                  <SettingsNavbar />
-                </div>
+          ) : error?.status === 401 || error?.status === 403 ? (
+            <JoinProject />
+          ) : error?.status === 404 ? (
+            <div className="container h-screen grid place-items-center">
+              <div className="text-center space-y-4">
+                <p className="text-2xl font-semibold">No such project exist. Create one?</p>
+                <PrimaryButton
+                  onClick={() => {
+                    const e = new KeyboardEvent("keydown", { key: "p" });
+                    document.dispatchEvent(e);
+                  }}
+                >
+                  Create project
+                </PrimaryButton>
+              </div>
+            </div>
+          ) : settingsLayout && (memberType?.isGuest || memberType?.isViewer) ? (
+            <NotAuthorizedView
+              actionButton={
+                <Link href={`/${workspaceSlug}/projects/${projectId}/issues`}>
+                  <a>
+                    <PrimaryButton className="flex items-center gap-1">
+                      <LayerDiagonalIcon height={16} width={16} color="white" /> Go to issues
+                    </PrimaryButton>
+                  </a>
+                </Link>
+              }
+              type="project"
+            />
+          ) : (
+            <main className="flex h-screen w-full min-w-0 flex-col overflow-y-auto">
+              {!noHeader && (
+                <AppHeader
+                  breadcrumbs={breadcrumbs}
+                  left={left}
+                  right={right}
+                  setToggleSidebar={setToggleSidebar}
+                />
               )}
-              {children}
-            </div>
-          </main>
-        )}
-      </div>
-    </Container>
+              <div
+                className={`flex w-full flex-grow flex-col ${
+                  noPadding ? "" : settingsLayout ? "p-8 lg:px-28" : "p-8"
+                } ${
+                  bg === "primary"
+                    ? "bg-primary"
+                    : bg === "secondary"
+                    ? "bg-secondary"
+                    : "bg-primary"
+                }`}
+              >
+                {settingsLayout && (
+                  <div className="mb-12 space-y-6">
+                    <div>
+                      <h3 className="text-3xl font-semibold">Project Settings</h3>
+                      <p className="mt-1 text-gray-600">
+                        This information will be displayed to every member of the project.
+                      </p>
+                    </div>
+                    <SettingsNavbar />
+                  </div>
+                )}
+                {children}
+              </div>
+            </main>
+          )}
+        </div>
+      </Container>
+    </WorkspaceAuthorizationLayout>
   );
 };
