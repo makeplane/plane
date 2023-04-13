f0c2ae631ed322737b4632b5b30aaedb4d442590
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2023-04-13 13:56:45 +0530

    feat: clarity script added

--- apps/app/pages/_document.tsx
+++ apps/app/pages/_document.tsx
@@ -9,6 +9,16 @@ class MyDocument extends Document {
           <link rel="apple-touch-icon" href="/icon.png" />
           <meta name="theme-color" content="#fff" />
           <script defer data-domain="app.plane.so" src="https://plausible.io/js/script.js" />
+          <script
+            defer
+            dangerouslySetInnerHTML={{
+              __html: `(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "g6lhwgkmrp");`,
+            }}
+          />
         </Head>
         <body>
           <Main />
