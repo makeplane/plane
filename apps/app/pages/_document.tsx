9b18e313ef2e2badddb098a872ad3445ea1ce2a1
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2023-04-14 14:48:48 +0530

    chore: add env flag to enable session recorder conditionally

--- apps/app/pages/_document.tsx
+++ apps/app/pages/_document.tsx
@@ -2,6 +2,10 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
+    const isSessionRecorderEnabled = parseInt(
+      process.env.NEXT_PUBLIC_ENABLE_SESSION_RECORDER || "0"
+    );
+
     return (
       <Html>
         <Head>
@@ -9,16 +13,18 @@ class MyDocument extends Document {
           <link rel="apple-touch-icon" href="/icon.png" />
           <meta name="theme-color" content="#fff" />
           <script defer data-domain="app.plane.so" src="https://plausible.io/js/script.js" />
-          <script
-            defer
-            dangerouslySetInnerHTML={{
-              __html: `(function(c,l,a,r,i,t,y){
+          {isSessionRecorderEnabled && (
+            <script
+              defer
+              dangerouslySetInnerHTML={{
+                __html: `(function(c,l,a,r,i,t,y){
               c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
               t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
               y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
           })(window, document, "clarity", "script", "g6lhwgkmrp");`,
-            }}
-          />
+              }}
+            />
+          )}
         </Head>
         <body>
           <Main />
