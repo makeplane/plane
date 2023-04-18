cbe8d73bd512eaa3a6dee6b23fcbc6c973a0ea62
Anmol Singh Bhatia <anmolsinghbhatia@caravel.tech>
2023-04-18 13:12:36 +0530

    fix: assignee and label count removed

--- apps/app/components/issues/select/label.tsx
+++ apps/app/components/issues/select/label.tsx
@@ -72,10 +72,10 @@ export const IssueLabelSelect: React.FC<Props> = ({ setIsOpen, value, onChange, 
               <span className="flex items-center justify-center gap-2 px-3 py-1 text-xs">
                 <IssueLabelsList
                   labels={value.map((v) => issueLabels?.find((l) => l.id === v)?.color) ?? []}
-                  length={3}
-                  showLength
+                  length={value.length}
+                  showLength={false}
                 />
-                <span className=" text-gray-600">{value.length} Labels</span>
+                <span className=" text-gray-600">Labels</span>
               </span>
             ) : (
               <span className="flex items-center justify-center gap-2 px-3  py-1.5 text-xs">
