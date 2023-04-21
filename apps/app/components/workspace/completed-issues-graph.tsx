0a54e9107b070bcdb5245c7a1998f48b05a566fc
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2023-04-21 17:40:03 +0530

    style: dashboard styling

--- apps/app/components/workspace/completed-issues-graph.tsx
+++ apps/app/components/workspace/completed-issues-graph.tsx
@@ -55,10 +55,10 @@ export const CompletedIssuesGraph: React.FC<Props> = ({ month, issues, setMonth 
           ))}
         </CustomMenu>
       </div>
-      <div className="rounded-[10px] border border-brand-base bg-brand-sidebar p-8 pl-4">
+      <div className="rounded-[10px] border border-brand-base bg-brand-base p-8 pl-4">
         <ResponsiveContainer width="100%" height={250}>
           <LineChart data={data}>
-            <CartesianGrid stroke="#e2e2e280" />
+            <CartesianGrid stroke="#858e9660" />
             <XAxis dataKey="week_in_month" padding={{ left: 48, right: 48 }} />
             <YAxis dataKey="completed_count" allowDecimals={false} />
             <Tooltip content={<CustomTooltip />} />
