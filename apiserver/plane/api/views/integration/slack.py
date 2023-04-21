ef2f122efd7f8cd919f182bab80513f1b9bea8a3
pablohashescobar <nikhilschacko@gmail.com>
2023-04-21 18:09:09 +0530

    dev: handle integrity exception

--- apiserver/plane/api/views/integration/slack.py
+++ apiserver/plane/api/views/integration/slack.py
@@ -1,3 +1,6 @@
+# Django import
+from django.db import IntegrityError
+
 # Third party imports
 from rest_framework import status
 from rest_framework.response import Response
@@ -41,6 +44,8 @@ class SlackProjectSyncViewSet(BaseViewSet):
 
                 return Response(serializer.data, status=status.HTTP_200_OK)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except IntegrityError:
+            return Response({"error": "Slack is already enabled for the project"}, status=status.HTTP_400_BAD_REQUEST)
         except WorkspaceIntegration.DoesNotExist:
             return Response(
                 {"error": "Workspace Integration does not exist"},
