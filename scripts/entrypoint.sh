5f8f056481de2a49d0d5e4d560b0d1d64cff6825
pablohashescobar <nikhilschacko@gmail.com>
2023-04-27 13:24:56 +0530

    dev: scripts for replacing variable during runtime

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# no verbose
+set +x
+# config
+envFilename='.env.production'
+nextFolder='./.next/'
+function apply_path {
+  # read all config file  
+  while read line; do
+    # no comment or not empty
+    if [ "${line:0:1}" == "#" ] || [ "${line}" == "" ]; then
+      continue
+    fi
+    
+    # split
+    configName="$(cut -d'=' -f1 <<<"$line")"
+    configValue="$(cut -d'=' -f2 <<<"$line")"
+    # get system env
+    envValue=$(env | grep "^$configName=" | grep -oe '[^=]*$');
+    
+    # if config found
+    if [ -n "$configValue" ] && [ -n "$envValue" ]; then
+      # replace all
+      echo "Replace: ${configValue} with: ${envValue}"
+      find $nextFolder \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#$configValue#$envValue#g"
+    fi
+  done < $envFilename
+}
+apply_path
+echo "Starting Nextjs"
+exec "$@"
---
+++ scripts/start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -x
+
+# Replace the statically built BUILT_NEXT_PUBLIC_WEBAPP_URL with run-time NEXT_PUBLIC_WEBAPP_URL
+# NOTE: if these values are the same, this will be skipped.
+scripts/replace-placeholder.sh "$BUILT_PUBLIC_API_BASE_URL" "$NEXT_PUBLIC_API_BASE_URL"
+
+node apps/app/server.js
