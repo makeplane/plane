6a83704225456de3ab8145a8eae29a6043d5d735
anmolsinghbhatia <anmolsinghbhatia@caravel.tech>
2023-05-02 18:48:34 +0530

    style: onboarding command menu

--- apps/app/components/onboarding/onboarding-card.tsx
+++ apps/app/components/onboarding/onboarding-card.tsx
@@ -19,7 +19,10 @@ export const OnboardingCard: React.FC<Props> = ({ data, gradient = false }) => (
       gradient ? "bg-gradient-to-b from-[#2C8DFF]/50 via-brand-base to-transparent" : ""
     }`}
   >
-    <div className="h-44 w-full">
+    <div className="relative h-44">
+      <h3 className="absolute top-3 left-28 text-base text-brand-secondary">
+        Open the contextual menu with:
+      </h3>
       <Image src={data.imgURL} height="180" width="450" alt={data.title} />
     </div>
     <h3 className="text-2xl font-medium">{data.title}</h3>
