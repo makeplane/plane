bfb4fbe4c90ec2c49f2e186436fda79a1b0864f9
anmolsinghbhatia <anmolsinghbhatia@caravel.tech>
2023-05-04 15:56:08 +0530

    style: integration banner

--- apps/app/pages/[workspaceSlug]/projects/[projectId]/settings/integrations.tsx
+++ apps/app/pages/[workspaceSlug]/projects/[projectId]/settings/integrations.tsx
@@ -59,8 +59,8 @@ const ProjectIntegrations: NextPage = () => {
           <section className="space-y-8">
             <div className="flex flex-col items-start gap-3">
               <h3 className="text-2xl font-semibold">Integrations</h3>
-              <div className="flex items-center gap-3 rounded-[10px] border border-brand-accent/75 bg-brand-accent/5 p-4 text-xs text-brand-base">
-                <ExclamationIcon height={20} width={20} className="fill-current text-brand-base" />
+              <div className="flex items-center gap-3 rounded-[10px] border border-brand-accent/75 bg-brand-accent/5 p-4 text-sm text-brand-base">
+                <ExclamationIcon height={24} width={24} className="fill-current text-brand-base" />
                 <p className="leading-5">
                   Integrations and importers are only available on the cloud version. We plan to
                   open-source our SDKs in the near future so that the community can request or
