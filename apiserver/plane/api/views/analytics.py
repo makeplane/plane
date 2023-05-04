b95a0d218c7562818ca3c0a0664d708eb30dc5f3
pablohashescobar <nikhilschacko@gmail.com>
2023-05-04 22:50:59 +0530

    dev: update endpoint to give data with segments as well

--- apiserver/plane/api/views/analytics.py
+++ apiserver/plane/api/views/analytics.py
@@ -1,7 +1,7 @@
 # Django imports
 from django.db.models import Count
 from django.db.models import CharField, Count, OuterRef, Func, F, Q, DateField
-from django.db.models.functions import Cast
+from django.db.models.functions import Cast, Trunc
 
 # Third party imports
 from rest_framework import status
@@ -19,7 +19,7 @@ class PlaneAnalyticsEndpoint(BaseAPIView):
         WorkSpaceAdminPermission,
     ]
 
-    def build_graph_plot(self, queryset, x_axis, y_axis):
+    def build_graph_plot(self, queryset, x_axis, y_axis, segment=None):
         y_axis_choices = [
             "issue_count",
             "effort",
@@ -27,36 +27,37 @@ class PlaneAnalyticsEndpoint(BaseAPIView):
             "issue_age",
             "lead_time",
         ]
-        x_axis_choices = [
-            "state",
-            "state__group",
-            "assignees",
-            "created_by",
-            "priority",
-            "labels",
-            "estimate_point",
-            "module",
-            "cycle",
-            "created_at",
-            "completed_at",
-            "start_date",
-            "target_date",
-        ]
-
         if x_axis in ["created_at", "completed_at"]:
-            queryset = queryset.annotate(x_axis=Cast(x_axis, DateField()))
+            queryset = queryset.annotate(date=Cast(x_axis, DateField()))
             x_axis = "date"
 
-        queryset = queryset.values(x_axis)
+        if segment:
+            queryset = queryset.annotate(segment=F(segment))
 
         if y_axis == "issue_count":
-            queryset = queryset.annotate(count=Count(x_axis)).order_by(x_axis)
+            queryset = queryset.annotate(y_axis=Count(x_axis)).order_by(x_axis)
         if y_axis == "effort":
-            queryset = queryset.annotate(count=Count("estimate_point")).order_by(x_axis)
-        return queryset
+            queryset = queryset.annotate(y_axis=Count("estimate_point")).order_by(
+                x_axis
+            )
+
+        result_dict = {}
+        for row in queryset:
+            x_value = row[x_axis]
+            y_value = row["y_axis"]
+            if x_value not in result_dict:
+                result_dict[x_value] = [y_value]
+            else:
+                result_dict[x_value].append(y_value)
+
+        result_list = []
+        for x, y_values in result_dict.items():
+            result_list.append({x_axis: x, y_axis: sum(y_values) / len(y_values)})
+
+        return result_list
 
     def get(self, request, slug):
-        try:
+        # try:
             x_axis = request.GET.get("x_axis", False)
             y_axis = request.GET.get("y_axis", False)
 
@@ -69,6 +70,7 @@ class PlaneAnalyticsEndpoint(BaseAPIView):
             project_id = request.GET.get("project_id", False)
             cycle_id = request.GET.get("cycle_id", False)
             module_id = request.GET.get("module_id", False)
+            segment = request.GET.get("segment", False)
 
             queryset = Issue.objects.filter(workspace__slug=slug)
             if project_id:
@@ -80,16 +82,16 @@ class PlaneAnalyticsEndpoint(BaseAPIView):
 
             total_issues = queryset.count()
             distribution = self.build_graph_plot(
-                queryset=queryset, x_axis=x_axis, y_axis=y_axis
+                queryset=queryset, x_axis=x_axis, y_axis=y_axis, segment=segment
             )
 
             return Response(
                 {"total": total_issues, "distribution": distribution},
                 status=status.HTTP_200_OK,
             )
-        except Exception as e:
-            print(e)
-            return Response(
-                {"error": "Something went wrong please try again later"},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
+        # except Exception as e:
+        #     print(e)
+        #     return Response(
+        #         {"error": "Something went wrong please try again later"},
+        #         status=status.HTTP_400_BAD_REQUEST,
+        #     )
