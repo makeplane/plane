9e06e035d7668445950687f2d6fe6a223fcc31eb
anmolsinghbhatia <anmolsinghbhatia@caravel.tech>
2023-05-11 17:51:43 +0530

    fix: build fix and preferences tab layout fix

--- apps/app/pages/[workspaceSlug]/me/profile/preferences.tsx
+++ apps/app/pages/[workspaceSlug]/me/profile/preferences.tsx
@@ -5,6 +5,7 @@ import { useTheme } from "next-themes";
 import useUser from "hooks/use-user";
 // layouts
 import { WorkspaceAuthorizationLayout } from "layouts/auth-layout";
+import SettingsNavbar from "layouts/settings-navbar";
 // components
 import { CustomThemeSelector, ThemeSwitch } from "components/core";
 // ui
@@ -36,27 +37,37 @@ const ProfilePreferences = () => {
           <BreadcrumbItem title="My Profile Preferences" />
         </Breadcrumbs>
       }
-      profilePage
     >
       {myProfile ? (
-        <div className="space-y-8 sm:space-y-12">
-          <div className="grid grid-cols-12 gap-4 sm:gap-16">
-            <div className="col-span-12 sm:col-span-6">
-              <h4 className="text-lg font-semibold text-brand-base">Theme</h4>
-              <p className="text-sm text-brand-secondary">
-                Select or customize your interface color scheme.
+        <div className="px-24 py-8">
+          <div className="mb-12 space-y-6">
+            <div>
+              <h3 className="text-3xl font-semibold">Profile Settings</h3>
+              <p className="mt-1 text-brand-secondary">
+                This information will be visible to only you.
               </p>
             </div>
-            <div className="col-span-12 sm:col-span-6">
-              <ThemeSwitch
-                user={myProfile}
-                setPreLoadedData={setPreLoadedData}
-                customThemeSelectorOptions={customThemeSelectorOptions}
-                setCustomThemeSelectorOptions={setCustomThemeSelectorOptions}
-              />
-            </div>
+            <SettingsNavbar profilePage />
           </div>
-          {customThemeSelectorOptions && <CustomThemeSelector preLoadedData={preLoadedData} />}
+          <div className="space-y-8 sm:space-y-12">
+            <div className="grid grid-cols-12 gap-4 sm:gap-16">
+              <div className="col-span-12 sm:col-span-6">
+                <h4 className="text-lg font-semibold text-brand-base">Theme</h4>
+                <p className="text-sm text-brand-secondary">
+                  Select or customize your interface color scheme.
+                </p>
+              </div>
+              <div className="col-span-12 sm:col-span-6">
+                <ThemeSwitch
+                  user={myProfile}
+                  setPreLoadedData={setPreLoadedData}
+                  customThemeSelectorOptions={customThemeSelectorOptions}
+                  setCustomThemeSelectorOptions={setCustomThemeSelectorOptions}
+                />
+              </div>
+            </div>
+            {customThemeSelectorOptions && <CustomThemeSelector preLoadedData={preLoadedData} />}
+          </div>
         </div>
       ) : (
         <div className="grid h-full w-full place-items-center px-4 sm:px-0">
