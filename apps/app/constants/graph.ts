5ee9e6d06ebd17ae811da275741b38f075768c90
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2023-05-11 17:25:53 +0530

    fix: merge conflicts

--- apps/app/constants/graph.ts
+++ apps/app/constants/graph.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // nivo
 import { Theme } from "@nivo/core";
 
@@ -9,17 +8,6 @@ export const CHARTS_THEME: Theme = {
     domain: {
       line: {
         stroke: "rgb(var(--color-border))",
-=======
-import { Theme } from "@nivo/core";
-
-export const CHARTS_THEME: Theme = {
-  background: "rgb(var(--color-bg-base))",
-  textColor: "rgb(var(--color-text-base))",
-  axis: {
-    domain: {
-      line: {
-        stroke: "rgb(var(--color-text-base))",
->>>>>>> d7928f853d08f9957c90fe58f5b183c662bc346c
         strokeWidth: 0.5,
       },
     },
@@ -29,10 +17,7 @@ export const CHARTS_THEME: Theme = {
       background: "rgb(var(--color-bg-surface-2))",
       color: "rgb(var(--color-text-secondary))",
       fontSize: "0.8rem",
-<<<<<<< HEAD
       border: "1px solid rgb(var(--color-border))",
-=======
->>>>>>> d7928f853d08f9957c90fe58f5b183c662bc346c
     },
   },
   grid: {
