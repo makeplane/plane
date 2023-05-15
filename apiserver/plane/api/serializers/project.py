b0b57ac73138bbcc0f4a4cf401f60ff8ecd48bce
pablohashescobar <nikhilschacko@gmail.com>
2023-05-15 18:17:01 +0530

    chore: project members, cycles and modules count

--- apiserver/plane/api/serializers/project.py
+++ apiserver/plane/api/serializers/project.py
@@ -82,6 +82,9 @@ class ProjectDetailSerializer(BaseSerializer):
     default_assignee = UserLiteSerializer(read_only=True)
     project_lead = UserLiteSerializer(read_only=True)
     is_favorite = serializers.BooleanField(read_only=True)
+    total_members = serializers.IntegerField(read_only=True)
+    total_cycles = serializers.IntegerField(read_only=True)
+    total_modules = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Project
