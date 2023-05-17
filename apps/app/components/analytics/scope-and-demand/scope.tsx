3427652c22c79866e07bc61521ba4494866119a4
Aaryan Khandelwal <65252264+aaryan610@users.noreply.github.com>
2023-05-17 13:25:58 +0530

    chore: update analytics sidebar and header content, fix: trash box positioning (#1065)
    * fix: labels dropdown on issue details page theming
    
    * style: trash box styling and positioning
    
    * chore: empty state for scope and demand analytics, show assignee name in scope graph tooltip
    
    * chore: empty state for analytics
    
    * chore: modify analytics sidebar and header

--- apps/app/components/analytics/scope-and-demand/scope.tsx
+++ apps/app/components/analytics/scope-and-demand/scope.tsx
@@ -13,56 +13,72 @@ export const AnalyticsScope: React.FC<Props> = ({ defaultAnalytics }) => (
     <div className="divide-y divide-brand-base">
       <div>
         <h6 className="px-3 text-base font-medium">Pending issues</h6>
-        <BarGraph
-          data={defaultAnalytics.pending_issue_user}
-          indexBy="assignees__email"
-          keys={["count"]}
-          height="250px"
-          colors={() => `#f97316`}
-          customYAxisTickValues={defaultAnalytics.pending_issue_user.map((d) => d.count)}
-          tooltip={(datum) => (
-            <div className="rounded-md border border-brand-base bg-brand-surface-2 p-2 text-xs">
-              <span className="font-medium text-brand-secondary">
-                Issue count- {datum.indexValue ?? "No assignee"}:{" "}
-              </span>
-              {datum.value}
-            </div>
-          )}
-          axisBottom={{
-            renderTick: (datum) => {
-              const avatar =
-                defaultAnalytics.pending_issue_user[datum.tickIndex].assignees__avatar ?? "";
+        {defaultAnalytics.pending_issue_user.length > 0 ? (
+          <BarGraph
+            data={defaultAnalytics.pending_issue_user}
+            indexBy="assignees__email"
+            keys={["count"]}
+            height="250px"
+            colors={() => `#f97316`}
+            customYAxisTickValues={defaultAnalytics.pending_issue_user.map((d) => d.count)}
+            tooltip={(datum) => {
+              const assignee = defaultAnalytics.pending_issue_user.find(
+                (a) => a.assignees__email === `${datum.indexValue}`
+              );
 
-              if (avatar && avatar !== "")
-                return (
-                  <g transform={`translate(${datum.x},${datum.y})`}>
-                    <image
-                      x={-8}
-                      y={10}
-                      width={16}
-                      height={16}
-                      xlinkHref={avatar}
-                      style={{ clipPath: "circle(50%)" }}
-                    />
-                  </g>
-                );
-              else
-                return (
-                  <g transform={`translate(${datum.x},${datum.y})`}>
-                    <circle cy={18} r={8} fill="#374151" />
-                    <text x={0} y={21} textAnchor="middle" fontSize={9} fill="#ffffff">
-                      {datum.value ? `${datum.value}`.toUpperCase()[0] : "?"}
-                    </text>
-                  </g>
-                );
-            },
-          }}
-          margin={{ top: 20 }}
-          theme={{
-            background: "rgb(var(--color-bg-base))",
-            axis: {},
-          }}
-        />
+              return (
+                <div className="rounded-md border border-brand-base bg-brand-surface-2 p-2 text-xs">
+                  <span className="font-medium text-brand-secondary">
+                    Issue count-{" "}
+                    {assignee
+                      ? assignee.assignees__first_name + " " + assignee.assignees__last_name
+                      : "No assignee"}
+                    :{" "}
+                  </span>
+                  {datum.value}
+                </div>
+              );
+            }}
+            axisBottom={{
+              renderTick: (datum) => {
+                const avatar =
+                  defaultAnalytics.pending_issue_user[datum.tickIndex].assignees__avatar ?? "";
+
+                if (avatar && avatar !== "")
+                  return (
+                    <g transform={`translate(${datum.x},${datum.y})`}>
+                      <image
+                        x={-8}
+                        y={10}
+                        width={16}
+                        height={16}
+                        xlinkHref={avatar}
+                        style={{ clipPath: "circle(50%)" }}
+                      />
+                    </g>
+                  );
+                else
+                  return (
+                    <g transform={`translate(${datum.x},${datum.y})`}>
+                      <circle cy={18} r={8} fill="#374151" />
+                      <text x={0} y={21} textAnchor="middle" fontSize={9} fill="#ffffff">
+                        {datum.value ? `${datum.value}`.toUpperCase()[0] : "?"}
+                      </text>
+                    </g>
+                  );
+              },
+            }}
+            margin={{ top: 20 }}
+            theme={{
+              background: "rgb(var(--color-bg-base))",
+              axis: {},
+            }}
+          />
+        ) : (
+          <div className="text-brand-secondary text-center text-sm py-8">
+            No matching data found.
+          </div>
+        )}
       </div>
     </div>
   </div>
--- apps/app/components/analytics/scope-and-demand/year-wise-issues.tsx
+++ apps/app/components/analytics/scope-and-demand/year-wise-issues.tsx
@@ -17,34 +17,38 @@ export const AnalyticsYearWiseIssues: React.FC<Props> = ({ defaultAnalytics }) =
   return (
     <div className="py-3 border border-brand-base rounded-[10px]">
       <h1 className="px-3 text-base font-medium">Issues closed in a year</h1>
-      <LineGraph
-        data={[
-          {
-            id: "issues_closed",
-            color: "rgb(var(--color-accent))",
-            data: MONTHS_LIST.map((month) => ({
-              x: month.label.substring(0, 3),
-              y:
-                defaultAnalytics.issue_completed_month_wise.find(
-                  (data) => data.month === month.value
-                )?.count || 0,
-            })),
-          },
-        ]}
-        customYAxisTickValues={defaultAnalytics.issue_completed_month_wise.map((data) => {
-          if (quarterMonthsList.includes(data.month)) return data.count;
+      {defaultAnalytics.issue_completed_month_wise.length > 0 ? (
+        <LineGraph
+          data={[
+            {
+              id: "issues_closed",
+              color: "rgb(var(--color-accent))",
+              data: MONTHS_LIST.map((month) => ({
+                x: month.label.substring(0, 3),
+                y:
+                  defaultAnalytics.issue_completed_month_wise.find(
+                    (data) => data.month === month.value
+                  )?.count || 0,
+              })),
+            },
+          ]}
+          customYAxisTickValues={defaultAnalytics.issue_completed_month_wise.map((data) => {
+            if (quarterMonthsList.includes(data.month)) return data.count;
 
-          return 0;
-        })}
-        height="300px"
-        colors={(datum) => datum.color}
-        curve="monotoneX"
-        margin={{ top: 20 }}
-        theme={{
-          background: "rgb(var(--color-bg-base))",
-        }}
-        enableArea
-      />
+            return 0;
+          })}
+          height="300px"
+          colors={(datum) => datum.color}
+          curve="monotoneX"
+          margin={{ top: 20 }}
+          theme={{
+            background: "rgb(var(--color-bg-base))",
+          }}
+          enableArea
+        />
+      ) : (
+        <div className="text-brand-secondary text-center text-sm py-8">No matching data found.</div>
+      )}
     </div>
   );
 };
