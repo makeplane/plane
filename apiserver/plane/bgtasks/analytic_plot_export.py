97effc3d9ddf1614baa7af5a1263a6141591da51
pablohashescobar <nikhilschacko@gmail.com>
2023-05-17 15:49:56 +0530

    dev: fix none type

--- apiserver/plane/bgtasks/analytic_plot_export.py
+++ apiserver/plane/bgtasks/analytic_plot_export.py
@@ -73,7 +73,7 @@ def analytic_export_task(email, data, slug):
 
                 data = distribution.get(item)
                 # Add y axis values
-                generated_row.append(sum(obj.get(y_axis) for obj in data))
+                generated_row.append(sum(obj.get(y_axis) for obj in data if obj.get(y_axis) is not None))
 
                 for segment in segment_zero:
                     value = [x for x in data if x.get("segment") == segment]
