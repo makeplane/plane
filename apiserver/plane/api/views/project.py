29604696abcb0dc0eb6be90c5f0386f0edbc08c1
pablohashescobar <nikhilschacko@gmail.com>
2023-05-17 12:17:22 +0530

    dev: fix exception when creating default inbox

--- apiserver/plane/api/views/project.py
+++ apiserver/plane/api/views/project.py
@@ -215,9 +215,9 @@ class ProjectViewSet(BaseViewSet):
             )
 
             if serializer.is_valid():
+                serializer.save()
                 if serializer.data["inbox_view"]:
                     Inbox.objects.get_or_create(name=f"{project.name} Inbox", project=project, is_default=True)
-                serializer.save()
                 return Response(serializer.data, status=status.HTTP_200_OK)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
