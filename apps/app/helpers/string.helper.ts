0ff6f04e01fdff996a98ef8bb53b8156618e8533
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2023-05-17 16:59:26 +0530

    chore: modify random color generator function

--- apps/app/helpers/string.helper.ts
+++ apps/app/helpers/string.helper.ts
@@ -89,13 +89,21 @@ export const cosineSimilarity = (a: string, b: string) => {
 };
 
 export const generateRandomColor = (string: string): string => {
-  if (!string) return "var(rgb(--color-accent))";
+  if (!string) return "var(--color-accent)";
 
-  const hash = string.split("").reduce((acc, char) => acc + char.charCodeAt(0), 0);
+  string = `${string}`;
+
+  const uniqueId = string.length.toString() + string; // Unique identifier based on string length
+  const combinedString = uniqueId + string;
+
+  const hash = Array.from(combinedString).reduce((acc, char) => {
+    const charCode = char.charCodeAt(0);
+    return (acc << 5) - acc + charCode;
+  }, 0);
 
   const hue = hash % 360;
-  const saturation = 70;
-  const lightness = 60;
+  const saturation = 70; // Higher saturation for pastel colors
+  const lightness = 60; // Mid-range lightness for pastel colors
 
   const randomColor = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
 
