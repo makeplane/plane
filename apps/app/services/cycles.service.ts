4fe32dee93b7843ceafdebacdf1e579c1edda30a
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2023-06-06 20:26:11 +0530

    chore: fetch only high priority issues for the active cycle

--- apps/app/services/cycles.service.ts
+++ apps/app/services/cycles.service.ts
@@ -72,7 +72,7 @@ class ProjectCycleServices extends APIService {
     workspaceSlug: string,
     projectId: string,
     cycleId: string,
-    queries?: Partial<IIssueViewOptions>
+    queries?: any
   ): Promise<IIssue[] | { [key: string]: IIssue[] }> {
     return this.get(
       `/api/workspaces/${workspaceSlug}/projects/${projectId}/cycles/${cycleId}/cycle-issues/`,
