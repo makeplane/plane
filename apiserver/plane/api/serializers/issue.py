56a4e18a3c681901ba56da8249403c6d91e08309
pablohashescobar <118773738+pablohashescobar@users.noreply.github.com>
2023-06-16 18:59:13 +0530

    chore: link and attachment count for cycles and modules (#1307)

--- apiserver/plane/api/serializers/issue.py
+++ apiserver/plane/api/serializers/issue.py
@@ -483,6 +483,8 @@ class IssueStateSerializer(BaseSerializer):
     assignee_details = UserLiteSerializer(read_only=True, source="assignees", many=True)
     sub_issues_count = serializers.IntegerField(read_only=True)
     bridge_id = serializers.UUIDField(read_only=True)
+    attachment_count = serializers.IntegerField(read_only=True)
+    link_count = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Issue
