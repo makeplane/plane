1da86b80b2e1a5b2f9554ae95afc90f4dc555576
Aaryan Khandelwal <65252264+aaryan610@users.noreply.github.com>
2023-06-19 12:59:57 +0530

    chore: change charts library (#1305)
    * fix: dashboard charts
    
    * fix: cycles new charts
    
    * chore: sidebar burn down chart and calendar graph
    
    * chore: update dashboard line and pie graph
    
    * chore: update axes width of burndown chart
    
    ---------
    
    Co-authored-by: Dakshesh Jain <dakshesh.jain14@gmail.com>

--- apps/app/components/cycles/sidebar.tsx
+++ apps/app/components/cycles/sidebar.tsx
@@ -485,7 +485,6 @@ export const CycleDetailsSidebar: React.FC<Props> = ({
                 </div>
               </div>
             </div>
-
             <div className="flex w-full flex-col items-center justify-start gap-2 border-t border-brand-base p-6">
               <Disclosure defaultOpen>
                 {({ open }) => (
@@ -552,7 +551,7 @@ export const CycleDetailsSidebar: React.FC<Props> = ({
                                 </div>
                               </div>
                             </div>
-                            <div className="relative h-40 w-80">
+                            <div className="relative">
                               <ProgressChart
                                 issues={issues ?? []}
                                 start={cycle?.start_date ?? ""}
@@ -569,7 +568,6 @@ export const CycleDetailsSidebar: React.FC<Props> = ({
                 )}
               </Disclosure>
             </div>
-
             <div className="flex w-full flex-col items-center justify-start gap-2 border-t border-brand-base p-6">
               <Disclosure defaultOpen>
                 {({ open }) => (
@@ -604,7 +602,7 @@ export const CycleDetailsSidebar: React.FC<Props> = ({
                     <Transition show={open}>
                       <Disclosure.Panel>
                         {cycle.total_issues > 0 ? (
-                          <div className=" h-full w-full py-4">
+                          <div className="h-full w-full py-4">
                             <SidebarProgressStats
                               issues={issues ?? []}
                               groupedIssues={{
