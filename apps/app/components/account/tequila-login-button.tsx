e7b24b64eb10df2a4d3d95ec223677a4d2a91f58
Ankur Singh <ankur.singh@epfl.ch>
2023-06-20 17:15:07 +0200

    fix tequila login link

--- apps/app/components/account/tequila-login-button.tsx
+++ apps/app/components/account/tequila-login-button.tsx
@@ -6,7 +6,7 @@ import userImage from "/public/user.png";
 
 export const TequilaLoginButton: FC = () => (
   <div className="w-full flex justify-center items-center px-[3px]">
-    <Link href="http://localhost:8000/api/login">
+    <Link href="/api/login">
       <button className="flex w-full items-center justify-center gap-3 rounded-md border border-gray-200 p-2 text-sm font-medium text-gray-600 duration-300 hover:bg-gray-50">
         <Image src={userImage} height={22} width={22} color="#000" alt="Tequila Logo" />
         <span>Sign In with Tequila</span>
