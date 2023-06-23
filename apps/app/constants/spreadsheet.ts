0b47d4a86d7091c428e4ab1976795c2f3bb4459b
anmolsinghbhatia <anmolsinghbhatia@caravel.tech>
2023-06-24 01:21:39 +0530

    chore: assignees sort order updated

--- apps/app/constants/spreadsheet.ts
+++ apps/app/constants/spreadsheet.ts
@@ -32,8 +32,8 @@ export const SPREADSHEET_COLUMN = [
     colName: "Assignees",
     colSize: "128px",
     icon: UserGroupIcon,
-    ascendingOrder: "assignees__name",
-    descendingOrder: "-assignees__name",
+    ascendingOrder: "assignees__first_name",
+    descendingOrder: "-assignees__first_name",
   },
   {
     propertyName: "labels",
