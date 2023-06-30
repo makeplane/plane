a5acfcd0dc3c999c09af565d6b98b467df06cd4e
Chandan Jal <chandan.jal.code@gmail.com>
2023-06-30 14:38:10 +0530

    fix: scrolling experience on page

--- apps/app/components/pages/pages-view.tsx
+++ apps/app/components/pages/pages-view.tsx
@@ -205,7 +205,7 @@ export const PagesView: React.FC<Props> = ({ pages, viewType }) => {
         user={user}
       />
       {pages ? (
-        <div className="space-y-4 h-full">
+        <div className="space-y-4 h-full overflow-y-auto">
           {pages.length > 0 ? (
             viewType === "list" ? (
               <ul role="list" className="divide-y divide-brand-base">
