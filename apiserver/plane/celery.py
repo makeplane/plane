34983a1f299ec4cc81dd077dcf04752d6e704082
NarayanBavisetti <narayan3119@gmail.com>
2023-06-30 17:22:25 +0530

    chore: added the beat scheduler command

--- apiserver/plane/celery.py
+++ apiserver/plane/celery.py
@@ -24,3 +24,5 @@ app.conf.beat_schedule = {
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+
+app.conf.beat_scheduler = 'django_celery_beat.schedulers.DatabaseScheduler'
