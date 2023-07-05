83f2351ba7a14a4d5b29ace98be246dbf80439e7
gurusainath <gurusainath007@gmail.com>
2023-07-05 16:13:16 +0530

    dev: integrated supabase and implemented web waitlist api endpoint

---
+++ apps/app/services/web-waitlist.service.ts
@@ -0,0 +1,21 @@
+// services
+import APIService from "services/api.service";
+
+// types
+import { IWebWaitListResponse } from "types";
+
+class WebWailtListServices extends APIService {
+  constructor() {
+    super("/");
+  }
+
+  async create({ email }: { email: string }): Promise<IWebWaitListResponse> {
+    return this.post(`/api/web-waitlist`, { email: email })
+      .then((response) => response?.data)
+      .catch((error) => {
+        throw error?.response;
+      });
+  }
+}
+
+export default new WebWailtListServices();
