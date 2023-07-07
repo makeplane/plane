cd5de99035699546d10e761b3437e7e6baac12ac
chandanvc <chandan.jal@viitor.cloud>
2023-07-07 11:52:02 +0530

    fix: page details search label consisting search query #1489

--- apps/app/components/ui/custom-search-select.tsx
+++ apps/app/components/ui/custom-search-select.tsx
@@ -65,6 +65,10 @@ export const CustomSearchSelect = ({
     disabled,
   };
 
+  const handleOpenChange = (open: boolean) => {
+    if (!open) setQuery("");
+  };
+
   if (multiple) props.multiple = true;
 
   return (
@@ -73,120 +77,123 @@ export const CustomSearchSelect = ({
       className={`${!selfPositioned ? "relative" : ""} flex-shrink-0 text-left`}
       {...props}
     >
-      {({ open }: any) => (
-        <>
-          {customButton ? (
-            <Combobox.Button as="div">{customButton}</Combobox.Button>
-          ) : (
-            <Combobox.Button
-              className={`flex w-full border border-brand-base ${
-                disabled ? "cursor-not-allowed" : "cursor-pointer hover:bg-brand-surface-2"
-              } ${
-                input ? "px-3 py-2 text-sm" : "px-2.5 py-1 text-xs"
-              } items-center justify-between gap-1 rounded-md shadow-sm duration-300 focus:outline-none focus:ring-1 focus:ring-brand-base ${
-                textAlignment === "right"
-                  ? "text-right"
-                  : textAlignment === "center"
-                  ? "text-center"
-                  : "text-left"
-              }`}
-            >
-              {label}
-              {!noChevron && !disabled && (
-                <ChevronDownIcon className="h-3 w-3" aria-hidden="true" />
-              )}
-            </Combobox.Button>
-          )}
-          <Transition
-            show={open}
-            as={React.Fragment}
-            enter="transition ease-out duration-200"
-            enterFrom="opacity-0 translate-y-1"
-            enterTo="opacity-100 translate-y-0"
-            leave="transition ease-in duration-150"
-            leaveFrom="opacity-100 translate-y-0"
-            leaveTo="opacity-0 translate-y-1"
-          >
-            <Combobox.Options
-              className={`${optionsClassName} absolute min-w-[10rem] border border-brand-base p-2 ${
-                position === "right" ? "right-0" : "left-0"
-              } ${
-                verticalPosition === "top" ? "bottom-full mb-1" : "mt-1"
-              } z-10 origin-top-right rounded-md bg-brand-surface-1 text-xs shadow-lg focus:outline-none ${
-                dropdownWidth ? dropdownWidth : ``
-              } `}
-            >
-              <div className="flex w-full items-center justify-start rounded-sm border-[0.6px] border-brand-base bg-brand-surface-1 px-2">
-                <MagnifyingGlassIcon className="h-3 w-3 text-brand-secondary" />
-                <Combobox.Input
-                  className="w-full bg-transparent py-1 px-2 text-xs text-brand-secondary focus:outline-none"
-                  onChange={(e) => setQuery(e.target.value)}
-                  placeholder="Type to search..."
-                  displayValue={(assigned: any) => assigned?.name}
-                />
-              </div>
-              <div
-                className={`mt-2 space-y-1 ${
-                  height === "sm"
-                    ? "max-h-28"
-                    : height === "md"
-                    ? "max-h-44"
-                    : height === "rg"
-                    ? "max-h-56"
-                    : height === "lg"
-                    ? "max-h-80"
-                    : ""
-                } overflow-y-scroll`}
+      {({ open }: any) => {
+        handleOpenChange(open);
+        return (
+          <>
+            {customButton ? (
+              <Combobox.Button as="div">{customButton}</Combobox.Button>
+            ) : (
+              <Combobox.Button
+                className={`flex w-full border border-brand-base ${
+                  disabled ? "cursor-not-allowed" : "cursor-pointer hover:bg-brand-surface-2"
+                } ${
+                  input ? "px-3 py-2 text-sm" : "px-2.5 py-1 text-xs"
+                } items-center justify-between gap-1 rounded-md shadow-sm duration-300 focus:outline-none focus:ring-1 focus:ring-brand-base ${
+                  textAlignment === "right"
+                    ? "text-right"
+                    : textAlignment === "center"
+                    ? "text-center"
+                    : "text-left"
+                }`}
               >
-                {filteredOptions ? (
-                  filteredOptions.length > 0 ? (
-                    filteredOptions.map((option) => (
-                      <Combobox.Option
-                        key={option.value}
-                        value={option.value}
-                        className={({ active, selected }) =>
-                          `${active || selected ? "bg-brand-surface-2" : ""} ${
-                            selected ? "font-medium" : ""
-                          } flex cursor-pointer select-none items-center justify-between gap-2 truncate rounded px-1 py-1.5 text-brand-secondary`
-                        }
-                      >
-                        {({ active, selected }) => (
-                          <>
-                            {option.content}
-                            {multiple ? (
-                              <div
-                                className={`flex items-center justify-center rounded border border-gray-500 p-0.5 ${
-                                  active || selected ? "opacity-100" : "opacity-0"
-                                }`}
-                              >
+                {label}
+                {!noChevron && !disabled && (
+                  <ChevronDownIcon className="h-3 w-3" aria-hidden="true" />
+                )}
+              </Combobox.Button>
+            )}
+            <Transition
+              show={open}
+              as={React.Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Combobox.Options
+                className={`${optionsClassName} absolute min-w-[10rem] border border-brand-base p-2 ${
+                  position === "right" ? "right-0" : "left-0"
+                } ${
+                  verticalPosition === "top" ? "bottom-full mb-1" : "mt-1"
+                } z-10 origin-top-right rounded-md bg-brand-surface-1 text-xs shadow-lg focus:outline-none ${
+                  dropdownWidth ? dropdownWidth : ``
+                } `}
+              >
+                <div className="flex w-full items-center justify-start rounded-sm border-[0.6px] border-brand-base bg-brand-surface-1 px-2">
+                  <MagnifyingGlassIcon className="h-3 w-3 text-brand-secondary" />
+                  <Combobox.Input
+                    className="w-full bg-transparent py-1 px-2 text-xs text-brand-secondary focus:outline-none"
+                    onChange={(e) => setQuery(e.target.value)}
+                    placeholder="Type to search..."
+                    displayValue={(assigned: any) => assigned?.name}
+                  />
+                </div>
+                <div
+                  className={`mt-2 space-y-1 ${
+                    height === "sm"
+                      ? "max-h-28"
+                      : height === "md"
+                      ? "max-h-44"
+                      : height === "rg"
+                      ? "max-h-56"
+                      : height === "lg"
+                      ? "max-h-80"
+                      : ""
+                  } overflow-y-scroll`}
+                >
+                  {filteredOptions ? (
+                    filteredOptions.length > 0 ? (
+                      filteredOptions.map((option) => (
+                        <Combobox.Option
+                          key={option.value}
+                          value={option.value}
+                          className={({ active, selected }) =>
+                            `${active || selected ? "bg-brand-surface-2" : ""} ${
+                              selected ? "font-medium" : ""
+                            } flex cursor-pointer select-none items-center justify-between gap-2 truncate rounded px-1 py-1.5 text-brand-secondary`
+                          }
+                        >
+                          {({ active, selected }) => (
+                            <>
+                              {option.content}
+                              {multiple ? (
+                                <div
+                                  className={`flex items-center justify-center rounded border border-gray-500 p-0.5 ${
+                                    active || selected ? "opacity-100" : "opacity-0"
+                                  }`}
+                                >
+                                  <CheckIcon
+                                    className={`h-3 w-3 ${selected ? "opacity-100" : "opacity-0"}`}
+                                  />
+                                </div>
+                              ) : (
                                 <CheckIcon
                                   className={`h-3 w-3 ${selected ? "opacity-100" : "opacity-0"}`}
                                 />
-                              </div>
-                            ) : (
-                              <CheckIcon
-                                className={`h-3 w-3 ${selected ? "opacity-100" : "opacity-0"}`}
-                              />
-                            )}
-                          </>
-                        )}
-                      </Combobox.Option>
-                    ))
+                              )}
+                            </>
+                          )}
+                        </Combobox.Option>
+                      ))
+                    ) : (
+                      <span className="flex items-center gap-2 p-1">
+                        {noResultIcon && noResultIcon}
+                        <p className="text-left text-brand-secondary ">No matching results</p>
+                      </span>
+                    )
                   ) : (
-                    <span className="flex items-center gap-2 p-1">
-                      {noResultIcon && noResultIcon}
-                      <p className="text-left text-brand-secondary ">No matching results</p>
-                    </span>
-                  )
-                ) : (
-                  <p className="text-center text-brand-secondary">Loading...</p>
-                )}
-              </div>
-              {footerOption}
-            </Combobox.Options>
-          </Transition>
-        </>
-      )}
+                    <p className="text-center text-brand-secondary">Loading...</p>
+                  )}
+                </div>
+                {footerOption}
+              </Combobox.Options>
+            </Transition>
+          </>
+        );
+      }}
     </Combobox>
   );
 };
