b12a00cf4aa1568212429c684dfacacb8bcbb920
Nikhil <118773738+pablohashescobar@users.noreply.github.com>
2023-07-18 15:10:01 +0530

    chore: default project state colors (#1540)
    * chore: add triggered by details for notifications
    
    * dev: update issue activity json to include extra fields
    
    * chore: project create default state colors

--- apiserver/plane/api/views/project.py
+++ apiserver/plane/api/views/project.py
@@ -160,32 +160,32 @@ class ProjectViewSet(BaseViewSet):
                 states = [
                     {
                         "name": "Backlog",
-                        "color": "#5e6ad2",
+                        "color": "#A3A3A3",
                         "sequence": 15000,
                         "group": "backlog",
                         "default": True,
                     },
                     {
                         "name": "Todo",
-                        "color": "#eb5757",
+                        "color": "#3A3A3A",
                         "sequence": 25000,
                         "group": "unstarted",
                     },
                     {
                         "name": "In Progress",
-                        "color": "#26b5ce",
+                        "color": "#F59E0B",
                         "sequence": 35000,
                         "group": "started",
                     },
                     {
                         "name": "Done",
-                        "color": "#f2c94c",
+                        "color": "#16A34A",
                         "sequence": 45000,
                         "group": "completed",
                     },
                     {
                         "name": "Cancelled",
-                        "color": "#4cb782",
+                        "color": "#EF4444",
                         "sequence": 55000,
                         "group": "cancelled",
                     },
