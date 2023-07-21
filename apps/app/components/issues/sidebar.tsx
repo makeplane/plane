dbff44435195ca922f607a2fa05ee646bf5e7945
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2023-07-21 23:49:06 +0530

    fix: build error

--- apps/app/components/issues/sidebar.tsx
+++ apps/app/components/issues/sidebar.tsx
@@ -43,7 +43,7 @@ import { ISSUE_DETAILS } from "constants/fetch-keys";
 
 type Props = {
   control: any;
-  submitChanges: (formData: Partial<IIssue>) => void;
+  submitChanges: (formData: any) => void;
   issueDetail: IIssue | undefined;
   watch: UseFormWatch<IIssue>;
   fieldsToShow?: (
