90876d151927631e05a957c7e1de242ea4d7e1af
pablohashescobar <nikhilschacko@gmail.com>
2023-07-25 11:43:30 +0530

    fix: handle model save error in issue activity

--- apiserver/plane/bgtasks/issue_activites_task.py
+++ apiserver/plane/bgtasks/issue_activites_task.py
@@ -1037,8 +1037,11 @@ def issue_activity(
             issue = Issue.objects.filter(pk=issue_id).first()
 
             if issue is not None:
-                issue.updated_at = timezone.now()
-                issue.save(update_fields=["updated_at"])
+                try:
+                    issue.updated_at = timezone.now()
+                    issue.save(update_fields=["updated_at"])
+                except Exception as e:
+                    pass
 
             if subscriber:
                 # add the user to issue subscriber
