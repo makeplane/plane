e8efe72ed8a9bc6bcc8c36e19d06406765aad1e5
pablohashescobar <nikhilschacko@gmail.com>
2023-07-27 14:29:45 +0530

    fix: cycles n+1

--- apiserver/plane/api/serializers/cycle.py
+++ apiserver/plane/api/serializers/cycle.py
@@ -12,6 +12,12 @@ from .workspace import WorkspaceLiteSerializer
 from .project import ProjectLiteSerializer
 from plane.db.models import Cycle, CycleIssue, CycleFavorite
 
+class CycleWriteSerializer(BaseSerializer):
+
+    class Meta:
+        model = Cycle
+        fields = "__all__"
+
 
 class CycleSerializer(BaseSerializer):
     owned_by = UserLiteSerializer(read_only=True)
