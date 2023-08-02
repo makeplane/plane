7943d8a7adce6d451eac273178b4b5f2b10b6f62
Tyler Harrison <tyleraharrison@gmail.com>
2023-08-02 15:30:45 -0500

    Added option to sort by first created

--- apps/app/constants/issue.ts
+++ apps/app/constants/issue.ts
@@ -16,6 +16,7 @@ export const ORDER_BY_OPTIONS: Array<{
   key: TIssueOrderByOptions;
 }> = [
   { name: "Manual", key: "sort_order" },
+  { name: 'First created', key: 'created_at' },
   { name: "Last created", key: "-created_at" },
   { name: "Last updated", key: "-updated_at" },
   { name: "Priority", key: "priority" },
