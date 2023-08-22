b32828222170fe4a8a624a051a9795ed2110a4eb
pablohashescobar <nikhilschacko@gmail.com>
2023-08-22 22:03:05 +0530

    feat: project deploy board endpoint

--- apiserver/plane/api/urls.py
+++ apiserver/plane/api/urls.py
@@ -174,6 +174,7 @@ from plane.api.views import (
     CommentReactionPublicViewSet,
     InboxIssuePublicViewSet,
     IssueVotePublicViewSet,
+    WorkspaceProjectDeployBoardEndpoint,
     ## End Public Boards
 )
 
@@ -1613,6 +1614,11 @@ urlpatterns = [
             }
         ),
         name="issue-vote-project-board",
+    ),
+    path(
+        "public/workspaces/<str:slug>/project-boards/",
+        WorkspaceProjectDeployBoardEndpoint.as_view(),
+        name="workspace-project-boards",
     ),
     ## End Public Boards
 ]
