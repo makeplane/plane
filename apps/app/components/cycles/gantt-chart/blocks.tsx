e7631bb2d9c939a4ad63ad08fadc9b20979b96f1
Aaryan Khandelwal <aaryan610@Aaryans-MacBook-Pro.local>
2023-08-23 13:15:46 +0530

    style: cycle blocks

--- apps/app/components/cycles/gantt-chart/blocks.tsx
+++ apps/app/components/cycles/gantt-chart/blocks.tsx
@@ -3,8 +3,10 @@ import { useRouter } from "next/router";
 
 // ui
 import { Tooltip } from "components/ui";
+// icons
+import { ContrastIcon } from "components/icons";
 // helpers
-import { renderShortDate } from "helpers/date-time.helper";
+import { getDateRangeStatus, renderShortDate } from "helpers/date-time.helper";
 // types
 import { ICycle } from "types";
 
@@ -12,23 +14,40 @@ export const CycleGanttBlock = ({ cycle }: { cycle: ICycle }) => {
   const router = useRouter();
   const { workspaceSlug } = router.query;
 
+  const cycleStatus = getDateRangeStatus(cycle?.start_date, cycle?.end_date);
+
   return (
-    <Link href={`/${workspaceSlug}/projects/${cycle.project}/cycles/${cycle.id}`}>
-      <a className="relative flex items-center w-full h-full shadow-sm transition-all duration-300">
-        <div className="flex-shrink-0 w-0.5 h-full bg-custom-primary-100" />
+    <Link href={`/${workspaceSlug}/projects/${cycle?.project}/cycles/${cycle?.id}`}>
+      <a
+        className="flex items-center relative h-full w-full rounded"
+        style={{
+          backgroundColor:
+            cycleStatus === "current"
+              ? "#09a953"
+              : cycleStatus === "upcoming"
+              ? "#f7ae59"
+              : cycleStatus === "completed"
+              ? "#3f76ff"
+              : cycleStatus === "draft"
+              ? "rgb(var(--color-text-200))"
+              : "",
+        }}
+      >
+        <div className="absolute top-0 left-0 h-full w-full bg-custom-background-100/50" />
         <Tooltip
           tooltipContent={
             <div className="space-y-1">
-              <h5>{cycle.name}</h5>
+              <h5>{cycle?.name}</h5>
               <div>
-                {renderShortDate(cycle.start_date ?? "")} to {renderShortDate(cycle.end_date ?? "")}
+                {renderShortDate(cycle?.start_date ?? "")} to{" "}
+                {renderShortDate(cycle?.end_date ?? "")}
               </div>
             </div>
           }
           position="top-left"
         >
-          <div className="text-custom-text-100 text-sm truncate py-1 px-2.5 w-full">
-            {cycle.name}
+          <div className="relative text-custom-text-100 text-sm truncate py-1 px-2.5 w-full">
+            {cycle?.name}
           </div>
         </Tooltip>
       </a>
@@ -40,9 +59,25 @@ export const CycleGanttSidebarBlock = ({ cycle }: { cycle: ICycle }) => {
   const router = useRouter();
   const { workspaceSlug } = router.query;
 
+  const cycleStatus = getDateRangeStatus(cycle?.start_date, cycle?.end_date);
+
   return (
     <Link href={`/${workspaceSlug}/projects/${cycle?.project}/issues/${cycle?.id}`}>
       <a className="relative w-full flex items-center gap-2 h-full">
+        <ContrastIcon
+          className="h-5 w-5"
+          color={`${
+            cycleStatus === "current"
+              ? "#09a953"
+              : cycleStatus === "upcoming"
+              ? "#f7ae59"
+              : cycleStatus === "completed"
+              ? "#3f76ff"
+              : cycleStatus === "draft"
+              ? "rgb(var(--color-text-200))"
+              : ""
+          }`}
+        />
         <h6 className="text-sm font-medium flex-grow truncate">{cycle?.name}</h6>
       </a>
     </Link>
