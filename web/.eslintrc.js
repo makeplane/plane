b77ddf9c9526692f5fbd1fa559b9e48a6970688e
sriramveeraghanta <veeraghanta.sriram@gmail.com>
2023-08-23 16:10:36 +0530

    eslint and tsconfig fixes

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,7 +1,16 @@
 module.exports = {
-  root: true,
-  extends: ["custom"],
+  extends: ["next", "prettier"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react", "@typescript-eslint"],
   rules: {
+    "@next/next/no-html-link-for-pages": "off",
+    "react/jsx-key": "off",
+    "prefer-const": "error",
+    "no-irregular-whitespace": "error",
+    "no-trailing-spaces": "error",
+    "no-duplicate-imports": "error",
+    "arrow-body-style": ["error", "as-needed"],
+    "react/self-closing-comp": ["error", { component: true, html: true }],
     "@next/next/no-img-element": "off",
   },
 };
