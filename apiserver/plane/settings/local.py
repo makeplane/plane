51821008baf9cfe7a3056cc792d5b4cb4f65f019
kai nagae <muhammadfaraz9736@gmail.com>
2023-08-31 21:47:05 +0500

    added whitenoise for static files

--- apiserver/plane/settings/local.py
+++ apiserver/plane/settings/local.py
@@ -40,7 +40,7 @@ CACHES = {
     }
 }
 
-INSTALLED_APPS += ("debug_toolbar",)
+INSTALLED_APPS += ("debug_toolbar","django_extensions")
 
 MIDDLEWARE += ("debug_toolbar.middleware.DebugToolbarMiddleware",)
 
