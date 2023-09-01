0080e651687d6feb12c7c0ad345f45a567d05c82
Aaryan Khandelwal <65252264+aaryan610@users.noreply.github.com>
2023-09-01 16:23:41 +0530

    chore: remove theme store (#2051)

--- apps/space/store/root.ts
+++ apps/space/store/root.ts
@@ -2,25 +2,20 @@
 import { enableStaticRendering } from "mobx-react-lite";
 // store imports
 import UserStore from "./user";
-import ThemeStore from "./theme";
 import IssueStore, { IIssueStore } from "./issue";
 import ProjectStore, { IProjectStore } from "./project";
 import IssueDetailStore, { IIssueDetailStore } from "./issue_details";
-// types
-import { IThemeStore } from "types/theme";
 
 enableStaticRendering(typeof window === "undefined");
 
 export class RootStore {
   user: UserStore;
-  theme: IThemeStore;
   issue: IIssueStore;
   issueDetails: IIssueDetailStore;
   project: IProjectStore;
 
   constructor() {
     this.user = new UserStore(this);
-    this.theme = new ThemeStore(this);
     this.issue = new IssueStore(this);
     this.project = new ProjectStore(this);
     this.issueDetails = new IssueDetailStore(this);
