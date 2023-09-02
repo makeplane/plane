33534e6fc9868ba032b43ab378dbe72d65ce2ce4
Henit Chobisa <chobisa.henit@gmail.com>
2023-09-02 13:35:29 +0000

    fix: resolved build errors with web

--- web/components/ui/dropdowns/index.ts
+++ web/components/ui/dropdowns/index.ts
@@ -2,4 +2,4 @@ export * from "./context-menu";
 export * from "./custom-menu";
 export * from "./custom-search-select";
 export * from "./custom-select";
-export * from "./types";
+export * from "./types.d"
