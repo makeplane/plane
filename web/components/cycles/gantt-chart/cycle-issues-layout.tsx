f583789584d029825e55ddc9c760c688983a6fa4
Aaryan Khandelwal <65252264+aaryan610@users.noreply.github.com>
2023-09-04 13:08:49 +0530

    chore: add authorization to the gantt chart (#2074)

--- web/components/cycles/gantt-chart/cycle-issues-layout.tsx
+++ web/components/cycles/gantt-chart/cycle-issues-layout.tsx
@@ -5,6 +5,7 @@ import useIssuesView from "hooks/use-issues-view";
 import useUser from "hooks/use-user";
 import useGanttChartCycleIssues from "hooks/gantt-chart/cycle-issues-view";
 import { updateGanttIssue } from "components/gantt-chart/hooks/block-update";
+import useProjectDetails from "hooks/use-project-details";
 // components
 import { GanttChartRoot, renderIssueBlocksStructure } from "components/gantt-chart";
 import { IssueGanttBlock, IssueGanttSidebarBlock } from "components/issues";
@@ -18,12 +19,15 @@ export const CycleIssuesGanttChartView = () => {
   const { orderBy } = useIssuesView();
 
   const { user } = useUser();
+  const { projectDetails } = useProjectDetails();
 
   const { ganttIssues, mutateGanttIssues } = useGanttChartCycleIssues(
     workspaceSlug as string,
     projectId as string,
     cycleId as string
   );
+
+  const isAllowed = projectDetails?.member_role === 20 || projectDetails?.member_role === 15;
 
   return (
     <div className="w-full h-full">
@@ -37,7 +41,10 @@ export const CycleIssuesGanttChartView = () => {
         }
         SidebarBlockRender={IssueGanttSidebarBlock}
         BlockRender={IssueGanttBlock}
-        enableReorder={orderBy === "sort_order"}
+        enableBlockLeftResize={isAllowed}
+        enableBlockRightResize={isAllowed}
+        enableBlockMove={isAllowed}
+        enableReorder={orderBy === "sort_order" && isAllowed}
         bottomSpacing
       />
     </div>
