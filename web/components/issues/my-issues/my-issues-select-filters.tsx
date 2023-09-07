945458fcaac6f2f95347c2a49fa1759774043c57
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2023-09-07 18:43:48 +0530

    fix: custom date filter not working on my issues and profile issues

--- web/components/issues/my-issues/my-issues-select-filters.tsx
+++ web/components/issues/my-issues/my-issues-select-filters.tsx
@@ -61,8 +61,10 @@ export const MyIssuesSelectFilters: React.FC<Props> = ({
         <DateFilterModal
           title={dateFilterType.title}
           field={dateFilterType.type}
-          isOpen={isDateFilterModalOpen}
+          filters={filters as IIssueFilterOptions}
           handleClose={() => setIsDateFilterModalOpen(false)}
+          isOpen={isDateFilterModalOpen}
+          onSelect={onSelect}
         />
       )}
       <MultiLevelDropdown
