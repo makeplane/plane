8d741eb725895d3bd7bd62faa8099674124d0342
sriram veeraghanta <veeraghanta.sriram@gmail.com>
2023-09-12 15:17:52 +0530

    API BASE URL global update

--- web/services/integration/jira.service.ts
+++ web/services/integration/jira.service.ts
@@ -11,7 +11,7 @@ const trackEvent =
 
 class JiraImportedService extends APIService {
   constructor() {
-    super(NEXT_PUBLIC_API_BASE_URL !== undefined ? NEXT_PUBLIC_API_BASE_URL : "http://localhost:8000");
+    super(API_BASE_URL);
   }
 
   async getJiraProjectInfo(workspaceSlug: string, params: IJiraMetadata): Promise<IJiraResponse> {
