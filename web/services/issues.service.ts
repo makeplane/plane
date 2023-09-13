9bac7cb03627a04d47ea2fbbc15bbfce9ae2b69f
Dakshesh Jain <65905942+dakshesh14@users.noreply.github.com>
2023-09-13 19:41:11 +0530

    feat: issue link to create relation between issues (#2171)
    * feat: issue linking
    
    * fix: search params to filter out selected issue
    
    * style: changed icons
    
    * fix: build error on web-view
    
    * fix: build error
    
    * fix: build error on web-view component

--- web/services/issues.service.ts
+++ web/services/issues.service.ts
@@ -149,6 +149,52 @@ class ProjectIssuesServices extends APIService {
       });
   }
 
+  async createIssueRelation(
+    workspaceSlug: string,
+    projectId: string,
+    issueId: string,
+    user: ICurrentUserResponse,
+    data: {
+      related_list: Array<{
+        relation_type: "duplicate" | "relates_to" | "blocked_by";
+        related_issue: string;
+      }>;
+    }
+  ) {
+    return this.post(
+      `/api/workspaces/${workspaceSlug}/projects/${projectId}/issues/${issueId}/issue-relation/`,
+      data
+    )
+      .then((response) => {
+        if (trackEvent)
+          trackEventServices.trackIssueRelationEvent(response.data, "ISSUE_RELATION_CREATE", user);
+        return response?.data;
+      })
+      .catch((error) => {
+        throw error?.response;
+      });
+  }
+
+  async deleteIssueRelation(
+    workspaceSlug: string,
+    projectId: string,
+    issueId: string,
+    relationId: string,
+    user: ICurrentUserResponse
+  ) {
+    return this.delete(
+      `/api/workspaces/${workspaceSlug}/projects/${projectId}/issues/${issueId}/issue-relation/${relationId}/`
+    )
+      .then((response) => {
+        if (trackEvent)
+          trackEventServices.trackIssueRelationEvent(response.data, "ISSUE_RELATION_DELETE", user);
+        return response?.data;
+      })
+      .catch((error) => {
+        throw error?.response;
+      });
+  }
+
   async createIssueProperties(workspaceSlug: string, projectId: string, data: any): Promise<any> {
     return this.post(
       `/api/workspaces/${workspaceSlug}/projects/${projectId}/issue-properties/`,
