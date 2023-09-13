b9c935092a225c31084be0abbb261ef75419c8c0
sriram veeraghanta <veeraghanta.sriram@gmail.com>
2023-09-13 12:06:17 +0530

    chore: eslint config package fixes (#2165)
    * eslint fixes
    
    * lint rules added

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -16,5 +16,7 @@ module.exports = {
     "no-duplicate-imports": "error",
     "arrow-body-style": ["error", "as-needed"],
     "react/self-closing-comp": ["error", { component: true, html: true }],
+    "@next/next/no-img-element": "off",
+    "@typescript-eslint/no-unused-vars": ["warn"],
   },
 };
