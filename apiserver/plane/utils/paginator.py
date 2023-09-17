5b7313b7d7536944ee133340db99511f1ec9148e
pablohashescobar <nikhilschacko@gmail.com>
2023-09-18 00:53:21 +0530

    dev: remove assert from paginator

--- apiserver/plane/utils/paginator.py
+++ apiserver/plane/utils/paginator.py
@@ -171,10 +171,6 @@ class BasePaginator:
         **paginator_kwargs,
     ):
         """Paginate the request"""
-        assert (paginator and not paginator_kwargs) or (
-            paginator_cls and paginator_kwargs
-        )
-
         per_page = self.get_per_page(request, default_per_page, max_per_page)
 
         # Convert the cursor value to integer and float from string
