b8ab55b3ade67c96c5b66e14c93bf2b0c160ed57
pablohashescobar <nikhilschacko@gmail.com>
2023-09-19 00:26:13 +0530

    dev: captures exception

--- apiserver/plane/api/views/property.py
+++ apiserver/plane/api/views/property.py
@@ -665,7 +665,7 @@ class PropertyValueViewSet(BaseViewSet):
                 {"error": "Project does not exists"}, status=status.HTTP_400_BAD_REQUEST
             )
         except Exception as e:
-            print(e)
+            capture_exception(e)
             return Response(
                 {"error": "Something went wrong please try again later"},
                 status=status.HTTP_400_BAD_REQUEST,
