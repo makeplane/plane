79bf7d4c0c42e945fa5beb6f9a4b126ae4a4ecdc
Dakshesh Jain <65905942+dakshesh14@users.noreply.github.com>
2023-09-19 12:56:32 +0530

    fix: hydration error and draft issue workflow (#2199)
    * fix: hydration error and draft issue workflow
    
    * fix: build error

--- web/components/issues/form.tsx
+++ web/components/issues/form.tsx
@@ -8,7 +8,6 @@ import { Controller, useForm } from "react-hook-form";
 import aiService from "services/ai.service";
 // hooks
 import useToast from "hooks/use-toast";
-import useLocalStorage from "hooks/use-local-storage";
 // components
 import { GptAssistantModal } from "components/core";
 import { ParentIssuesListModal } from "components/issues";
@@ -62,11 +61,9 @@ export interface IssueFormProps {
   setActiveProject: React.Dispatch<React.SetStateAction<string | null>>;
   createMore: boolean;
   setCreateMore: React.Dispatch<React.SetStateAction<boolean>>;
-  handleClose: () => void;
   handleDiscardClose: () => void;
   status: boolean;
   user: ICurrentUserResponse | undefined;
-  setIsConfirmDiscardOpen: React.Dispatch<React.SetStateAction<boolean>>;
   handleFormDirty: (payload: Partial<IIssue> | null) => void;
   fieldsToShow: (
     | "project"
@@ -107,8 +104,6 @@ export const IssueForm: FC<IssueFormProps> = (props) => {
   const [gptAssistantModal, setGptAssistantModal] = useState(false);
   const [iAmFeelingLucky, setIAmFeelingLucky] = useState(false);
 
-  const { setValue: setValueInLocalStorage } = useLocalStorage<any>("draftedIssue", null);
-
   const editorRef = useRef<any>(null);
 
   const router = useRouter();
@@ -139,9 +134,11 @@ export const IssueForm: FC<IssueFormProps> = (props) => {
     state: getValues("state"),
     priority: getValues("priority"),
     assignees: getValues("assignees"),
-    target_date: getValues("target_date"),
     labels: getValues("labels"),
+    start_date: getValues("start_date"),
+    target_date: getValues("target_date"),
     project: getValues("project"),
+    parent: getValues("parent"),
   };
 
   useEffect(() => {
@@ -571,8 +568,6 @@ export const IssueForm: FC<IssueFormProps> = (props) => {
           <div className="flex items-center gap-2">
             <SecondaryButton
               onClick={() => {
-                const data = JSON.stringify(getValues());
-                setValueInLocalStorage(data);
                 handleDiscardClose();
               }}
             >
