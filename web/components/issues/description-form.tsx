8e71ce0c50d2ceb7175b4645168c84583eb7e1a7
Palanikannan1437 <73993394+Palanikannan1437@users.noreply.github.com>
2023-09-20 19:03:06 +0530

    finally fixed import errors

--- web/components/issues/description-form.tsx
+++ web/components/issues/description-form.tsx
@@ -9,7 +9,7 @@ import { useDebouncedCallback } from "use-debounce";
 import { TextArea } from "components/ui";
 // types
 import { IIssue } from "types";
-import { TiptapEditor } from "@plane/editor"
+import { TiptapEditor } from "@plane/editor";
 import fileService from "services/file.service";
 
 export interface IssueDescriptionFormValues {
