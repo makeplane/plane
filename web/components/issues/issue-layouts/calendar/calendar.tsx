c932c9f87fb0c3482c658dfcf66a9bfa4ae85d63
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2023-09-26 14:20:29 +0530

    chore: new render logic

--- web/components/issues/issue-layouts/calendar/calendar.tsx
+++ web/components/issues/issue-layouts/calendar/calendar.tsx
@@ -12,22 +12,38 @@ export const CalendarView: React.FC<Props> = (props) => {
   console.log("calendar payload", CALENDAR_PAYLOAD);
 
   return (
-    <div className="h-full w-full overflow-y-auto">
-      {Object.keys(CALENDAR_PAYLOAD).map((year) => (
-        <div key={year}>
-          {Object.keys(CALENDAR_PAYLOAD[year]).map((month) => (
-            <div key={`${year}-${month}`}>
-              {Object.keys(CALENDAR_PAYLOAD[year][month]).map((week) => (
-                <div key={`${year}-${month}-${week}`}>
-                  {CALENDAR_PAYLOAD[year][month][week].map((day) => (
-                    <div key={day}>{day ? renderLongDateFormat(day) : "No date"}</div>
+    <div className="calendar">
+      {Object.entries(CALENDAR_PAYLOAD).map(([year, months]) =>
+        Object.entries(months).map(([month, weeks]) => (
+          <div key={`${year}-${month}`} className="month">
+            <h2>{`${year}-${Number(month) + 1}`}</h2>
+            <div className="weekdays">
+              <div className="day">Sun</div>
+              <div className="day">Mon</div>
+              <div className="day">Tue</div>
+              <div className="day">Wed</div>
+              <div className="day">Thu</div>
+              <div className="day">Fri</div>
+              <div className="day">Sat</div>
+            </div>
+            <div className="weeks">
+              {Object.values(weeks).map((week: Date[], weekIndex) => (
+                <div key={weekIndex} className="week">
+                  {week.map((date, dayIndex) => (
+                    <div key={dayIndex} className="day">
+                      {date && (
+                        <div className="date">
+                          {date.getDate()} {/* Assuming you want to display the day of the month */}
+                        </div>
+                      )}
+                    </div>
                   ))}
                 </div>
               ))}
             </div>
-          ))}
-        </div>
-      ))}
+          </div>
+        ))
+      )}
     </div>
   );
 };
