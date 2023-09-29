405a398c6b0eac5141740860c29cbd63a0dfc232
sriram veeraghanta <veeraghanta.sriram@gmail.com>
2023-09-29 17:33:17 +0530

    feat: adding additional ui components

--- packages/ui/dist/index.js
+++ packages/ui/dist/index.js
@@ -27,7 +27,9 @@ var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: tru
 var src_exports = {};
 __export(src_exports, {
   Button: () => Button,
-  Input: () => Input
+  Input: () => Input,
+  RadialProgressBar: () => RadialProgressBar,
+  TextArea: () => TextArea
 });
 module.exports = __toCommonJS(src_exports);
 
@@ -66,8 +68,90 @@ var Input = React2.forwardRef((props, ref) => {
   });
 });
 Input.displayName = "form-input-field";
+
+// src/form-fields/textarea.tsx
+var React3 = __toESM(require("react"));
+var useAutoSizeTextArea = (textAreaRef, value) => {
+  React3.useEffect(() => {
+    if (textAreaRef) {
+      textAreaRef.style.height = "0px";
+      const scrollHeight = textAreaRef.scrollHeight;
+      textAreaRef.style.height = scrollHeight + "px";
+    }
+  }, [textAreaRef, value]);
+};
+var TextArea = React3.forwardRef(
+  (props, ref) => {
+    const {
+      id,
+      name,
+      placeholder = "",
+      value = "",
+      rows = 1,
+      cols = 1,
+      disabled,
+      onChange,
+      mode = "primary",
+      hasError = false,
+      className = ""
+    } = props;
+    const textAreaRef = React3.useRef(ref);
+    ref && useAutoSizeTextArea(textAreaRef == null ? void 0 : textAreaRef.current, value);
+    return /* @__PURE__ */ React3.createElement("textarea", {
+      id,
+      name,
+      ref: textAreaRef,
+      placeholder,
+      value,
+      rows,
+      cols,
+      disabled,
+      onChange,
+      className: `no-scrollbar w-full bg-transparent placeholder-custom-text-400 px-3 py-2 outline-none ${mode === "primary" ? "rounded-md border border-custom-border-200" : mode === "transparent" ? "rounded border-none bg-transparent ring-0 transition-all focus:ring-1 focus:ring-theme" : ""} ${hasError ? "border-red-500" : ""} ${hasError && mode === "primary" ? "bg-red-100" : ""} ${className}`
+    });
+  }
+);
+
+// src/progress/radial-progress.tsx
+var import_react = __toESM(require("react"));
+var RadialProgressBar = (props) => {
+  const { progress } = props;
+  const [circumference, setCircumference] = (0, import_react.useState)(0);
+  (0, import_react.useEffect)(() => {
+    const radius = 40;
+    const circumference2 = 2 * Math.PI * radius;
+    setCircumference(circumference2);
+  }, []);
+  const progressOffset = (100 - progress) / 100 * circumference;
+  return /* @__PURE__ */ import_react.default.createElement("div", {
+    className: "relative h-4 w-4"
+  }, /* @__PURE__ */ import_react.default.createElement("svg", {
+    className: "absolute top-0 left-0",
+    viewBox: "0 0 100 100"
+  }, /* @__PURE__ */ import_react.default.createElement("circle", {
+    className: "stroke-current opacity-10",
+    cx: "50",
+    cy: "50",
+    r: "40",
+    strokeWidth: "12",
+    fill: "none",
+    strokeDasharray: `${circumference} ${circumference}`
+  }), /* @__PURE__ */ import_react.default.createElement("circle", {
+    className: `stroke-current`,
+    cx: "50",
+    cy: "50",
+    r: "40",
+    strokeWidth: "12",
+    fill: "none",
+    strokeDasharray: `${circumference} ${circumference}`,
+    strokeDashoffset: progressOffset,
+    transform: "rotate(-90 50 50)"
+  })));
+};
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
   Button,
-  Input
+  Input,
+  RadialProgressBar,
+  TextArea
 });
--- packages/ui/dist/index.mjs
+++ packages/ui/dist/index.mjs
@@ -33,7 +33,89 @@ var Input = React2.forwardRef((props, ref) => {
   });
 });
 Input.displayName = "form-input-field";
+
+// src/form-fields/textarea.tsx
+import * as React3 from "react";
+var useAutoSizeTextArea = (textAreaRef, value) => {
+  React3.useEffect(() => {
+    if (textAreaRef) {
+      textAreaRef.style.height = "0px";
+      const scrollHeight = textAreaRef.scrollHeight;
+      textAreaRef.style.height = scrollHeight + "px";
+    }
+  }, [textAreaRef, value]);
+};
+var TextArea = React3.forwardRef(
+  (props, ref) => {
+    const {
+      id,
+      name,
+      placeholder = "",
+      value = "",
+      rows = 1,
+      cols = 1,
+      disabled,
+      onChange,
+      mode = "primary",
+      hasError = false,
+      className = ""
+    } = props;
+    const textAreaRef = React3.useRef(ref);
+    ref && useAutoSizeTextArea(textAreaRef == null ? void 0 : textAreaRef.current, value);
+    return /* @__PURE__ */ React3.createElement("textarea", {
+      id,
+      name,
+      ref: textAreaRef,
+      placeholder,
+      value,
+      rows,
+      cols,
+      disabled,
+      onChange,
+      className: `no-scrollbar w-full bg-transparent placeholder-custom-text-400 px-3 py-2 outline-none ${mode === "primary" ? "rounded-md border border-custom-border-200" : mode === "transparent" ? "rounded border-none bg-transparent ring-0 transition-all focus:ring-1 focus:ring-theme" : ""} ${hasError ? "border-red-500" : ""} ${hasError && mode === "primary" ? "bg-red-100" : ""} ${className}`
+    });
+  }
+);
+
+// src/progress/radial-progress.tsx
+import React4, { useState, useEffect as useEffect2 } from "react";
+var RadialProgressBar = (props) => {
+  const { progress } = props;
+  const [circumference, setCircumference] = useState(0);
+  useEffect2(() => {
+    const radius = 40;
+    const circumference2 = 2 * Math.PI * radius;
+    setCircumference(circumference2);
+  }, []);
+  const progressOffset = (100 - progress) / 100 * circumference;
+  return /* @__PURE__ */ React4.createElement("div", {
+    className: "relative h-4 w-4"
+  }, /* @__PURE__ */ React4.createElement("svg", {
+    className: "absolute top-0 left-0",
+    viewBox: "0 0 100 100"
+  }, /* @__PURE__ */ React4.createElement("circle", {
+    className: "stroke-current opacity-10",
+    cx: "50",
+    cy: "50",
+    r: "40",
+    strokeWidth: "12",
+    fill: "none",
+    strokeDasharray: `${circumference} ${circumference}`
+  }), /* @__PURE__ */ React4.createElement("circle", {
+    className: `stroke-current`,
+    cx: "50",
+    cy: "50",
+    r: "40",
+    strokeWidth: "12",
+    fill: "none",
+    strokeDasharray: `${circumference} ${circumference}`,
+    strokeDashoffset: progressOffset,
+    transform: "rotate(-90 50 50)"
+  })));
+};
 export {
   Button,
-  Input
+  Input,
+  RadialProgressBar,
+  TextArea
 };
