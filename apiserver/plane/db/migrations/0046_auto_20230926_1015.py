9fcf4126bbc4234c644b84863f6be49e29dfc746
NarayanBavisetti <narayan3119@gmail.com>
2023-09-29 15:18:05 +0530

    chore: epoch migration batch size changed

--- apiserver/plane/db/migrations/0046_auto_20230926_1015.py
+++ apiserver/plane/db/migrations/0046_auto_20230926_1015.py
@@ -12,7 +12,7 @@ def update_issue_activity(apps, schema_editor):
     IssueActivity.objects.bulk_update(
         updated_issue_activity,
         ["epoch"],
-        batch_size=5000,
+        batch_size=10000,
     )
 
 class Migration(migrations.Migration):
