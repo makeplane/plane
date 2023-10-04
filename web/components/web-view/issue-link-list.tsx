2507e021ddeeb295f0d6162e23cffa4d70bb3da9
dakshesh14 <dakshesh.jain14@gmail.com>
2023-10-04 17:31:00 +0530

    fix: logging link instead of redirecting to the link

--- web/components/web-view/issue-link-list.tsx
+++ web/components/web-view/issue-link-list.tsx
@@ -105,14 +105,18 @@ export const IssueLinks: React.FC<Props> = (props) => {
             key={link.id}
             className="px-3 border border-custom-border-200 rounded-[4px] py-2 flex justify-between items-center bg-custom-background-100"
           >
-            <Link href={link.url}>
-              <a target="_blank" className="text-custom-text-200 truncate">
-                <span>
-                  <LinkIcon className="w-4 h-4 inline-block mr-1" />
-                </span>
-                <span>{link.title || link.metadata?.title || link.url}</span>
-              </a>
-            </Link>
+            <button
+              type="button"
+              onClick={() => {
+                console.log("link", link.url);
+              }}
+              className="text-custom-text-200 truncate"
+            >
+              <span>
+                <LinkIcon className="w-4 h-4 inline-block mr-1" />
+              </span>
+              <span>{link.title || link.metadata?.title || link.url}</span>
+            </button>
             {allowed && (
               <div className="flex gap-2 items-center">
                 <button
