70dec869b934ff0bfc878e1c8929b90ec9c34bdb
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2023-10-18 16:23:32 +0530

    fix: svg icon properties naming convention fix

--- packages/ui/src/icons/archive-icon.tsx
+++ packages/ui/src/icons/archive-icon.tsx
@@ -16,24 +16,20 @@ export const ArchiveIcon: React.FC<ISvgIcons> = ({
   >
     <path
       d="M21 3H3C2.44772 3 2 3.44772 2 4V7C2 7.55228 2.44772 8 3 8H21C21.5523 8 22 7.55228 22 7V4C22 3.44772 21.5523 3 21 3Z"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M4 8V19C4 19.5304 4.21071 20.0391 4.58579 20.4142C4.96086 20.7893 5.46957 21 6 21H8"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M20 8V19C20 19.5304 19.7893 20.0391 19.4142 20.4142C19.0391 20.7893 18.5304 21 18 21H16"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
-    <path
-      d="M15 18L12 21L9 18"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
-    <path d="M12 21L12 12" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M15 18L12 21L9 18" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M12 21L12 12" strokeLinecap="round" strokeLinejoin="round" />
   </svg>
 );
--- packages/ui/src/icons/create-icon.tsx
+++ packages/ui/src/icons/create-icon.tsx
@@ -16,10 +16,10 @@ export const CreateIcon: React.FC<ISvgIcons> = ({
   >
     <path
       d="M21 12V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V5C3 4.46957 3.21071 3.96086 3.58579 3.58579C3.96086 3.21071 4.46957 3 5 3H12"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
-    <path d="M16 5H22" stroke-linecap="round" stroke-linejoin="round" />
-    <path d="M19 2V8" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M16 5H22" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M19 2V8" strokeLinecap="round" strokeLinejoin="round" />
   </svg>
 );
