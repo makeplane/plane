9482ee5d7f607946f09501c60a2cd427cc9fd48a
Manish Gupta <manish@mgupta.me>
2023-10-18 15:48:56 +0530

    checking selfhost

--- selfhost.sh
+++ selfhost.sh
@@ -2,7 +2,21 @@
 
 BRANCH=${BRANCH:-master}
 
-mkdir -p ./plane-app
+mkdir -p ./plane-app/archive
 cd plane-app
-curl -o docker-compose.yaml  https://raw.githubusercontent.com/makeplane/plane/dev/mg-self-host/docker-compose-hub.yml
-curl -o .env https://raw.githubusercontent.com/makeplane/plane/dev/mg-self-host/selfhost.env
+
+TS=$(date +%s)
+if [ -e docker-compose.yaml ]
+then
+    mv docker-compose.yaml ./archive/$TS.docker-compose.yaml
+fi
+if [ -e .env ]
+then
+    cp .env previous.env
+    mv .env ./archive/$TS.env
+fi
+
+curl -s -o docker-compose.yaml  https://raw.githubusercontent.com/makeplane/plane/dev/mg-self-host/docker-compose-hub.yml
+curl -s -o .env https://raw.githubusercontent.com/makeplane/plane/dev/mg-self-host/selfhost.env
+
+# docker compose up -d
