082e48c9cf39086f6308dee9f5665d476cd412ae
guru_sainath <gurusainath007@gmail.com>
2023-10-19 16:23:30 +0530

    chore: Issue poprties options in kanban are passing through props (#2490)

--- web/components/issues/issue-layouts/kanban/headers/assignee.tsx
+++ web/components/issues/issue-layouts/kanban/headers/assignee.tsx
@@ -1,15 +1,13 @@
-// mobx
+import { FC } from "react";
 import { observer } from "mobx-react-lite";
 // components
 import { HeaderGroupByCard } from "./group-by-card";
 import { HeaderSubGroupByCard } from "./sub-group-by-card";
 import { Avatar } from "components/ui";
-// store
-import { useMobxStore } from "lib/mobx/store-provider";
-import { RootStore } from "store/root";
 
 export interface IAssigneesHeader {
   column_id: string;
+  column_value: any;
   sub_group_by: string | null;
   group_by: string | null;
   header_type: "group_by" | "sub_group_by";
@@ -20,37 +18,44 @@ export interface IAssigneesHeader {
 
 export const Icon = ({ user }: any) => <Avatar user={user} height="22px" width="22px" fontSize="12px" />;
 
-export const AssigneesHeader: React.FC<IAssigneesHeader> = observer(
-  ({ column_id, sub_group_by, group_by, header_type, issues_count, kanBanToggle, handleKanBanToggle }) => {
-    const { project: projectStore }: RootStore = useMobxStore();
+export const AssigneesHeader: FC<IAssigneesHeader> = observer((props) => {
+  const {
+    column_id,
+    column_value,
+    sub_group_by,
+    group_by,
+    header_type,
+    issues_count,
+    kanBanToggle,
+    handleKanBanToggle,
+  } = props;
 
-    const assignee = (column_id && projectStore?.getProjectMemberByUserId(column_id)) ?? null;
+  const assignee = column_value ?? null;
 
-    return (
-      <>
-        {assignee &&
-          (sub_group_by && header_type === "sub_group_by" ? (
-            <HeaderSubGroupByCard
-              column_id={column_id}
-              icon={<Icon user={assignee?.member} />}
-              title={assignee?.member?.display_name || ""}
-              count={issues_count}
-              kanBanToggle={kanBanToggle}
-              handleKanBanToggle={handleKanBanToggle}
-            />
-          ) : (
-            <HeaderGroupByCard
-              sub_group_by={sub_group_by}
-              group_by={group_by}
-              column_id={column_id}
-              icon={<Icon user={assignee?.member} />}
-              title={assignee?.member?.display_name || ""}
-              count={issues_count}
-              kanBanToggle={kanBanToggle}
-              handleKanBanToggle={handleKanBanToggle}
-            />
-          ))}
-      </>
-    );
-  }
-);
+  return (
+    <>
+      {assignee &&
+        (sub_group_by && header_type === "sub_group_by" ? (
+          <HeaderSubGroupByCard
+            column_id={column_id}
+            icon={<Icon user={assignee?.member} />}
+            title={assignee?.member?.display_name || ""}
+            count={issues_count}
+            kanBanToggle={kanBanToggle}
+            handleKanBanToggle={handleKanBanToggle}
+          />
+        ) : (
+          <HeaderGroupByCard
+            sub_group_by={sub_group_by}
+            group_by={group_by}
+            column_id={column_id}
+            icon={<Icon user={assignee?.member} />}
+            title={assignee?.member?.display_name || ""}
+            count={issues_count}
+            kanBanToggle={kanBanToggle}
+            handleKanBanToggle={handleKanBanToggle}
+          />
+        ))}
+    </>
+  );
+});
