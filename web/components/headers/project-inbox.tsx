51600144f2f75bc9d80ae5b2d42693ca5ad6bc28
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2023-10-23 14:52:20 +0530

    chore: inbox layout refactor

--- web/components/headers/project-inbox.tsx
+++ web/components/headers/project-inbox.tsx
@@ -1,21 +1,63 @@
-import { useState } from "react";
-
+import { FC, useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { observer } from "mobx-react-lite";
+import { ArrowLeft, Plus } from "lucide-react";
+// hooks
+import { useMobxStore } from "lib/mobx/store-provider";
+// ui
+import { Breadcrumbs, BreadcrumbItem, Button } from "@plane/ui";
 // components
 import { CreateInboxIssueModal } from "components/inbox";
-// ui
-import { Button } from "@plane/ui";
-// icons
-import { Plus } from "lucide-react";
+// helper
+import { truncateText } from "helpers/string.helper";
 
-export const ProjectInboxHeader = () => {
+export const ProjectInboxHeader: FC = observer(() => {
+  const router = useRouter();
+  const { workspaceSlug, projectId } = router.query;
   const [createIssueModal, setCreateIssueModal] = useState(false);
 
+  const { project: projectStore } = useMobxStore();
+
+  const projectDetails =
+    workspaceSlug && projectId
+      ? projectStore.getProjectById(workspaceSlug.toString(), projectId.toString())
+      : undefined;
+
   return (
-    <>
-      <CreateInboxIssueModal isOpen={createIssueModal} onClose={() => setCreateIssueModal(false)} />
-      <Button onClick={() => setCreateIssueModal(true)} size="sm" prependIcon={<Plus />}>
-        Add Issue
-      </Button>
-    </>
+    <div className="relative flex w-full flex-shrink-0 flex-row z-10 items-center justify-between gap-x-2 gap-y-4 border-b border-custom-border-200 bg-custom-sidebar-background-100 p-4">
+      <div className="flex items-center gap-2 flex-grow w-full whitespace-nowrap overflow-ellipsis">
+        <div className="block md:hidden">
+          <button
+            type="button"
+            className="grid h-8 w-8 place-items-center rounded border border-custom-border-200"
+            onClick={() => router.back()}
+          >
+            <ArrowLeft fontSize={14} strokeWidth={2} />
+          </button>
+        </div>
+        <div>
+          <Breadcrumbs onBack={() => router.back()}>
+            <BreadcrumbItem
+              link={
+                <Link href={`/${workspaceSlug}/projects`}>
+                  <a className={`border-r-2 border-custom-sidebar-border-200 px-3 text-sm `}>
+                    <p>Projects</p>
+                  </a>
+                </Link>
+              }
+            />
+            <BreadcrumbItem title={`${truncateText(projectDetails?.name ?? "Project", 32)} Inbox`} />
+          </Breadcrumbs>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-2">
+        <CreateInboxIssueModal isOpen={createIssueModal} onClose={() => setCreateIssueModal(false)} />
+        <Button variant="primary" prependIcon={<Plus />} size="sm" onClick={() => setCreateIssueModal(true)}>
+          Add Issue
+        </Button>
+      </div>
+    </div>
   );
-};
+});
