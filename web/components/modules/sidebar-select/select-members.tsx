fc82d6fc230da6c73b21316bfb4e91da1c41f768
Anmol Singh Bhatia <121005188+anmolsinghbhatia@users.noreply.github.com>
2023-10-27 18:45:10 +0530

    style: module ui revamp (#2548)
    * chore: module constant and helper function added
    
    * style: module card ui revamp
    
    * chore: custom media query added
    
    * chore: circular progress indicator added
    
    * chore: module card item ui improvement
    
    * chore: module list view added
    
    * chore: module sidebar added in list and card view
    
    * chore: module list and card ui improvement
    
    * chore: module sidebar select, avatar and link list component improvement
    
    * chore: sidebar improvement and refactor
    
    * style: module sidebar revamp
    
    * style: module sidebar ui improvement
    
    * chore: module sidebar lead and member select improvement
    
    * style: module sidebar progress section empty state added
    
    * chore: module card issue count validation added
    
    * style: module card and list item ui improvement

--- web/components/modules/sidebar-select/select-members.tsx
+++ web/components/modules/sidebar-select/select-members.tsx
@@ -10,6 +10,7 @@ import { ProjectService } from "services/project";
 import { AssigneesList, Avatar } from "components/ui";
 import { CustomSearchSelect, UserGroupIcon } from "@plane/ui";
 // icons
+import { ChevronDown } from "lucide-react";
 // fetch-keys
 import { PROJECT_MEMBERS } from "constants/fetch-keys";
 
@@ -37,7 +38,7 @@ export const SidebarMembersSelect: React.FC<Props> = ({ value, onChange }) => {
     query: member.member.display_name,
     content: (
       <div className="flex items-center gap-2">
-        <Avatar user={member.member} />
+        <Avatar user={member.member} height="18px" width="18px" />
         {member.member.display_name}
       </div>
     ),
@@ -45,24 +46,26 @@ export const SidebarMembersSelect: React.FC<Props> = ({ value, onChange }) => {
 
   return (
     <div className="flex items-center justify-start gap-1">
-      <div className="flex w-40 items-center justify-start gap-2 text-custom-text-200">
-        <UserGroupIcon className="h-5 w-5" />
-        <span>Members</span>
+      <div className="flex w-1/2 items-center justify-start gap-2 text-custom-text-300">
+        <UserGroupIcon className="h-4 w-4" />
+        <span className="text-base">Members</span>
       </div>
-      <div className="sm:basis-1/2">
+      <div className="flex items-center w-1/2 rounded-sm ">
         <CustomSearchSelect
+          className="w-full rounded-sm"
           value={value ?? []}
-          label={
-            <div className="flex items-center gap-2 text-custom-text-200">
-              {value && value.length > 0 && Array.isArray(value) ? (
-                <div className="flex items-center justify-center gap-2">
-                  <AssigneesList userIds={value} length={3} showLength={false} />
-                  <span className="text-custom-text-200">{value.length} Assignees</span>
-                </div>
-              ) : (
-                "No members"
-              )}
-            </div>
+          customButtonClassName="rounded-sm"
+          customButton={
+            value && value.length > 0 && Array.isArray(value) ? (
+              <div className="flex items-center gap-2 p-0.5 w-full">
+                <AssigneesList userIds={value} length={2} />
+              </div>
+            ) : (
+              <div className="group flex items-center justify-between gap-2 p-1 text-sm text-custom-text-400 w-full">
+                <span>No members</span>
+                <ChevronDown className="h-3.5 w-3.5 hidden group-hover:flex" />
+              </div>
+            )
           }
           options={options}
           onChange={onChange}
