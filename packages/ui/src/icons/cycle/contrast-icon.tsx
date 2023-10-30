95166e8d6d990d9e058b9116b8dafe0956fd0bb1
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2023-10-30 14:22:02 +0530

    chore: cycle custom svg icon added and code refactor

--- packages/ui/src/icons/cycle/contrast-icon.tsx
+++ packages/ui/src/icons/cycle/contrast-icon.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { ISvgIcons } from "./type";
+import { ISvgIcons } from "../type";
 
 export const ContrastIcon: React.FC<ISvgIcons> = ({
   className = "text-current",
