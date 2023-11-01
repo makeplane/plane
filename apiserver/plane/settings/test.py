e086c0c2bfbcaeaaeffdb749fd756522282da468
pablohashescobar <nikhilschacko@gmail.com>
2023-11-01 17:15:22 +0530

    dev: update settings file structure and added extra settings for CORS

--- apiserver/plane/settings/test.py
+++ apiserver/plane/settings/test.py
@@ -1,45 +1,9 @@
-from __future__ import absolute_import
-
+"""Test Settings"""
 from .common import * # noqa
 
 DEBUG = True
 
-INSTALLED_APPS.append("plane.tests")
+# Send it in a dummy outbox
+EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
 
-if os.environ.get('GITHUB_WORKFLOW'):
-    DATABASES = {
-        'default': {
-           'ENGINE': 'django.db.backends.postgresql',
-           'NAME': 'github_actions',
-           'USER': 'postgres',
-           'PASSWORD': 'postgres',
-           'HOST': '127.0.0.1',
-           'PORT': '5432',
-        }
-    }
-else:
-    DATABASES = {
-        'default': {
-           'ENGINE': 'django.db.backends.postgresql',
-           'NAME': 'plane_test',
-           'USER': 'postgres',
-           'PASSWORD': 'password123',
-           'HOST': '127.0.0.1',
-           'PORT': '5432',
-        }
-    }
-
-REDIS_HOST = "localhost"
-REDIS_PORT = 6379
-REDIS_URL = False
-
-RQ_QUEUES = {
-    "default": {
-        "HOST": "localhost",
-        "PORT": 6379,
-        "DB": 0,
-        "DEFAULT_TIMEOUT": 360,
-    },
-}
-
-WEB_URL = "http://localhost:3000"
+INSTALLED_APPS.append("plane.tests",)
