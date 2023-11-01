cbdcddbb65722fb46dba30022a41f99147fe0913
sriram veeraghanta <veeraghanta.sriram@gmail.com>
2023-11-01 16:08:25 +0530

    fix: minor merge fixes

--- web/components/issues/issue-peek-overview/issue-detail.tsx
+++ web/components/issues/issue-peek-overview/issue-detail.tsx
@@ -8,11 +8,12 @@ import { IssueReaction } from "./reactions";
 // hooks
 import { useDebouncedCallback } from "use-debounce";
 import useReloadConfirmations from "hooks/use-reload-confirmation";
-// import useEditorSuggestions from "hooks/use-editor-suggestions";
+import useEditorSuggestions from "hooks/use-editor-suggestions";
 // types
 import { IIssue } from "types";
 // services
 import { FileService } from "services/file.service";
+import { useMobxStore } from "lib/mobx/store-provider";
 
 const fileService = new FileService();
 
@@ -28,12 +29,15 @@ interface IPeekOverviewIssueDetails {
 
 export const PeekOverviewIssueDetails: FC<IPeekOverviewIssueDetails> = (props) => {
   const { workspaceSlug, issue, issueReactions, user, issueUpdate, issueReactionCreate, issueReactionRemove } = props;
+  // store
+  const { user: userStore } = useMobxStore();
+  const isAllowed = [5, 10].includes(userStore.projectMemberInfo?.role || 0);
   // states
   const [isSubmitting, setIsSubmitting] = useState<"submitting" | "submitted" | "saved">("saved");
   const [characterLimit, setCharacterLimit] = useState(false);
   // hooks
   const { setShowAlert } = useReloadConfirmations();
-  // const editorSuggestions = useEditorSuggestions(workspaceSlug, issue.project_detail.id);
+  const editorSuggestions = useEditorSuggestions(workspaceSlug, issue.project_detail.id);
 
   const {
     handleSubmit,
@@ -96,7 +100,7 @@ export const PeekOverviewIssueDetails: FC<IPeekOverviewIssueDetails> = (props) =
       </span>
 
       <div className="relative">
-        {true ? (
+        {isAllowed ? (
           <Controller
             name="name"
             control={control}
@@ -113,8 +117,6 @@ export const PeekOverviewIssueDetails: FC<IPeekOverviewIssueDetails> = (props) =
                   debouncedTitleSave();
                   onChange(e.target.value);
                 }}
-                // mentionHighlights={editorSuggestions.mentionHighlights}
-                // mentionSuggestions={editorSuggestions.mentionSuggestions}
                 required={true}
                 className="min-h-10 block w-full resize-none overflow-hidden rounded border-none bg-transparent  text-xl outline-none ring-0 focus:ring-1 focus:ring-custom-primary !p-0 focus:!px-3 focus:!py-2"
                 hasError={Boolean(errors?.description)}
@@ -147,6 +149,8 @@ export const PeekOverviewIssueDetails: FC<IPeekOverviewIssueDetails> = (props) =
             debouncedIssueDescription(description_html);
           }}
           customClassName="mt-0"
+          mentionSuggestions={editorSuggestions.mentionSuggestions}
+          mentionHighlights={editorSuggestions.mentionHighlights}
         />
       </span>
 
