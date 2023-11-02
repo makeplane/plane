da391064aa0ef774094bd52351789d8b14365b86
Henit Chobisa <chobisa.henit@gmail.com>
2023-11-02 19:31:25 +0530

    [FIX] Minor bug fixes in `MentionList` and `MentionNode` UI (#2600)
    * fix: removed text color in peek view
    
    * fix: fixed list view UI bugs and node view colors
    
    * feat: update imports in suggestions for mentionSuggestion type
    
    * fix: updated mention list css
    
    * fix: updated mention node UI according to the design provided
    
    * style: update the mentions dropdown UI
    
    * style: mentioned users UI in the editor
    
    ---------
    
    Co-authored-by: Aaryan Khandelwal <aaryankhandu123@gmail.com>

--- packages/editor/core/src/ui/mentions/mentionNodeView.tsx
+++ packages/editor/core/src/ui/mentions/mentionNodeView.tsx
@@ -1,32 +1,41 @@
 /* eslint-disable react/display-name */
 // @ts-nocheck
-import { NodeViewWrapper } from '@tiptap/react'
-import { cn } from '../../lib/utils'
-import React from 'react'
-import { useRouter } from 'next/router'
-import { IMentionHighlight } from '../../types/mention-suggestion'
+import { NodeViewWrapper } from "@tiptap/react";
+import { cn } from "../../lib/utils";
+import { useRouter } from "next/router";
+import { IMentionHighlight } from "../../types/mention-suggestion";
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default props => {
-
-  const router = useRouter()
-  const highlights = props.extension.options.mentionHighlights as IMentionHighlight[]
+export default (props) => {
+  const router = useRouter();
+  const highlights = props.extension.options
+    .mentionHighlights as IMentionHighlight[];
 
   const handleClick = () => {
-    if (!props.extension.options.readonly){
-      router.push(props.node.attrs.redirect_uri)
+    if (!props.extension.options.readonly) {
+      router.push(props.node.attrs.redirect_uri);
     }
-  }
+  };
 
   return (
-    <NodeViewWrapper className="w-fit inline mention-component" >
-      <span className={cn("px-1 py-0.5 inline rounded-md font-bold bg-custom-primary-500 mention", {
-        "text-[#D9C942] bg-[#544D3B] hover:bg-[#544D3B]" : highlights ? highlights.includes(props.node.attrs.id) : false,
-        "cursor-pointer" : !props.extension.options.readonly,
-        "hover:bg-custom-primary-300" : !props.extension.options.readonly && !highlights.includes(props.node.attrs.id)
-      })} onClick={handleClick} data-mention-target={props.node.attrs.target} data-mention-id={props.node.attrs.id}>@{ props.node.attrs.label }</span>
+    <NodeViewWrapper className="w-fit inline mention-component">
+      <span
+        className={cn(
+          "px-1 py-0.5 bg-custom-primary-100/20 text-custom-primary-100 rounded font-medium mention",
+          {
+            "text-yellow-500 bg-yellow-500/20": highlights
+              ? highlights.includes(props.node.attrs.id)
+              : false,
+            "cursor-pointer": !props.extension.options.readonly,
+            // "hover:bg-custom-primary-300" : !props.extension.options.readonly && !highlights.includes(props.node.attrs.id)
+          },
+        )}
+        onClick={handleClick}
+        data-mention-target={props.node.attrs.target}
+        data-mention-id={props.node.attrs.id}
+      >
+        @{props.node.attrs.label}
+      </span>
     </NodeViewWrapper>
-  )
-}
-
-
+  );
+};
