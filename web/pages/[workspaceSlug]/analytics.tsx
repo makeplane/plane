5efc6993cd8f4aab7211bde2440de3ba8aada3fa
sriram veeraghanta <veeraghanta.sriram@gmail.com>
2023-11-03 00:09:13 +0530

    fix: Update analytics page layout fixes (#2623)

--- web/pages/[workspaceSlug]/analytics.tsx
+++ web/pages/[workspaceSlug]/analytics.tsx
@@ -58,57 +58,55 @@ const AnalyticsPage: NextPageWithLayout = observer(() => {
   }, [user, workspaceSlug]);
 
   return (
-    <AppLayout header={<WorkspaceAnalyticsHeader />}>
-      <>
-        {projects && projects.length > 0 ? (
-          <div className="h-full flex flex-col overflow-hidden bg-custom-background-100">
-            <Tab.Group as={Fragment}>
-              <Tab.List as="div" className="space-x-2 border-b border-custom-border-200 px-5 py-3">
-                {ANALYTICS_TABS.map((tab) => (
-                  <Tab
-                    key={tab.key}
-                    className={({ selected }) =>
-                      `rounded-3xl border border-custom-border-200 px-4 py-2 text-xs hover:bg-custom-background-80 ${
-                        selected ? "bg-custom-background-80" : ""
-                      }`
-                    }
-                    onClick={() => trackAnalyticsEvent(tab.key)}
-                  >
-                    {tab.title}
-                  </Tab>
-                ))}
-              </Tab.List>
-              <Tab.Panels as={Fragment}>
-                <Tab.Panel as={Fragment}>
-                  <ScopeAndDemand fullScreen />
-                </Tab.Panel>
-                <Tab.Panel as={Fragment}>
-                  <CustomAnalytics fullScreen />
-                </Tab.Panel>
-              </Tab.Panels>
-            </Tab.Group>
-          </div>
-        ) : (
-          <>
-            <EmptyState
-              title="You can see your all projects' analytics here"
-              description="Let's create your first project and analyze the stats with various graphs."
-              image={emptyAnalytics}
-              primaryButton={{
-                icon: <Plus className="h-4 w-4" />,
-                text: "New Project",
-                onClick: () => {
-                  const e = new KeyboardEvent("keydown", {
-                    key: "p",
-                  });
-                  document.dispatchEvent(e);
-                },
-              }}
-            />
-          </>
-        )}
-      </>
-    </AppLayout>
+    <>
+      {projects && projects.length > 0 ? (
+        <div className="h-full flex flex-col overflow-hidden bg-custom-background-100">
+          <Tab.Group as={Fragment}>
+            <Tab.List as="div" className="space-x-2 border-b border-custom-border-200 px-5 py-3">
+              {ANALYTICS_TABS.map((tab) => (
+                <Tab
+                  key={tab.key}
+                  className={({ selected }) =>
+                    `rounded-3xl border border-custom-border-200 px-4 py-2 text-xs hover:bg-custom-background-80 ${
+                      selected ? "bg-custom-background-80" : ""
+                    }`
+                  }
+                  onClick={() => trackAnalyticsEvent(tab.key)}
+                >
+                  {tab.title}
+                </Tab>
+              ))}
+            </Tab.List>
+            <Tab.Panels as={Fragment}>
+              <Tab.Panel as={Fragment}>
+                <ScopeAndDemand fullScreen />
+              </Tab.Panel>
+              <Tab.Panel as={Fragment}>
+                <CustomAnalytics fullScreen />
+              </Tab.Panel>
+            </Tab.Panels>
+          </Tab.Group>
+        </div>
+      ) : (
+        <>
+          <EmptyState
+            title="You can see your all projects' analytics here"
+            description="Let's create your first project and analyze the stats with various graphs."
+            image={emptyAnalytics}
+            primaryButton={{
+              icon: <Plus className="h-4 w-4" />,
+              text: "New Project",
+              onClick: () => {
+                const e = new KeyboardEvent("keydown", {
+                  key: "p",
+                });
+                document.dispatchEvent(e);
+              },
+            }}
+          />
+        </>
+      )}
+    </>
   );
 });
 
