6c2c916a82bb1ef4a9018d4bc2de14ce1971741c
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2023-11-05 23:09:01 +0530

    refactor: removed unused packages

--- web/components/dnd/StrictModeDroppable.tsx
+++ web/components/dnd/StrictModeDroppable.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 // react beautiful dnd
-import { Droppable, DroppableProps } from "react-beautiful-dnd";
+import { Droppable, DroppableProps } from "@hello-pangea/dnd";
 
 const StrictModeDroppable = ({ children, ...props }: DroppableProps) => {
   const [enabled, setEnabled] = useState(false);
