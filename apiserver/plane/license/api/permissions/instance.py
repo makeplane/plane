ccfd1c703eed0c46e18c8abf70f3600537f0bba9
pablohashescobar <nikhilschacko@gmail.com>
2023-11-14 20:30:54 +0530

    dev: fix instance permissions and serializer

--- apiserver/plane/license/api/permissions/instance.py
+++ apiserver/plane/license/api/permissions/instance.py
@@ -7,17 +7,27 @@ from plane.license.models import Instance, InstanceAdmin
 
 class InstanceOwnerPermission(BasePermission):
     def has_permission(self, request, view):
+
+        if request.user.is_anonymous:
+            return False
+    
         instance = Instance.objects.first()
         return InstanceAdmin.objects.filter(
             role=20,
             instance=instance,
+            user=request.user,
         ).exists()
 
 
 class InstanceAdminPermission(BasePermission):
     def has_permission(self, request, view):
+
+        if request.user.is_anonymous:
+            return False
+
         instance = Instance.objects.first()
         return InstanceAdmin.objects.filter(
             role__gte=15,
             instance=instance,
+            user=request.user,
         ).exists()
