a25d80fdeb6479b98fc2191fbe05fdbd319ecddd
Henit Chobisa <chobisa.henit@gmail.com>
2023-11-14 18:50:29 +0530

    feat: seperated document editor configs as seperate interfaces

---
+++ packages/editor/document-editor/src/ui/types/menu-actions.d.ts
@@ -0,0 +1,10 @@
+
+export interface IDuplicationConfig {
+  action: () => Promise<void>
+}
+export interface IPageLockConfig {
+  action: () => Promise<void>
+}
+export interface IPageArchiveConfig {
+  action: () => Promise<void>
+  }
