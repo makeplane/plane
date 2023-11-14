48ed43952302a31bf2e471d5faf4755229972cfa
sabith-tu <95301637+sabith-tu@users.noreply.github.com>
2023-11-14 18:26:50 +0530

    style: ui fixes for pages and views (#2770)

--- web/components/issues/issue-layouts/filters/header/filters/mentions.tsx
+++ web/components/issues/issue-layouts/filters/header/filters/mentions.tsx
@@ -49,7 +49,7 @@ export const FilterMentions: React.FC<Props> = (props) => {
                     key={`mentions-${member.id}`}
                     isChecked={appliedFilters?.includes(member.id) ? true : false}
                     onClick={() => handleUpdate(member.id)}
-                    icon={<Avatar name={member?.display_name} src={member?.avatar} showTooltip={false} />}
+                    icon={<Avatar name={member?.display_name} src={member?.avatar} showTooltip={false} size={"md"} />}
                     title={member.display_name}
                   />
                 ))}
