29a5ae781a3a8f91721fa13c49b1772059329471
Henit Chobisa <chobisa.henit@gmail.com>
2023-11-16 14:01:53 +0530

    chore: removed unused utility actions

--- packages/editor/document-editor/src/ui/utils/menu-actions.ts
+++ packages/editor/document-editor/src/ui/utils/menu-actions.ts
@@ -1,17 +1,8 @@
 import { Editor } from "@tiptap/core"
-import { useRouter } from "next/router"
 
 export const copyMarkdownToClipboard = (editor: Editor | null) => {
   const markdownOutput = editor?.storage.markdown.getMarkdown();
 	navigator.clipboard.writeText(markdownOutput)	
-}
-
-export const duplicatePage = () => {
-  
-}
-
-export const lockPage = () => {
-
 }
 
 export const CopyPageLink = () => {
@@ -19,9 +10,3 @@ export const CopyPageLink = () => {
     navigator.clipboard.writeText(window.location.toString())
   }
 }
-
-export const DuplicatePage = () => {
-
-
-}
-
