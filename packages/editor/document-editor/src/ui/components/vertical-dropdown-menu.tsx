eeb71739ac71f540d560122bb6e7c98ed2dc9039
Henit Chobisa <chobisa.henit@gmail.com>
2023-11-16 14:46:22 +0530

    fix: button classnames in vertical dropdowns

--- packages/editor/document-editor/src/ui/components/vertical-dropdown-menu.tsx
+++ packages/editor/document-editor/src/ui/components/vertical-dropdown-menu.tsx
@@ -19,8 +19,8 @@ export interface IVerticalDropdownMenuProps {
 const VerticalDropdownItem = ({ Icon, label, action }: IVerticalDropdownItemProps) => {
 
   return (
-    <CustomMenu.MenuItem >
-      <Button variant={"accent-primary"} onClick={action} className="flex flex-row items-center m-1 max-md:pr-5 cursor-pointer">
+    <CustomMenu.MenuItem>
+      <Button variant={"neutral-primary"} onClick={action} className="flex flex-row border-none items-center m-1 max-md:pr-5 cursor-pointer">
         <Icon size={16} />
         <div className="text-custom-text-300 ml-2 mr-2 leading-5 tracking-tight whitespace-nowrap self-start text-md">
           {label}
@@ -33,7 +33,7 @@ const VerticalDropdownItem = ({ Icon, label, action }: IVerticalDropdownItemProp
 export const VerticalDropdownMenu = ({ items }: IVerticalDropdownMenuProps) => {
 
   return (
-    <CustomMenu maxHeight={"lg"} className={"h-4"} placement={"bottom-start"} customButton={
+    <CustomMenu maxHeight={"lg"} className={"h-4"} placement={"bottom-start"} optionsClassName={"border-custom-border border-r border-solid transition-all duration-200 ease-in-out "} customButton={
 				<MoreVertical size={18}/>
 			}>
       {items.map((item, index) => (
