16a764d31ed6fa03ef82f20ebb698611031f57e5
Henit Chobisa <chobisa.henit@gmail.com>
2023-11-16 14:51:10 +0530

    feat: changed types for archived updates and document last updates

--- packages/editor/document-editor/src/ui/types/menu-actions.d.ts
+++ packages/editor/document-editor/src/ui/types/menu-actions.d.ts
@@ -5,8 +5,10 @@ export interface IDuplicationConfig {
 export interface IPageLockConfig {
 	is_locked: boolean,
   action: () => Promise<void>
+	locked_by?: string,
 }
 export interface IPageArchiveConfig {
 	is_archived: boolean,
+	archived_at?: Date,
   action: () => Promise<void>
   }
