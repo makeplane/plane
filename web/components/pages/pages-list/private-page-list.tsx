5e706ee5dc3dc072ec9a63d810dd1bae06f6b4e5
Henit Chobisa <chobisa.henit@gmail.com>
2023-11-17 11:43:08 +0530

    feat: updated other-page-view to shared page view && same with private pages

--- web/components/pages/pages-list/private-page-list.tsx
+++ web/components/pages/pages-list/private-page-list.tsx
@@ -9,19 +9,19 @@ import { PagesView } from "components/pages";
 // types
 import { TPagesListProps } from "./types";
 // fetch-keys
-import { OTHER_PAGES_LIST } from "constants/fetch-keys";
+import { PRIVATE_PAGES_LIST } from "constants/fetch-keys";
 
 // services
 const pageService = new PageService();
 
-export const OtherPagesList: React.FC<TPagesListProps> = ({ viewType }) => {
+export const PrivatePagesList: React.FC<TPagesListProps> = ({ viewType }) => {
   const router = useRouter();
   const { workspaceSlug, projectId } = router.query;
 
   const { data: pages } = useSWR(
-    workspaceSlug && projectId ? OTHER_PAGES_LIST(projectId as string) : null,
+    workspaceSlug && projectId ? PRIVATE_PAGES_LIST(projectId as string) : null,
     workspaceSlug && projectId
-      ? () => pageService.getPagesWithParams(workspaceSlug as string, projectId as string, "created_by_other")
+      ? () => pageService.getPagesWithParams(workspaceSlug as string, projectId as string, "private")
       : null
   );
 
--- web/components/pages/pages-list/shared-pages-list.tsx
+++ web/components/pages/pages-list/shared-pages-list.tsx
@@ -9,19 +9,19 @@ import { PagesView } from "components/pages";
 // types
 import { TPagesListProps } from "./types";
 // fetch-keys
-import { MY_PAGES_LIST } from "constants/fetch-keys";
+import { SHARED_PAGES_LIST } from "constants/fetch-keys";
 
 // services
 const pageService = new PageService();
 
-export const MyPagesList: React.FC<TPagesListProps> = ({ viewType }) => {
+export const SharedPagesList: React.FC<TPagesListProps> = ({ viewType }) => {
   const router = useRouter();
   const { workspaceSlug, projectId } = router.query;
 
   const { data: pages } = useSWR(
-    workspaceSlug && projectId ? MY_PAGES_LIST(projectId as string) : null,
+    workspaceSlug && projectId ? SHARED_PAGES_LIST(projectId as string) : null,
     workspaceSlug && projectId
-      ? () => pageService.getPagesWithParams(workspaceSlug as string, projectId as string, "created_by_me")
+      ? () => pageService.getPagesWithParams(workspaceSlug as string, projectId as string, "shared")
       : null
   );
 
