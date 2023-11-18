3d8da99eec4c135b2242995ebf14df8a4e24d896
Nikhil <118773738+pablohashescobar@users.noreply.github.com>
2023-11-18 16:18:06 +0530

    chore: user onboarding workflow (#2791)

--- apiserver/plane/db/models/workspace.py
+++ apiserver/plane/db/models/workspace.py
@@ -72,7 +72,7 @@ class Workspace(BaseModel):
         related_name="owner_workspace",
     )
     slug = models.SlugField(max_length=48, db_index=True, unique=True)
-    organization_size = models.CharField(max_length=20)
+    organization_size = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
         """Return name of the Workspace"""
