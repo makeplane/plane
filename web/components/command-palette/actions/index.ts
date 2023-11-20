9bf2092fc06ca24ea02c99066f5690af1777cad3
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2023-11-20 13:19:57 +0530

    refactor: command palette file structure

---
+++ web/components/command-palette/actions/index.ts
@@ -0,0 +1,5 @@
+export * from "./issue-actions";
+export * from "./help-actions";
+export * from "./project-actions";
+export * from "./theme-actions";
+export * from "./workspace-settings-actions";
