8a644056f7413195c1455a39986122967189e410
LAKHAN BAHETI <lakhanbaheti9@gmail.com>
2023-11-20 12:48:38 +0530

    feat: delete account

--- web/components/account/delete-account-modal.tsx
+++ web/components/account/delete-account-modal.tsx
@@ -12,6 +12,7 @@ import { AuthService } from "services/auth.service";
 import { Dialog, Transition } from "@headlessui/react";
 // icons
 import { AlertTriangle } from "lucide-react";
+import { UserService } from "services/user.service";
 
 type Props = {
   isOpen: boolean;
@@ -19,6 +20,7 @@ type Props = {
 };
 
 const authService = new AuthService();
+const userService = new UserService();
 
 const DeleteAccountModal: React.FC<Props> = (props) => {
   const { isOpen, onClose } = props;
@@ -39,6 +41,28 @@ const DeleteAccountModal: React.FC<Props> = (props) => {
           message: "Failed to sign out. Please try again.",
         })
       );
+  };
+
+  const handleDeleteAccount = async () => {
+    setIsDeleteLoading(true);
+    await userService
+      .deleteAccount()
+      .then(() => {
+        setToastAlert({
+          type: "success",
+          title: "Success!",
+          message: "Account deleted successfully.",
+        });
+        router.push("/");
+      })
+      .catch((err) =>
+        setToastAlert({
+          type: "error",
+          title: "Error!",
+          message: err?.data?.error,
+        })
+      );
+    setIsDeleteLoading(false);
   };
 
   const handleClose = () => {
@@ -95,7 +119,10 @@ const DeleteAccountModal: React.FC<Props> = (props) => {
                   <span className="text-sm font-medium hover:cursor-pointer" onClick={handleSignOut}>
                     Switch account
                   </span>
-                  <button className="py-1.5 px-3 font-medium rounded-sm text-red-500 border border-red-500 text-sm ">
+                  <button
+                    className="py-1.5 px-3 font-medium rounded-sm text-red-500 border border-red-500 text-sm "
+                    onClick={handleDeleteAccount}
+                  >
                     {isDeleteLoading ? "Deleting..." : "Delete account"}
                   </button>
                 </div>
