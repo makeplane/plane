10cde58363fbdb8742bdaa1fd19e0a64bfc98e5c
M. Palanikannan <73993394+Palanikannan1437@users.noreply.github.com>
2023-11-27 20:37:40 +0530

    image can't be inserted inside table (#2904)
    * image can't be inserted inside table
    
    Now we've diabled image icon from showing up if the cursor is inside a table node or if a table cell is selected
    
    * added drag drop support for document editor
    
    * fixed missing dependencies

--- packages/editor/document-editor/src/ui/extensions/index.tsx
+++ packages/editor/document-editor/src/ui/extensions/index.tsx
@@ -1,59 +1,28 @@
-import HorizontalRule from "@tiptap/extension-horizontal-rule";
 import Placeholder from "@tiptap/extension-placeholder";
-import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
-import { common, createLowlight } from 'lowlight'
-import { InputRule } from "@tiptap/core";
+import { SlashCommand } from "@plane/editor-extensions";
 
-import ts from "highlight.js/lib/languages/typescript";
-
-import SlashCommand from "./slash-command";
-import { UploadImage } from "../";
-
-const lowlight = createLowlight(common)
-lowlight.register("ts", ts);
+import { UploadImage } from "@plane/editor-types";
+import { DragAndDrop } from "@plane/editor-extensions";
 
 export const DocumentEditorExtensions = (
   uploadFile: UploadImage,
-  setIsSubmitting?: (isSubmitting: "submitting" | "submitted" | "saved") => void
+  setIsSubmitting?: (
+    isSubmitting: "submitting" | "submitted" | "saved",
+  ) => void,
 ) => [
-    HorizontalRule.extend({
-      addInputRules() {
-        return [
-          new InputRule({
-            find: /^(?:---|—-|___\s|\*\*\*\s)$/,
-            handler: ({ state, range, commands }) => {
-              commands.splitBlock();
+  SlashCommand(uploadFile, setIsSubmitting),
+  DragAndDrop,
+  Placeholder.configure({
+    placeholder: ({ node }) => {
+      if (node.type.name === "heading") {
+        return `Heading ${node.attrs.level}`;
+      }
+      if (node.type.name === "image" || node.type.name === "table") {
+        return "";
+      }
 
-              const attributes = {};
-              const { tr } = state;
-              const start = range.from;
-              const end = range.to;
-              // @ts-ignore
-              tr.replaceWith(start - 1, end, this.type.create(attributes));
-            },
-          }),
-        ];
-      },
-    }).configure({
-      HTMLAttributes: {
-        class: "mb-6 border-t border-custom-border-300",
-      },
-    }),
-    SlashCommand(uploadFile, setIsSubmitting),
-    CodeBlockLowlight.configure({
-      lowlight,
-    }),
-    Placeholder.configure({
-      placeholder: ({ node }) => {
-        if (node.type.name === "heading") {
-          return `Heading ${node.attrs.level}`;
-        }
-        if (node.type.name === "image" || node.type.name === "table") {
-          return "";
-        }
-
-        return "Press '/' for commands...";
-      },
-      includeChildren: true,
-    }),
-  ];
+      return "Press '/' for commands...";
+    },
+    includeChildren: true,
+  }),
+];
---
+++ packages/editor/types/src/index.ts
@@ -0,0 +1,6 @@
+export type { DeleteImage } from "./types/delete-image";
+export type { UploadImage } from "./types/upload-image";
+export type {
+  IMentionHighlight,
+  IMentionSuggestion,
+} from "./types/mention-suggestion";
