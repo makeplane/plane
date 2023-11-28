fc09dd3e042599b87a5cc3ddc89b60141f72889d
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2023-11-28 20:16:20 +0530

    chore: instance layout updated

--- web/layouts/instance-layout/index.tsx
+++ web/layouts/instance-layout/index.tsx
@@ -2,10 +2,11 @@ import { FC, ReactNode } from "react";
 
 import useSWR from "swr";
 
+// route
+import { useRouter } from "next/router";
 // store
 import { observer } from "mobx-react-lite";
 import { useMobxStore } from "lib/mobx/store-provider";
-
 // components
 import { Spinner } from "@plane/ui";
 import { InstanceNotReady } from "components/instance";
@@ -20,14 +21,15 @@ const InstanceLayout: FC<Props> = observer(({ children }) => {
     instance: { fetchInstanceInfo, instance },
   } = useMobxStore();
 
-  console.log("instance", instance);
+  const router = useRouter();
+  const isGodMode = router.pathname.includes("god-mode");
 
   useSWR("INSTANCE_INFO", () => fetchInstanceInfo());
 
   return (
     <div className="h-screen w-full overflow-hidden">
       {instance ? (
-        !instance.is_setup_done ? (
+        !instance.is_setup_done && !isGodMode ? (
           <InstanceNotReady />
         ) : (
           children
