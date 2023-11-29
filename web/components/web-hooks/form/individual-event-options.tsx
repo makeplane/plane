7f6d59559d7896e1af6428dd680f3dc70cb7be8d
Aaryan Khandelwal <65252264+aaryan610@users.noreply.github.com>
2023-11-29 14:02:13 +0530

    chore: update the content of webhooks form (#2932)

--- web/components/web-hooks/form/individual-event-options.tsx
+++ web/components/web-hooks/form/individual-event-options.tsx
@@ -1,7 +1,7 @@
 import { Control, Controller } from "react-hook-form";
 import { IWebhook } from "types/webhook";
 
-export const individualWebhookOptions: {
+export const INDIVIDUAL_WEBHOOK_OPTIONS: {
   key: keyof IWebhook;
   label: string;
   description: string;
@@ -9,27 +9,27 @@ export const individualWebhookOptions: {
   {
     key: "project",
     label: "Projects",
-    description: "Project created, updated or deleted.",
+    description: "Project created, updated, or deleted",
   },
   {
     key: "cycle",
     label: "Cycles",
-    description: "Cycle created, updated or deleted.",
+    description: "Cycle created, updated, or deleted",
   },
   {
     key: "issue",
     label: "Issues",
-    description: "Issue created, updated, deleted, added to a cycle or module.",
+    description: "Issue created, updated, deleted, added to a cycle or module",
   },
   {
     key: "module",
     label: "Modules",
-    description: "Module created, updated or deleted.",
+    description: "Module created, updated, or deleted",
   },
   {
     key: "issue_comment",
     label: "Issue comments",
-    description: "Comment posted, updated or deleted.",
+    description: "Comment posted, updated, or deleted",
   },
 ];
 
@@ -38,32 +38,30 @@ type Props = {
 };
 
 export const WebhookIndividualEventOptions = ({ control }: Props) => (
-  <>
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-4 gap-y-8 px-6 mt-5">
-      {individualWebhookOptions.map((option) => (
-        <Controller
-          key={option.key}
-          control={control}
-          name={option.key}
-          render={({ field: { onChange, value } }) => (
-            <div>
-              <div className="flex items-center gap-2">
-                <input
-                  id={option.key}
-                  onChange={() => onChange(!value)}
-                  type="checkbox"
-                  name="selectIndividualEvents"
-                  checked={value === true}
-                />
-                <label className="text-sm" htmlFor={option.key}>
-                  {option.label}
-                </label>
-              </div>
-              <p className="text-xs text-custom-text-300 ml-6 mt-0.5">{option.description}</p>
+  <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-4 gap-y-8 px-6">
+    {INDIVIDUAL_WEBHOOK_OPTIONS.map((option) => (
+      <Controller
+        key={option.key}
+        control={control}
+        name={option.key}
+        render={({ field: { onChange, value } }) => (
+          <div>
+            <div className="flex items-center gap-2">
+              <input
+                id={option.key}
+                onChange={() => onChange(!value)}
+                type="checkbox"
+                name="selectIndividualEvents"
+                checked={value === true}
+              />
+              <label className="text-sm" htmlFor={option.key}>
+                {option.label}
+              </label>
             </div>
-          )}
-        />
-      ))}
-    </div>
-  </>
+            <p className="text-xs text-custom-text-300 ml-6 mt-0.5">{option.description}</p>
+          </div>
+        )}
+      />
+    ))}
+  </div>
 );
