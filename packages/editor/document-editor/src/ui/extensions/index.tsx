8841885dd9bd6d3d215e9fc9b33e67d8a964c4bf
Henit Chobisa <chobisa.henit@gmail.com>
2023-11-29 15:50:50 +0530

    feat: added issue embed suggestions into extensions module

--- packages/editor/document-editor/src/ui/extensions/index.tsx
+++ packages/editor/document-editor/src/ui/extensions/index.tsx
@@ -1,12 +1,12 @@
 import Placeholder from "@tiptap/extension-placeholder";
+import { IssueWidgetExtension } from "./widgets/IssueEmbedWidget"
 
-import { IssueWidgetExtension } from "./widgets";
-import { IIssueEmbedConfig } from "./widgets/types";
+import { IIssueEmbedConfig } from "./widgets/IssueEmbedWidget/types";
 
-import { SlashCommand } from "@plane/editor-extensions";
-
-import { UploadImage } from "@plane/editor-types";
-import { DragAndDrop } from "@plane/editor-extensions";
+import { SlashCommand, DragAndDrop } from "@plane/editor-extensions";
+import { ISlashCommandItem, UploadImage } from "@plane/editor-types";
+import { CaretUpIcon } from "@radix-ui/react-icons";
+import { IssueSuggestions } from "./widgets/IssueEmbedSuggestionList";
 
 export const DocumentEditorExtensions = (
   uploadFile: UploadImage,
@@ -14,22 +14,35 @@ export const DocumentEditorExtensions = (
   setIsSubmitting?: (
     isSubmitting: "submitting" | "submitted" | "saved",
   ) => void,
-) => [
+) => {
 
-  SlashCommand(uploadFile, setIsSubmitting),
-  DragAndDrop,
-  Placeholder.configure({
-    placeholder: ({ node }) => {
-      if (node.type.name === "heading") {
-        return `Heading ${node.attrs.level}`;
-      }
-      if (node.type.name === "image" || node.type.name === "table") {
-        return "";
-      }
+  const additonalOptions: ISlashCommandItem[] = [{
+    title: "IssueEmbed",
+    description: "Embed an issue",
+    searchTerms: ["Issue", "Iss"],
+    icon: <CaretUpIcon size={18} />,
+    command: ({ editor, range }) => {
+      editor.chain().focus().insertContentAt(range, "<p class='text-sm bg-gray-300 w-fit pl-3 pr-3 pt-1 pb-1 rounded shadow-sm'>#issue_</p>").run()
+    }
+  }]
 
-      return "Press '/' for commands...";
-    },
-    includeChildren: true,
-  }),
-  IssueWidgetExtension({ issueEmbedConfig }),
-];
+  return [
+    SlashCommand(uploadFile, setIsSubmitting, additonalOptions),
+    DragAndDrop,
+    Placeholder.configure({
+      placeholder: ({ node }) => {
+        if (node.type.name === "heading") {
+          return `Heading ${node.attrs.level}`;
+        }
+        if (node.type.name === "image" || node.type.name === "table") {
+          return "";
+        }
+
+        return "Press '/' for commands...";
+      },
+      includeChildren: true,
+    }),
+    IssueWidgetExtension({ issueEmbedConfig }),
+    IssueSuggestions(issueEmbedConfig ? issueEmbedConfig.issues : [])
+  ]
+};
