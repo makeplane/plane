6d97dd814a956127825cc798bc7644a7ba76da96
Aaryan Khandelwal <65252264+aaryan610@users.noreply.github.com>
2023-11-29 19:07:33 +0530

    chore: updated sign in workflow (#2939)
    * chore: new sign in workflow
    
    * chore: request new code button added
    
    * chore: create new password form added
    
    * fix: build errors
    
    * chore: remove unused components
    
    * chore: update submitting state texts
    
    * fix: oauth sign in process

--- web/services/auth.service.ts
+++ web/services/auth.service.ts
@@ -2,23 +2,66 @@
 import { APIService } from "services/api.service";
 // helpers
 import { API_BASE_URL } from "helpers/common.helper";
-
-export interface ILoginTokenResponse {
-  access_token: string;
-  refresh_toke: string;
-}
+// types
+import { IEmailCheckData, ILoginTokenResponse, IMagicSignInData, IPasswordSignInData } from "types/auth";
 
 export class AuthService extends APIService {
   constructor() {
     super(API_BASE_URL);
   }
 
-  async emailLogin(data: any): Promise<ILoginTokenResponse> {
+  async emailCheck(data: IEmailCheckData): Promise<{
+    is_password_autoset: boolean;
+  }> {
+    return this.post("/api/email-check/", data, { headers: {} })
+      .then((response) => response?.data)
+      .catch((error) => {
+        throw error?.response?.data;
+      });
+  }
+
+  async passwordSignIn(data: IPasswordSignInData): Promise<ILoginTokenResponse> {
     return this.post("/api/sign-in/", data, { headers: {} })
       .then((response) => {
         this.setAccessToken(response?.data?.access_token);
         this.setRefreshToken(response?.data?.refresh_token);
         return response?.data;
+      })
+      .catch((error) => {
+        throw error?.response?.data;
+      });
+  }
+
+  async sendResetPasswordLink(data: { email: string }): Promise<any> {
+    return this.post(`/api/forgot-password/`, data)
+      .then((response) => response?.data)
+      .catch((error) => {
+        throw error?.response;
+      });
+  }
+
+  async setPassword(data: { password: string }): Promise<any> {
+    return this.post(`/api/users/me/set-password/`, data)
+      .then((response) => response?.data)
+      .catch((error) => {
+        throw error?.response?.data;
+      });
+  }
+
+  async resetPassword(
+    uidb64: string,
+    token: string,
+    data: {
+      new_password: string;
+    }
+  ): Promise<ILoginTokenResponse> {
+    return this.post(`/api/reset-password/${uidb64}/${token}/`, data)
+      .then((response) => {
+        if (response?.status === 200) {
+          this.setAccessToken(response?.data?.access_token);
+          this.setRefreshToken(response?.data?.refresh_token);
+          return response?.data;
+        }
       })
       .catch((error) => {
         throw error?.response?.data;
@@ -57,14 +100,18 @@ export class AuthService extends APIService {
       });
   }
 
-  async magicSignIn(data: any): Promise<any> {
-    const response = await this.post("/api/magic-sign-in/", data, { headers: {} });
-    if (response?.status === 200) {
-      this.setAccessToken(response?.data?.access_token);
-      this.setRefreshToken(response?.data?.refresh_token);
-      return response?.data;
-    }
-    throw response.response.data;
+  async magicSignIn(data: IMagicSignInData): Promise<any> {
+    return await this.post("/api/magic-sign-in/", data, { headers: {} })
+      .then((response) => {
+        if (response?.status === 200) {
+          this.setAccessToken(response?.data?.access_token);
+          this.setRefreshToken(response?.data?.refresh_token);
+          return response?.data;
+        }
+      })
+      .catch((error) => {
+        throw error?.response?.data;
+      });
   }
 
   async signOut(): Promise<any> {
