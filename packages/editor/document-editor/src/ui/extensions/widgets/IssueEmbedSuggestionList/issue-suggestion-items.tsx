8573dbaccaf9923e9744a5988ee7fbc91bcedffd
Henit Chobisa <chobisa.henit@gmail.com>
2023-11-29 15:50:06 +0530

    feat: added issue embed suggestion extension

---
+++ packages/editor/document-editor/src/ui/extensions/widgets/IssueEmbedSuggestionList/issue-suggestion-items.tsx
@@ -0,0 +1,34 @@
+import { IIssueListSuggestion } from ".";
+
+export const getIssueSuggestionItems =
+  (
+    issueSuggestions: Array<IIssueListSuggestion>
+  ) => {
+    return ({ query }: { query: string }) => {
+      const filteredSuggestions = issueSuggestions.filter((item) => {
+        if (typeof query === "string" && query.length > 0) {
+          const search = query.toLowerCase();
+          console.log(
+            item.title,
+            item.priority,
+            search,
+            item.title.toLowerCase().includes(search),
+            item.identifier.toLowerCase().includes(search),
+            item.priority.toLowerCase().includes(search)
+          )
+
+
+          return (
+            item.title.toLowerCase().includes(search) ||
+            item.identifier.toLowerCase().includes(search) ||
+            item.priority.toLowerCase().includes(search) ||
+            (item.searchTerms &&
+              item.searchTerms.some((term: string) => term.includes(search)))
+          );
+        }
+        return true;
+      });
+
+      return filteredSuggestions
+    }
+	}
