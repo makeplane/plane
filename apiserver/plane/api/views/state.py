f1bfc5b4da67add875b0e71657a0369e755662fa
NarayanBavisetti <narayan3119@gmail.com>
2023-12-01 17:30:26 +0530

    chore: state status code

--- apiserver/plane/api/views/state.py
+++ apiserver/plane/api/views/state.py
@@ -64,7 +64,7 @@ class StateAPIEndpoint(BaseAPIView):
         )
 
         if state.default:
-            return Response({"error": "Default state cannot be deleted"}, status=False)
+            return Response({"error": "Default state cannot be deleted"}, status=status.HTTP_400_BAD_REQUEST)
 
         # Check for any issues in the state
         issue_exist = Issue.issue_objects.filter(state=state_id).exists()
