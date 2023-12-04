06fed034a28c4657a8cae70ebda1bdc8932ddf77
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2023-12-04 12:41:44 +0530

    chore: login page content updated

--- space/components/views/login.tsx
+++ space/components/views/login.tsx
@@ -15,7 +15,7 @@ import { IUser } from "types/user";
 import { Lightbulb } from "lucide-react";
 // images
 import BluePlaneLogoWithoutText from "public/plane-logos/blue-without-text.png";
-import signInIssues from "public/onboarding/onboarding-issues.svg";
+import latestFeatures from "public/onboarding/onboarding-pages.svg";
 
 export const LoginView = observer(() => {
   // store
@@ -88,22 +88,19 @@ export const LoginView = observer(() => {
                         <Lightbulb className="h-7 w-7 mr-2 mx-3" />
                         <p className="text-sm text-left text-onboarding-text-100">
                           Pages gets a facelift! Write anything and use Galileo to help you start.{" "}
-                          <Link
-                            href="https://plane.so/changelog"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="font-medium text-sm underline hover:cursor-pointer"
-                          >
-                            Learn more
+                          <Link href="https://plane.so/changelog" target="_blank" rel="noopener noreferrer">
+                            <span className="font-medium text-sm underline hover:cursor-pointer">Learn more</span>
                           </Link>
                         </p>
                       </div>
-                      <div className="flex justify-center border border-onboarding-border-200 sm:w-96 sm:h-52 object-cover mt-8 mx-auto rounded-md bg-onboarding-background-100 ">
-                        <Image
-                          src={signInIssues}
-                          alt="Plane Issues"
-                          className={`flex object-cover rounded-md bg-custom-primary-70 `}
-                        />
+                      <div className="border border-onboarding-border-200 sm:w-96 sm:h-52 object-cover mt-8 mx-auto rounded-md bg-onboarding-background-100 overflow-hidden">
+                        <div className="h-[90%]">
+                          <Image
+                            src={latestFeatures}
+                            alt="Plane Issues"
+                            className="rounded-md h-full ml-8 -mt-2 bg-onboarding-background-100"
+                          />
+                        </div>
                       </div>
                     </>
                   )}
