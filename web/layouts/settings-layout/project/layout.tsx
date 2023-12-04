03d33fa559cf72857832f7456dc3e9146841eb3a
rahulramesha <rahulramesham@gmail.com>
2023-12-04 14:37:51 +0530

    disable access to project settings for viewers and guests

--- web/layouts/settings-layout/project/layout.tsx
+++ web/layouts/settings-layout/project/layout.tsx
@@ -1,6 +1,9 @@
 import { FC, ReactNode } from "react";
 // components
 import { ProjectSettingsSidebar } from "./sidebar";
+import { useMobxStore } from "lib/mobx/store-provider";
+import { EUserWorkspaceRoles } from "constants/workspace";
+import { NotAuthorizedView } from "components/auth-screens";
 
 export interface IProjectSettingLayout {
   children: ReactNode;
@@ -9,12 +12,20 @@ export interface IProjectSettingLayout {
 export const ProjectSettingLayout: FC<IProjectSettingLayout> = (props) => {
   const { children } = props;
 
-  return (
+  const {
+    user: { currentProjectRole },
+  } = useMobxStore();
+
+  const canViewSettings = !!currentProjectRole && currentProjectRole >= EUserWorkspaceRoles.MEMBER;
+
+  return canViewSettings ? (
     <div className="flex gap-2 h-full w-full overflow-x-hidden overflow-y-scroll">
       <div className="w-80 pt-8 overflow-y-hidden flex-shrink-0">
         <ProjectSettingsSidebar />
       </div>
       {children}
     </div>
+  ) : (
+    <NotAuthorizedView type="project" />
   );
 };
