11e447c15ee5484579a513435dfb79374b2a8c3d
gurusainath <gurusainath007@gmail.com>
2023-12-07 15:42:10 +0530

    fix: next_path redirection

--- space/pages/index.tsx
+++ space/pages/index.tsx
@@ -1,20 +1,27 @@
 import { useEffect } from "react";
-
-// next
 import { NextPage } from "next";
 import { useRouter } from "next/router";
+import { observer } from "mobx-react-lite";
+
 // components
 import { LoginView } from "components/views";
+// store
+import { RootStore } from "store/root";
+import { useMobxStore } from "lib/mobx/store-provider";
 
-const Index: NextPage = () => {
+const Index: NextPage = observer(() => {
   const router = useRouter();
   const { next_path } = router.query as { next_path: string };
 
+  const {
+    user: { currentUser },
+  }: RootStore = useMobxStore();
+
   useEffect(() => {
-    if (next_path) router.push(`/?next_path=${next_path}`);
-  }, [router, next_path]);
+    if (next_path && currentUser?.onboarding_step?.profile_complete) router.push(next_path);
+  }, [router, next_path, currentUser]);
 
   return <LoginView />;
-};
+});
 
 export default Index;
