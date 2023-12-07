346c76dd3c4e72781c0bf70104547ac7f9924e73
sriram veeraghanta <veeraghanta.sriram@gmail.com>
2023-12-07 18:39:08 +0530

    fix: sanitized pathname

--- space/pages/index.tsx
+++ space/pages/index.tsx
@@ -11,14 +11,15 @@ import { useMobxStore } from "lib/mobx/store-provider";
 
 const Index: NextPage = observer(() => {
   const router = useRouter();
-  const { next_path } = router.query as { next_path: string };
+  const { next_path } = router.query;
 
   const {
     user: { currentUser },
   }: RootStore = useMobxStore();
 
   useEffect(() => {
-    if (next_path && currentUser?.onboarding_step?.profile_complete) router.push(next_path);
+    if (next_path && currentUser?.onboarding_step?.profile_complete)
+      router.push(next_path.toString().replace(/[^a-zA-Z0-9\-._~:/?#[\]@!$&'()*+,;=]/g, ""));
   }, [router, next_path, currentUser]);
 
   return <LoginView />;
