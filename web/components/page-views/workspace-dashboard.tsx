158cd34841c377a36f8187f3989c0e3fb21aead5
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2023-12-12 17:03:00 +0530

    chore: empty state action button validation

--- web/components/page-views/workspace-dashboard.tsx
+++ web/components/page-views/workspace-dashboard.tsx
@@ -8,6 +8,8 @@ import { useMobxStore } from "lib/mobx/store-provider";
 import { TourRoot } from "components/onboarding";
 import { UserGreetingsView } from "components/user";
 import { CompletedIssuesGraph, IssuesList, IssuesPieChart, IssuesStats } from "components/workspace";
+// constants
+import { EUserWorkspaceRoles } from "constants/workspace";
 // images
 import { NewEmptyState } from "components/common/new-empty-state";
 import emptyProject from "public/empty-state/dashboard_empty_project.webp";
@@ -35,6 +37,8 @@ export const WorkspaceDashboardView = observer(() => {
     workspaceSlug ? `USER_WORKSPACE_DASHBOARD_${workspaceSlug}_${month}` : null,
     workspaceSlug ? () => userStore.fetchUserDashboardInfo(workspaceSlug.toString(), month) : null
   );
+
+  const isEditingAllowed = !!userStore.currentProjectRole && userStore.currentProjectRole >= EUserWorkspaceRoles.MEMBER;
 
   const handleTourCompleted = () => {
     userStore
@@ -96,6 +100,7 @@ export const WorkspaceDashboardView = observer(() => {
                   commandPaletteStore.toggleCreateProjectModal(true);
                 },
               }}
+              disabled={!isEditingAllowed}
             />
           )
         ) : null}
