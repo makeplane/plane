7a15cb6df5d8b713c0bf996656eb96220cc7e835
LAKHAN BAHETI <lakhanbaheti9@gmail.com>
2023-12-13 14:16:24 +0530

    fix: spreadsheet subissues property update

--- web/components/issues/issue-layouts/spreadsheet/columns/priority-column.tsx
+++ web/components/issues/issue-layouts/spreadsheet/columns/priority-column.tsx
@@ -9,7 +9,7 @@ import { IIssue } from "types";
 
 type Props = {
   issue: IIssue;
-  onChange: (data: Partial<IIssue>) => void;
+  onChange: (issue: IIssue, data: Partial<IIssue>) => void;
   expandedIssues: string[];
   disabled: boolean;
 };
@@ -17,13 +17,18 @@ type Props = {
 export const SpreadsheetPriorityColumn: React.FC<Props> = ({ issue, onChange, expandedIssues, disabled }) => {
   const isExpanded = expandedIssues.indexOf(issue.id) > -1;
 
-  const { subIssues, isLoading } = useSubIssue(issue.project_detail?.id, issue.id, isExpanded);
+  const { subIssues, isLoading, mutateSubIssues } = useSubIssue(issue.project_detail?.id, issue.id, isExpanded);
 
   return (
     <>
       <PrioritySelect
         value={issue.priority}
-        onChange={(data) => onChange({ priority: data })}
+        onChange={(data) => {
+          onChange(issue, { priority: data });
+          if (issue.parent) {
+            mutateSubIssues(issue, { priority: data });
+          }
+        }}
         className="h-11 w-full border-b-[0.5px] border-custom-border-200"
         buttonClassName="!shadow-none !border-0 h-full w-full px-2.5 py-1"
         showTitle
--- web/hooks/use-sub-issue.tsx
+++ web/hooks/use-sub-issue.tsx
@@ -1,11 +1,11 @@
 import { useRouter } from "next/router";
 
-import useSWR from "swr";
+import useSWR, { mutate } from "swr";
 
 // services
 import { IssueService } from "services/issue";
 // types
-import { ISubIssueResponse } from "types";
+import { IIssue, ISubIssueResponse } from "types";
 // fetch-keys
 import { SUB_ISSUES } from "constants/fetch-keys";
 
@@ -19,12 +19,39 @@ const useSubIssue = (projectId: string, issueId: string, isExpanded: boolean) =>
 
   const { data: subIssuesResponse, isLoading } = useSWR<ISubIssueResponse>(
     shouldFetch ? SUB_ISSUES(issueId as string) : null,
-    shouldFetch ? () => issueService.subIssues(workspaceSlug as string, projectId as string, issueId as string) : null
+    shouldFetch ? () => issueService.subIssues(workspaceSlug as string, projectId as string, issueId as string) : null,
+    {
+      revalidateOnMount: true,
+    }
   );
+
+  const mutateSubIssues = (issue: IIssue, data: Partial<IIssue>) => {
+    mutate(
+      issue.parent ? SUB_ISSUES(issue.parent) : null,
+      issue.parent
+        ? (prev_data: any) => {
+            return {
+              ...prev_data,
+              sub_issues: prev_data.sub_issues.map((sub_issue: any) => {
+                if (sub_issue.id === issue.id) {
+                  return {
+                    ...sub_issue,
+                    ...data,
+                  };
+                }
+                return sub_issue;
+              }),
+            };
+          }
+        : null,
+      false
+    );
+  };
 
   return {
     subIssues: subIssuesResponse?.sub_issues ?? [],
     isLoading,
+    mutateSubIssues,
   };
 };
 
