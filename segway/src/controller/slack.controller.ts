2af97eb5208473facbc8590eb1b42652e9bf035b
Henit Chobisa <chobisa.henit@gmail.com>
2023-12-20 10:54:43 +0530

    feat: added operations to perform and retrieve operation to and from slack

--- segway/src/controller/slack.controller.ts
+++ segway/src/controller/slack.controller.ts
@@ -1,10 +1,80 @@
 import { Request, Response } from "express";
-import { Controller, Get } from "@overnightjs/core";
+import { Controller, Get, Post } from "@overnightjs/core";
+import { getSlackMessageTemplate } from "../utils/slack/message-templates";
+import { SlackService } from "../services/slack.service";
+import { issueActivitySummary } from "../utils/slack/generateActivityMessage";
+import { logger } from "../utils/logger";
+import {
+  CreateIssueModalViewFull,
+  CreateIssueModalViewProjects,
+} from "../utils/slack/create-issue-modal";
+import { ProjectService } from "../services/project.service";
+import { convertProjectToOptions } from "../utils/slack/convert-project-to-options";
 
+@Controller("api/slack")
+export class SlackController {
+  @Post("")
+  async postActivity(req: Request, res: Response) {
+    try {
+      // Initiate Slack Service
+      const slackService = new SlackService();
 
-@Controller("/api/slack")
-export class SlackController  {
-    /**
-     * This controller houses all routes for the Slack Integration
-     */
+      // Convert Slack message from the issueActivity provided from the request
+      const issueActivityData = req.body;
+      const activitySummary = await issueActivitySummary(issueActivityData);
+
+      // Convert message string into slack message blocks format
+      const issue_template = getSlackMessageTemplate(activitySummary);
+
+      // send message to slack
+      slackService.sendMessage(issue_template);
+
+      res.status(200);
+      return;
+    } catch (error) {
+      logger.error(error);
+      return res.json({ message: "Server error" });
+    }
+  }
+
+  @Post("trigger/ui/create-issue/")
+  async triggerCreateIssueModal(req: Request, res: Response) {
+    const slackService = new SlackService();
+    const projectService = new ProjectService();
+
+    const teamId = req.body.team_id;
+
+    const workspaceId = await slackService.getWorkspaceId(teamId);
+
+    if (!workspaceId) {
+      res.sendStatus(500);
+      return;
+    }
+
+    const projectList =
+      await projectService.getProjectsForWorkspace(workspaceId);
+    const projectPlainTextOption = convertProjectToOptions(projectList);
+
+    await slackService.openModal(
+      req.body.trigger_id,
+      CreateIssueModalViewProjects(projectPlainTextOption),
+    );
+    res.sendStatus(200);
+  }
+
+  @Post("events")
+  async handleSlackEvents(req: Request, res: Response) {
+    const slackService = new SlackService();
+    const projectService = new ProjectService();
+
+    const payload = JSON.parse(req.body.payload);
+    const selectedProjectId = payload.actions[0].selected_option.value;
+    // const states = await projectService.getProjectStates(selectedProjectId);
+
+    const viewId = payload.container.view_id;
+
+    await slackService.updateModal(viewId, CreateIssueModalViewFull);
+
+    res.sendStatus(200);
+  }
 }
