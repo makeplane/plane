06f1b1c14ae8ce0db33332aefd8e59bb910aa085
Henit Chobisa <chobisa.henit@gmail.com>
2023-12-20 10:51:52 +0530

    feat: added user and workspace schema, relations

--- segway/src/db/slack.schema.ts
+++ segway/src/db/slack.schema.ts
@@ -43,6 +43,25 @@ export const apiTokens = pgTable("api_tokens", {
   isActive: boolean("is_active"),
 });
 
+export const users = pgTable("users", {
+  id: uuid("id").primaryKey(),
+  username: text("username"),
+  email: text("email"),
+  firstName: text("first_name"),
+  lastName: text("last_name"),
+  isActive: boolean("is_active"),
+  role: text("role"),
+  isBot: boolean("is_bot"),
+  displayName: text("display_name"),
+});
+
+export const workspaces = pgTable("workspaces", {
+  id: uuid("id").primaryKey(),
+  name: text("name"),
+  slug: text("slug"),
+  createdById: uuid("created_by_id"),
+  ownerId: uuid("owner_id"),
+});
 export const workspaceIntegrationsRelations = relations(
   workspaceIntegrations,
   ({ one }) => ({
@@ -76,3 +95,17 @@ export const apiTokensRelations = relations(apiTokens, ({ one }) => ({
   }),
 }));
 
+export const workspacesRelations = relations(workspaces, ({ one }) => ({
+  createdBy: one(users, {
+    fields: [workspaces.createdById],
+    references: [users.id],
+  }),
+  owner: one(users, {
+    fields: [workspaces.ownerId],
+    references: [users.id],
+  }),
+  workspaceIntegrations: one(workspaceIntegrations, {
+    fields: [workspaces.id],
+    references: [workspaceIntegrations.workspaceId],
+  }),
+}));
