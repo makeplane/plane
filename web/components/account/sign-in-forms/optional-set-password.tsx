6ecaa661a768078034fbcc531e0bbba0a0885dfe
Lakhan Baheti <94619783+1akhanBaheti@users.noreply.github.com>
2023-12-21 16:02:56 +0530

    style: email placeholder changed across the platform (#3206)
    * style: email placeholder changed across the platform
    
    * fix: placeholder text

--- web/components/account/sign-in-forms/optional-set-password.tsx
+++ web/components/account/sign-in-forms/optional-set-password.tsx
@@ -61,7 +61,7 @@ export const OptionalSetPasswordForm: React.FC<Props> = (props) => {
               onChange={onChange}
               ref={ref}
               hasError={Boolean(errors.email)}
-              placeholder="orville.wright@firstflight.com"
+              placeholder="orville.wright@frstflt.com"
               className="h-[46px] w-full border border-onboarding-border-100 pr-12 text-onboarding-text-400"
               disabled
             />
