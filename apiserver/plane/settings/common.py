1838c405e81ae677a4c60366cdea9a0b1845b214
sriram veeraghanta <veeraghanta.sriram@gmail.com>
2023-12-24 16:33:18 +0530

    fix: removing segway files

--- apiserver/plane/settings/common.py
+++ apiserver/plane/settings/common.py
@@ -352,7 +352,3 @@ INSTANCE_KEY = os.environ.get(
 SKIP_ENV_VAR = os.environ.get("SKIP_ENV_VAR", "1") == "1"
 
 DATA_UPLOAD_MAX_MEMORY_SIZE = int(os.environ.get("FILE_SIZE_LIMIT", 5242880))
-
-# Segway
-SEGWAY_BASE_URL = os.environ.get("SEGWAY_BASE_URL", "http://localhost:9000")
-SEGWAY_KEY = os.environ.get("SEGWAY_KEY", False)
--- segway/src/utils/logger.ts
+++
@@ -1,8 +0,0 @@
-import winston from "winston";
-
-export const logger = winston.createLogger({
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: "combined.log" }),
-  ],
-});
