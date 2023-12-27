1a05c07f672c518ed3ec56d49b1ece17137cd945
Prateek Shourya <prateekshourya29@gmail.com>
2023-12-27 13:31:33 +0530

    chore: allow guest/ viewer to clear applied filters in project issues.

--- web/components/issues/issue-layouts/filters/applied-filters/members.tsx
+++ web/components/issues/issue-layouts/filters/applied-filters/members.tsx
@@ -9,11 +9,10 @@ type Props = {
   handleRemove: (val: string) => void;
   members: IUserLite[] | undefined;
   values: string[];
-  editable: boolean | undefined;
 };
 
 export const AppliedMembersFilters: React.FC<Props> = observer((props) => {
-  const { handleRemove, members, values, editable } = props;
+  const { handleRemove, members, values } = props;
 
   return (
     <>
@@ -26,15 +25,13 @@ export const AppliedMembersFilters: React.FC<Props> = observer((props) => {
           <div key={memberId} className="flex items-center gap-1 rounded bg-custom-background-80 p-1 text-xs">
             <Avatar name={memberDetails.display_name} src={memberDetails.avatar} showTooltip={false} />
             <span className="normal-case">{memberDetails.display_name}</span>
-            {editable && (
-              <button
-                type="button"
-                className="grid place-items-center text-custom-text-300 hover:text-custom-text-200"
-                onClick={() => handleRemove(memberId)}
-              >
-                <X size={10} strokeWidth={2} />
-              </button>
-            )}
+            <button
+              type="button"
+              className="grid place-items-center text-custom-text-300 hover:text-custom-text-200"
+              onClick={() => handleRemove(memberId)}
+            >
+              <X size={10} strokeWidth={2} />
+            </button>
           </div>
         );
       })}
