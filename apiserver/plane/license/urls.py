e3d0b928f16001f69112c477546e713ea350d03e
pablohashescobar <nikhilschacko@gmail.com>
2024-01-08 17:51:07 +0530

    dev: update the instance urls

--- apiserver/plane/license/urls.py
+++ apiserver/plane/license/urls.py
@@ -10,32 +10,32 @@ from plane.license.api.views import (
 
 urlpatterns = [
     path(
-        "instances/",
+        "",
         InstanceEndpoint.as_view(),
         name="instance",
     ),
     path(
-        "instances/admins/",
+        "admins/",
         InstanceAdminEndpoint.as_view(),
         name="instance-admins",
     ),
     path(
-        "instances/admins/<uuid:pk>/",
+        "admins/<uuid:pk>/",
         InstanceAdminEndpoint.as_view(),
         name="instance-admins",
     ),
     path(
-        "instances/configurations/",
+        "configurations/",
         InstanceConfigurationEndpoint.as_view(),
         name="instance-configuration",
     ),
     path(
-        "instances/admins/sign-in/",
+        "admins/sign-in/",
         InstanceAdminSignInEndpoint.as_view(),
         name="instance-admin-sign-in",
     ),
     path(
-        "instances/admins/sign-up-screen-visited/",
+        "admins/sign-up-screen-visited/",
         SignUpScreenVisitedEndpoint.as_view(),
         name="instance-sign-up",
     ),
