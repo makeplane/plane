5e03f3dd8238b88eb9fcd242f7f9038cb85547b2
Bavisetti Narayan <72156168+NarayanBavisetti@users.noreply.github.com>
2024-01-08 23:25:14 +0530

    chore: mobile configs (#3328)
    * chore: mobile configs
    
    * chore: mobile configurations changed
    
    * chore: removed the slack id
    
    * chore: reversed google client id

--- apiserver/plane/app/urls/config.py
+++ apiserver/plane/app/urls/config.py
@@ -1,12 +1,17 @@
 from django.urls import path
 
 
-from plane.app.views import ConfigurationEndpoint
+from plane.app.views import ConfigurationEndpoint, MobileConfigurationEndpoint
 
 urlpatterns = [
     path(
         "configs/",
         ConfigurationEndpoint.as_view(),
+        name="configuration",
+    ),
+    path(
+        "mobile-configs/",
+        MobileConfigurationEndpoint.as_view(),
         name="configuration",
     ),
 ]
