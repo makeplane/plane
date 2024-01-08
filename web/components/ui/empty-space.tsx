eccb1f5d102fe3172107db7df139a22c21a84e66
rahulramesha <71900764+rahulramesha@users.noreply.github.com>
2024-01-08 19:20:42 +0530

    fix: breaking cycle issues and replacing router.push with Links (#3330)
    * fix cycle creation and active cycle map
    
    * minor fix in cycle store
    
    * create cycle breaking fix
    
    * replace last possible bits of router.push with Link
    
    ---------
    
    Co-authored-by: Rahul R <rahulr@Rahuls-MacBook-Pro.local>

--- web/components/ui/empty-space.tsx
+++ web/components/ui/empty-space.tsx
@@ -45,28 +45,39 @@ type EmptySpaceItemProps = {
   title: string;
   description?: React.ReactNode | string;
   Icon: any;
-  action: () => void;
+  action?: () => void;
+  href?: string;
 };
 
-const EmptySpaceItem: React.FC<EmptySpaceItemProps> = ({ title, description, Icon, action }) => (
-  <>
-    <li className="cursor-pointer" onClick={action} role="button">
-      <div className={`group relative flex ${description ? "items-start" : "items-center"} space-x-3 py-4`}>
-        <div className="flex-shrink-0">
-          <span className="inline-flex h-10 w-10 items-center justify-center rounded-lg bg-custom-primary">
-            <Icon className="h-6 w-6 text-white" aria-hidden="true" />
-          </span>
-        </div>
-        <div className="min-w-0 flex-1 text-custom-text-200">
-          <div className="text-sm font-medium group-hover:text-custom-text-100">{title}</div>
-          {description ? <div className="text-sm">{description}</div> : null}
-        </div>
-        <div className="flex-shrink-0 self-center">
-          <ChevronRight className="h-5 w-5 text-custom-text-200 group-hover:text-custom-text-100" aria-hidden="true" />
-        </div>
+const EmptySpaceItem: React.FC<EmptySpaceItemProps> = ({ title, description, Icon, action, href }) => {
+  let spaceItem = (
+    <div className={`group relative flex ${description ? "items-start" : "items-center"} space-x-3 py-4`}>
+      <div className="flex-shrink-0">
+        <span className="inline-flex h-10 w-10 items-center justify-center rounded-lg bg-custom-primary">
+          <Icon className="h-6 w-6 text-white" aria-hidden="true" />
+        </span>
       </div>
-    </li>
-  </>
-);
+      <div className="min-w-0 flex-1 text-custom-text-200">
+        <div className="text-sm font-medium group-hover:text-custom-text-100">{title}</div>
+        {description ? <div className="text-sm">{description}</div> : null}
+      </div>
+      <div className="flex-shrink-0 self-center">
+        <ChevronRight className="h-5 w-5 text-custom-text-200 group-hover:text-custom-text-100" aria-hidden="true" />
+      </div>
+    </div>
+  );
+
+  if (href) {
+    spaceItem = <Link href={href}>{spaceItem}</Link>;
+  }
+
+  return (
+    <>
+      <li className="cursor-pointer" onClick={action} role="button">
+        {spaceItem}
+      </li>
+    </>
+  );
+};
 
 export { EmptySpace, EmptySpaceItem };
