d7735bb52d9abb48b91fa6a3b0800b5eb1cdac8f
gurusainath <gurusainath007@gmail.com>
2024-01-09 19:41:13 +0530

    chore: fixed peekoverview in calendar

--- web/components/issues/issue-layouts/kanban/block.tsx
+++ web/components/issues/issue-layouts/kanban/block.tsx
@@ -61,14 +61,15 @@ const KanbanIssueDetailsBlock: React.FC<IssueDetailsBlockProps> = observer((prop
           <div className="absolute -top-1 right-0 hidden group-hover/kanban-block:block">{quickActions(issue)}</div>
         </div>
       </WithDisplayPropertiesHOC>
+
       <ControlLink
         href={`/${workspaceSlug}/projects/${projectId}/issues/${issue.id}`}
         target="_blank"
         onClick={() => handleIssuePeekOverview(issue)}
-        className="w-full line-clamp-1 cursor-pointer text-sm font-medium text-custom-text-100"
+        className="w-full line-clamp-1 cursor-pointer text-sm text-custom-text-100"
       >
         <Tooltip tooltipHeading="Title" tooltipContent={issue.name}>
-          <div className="line-clamp-2 text-sm font-medium text-custom-text-100">{issue.name}</div>
+          <span>{issue.name}</span>
         </Tooltip>
       </ControlLink>
 
