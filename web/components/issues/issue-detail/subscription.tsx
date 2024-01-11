86d9c4491acad2446dd340a9ad327f2b5d355a41
gurusainath <gurusainath007@gmail.com>
2024-01-11 16:51:22 +0530

    chore: issue subscription mutation

--- web/components/issues/issue-detail/subscription.tsx
+++ web/components/issues/issue-detail/subscription.tsx
@@ -5,6 +5,7 @@ import { observer } from "mobx-react-lite";
 import { Button } from "@plane/ui";
 // hooks
 import { useIssueDetail } from "hooks/store";
+import useToast from "hooks/use-toast";
 
 export type TIssueSubscription = {
   workspaceSlug: string;
@@ -22,16 +23,32 @@ export const IssueSubscription: FC<TIssueSubscription> = observer((props) => {
     createSubscription,
     removeSubscription,
   } = useIssueDetail();
+  const { setToastAlert } = useToast();
   // state
   const [loading, setLoading] = useState(false);
 
   const issue = getIssueById(issueId);
   const subscription = getSubscriptionByIssueId(issueId);
 
-  const handleSubscription = () => {
+  const handleSubscription = async () => {
     setLoading(true);
-    if (subscription?.subscribed) removeSubscription(workspaceSlug, projectId, issueId);
-    else createSubscription(workspaceSlug, projectId, issueId);
+    try {
+      if (subscription?.subscribed) await removeSubscription(workspaceSlug, projectId, issueId);
+      else await createSubscription(workspaceSlug, projectId, issueId);
+      setToastAlert({
+        type: "success",
+        title: `Issue ${subscription?.subscribed ? `unsubscribed` : `subscribed`} successfully.!`,
+        message: `Issue ${subscription?.subscribed ? `unsubscribed` : `subscribed`} successfully.!`,
+      });
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setToastAlert({
+        type: "error",
+        title: "Error",
+        message: "Something went wrong. Please try again later.",
+      });
+    }
   };
 
   if (issue?.created_by === currentUserId || issue?.assignee_ids.includes(currentUserId)) return <></>;
