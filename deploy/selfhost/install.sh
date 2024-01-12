96b48e95e727c9834b5c6dbfcc22bd8709903121
Manish Gupta <manish@mgupta.me>
2024-01-12 12:56:37 +0530

    handled x86_64 along with amd64

--- deploy/selfhost/install.sh
+++ deploy/selfhost/install.sh
@@ -3,18 +3,17 @@
 BRANCH=master
 SCRIPT_DIR=$PWD
 PLANE_INSTALL_DIR=$PWD/plane-app
-ARCH=$(uname -m)
-export DOCKERHUB_USER=makeplane
 export APP_RELEASE=$BRANCH
+export DOCKERHUB_USER=makeplane
 export PULL_POLICY=always
-NON_AMD_DOCKERHUB_USER=myplane
+USE_GLOBAL_IMAGES=1
 
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-function buildNonAMD64(){
+function buildLocalImage() {
     if [ "$1" == "--force-build" ]; then
         DO_BUILD="1"
     elif [ "$1" == "--skip-build" ]; then
@@ -40,10 +39,7 @@ function buildNonAMD64(){
 
     if [ "$DO_BUILD" == "1" ] || [ "$DO_BUILD" == "" ];
     then
-        DOCKERHUB_USER=$NON_AMD_DOCKERHUB_USER
-
         REPO=https://github.com/makeplane/plane.git
-
         CURR_DIR=$PWD
         PLANE_TEMP_CODE_DIR=$(mktemp -d)
         git clone $REPO $PLANE_TEMP_CODE_DIR  --branch $BRANCH --single-branch
@@ -71,29 +67,16 @@ function buildNonAMD64(){
         printf "INVALID OPTION SUPPLIED" >&2
     fi
 }
-function install(){
+function install() {
     echo "Installing Plane.........."
     download
 }
-function download(){
+function download() {
     cd $SCRIPT_DIR
     TS=$(date +%s)
     if [ -f "$PLANE_INSTALL_DIR/docker-compose.yaml" ]
     then
         mv $PLANE_INSTALL_DIR/docker-compose.yaml $PLANE_INSTALL_DIR/archive/$TS.docker-compose.yaml
-    fi
-
-    if [ $ARCH != "amd64" ]; then
-        local res=$(buildNonAMD64)
-        # echo $res
-
-        if [ "$res" == "build_exited" ];
-        then
-            echo
-            echo "Install action cancelled by you. Exiting now."
-            echo
-            exit 0
-        fi
     fi
 
     curl -H 'Cache-Control: no-cache, no-store' -s -o $PLANE_INSTALL_DIR/docker-compose.yaml  https://raw.githubusercontent.com/makeplane/plane/$BRANCH/deploy/selfhost/docker-compose.yml?$(date +%s)
@@ -114,6 +97,21 @@ function download(){
 
         rm $PLANE_INSTALL_DIR/temp.yaml
     fi
+
+    if [ $USE_GLOBAL_IMAGES == 0 ]; then
+        local res=$(buildLocalImage)
+        # echo $res
+
+        if [ "$res" == "build_exited" ];
+        then
+            echo
+            echo "Install action cancelled by you. Exiting now."
+            echo
+            exit 0
+        fi
+    else
+        docker compose -f $PLANE_INSTALL_DIR/docker-compose.yaml pull
+    fi
     
     echo ""
     echo "Latest version is now available for you to use"
@@ -122,39 +120,22 @@ function download(){
     echo ""
 
 }
-function startServices(){
+function startServices() {
     cd $PLANE_INSTALL_DIR
-    if [ $ARCH == "amd64" ];
-    then
-        PULL_POLICY=always
-        docker compose up -d --quiet-pull
-    else 
-        DOCKERHUB_USER=$NON_AMD_DOCKERHUB_USER
-        PULL_POLICY=never
-        docker compose up -d --quiet-pull
-    fi
+    docker compose up -d --quiet-pull
     cd $SCRIPT_DIR
 }
-function stopServices(){
+function stopServices() {
     cd $PLANE_INSTALL_DIR
-
-    if [ $ARCH != "amd64" ];
-    then
-        DOCKERHUB_USER=$NON_AMD_DOCKERHUB_USER
-    fi
     docker compose down
     cd $SCRIPT_DIR
 }
-function restartServices(){
+function restartServices() {
     cd $PLANE_INSTALL_DIR
-    if [ $ARCH != "amd64" ];
-    then
-        DOCKERHUB_USER=$NON_AMD_DOCKERHUB_USER
-    fi
     docker compose restart
     cd $SCRIPT_DIR
 }
-function upgrade(){
+function upgrade() {
     echo "***** STOPPING SERVICES ****"
     stopServices
 
@@ -165,7 +146,7 @@ function upgrade(){
     echo "***** PLEASE VALIDATE AND START SERVICES ****"
 
 }
-function askForAction(){
+function askForAction() {
     echo
     echo "Select a Action you want to perform:"
     echo "   1) Install (${ARCH})"
@@ -211,6 +192,20 @@ function askForAction(){
     fi
 }
 
+# CPU ARCHITECHTURE BASED SETTINGS
+ARCH=$(uname -m)
+if [ $ARCH == "amd64" ] || [ $ARCH == "x86_64" ];
+then
+    USE_GLOBAL_IMAGES=1
+    DOCKERHUB_USER=makeplane
+    PULL_POLICY=always
+else
+    USE_GLOBAL_IMAGES=0
+    DOCKERHUB_USER=myplane
+    PULL_POLICY=never
+fi
+
+# REMOVE SPECIAL CHARACTERS FROM BRANCH NAME
 if [ "$BRANCH" != "master" ];
 then
     PLANE_INSTALL_DIR=$PWD/plane-app-$(echo $BRANCH | sed -r 's@(\/|" "|\.)@-@g')
