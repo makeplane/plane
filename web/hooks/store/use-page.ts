4fb2ca4df99e06bc3e9b05a0d16b4a7f6f58bfa0
Henit Chobisa <chobisa.henit@gmail.com>
2024-01-17 14:25:50 +0530

    fix: issue embeds and suggestions breaking

--- web/hooks/store/use-page.ts
+++ web/hooks/store/use-page.ts
@@ -4,8 +4,11 @@ import { StoreContext } from "contexts/store-context";
 
 export const usePage = (pageId: string) => {
   const context = useContext(StoreContext);
+  // TODO: Handle fetching of Pages when they are not in the store
   if (context === undefined) throw new Error("usePage must be used within StoreProvider");
+
   const { projectPageMap, projectArchivedPageMap } = context.projectPages;
+
   const projectId = context.app.router.projectId;
   if (!projectId) throw new Error("usePage must be used within ProjectProvider");
   if (projectPageMap[projectId] && projectPageMap[projectId][pageId]) {
@@ -13,6 +16,7 @@ export const usePage = (pageId: string) => {
   } else if (projectArchivedPageMap[projectId] && projectArchivedPageMap[projectId][pageId]) {
     return projectArchivedPageMap[projectId][pageId];
   } else {
+    //TODO: handle this error
     throw new Error(`Page with id ${pageId} does not exist in project with id ${projectId}`);
   }
 };
--- web/hooks/use-issue-embeds.tsx
+++ web/hooks/use-issue-embeds.tsx
@@ -1,6 +1,8 @@
+import { TIssue } from "@plane/types";
 import { PROJECT_ISSUES_LIST, STATES_LIST } from "constants/fetch-keys";
 import { StoreContext } from "contexts/store-context";
 import { toJS } from "mobx";
+import { useRouter } from "next/router";
 import { useContext } from "react";
 import { IssueService } from "services/issue";
 import useSWR from "swr";
@@ -14,6 +16,7 @@ export const useIssueEmbeds = () => {
 
   const { getProjectById, fetchProjects } = useProject();
   const { getStateById, fetchProjectStates } = useProjectState();
+  const router = useRouter();
 
   const { data: issuesResponse, isLoading: issuesLoading } = useSWR(
     workspaceSlug && projectId ? PROJECT_ISSUES_LIST(workspaceSlug as string, projectId as string) : null,
@@ -37,8 +40,32 @@ export const useIssueEmbeds = () => {
     project_detail: toJS(getProjectById(issue.project_id)),
   }));
 
+  const fetchIssue = async (issueId: string) => {
+    const issue = await issueService.retrieve(workspaceSlug as string, projectId as string, issueId as string);
+    return {
+      ...issue,
+      state_detail: toJS(getStateById(issue.state_id)),
+      project_detail: toJS(getProjectById(issue.project_id)),
+    };
+  };
+
+  const issueWidgetClickAction = (issueId: string) => {
+    const url = new URL(router.asPath, window.location.origin);
+    const params = new URLSearchParams(url.search);
+
+    if (params.has("peekIssueId")) {
+      params.set("peekIssueId", issueId);
+    } else {
+      params.append("peekIssueId", issueId);
+    }
+    // Replace the current URL with the new one
+    router.replace(`${url.pathname}?${params.toString()}`, undefined, { shallow: true });
+  };
+
   return {
     issues: issuesWithStateAndProject,
     isLoading: issuesLoading || projectsLoading || statesLoading,
+    fetchIssue,
+    issueWidgetClickAction,
   };
 };
