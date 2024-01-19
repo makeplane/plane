ba27cc57bd4229e42fd9d8cc2b9e3dad72f4fc22
pablohashescobar <nikhilschacko@gmail.com>
2024-01-19 13:51:12 +0530

    dev: workspace states and estimates

--- apiserver/plane/app/views/state.py
+++ apiserver/plane/app/views/state.py
@@ -9,9 +9,12 @@ from rest_framework.response import Response
 from rest_framework import status
 
 # Module imports
-from . import BaseViewSet
+from . import BaseViewSet, BaseAPIView
 from plane.app.serializers import StateSerializer
-from plane.app.permissions import ProjectEntityPermission
+from plane.app.permissions import (
+    ProjectEntityPermission,
+    WorkspaceEntityPermission,
+)
 from plane.db.models import State, Issue
 
 
