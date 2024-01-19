652b1b717ca4f2c2c93ff8a3169cdfa224b343cf
Henit Chobisa <chobisa.henit@gmail.com>
2024-01-19 12:14:04 +0530

    fix: resolved reviewed comments

--- web/hooks/store/use-page.ts
+++ web/hooks/store/use-page.ts
@@ -1,5 +1,4 @@
 import { useContext } from "react";
-import useSWR from "swr";
 // mobx store
 import { StoreContext } from "contexts/store-context";
 
@@ -7,31 +6,16 @@ export const usePage = (pageId: string) => {
   const context = useContext(StoreContext);
   if (context === undefined) throw new Error("usePage must be used within StoreProvider");
 
-  const { projectPageMap, projectArchivedPageMap, fetchProjectPages, fetchArchivedProjectPages } = context.projectPages;
+  const { projectPageMap, projectArchivedPageMap } = context.projectPages;
 
   const { projectId, workspaceSlug } = context.app.router;
   if (!projectId || !workspaceSlug) throw new Error("usePage must be used within ProjectProvider");
-
-  // const { isLoading: projectPagesLoading } = useSWR(
-  //   workspaceSlug && projectId ? `ALL_PAGES_LIST_${projectId}` : null,
-  //   workspaceSlug && projectId && !projectPageMap[projectId] && !projectArchivedPageMap[projectId]
-  //     ? () => fetchProjectPages(workspaceSlug.toString(), projectId.toString())
-  //     : null
-  // );
-  // // fetching archived pages from API
-  // const { isLoading: archivePageLoading } = useSWR(
-  //   workspaceSlug && projectId ? `ALL_ARCHIVED_PAGES_LIST_${projectId}` : null,
-  //   workspaceSlug && projectId && !projectArchivedPageMap[projectId] && !projectPageMap[projectId]
-  //     ? () => fetchArchivedProjectPages(workspaceSlug.toString(), projectId.toString())
-  //     : null
-  // );
 
   if (projectPageMap[projectId] && projectPageMap[projectId][pageId]) {
     return projectPageMap[projectId][pageId];
   } else if (projectArchivedPageMap[projectId] && projectArchivedPageMap[projectId][pageId]) {
     return projectArchivedPageMap[projectId][pageId];
   } else {
-    //TODO: handle this error
-    // throw new Error(`Page with id ${pageId} does not exist in project with id ${projectId}`);
+    return;
   }
 };
