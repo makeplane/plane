73e84224fb92852316dad787ed9b05069b7eb46e
pablohashescobar <nikhilschacko@gmail.com>
2024-01-22 15:25:35 +0530

    dev: update the schedule to 5 minutes

--- apiserver/plane/celery.py
+++ apiserver/plane/celery.py
@@ -31,7 +31,7 @@ app.conf.beat_schedule = {
     },
     "check-every-five-minutes-to-send-email-notifications": {
         "task": "plane.bgtasks.email_notification_task.stack_email_notification",
-        "schedule": timedelta(seconds=10)
+        "schedule": crontab(minute='*/5')
     },
 }
 
