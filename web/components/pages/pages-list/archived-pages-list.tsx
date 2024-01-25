7fd625e0e3a820875d7ea49b3e256289b4d105d9
Anmol Singh Bhatia <121005188+anmolsinghbhatia@users.noreply.github.com>
2024-01-25 12:38:55 +0530

    fix: project pages loader (#3461)

--- web/components/pages/pages-list/archived-pages-list.tsx
+++ web/components/pages/pages-list/archived-pages-list.tsx
@@ -4,13 +4,20 @@ import { observer } from "mobx-react-lite";
 import { PagesListView } from "components/pages/pages-list";
 // hooks
 // ui
-import { Loader } from "@plane/ui";
+import { Loader, Spinner } from "@plane/ui";
 import { useProjectPages } from "hooks/store/use-project-specific-pages";
 
 export const ArchivedPagesList: FC = observer(() => {
   const projectPageStore = useProjectPages();
-  const { archivedPageIds } = projectPageStore;
+  const { archivedPageIds, archivedProjectLoader } = projectPageStore;
 
+  if (archivedProjectLoader) {
+    return (
+      <div className="flex items-center justify-center h-full w-full">
+        <Spinner />
+      </div>
+    );
+  }
   if (!archivedPageIds)
     return (
       <Loader className="space-y-4">
