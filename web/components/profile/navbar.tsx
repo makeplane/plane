dd367e22e68623a7d255dacbba81c858cee34d16
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2024-01-25 16:04:04 +0530

    chore: profile issue empty state improvement

--- web/components/profile/navbar.tsx
+++ web/components/profile/navbar.tsx
@@ -5,37 +5,13 @@ import Link from "next/link";
 
 // components
 import { ProfileIssuesFilter } from "components/profile";
+// constants
+import { PROFILE_ADMINS_TAB, PROFILE_VIEWER_TAB } from "constants/profile";
 
 type Props = {
   isAuthorized: boolean;
   showProfileIssuesFilter?: boolean;
 };
-
-const viewerTabs = [
-  {
-    route: "",
-    label: "Summary",
-    selected: "/[workspaceSlug]/profile/[userId]",
-  },
-];
-
-const adminTabs = [
-  {
-    route: "assigned",
-    label: "Assigned",
-    selected: "/[workspaceSlug]/profile/[userId]/assigned",
-  },
-  {
-    route: "created",
-    label: "Created",
-    selected: "/[workspaceSlug]/profile/[userId]/created",
-  },
-  {
-    route: "subscribed",
-    label: "Subscribed",
-    selected: "/[workspaceSlug]/profile/[userId]/subscribed",
-  },
-];
 
 export const ProfileNavbar: React.FC<Props> = (props) => {
   const { isAuthorized, showProfileIssuesFilter } = props;
@@ -43,7 +19,7 @@ export const ProfileNavbar: React.FC<Props> = (props) => {
   const router = useRouter();
   const { workspaceSlug, userId } = router.query;
 
-  const tabsList = isAuthorized ? [...viewerTabs, ...adminTabs] : viewerTabs;
+  const tabsList = isAuthorized ? [...PROFILE_VIEWER_TAB, ...PROFILE_ADMINS_TAB] : PROFILE_VIEWER_TAB;
 
   return (
     <div className="sticky -top-0.5 z-10 flex items-center justify-between gap-4 border-b border-custom-border-300 bg-custom-background-100 px-4 sm:px-5 md:static">
