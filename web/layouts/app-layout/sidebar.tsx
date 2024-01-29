d0ed15b7e3dfcb36633f9ea3885ca5ca6bedb006
Ramesh Kumar Chandra <rameshkumar2299@gmail.com>
2024-01-29 16:33:41 +0530

    chore: typo changes

--- web/layouts/app-layout/sidebar.tsx
+++ web/layouts/app-layout/sidebar.tsx
@@ -19,10 +19,10 @@ export const AppSidebar: FC<IAppSidebar> = observer(() => {
 
   return (
     <div
-      className={`fixed inset-y-0 z-20 flex h-full flex-shrink-0 flex-grow-0 flex-col border-r border-custom-sidebar-border-200 bg-custom-sidebar-background-100 duration-300 md:relative ${
-        themStore?.sidebarCollapsed ? "" : "md:w-[280px]"
-      } ${themStore?.sidebarCollapsed ? "left-0" : "-left-full md:left-0"}`}
-    >
+      className={`inset-y-0 z-20 flex h-full flex-shrink-0 flex-grow-0 flex-col border-r border-custom-sidebar-border-200 bg-custom-sidebar-background-100 duration-300
+        fixed md:relative 
+        -ml-[280px] ${themStore.sidebarCollapsed ? "w-[80px]" : "w-[280px]"} md:ml-[0px] 
+      `}    >
       <div className="flex h-full w-full flex-1 flex-col">
         <WorkspaceSidebarDropdown />
         <WorkspaceSidebarQuickAction />
