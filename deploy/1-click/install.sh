49fd73ca88d941f25586477626aaa022b542884a
Manish Gupta <manish@mgupta.me>
2024-02-06 09:48:40 +0530

    sudo fixes

--- deploy/1-click/install.sh
+++ deploy/1-click/install.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
 # Check if the user has sudo access
-if [ "$(id -u)" -eq 0 ]; then
-    SUDO=""
-else
-    SUDO="sudo"
-fi
-
-
 if command -v curl &> /dev/null; then
-    $SUDO curl -sSL \
+    curl -sSL \
         -o /usr/local/bin/plane-app \
         https://raw.githubusercontent.com/makeplane/plane/${BRANCH:-master}/deploy/1-click/plane-app?token=$(date +%s)
 else 
-    $SUDO wget -q \
+    wget -q \
         -O /usr/local/bin/plane-app \
         https://raw.githubusercontent.com/makeplane/plane/${BRANCH:-master}/deploy/1-click/plane-app?token=$(date +%s)
 fi
 
-$SUDO chmod +x /usr/local/bin/plane-app
-$SUDO sed -i 's/export DEPLOY_BRANCH=${BRANCH:-master}/export DEPLOY_BRANCH='${BRANCH:-master}'/' /usr/local/bin/plane-app
+chmod +x /usr/local/bin/plane-app
+sed -i 's/export DEPLOY_BRANCH=${BRANCH:-master}/export DEPLOY_BRANCH='${BRANCH:-master}'/' /usr/local/bin/plane-app
 
-$SUDO plane-app --help
+plane-app --help
