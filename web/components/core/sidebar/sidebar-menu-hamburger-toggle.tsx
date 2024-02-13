4f3980f8bf7bf4e1ed18d3c8ef5a7524697d8aff
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2024-02-13 12:34:55 +0530

    chore: app sidebar state fix

--- web/components/core/sidebar/sidebar-menu-hamburger-toggle.tsx
+++ web/components/core/sidebar/sidebar-menu-hamburger-toggle.tsx
@@ -8,7 +8,7 @@ export const SidebarHamburgerToggle: FC = observer(() => {
   return (
     <div
       className="w-7 h-7 flex-shrink-0 rounded flex justify-center items-center bg-custom-background-80 transition-all hover:bg-custom-background-90 cursor-pointer group md:hidden"
-      onClick={() => themStore.toggleSidebar()}
+      onClick={() => themStore.toggleMobileSidebar()}
     >
       <Menu size={14} className="text-custom-text-200 group-hover:text-custom-text-100 transition-all" />
     </div>
