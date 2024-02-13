c73929fe6f8a2b7e7e3742c3f19cff7b78ac41a4
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2024-02-13 05:36:23 +0530

    chore: implemented layout loader

--- web/components/profile/profile-issues.tsx
+++ web/components/profile/profile-issues.tsx
@@ -7,7 +7,7 @@ import { useTheme } from "next-themes";
 import { ProfileIssuesListLayout } from "components/issues/issue-layouts/list/roots/profile-issues-root";
 import { ProfileIssuesKanBanLayout } from "components/issues/issue-layouts/kanban/roots/profile-issues-root";
 import { IssuePeekOverview, ProfileIssuesAppliedFiltersRoot } from "components/issues";
-import { Spinner } from "@plane/ui";
+import { KanbanLayoutLoader, ListLayoutLoader } from "components/ui";
 import { EmptyState, getEmptyStateImagePath } from "components/empty-state";
 // hooks
 import { useIssues, useUser } from "hooks/store";
@@ -57,38 +57,33 @@ export const ProfileIssuesPage = observer((props: IProfileIssuesPage) => {
 
   const isEditingAllowed = !!currentWorkspaceRole && currentWorkspaceRole >= EUserWorkspaceRoles.MEMBER;
 
+  if (!groupedIssueIds || loader === "init-loader")
+    return <>{activeLayout === "list" ? <ListLayoutLoader /> : <KanbanLayoutLoader />}</>;
+
+  if (groupedIssueIds.length === 0) {
+    return (
+      <EmptyState
+        image={emptyStateImage}
+        title={PROFILE_EMPTY_STATE_DETAILS[type].title}
+        description={PROFILE_EMPTY_STATE_DETAILS[type].description}
+        size="sm"
+        disabled={!isEditingAllowed}
+      />
+    );
+  }
+
   return (
     <>
-      {loader === "init-loader" ? (
-        <div className="flex h-full w-full items-center justify-center">
-          <Spinner />
-        </div>
-      ) : (
-        <>
-          {groupedIssueIds ? (
-            <>
-              <ProfileIssuesAppliedFiltersRoot />
-              <div className="-z-1 relative h-full w-full overflow-auto">
-                {activeLayout === "list" ? (
-                  <ProfileIssuesListLayout />
-                ) : activeLayout === "kanban" ? (
-                  <ProfileIssuesKanBanLayout />
-                ) : null}
-              </div>
-              {/* peek overview */}
-              <IssuePeekOverview />
-            </>
-          ) : (
-            <EmptyState
-              image={emptyStateImage}
-              title={PROFILE_EMPTY_STATE_DETAILS[type].title}
-              description={PROFILE_EMPTY_STATE_DETAILS[type].description}
-              size="sm"
-              disabled={!isEditingAllowed}
-            />
-          )}
-        </>
-      )}
+      <ProfileIssuesAppliedFiltersRoot />
+      <div className="-z-1 relative h-full w-full overflow-auto">
+        {activeLayout === "list" ? (
+          <ProfileIssuesListLayout />
+        ) : activeLayout === "kanban" ? (
+          <ProfileIssuesKanBanLayout />
+        ) : null}
+      </div>
+      {/* peek overview */}
+      <IssuePeekOverview />
     </>
   );
 });
