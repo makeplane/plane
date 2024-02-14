17bca8d60938edddd1db267ac39946873903f026
Ramesh Kumar Chandra <rameshkumar2299@gmail.com>
2024-02-14 14:36:05 +0530

    fix: mobile responsive sidebar close on item tap

--- web/components/core/sidebar/sidebar-menu-hamburger-toggle.tsx
+++ web/components/core/sidebar/sidebar-menu-hamburger-toggle.tsx
@@ -3,12 +3,20 @@ import { Menu } from "lucide-react";
 import { useApplication } from "hooks/store";
 import { observer } from "mobx-react";
 
-export const SidebarHamburgerToggle: FC = observer(() => {
-  const { theme: themStore } = useApplication();
+type Props = {
+  onClick?: () => void;
+}
+
+export const SidebarHamburgerToggle: FC<Props> = observer((props) => {
+  const { onClick } = props
+  const { theme: themeStore } = useApplication();
   return (
     <div
       className="w-7 h-7 flex-shrink-0 rounded flex justify-center items-center bg-custom-background-80 transition-all hover:bg-custom-background-90 cursor-pointer group md:hidden"
-      onClick={() => themStore.toggleMobileSidebar()}
+      onClick={() => {
+        if (onClick) onClick()
+        else themeStore.toggleMobileSidebar()
+      }}
     >
       <Menu size={14} className="text-custom-text-200 group-hover:text-custom-text-100 transition-all" />
     </div>
