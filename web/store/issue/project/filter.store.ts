18b5115546f398a766d4e4f13d8dde9baa580109
rahulramesha <71900764+rahulramesha@users.noreply.github.com>
2024-02-23 18:45:48 +0530

    re enable sub issues toggle in spreadsheet layout (#3779)

--- web/store/issue/project/filter.store.ts
+++ web/store/issue/project/filter.store.ts
@@ -89,8 +89,6 @@ export class ProjectIssuesFilter extends IssueFilterHelperStore implements IProj
       filteredParams
     );
 
-    if (userFilters?.displayFilters?.layout === "spreadsheet") filteredRouteParams.sub_issue = false;
-
     return filteredRouteParams;
   }
 
@@ -191,11 +189,6 @@ export class ProjectIssuesFilter extends IssueFilterHelperStore implements IProj
             updatedDisplayFilters.group_by = "state";
           }
 
-          // set sub_issue to false if layout is switched to spreadsheet and sub_issue is true
-          if (_filters.displayFilters.layout === "spreadsheet" && _filters.displayFilters.sub_issue === true) {
-            _filters.displayFilters.sub_issue = false;
-            updatedDisplayFilters.sub_issue = false;
-          }
 
           runInAction(() => {
             Object.keys(updatedDisplayFilters).forEach((_key) => {
