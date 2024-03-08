33dd9c381f3319eb012eb7ede74a08828263f2c0
Palanikannan1437 <73993394+Palanikannan1437@users.noreply.github.com>
2024-03-08 17:52:24 +0530

    feat: rewrite entire logic handling all cases

--- packages/editor/core/src/ui/components/editor-container.tsx
+++ packages/editor/core/src/ui/components/editor-container.tsx
@@ -1,6 +1,5 @@
 import { Editor } from "@tiptap/react";
 import { ReactNode } from "react";
-import { findParentNodeOfType } from "../extensions/table/table/utilities/insert-line-below-table-action";
 
 interface EditorContainerProps {
   editor: Editor | null;
@@ -15,20 +14,32 @@ export const EditorContainer = ({ editor, editorClassNames, hideDragHandle, chil
     onClick={() => {
       if (!editor) return;
       if (!editor.isEditable) return;
-      if (editor.isFocused) return; // if editor is already focused, do nothing
+      if (editor.isFocused) return; // If editor is already focused, do nothing
 
       const { selection } = editor.state;
       const currentNode = selection.$from.node();
 
       editor?.chain().focus("end", { scrollIntoView: false }).run(); // Focus the editor at the end
 
-      if (currentNode.content.size <= 0) return; // if the current node is empty, do nothing
+      if (
+        currentNode.content.size === 0 && // Check if the current node is empty
+        !(
+          editor.isActive("orderedList") ||
+          editor.isActive("bulletList") ||
+          editor.isActive("taskItem") ||
+          editor.isActive("table") ||
+          editor.isActive("blockquote") ||
+          editor.isActive("codeBlock")
+        ) // Check if it's an empty node within an orderedList, bulletList, taskItem, table, quote or code block
+      ) {
+        return;
+      }
 
       // Insert a new paragraph at the end of the document
       const endPosition = editor?.state.doc.content.size;
       editor?.chain().insertContentAt(endPosition, { type: "paragraph" }).run();
 
-      // Optionally, focus the newly added paragraph for immediate editing
+      // Focus the newly added paragraph for immediate editing
       editor
         .chain()
         .setTextSelection(endPosition + 1)
@@ -42,19 +53,3 @@ export const EditorContainer = ({ editor, editorClassNames, hideDragHandle, chil
     {children}
   </div>
 );
-
-function handleNodeInsertionIfLastNodeTable(editor: Editor) {
-  const { selection } = editor.state;
-  const tableNodeResult = findParentNodeOfType(selection, "table");
-  if (!tableNodeResult) return;
-
-  const { node: tableNode, pos: tablePos } = tableNodeResult;
-  const nextNodePos = tablePos + tableNode.nodeSize;
-  const nextNode = editor.state.doc.nodeAt(nextNodePos);
-
-  if (nextNode && nextNode.type.name === "paragraph") {
-    editor.commands.setTextSelection(nextNodePos + 1);
-  } else if (!nextNode) {
-    editor.commands.insertContentAt(nextNodePos, { type: "paragraph" });
-  }
-}
