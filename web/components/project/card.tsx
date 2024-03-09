cc6afcbc38baba3dcd047a0de35a221dc1682381
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2024-03-10 02:29:43 +0530

    style: project card UI updated

--- web/components/project/card.tsx
+++ web/components/project/card.tsx
@@ -2,36 +2,39 @@ import React, { useState } from "react";
 import { observer } from "mobx-react-lite";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { LinkIcon, Lock, Pencil, Star } from "lucide-react";
+import { Globe2, LinkIcon, Lock, Pencil, Star } from "lucide-react";
 // ui
 import { Avatar, AvatarGroup, Button, Tooltip, TOAST_TYPE, setToast, setPromiseToast } from "@plane/ui";
 // components
 import { DeleteProjectModal, JoinProjectModal, ProjectLogo } from "components/project";
 // helpers
-import { copyTextToClipboard } from "helpers/string.helper";
+import { copyUrlToClipboard } from "helpers/string.helper";
+import { cn } from "helpers/common.helper";
+import { renderFormattedDate } from "helpers/date-time.helper";
 // hooks
 import { useProject } from "hooks/store";
 // types
 import type { IProject } from "@plane/types";
-import { EUserProjectRoles } from "constants/project";
 // constants
+import { EUserProjectRoles } from "constants/project";
 
-export type ProjectCardProps = {
+type Props = {
   project: IProject;
 };
 
-export const ProjectCard: React.FC<ProjectCardProps> = observer((props) => {
+export const ProjectCard: React.FC<Props> = observer((props) => {
   const { project } = props;
-  // router
-  const router = useRouter();
-  const { workspaceSlug } = router.query;
   // states
   const [deleteProjectModalOpen, setDeleteProjectModal] = useState(false);
   const [joinProjectModalOpen, setJoinProjectModal] = useState(false);
+  // router
+  const router = useRouter();
+  const { workspaceSlug } = router.query;
   // store hooks
   const { addProjectToFavorites, removeProjectFromFavorites } = useProject();
-
-  project.member_role;
+  // derived values
+  const projectMembersIds = project.members?.map((member) => member.member_id);
+  // auth
   const isOwner = project.member_role === EUserProjectRoles.ADMIN;
   const isMember = project.member_role === EUserProjectRoles.MEMBER;
 
@@ -53,7 +56,7 @@ export const ProjectCard: React.FC<ProjectCardProps> = observer((props) => {
   };
 
   const handleRemoveFromFavorites = () => {
-    if (!workspaceSlug || !project) return;
+    if (!workspaceSlug) return;
 
     const removeFromFavoritePromise = removeProjectFromFavorites(workspaceSlug.toString(), project.id);
     setPromiseToast(removeFromFavoritePromise, {
@@ -69,23 +72,18 @@ export const ProjectCard: React.FC<ProjectCardProps> = observer((props) => {
     });
   };
 
-  const handleCopyText = () => {
-    const originURL = typeof window !== "undefined" && window.location.origin ? window.location.origin : "";
-
-    copyTextToClipboard(`${originURL}/${workspaceSlug}/projects/${project.id}/issues`).then(() => {
+  const handleCopyText = () =>
+    copyUrlToClipboard(`${workspaceSlug}/projects/${project.id}/issues`).then(() =>
       setToast({
         type: TOAST_TYPE.SUCCESS,
         title: "Link Copied!",
         message: "Project link copied to clipboard.",
-      });
-    });
-  };
-
-  const projectMembersIds = project.members?.map((member) => member.member_id);
+      })
+    );
 
   return (
     <>
-      {/* Delete Project Modal  */}
+      {/* Delete Project Modal */}
       <DeleteProjectModal
         project={project}
         isOpen={deleteProjectModalOpen}
@@ -94,14 +92,12 @@ export const ProjectCard: React.FC<ProjectCardProps> = observer((props) => {
       {/* Join Project Modal */}
       {workspaceSlug && (
         <JoinProjectModal
-          workspaceSlug={workspaceSlug?.toString()}
+          workspaceSlug={workspaceSlug.toString()}
           project={project}
           isOpen={joinProjectModalOpen}
           handleClose={() => setJoinProjectModal(false)}
         />
       )}
-
-      {/* Card Information */}
       <div
         onClick={() => {
           if (project.is_member) router.push(`/${workspaceSlug?.toString()}/projects/${project.id}/issues`);
@@ -109,9 +105,7 @@ export const ProjectCard: React.FC<ProjectCardProps> = observer((props) => {
         }}
         className="flex cursor-pointer flex-col rounded border border-custom-border-200 bg-custom-background-100"
       >
-        <div className="relative h-[118px] w-full rounded-t ">
-          <div className="absolute inset-0 z-[1] bg-gradient-to-t from-black/60 to-transparent" />
-
+        <div className="relative h-16 w-full rounded-t">
           <img
             src={
               project.cover_image ??
@@ -121,26 +115,11 @@ export const ProjectCard: React.FC<ProjectCardProps> = observer((props) => {
             className="absolute left-0 top-0 h-full w-full rounded-t object-cover"
           />
 
-          <div className="absolute bottom-4 z-10 flex h-10 w-full items-center justify-between gap-3 px-4">
-            <div className="flex flex-grow items-center gap-2.5 truncate">
-              <div className="flex item-center justify-center h-9 w-9 flex-shrink-0 rounded bg-white/90">
-                <span className="grid place-items-center">
-                  <ProjectLogo logo={project.logo_props} />
-                </span>
-              </div>
-
-              <div className="flex w-full flex-col justify-between gap-0.5 truncate">
-                <h3 className="truncate font-semibold text-white">{project.name}</h3>
-                <span className="flex items-center gap-1.5">
-                  <p className="text-xs font-medium text-white">{project.identifier} </p>
-                  {project.network === 0 && <Lock className="h-2.5 w-2.5 text-white " />}
-                </span>
-              </div>
-            </div>
-
-            <div className="flex h-full flex-shrink-0 items-center gap-2">
+          <div className="absolute bottom-4 z-10 flex h-10 w-full items-center justify-end gap-3 px-4">
+            <div className="h-full flex-shrink-0 flex items-center gap-2">
               <button
-                className="flex h-6 w-6 items-center justify-center rounded bg-white/10"
+                type="button"
+                className="h-6 w-6 flex items-center justify-center rounded bg-white/10"
                 onClick={(e) => {
                   e.stopPropagation();
                   e.preventDefault();
@@ -150,80 +129,98 @@ export const ProjectCard: React.FC<ProjectCardProps> = observer((props) => {
                 <LinkIcon className="h-3 w-3 text-white" />
               </button>
               <button
+                type="button"
                 className="flex h-6 w-6 items-center justify-center rounded bg-white/10"
                 onClick={(e) => {
-                  if (project.is_favorite) {
-                    e.preventDefault();
-                    e.stopPropagation();
-                    handleRemoveFromFavorites();
-                  } else {
-                    e.preventDefault();
-                    e.stopPropagation();
-                    handleAddToFavorites();
-                  }
+                  e.preventDefault();
+                  e.stopPropagation();
+                  if (project.is_favorite) handleRemoveFromFavorites();
+                  else handleAddToFavorites();
                 }}
               >
                 <Star
-                  className={`h-3 w-3 ${project.is_favorite ? "fill-amber-400 text-transparent" : "text-white"} `}
+                  className={cn("h-3 w-3 text-white", {
+                    "fill-amber-400 text-transparent": project.is_favorite,
+                  })}
                 />
               </button>
             </div>
           </div>
+
+          <div className="absolute left-4 -bottom-5 h-10 w-10 flex-shrink-0 grid place-items-center rounded bg-amber-50">
+            <ProjectLogo logo={project.logo_props} />
+          </div>
         </div>
 
-        <div className="flex h-[104px] w-full flex-col justify-between rounded-b p-4">
-          <p className="line-clamp-2 break-words text-sm text-custom-text-300">{project.description}</p>
-          <div className="item-center flex justify-between">
+        <div className="h-44 w-full flex flex-col justify-between rounded-b">
+          <div className="flex flex-col flex-grow gap-3 text-custom-text-200 px-4 pt-10">
+            <div className="flex flex-col gap-1">
+              <h3 className="truncate font-semibold">{project.name}</h3>
+              <span className="flex items-center gap-1 text-custom-text-400 text-sm">
+                <span className="text-xs font-medium mr-1">{project.identifier}</span>
+                {project.network === 0 ? (
+                  <>
+                    <Lock className="h-2.5 w-2.5" />
+                    Private
+                  </>
+                ) : (
+                  <>
+                    <Globe2 className="h-2.5 w-2.5" />
+                    Public
+                  </>
+                )}
+              </span>
+            </div>
+            <p className="line-clamp-2 break-words text-sm text-custom-text-300">
+              {project.description && project.description.trim() !== ""
+                ? project.description
+                : `Created on ${renderFormattedDate(project.created_at)}`}
+            </p>
+          </div>
+          <div className="flex px-4 py-2.5 items-center justify-between border-t-[0.5px] border-custom-border-200">
             <Tooltip
               tooltipHeading="Members"
               tooltipContent={
-                project.members && project.members.length > 0 ? `${project.members.length} Members` : "No Member"
+                project.members && project.members.length > 0 ? `${project.members.length} members` : "No member"
               }
               position="top"
             >
               {projectMembersIds && projectMembersIds.length > 0 ? (
-                <div className="flex cursor-pointer items-center gap-2 text-custom-text-200">
+                <div>
                   <AvatarGroup showTooltip={false}>
                     {projectMembersIds.map((memberId) => {
                       const member = project.members?.find((m) => m.member_id === memberId);
-
                       if (!member) return null;
-
                       return <Avatar key={member.id} name={member.member__display_name} src={member.member__avatar} />;
                     })}
                   </AvatarGroup>
                 </div>
               ) : (
-                <span className="text-sm italic text-custom-text-400">No Member Yet</span>
+                <span className="text-sm italic text-custom-text-400">No member yet</span>
               )}
             </Tooltip>
-            {(isOwner || isMember) && (
+            {project.is_member && (isOwner || isMember) && (
               <Link
-                className="flex items-center justify-center rounded p-1 text-custom-text-400 hover:bg-custom-background-80 hover:text-custom-text-200"
-                onClick={(e) => {
-                  e.stopPropagation();
-                }}
                 href={`/${workspaceSlug}/projects/${project.id}/settings`}
+                className="flex items-center justify-center rounded p-1 text-custom-text-400 hover:bg-custom-background-80 hover:text-custom-text-200"
+                onClick={(e) => e.stopPropagation()}
               >
                 <Pencil className="h-3.5 w-3.5" />
               </Link>
             )}
-
-            {!project.is_member ? (
-              <div className="flex items-center">
-                <Button
-                  variant="link-primary"
-                  className="!p-0 font-semibold"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    e.stopPropagation();
-                    setJoinProjectModal(true);
-                  }}
-                >
-                  Join
-                </Button>
-              </div>
-            ) : null}
+            {!project.is_member && (
+              <Button
+                variant="link-primary"
+                className="!p-0 font-semibold"
+                onClick={(e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                  setJoinProjectModal(true);
+                }}
+              >
+                Join
+              </Button>
+            )}
           </div>
         </div>
       </div>
