535731141ff30c7a521fc86171e6d981e18101cf
Aaryan Khandelwal <65252264+aaryan610@users.noreply.github.com>
2024-03-11 21:00:05 +0530

    [WEB-682] feat: cycles list filtering and searching (#3910)
    * chore: implemented cycles list filters and ordering
    
    * chore: active cycle tab updated
    
    * refactor: cycles folder structure
    
    * fix: name search inout auto-focus
    
    * fix: cycles ordering
    
    * refactor: move cycle filters logic to mobx store from local storage
    
    * chore: show completed cycles in a disclosure
    
    * chore: added completed cycles count
    
    * refactor: cycles mapping logic
    
    ---------
    
    Co-authored-by: sriram veeraghanta <veeraghanta.sriram@gmail.com>

--- web/pages/[workspaceSlug]/projects/[projectId]/cycles/index.tsx
+++ web/pages/[workspaceSlug]/projects/[projectId]/cycles/index.tsx
@@ -1,28 +1,35 @@
-import { Fragment, useCallback, useState, ReactElement } from "react";
+import { Fragment, useState, ReactElement } from "react";
 import { observer } from "mobx-react-lite";
 import { useRouter } from "next/router";
 import { Tab } from "@headlessui/react";
 // hooks
-import { useEventTracker, useCycle, useProject } from "hooks/store";
-import useLocalStorage from "hooks/use-local-storage";
+import { useEventTracker, useCycle, useProject, useCycleFilter } from "hooks/store";
 // layouts
 import { AppLayout } from "layouts/app-layout";
 // components
 import { PageHead } from "components/core";
 import { CyclesHeader } from "components/headers";
-import { CyclesView, ActiveCycleDetails, CycleCreateUpdateModal } from "components/cycles";
+import {
+  CyclesView,
+  CycleCreateUpdateModal,
+  CyclesViewHeader,
+  CycleAppliedFiltersList,
+  ActiveCycleRoot,
+} from "components/cycles";
 import { EmptyState } from "components/empty-state";
-import { CycleModuleBoardLayout, CycleModuleListLayout, GanttLayoutLoader } from "components/ui";
 // ui
-import { Tooltip } from "@plane/ui";
+import { CycleModuleBoardLayout, CycleModuleListLayout, GanttLayoutLoader } from "components/ui";
+// helpers
+import { calculateTotalFilters } from "helpers/filter.helper";
 // types
 import { NextPageWithLayout } from "lib/types";
-import { TCycleView, TCycleLayout } from "@plane/types";
+import { TCycleFilters } from "@plane/types";
 // constants
-import { CYCLE_TAB_LIST, CYCLE_VIEW_LAYOUTS } from "constants/cycle";
+import { CYCLE_TABS_LIST } from "constants/cycle";
 import { EmptyStateType } from "constants/empty-state";
 
 const ProjectCyclesPage: NextPageWithLayout = observer(() => {
+  // states
   const [createModal, setCreateModal] = useState(false);
   // store hooks
   const { setTrackElement } = useEventTracker();
@@ -31,28 +38,26 @@ const ProjectCyclesPage: NextPageWithLayout = observer(() => {
   // router
   const router = useRouter();
   const { workspaceSlug, projectId, peekCycle } = router.query;
-  // local storage
-  const { storedValue: cycleTab, setValue: setCycleTab } = useLocalStorage<TCycleView>("cycle_tab", "active");
-  const { storedValue: cycleLayout, setValue: setCycleLayout } = useLocalStorage<TCycleLayout>("cycle_layout", "list");
+  // cycle filters hook
+  const { clearAllFilters, currentProjectDisplayFilters, currentProjectFilters, updateDisplayFilters, updateFilters } =
+    useCycleFilter();
   // derived values
   const totalCycles = currentProjectCycleIds?.length ?? 0;
   const project = projectId ? getProjectById(projectId?.toString()) : undefined;
   const pageTitle = project?.name ? `${project?.name} - Cycles` : undefined;
+  // selected display filters
+  const cycleTab = currentProjectDisplayFilters?.active_tab;
+  const cycleLayout = currentProjectDisplayFilters?.layout;
 
-  const handleCurrentLayout = useCallback(
-    (_layout: TCycleLayout) => {
-      setCycleLayout(_layout);
-    },
-    [setCycleLayout]
-  );
+  const handleRemoveFilter = (key: keyof TCycleFilters, value: string | null) => {
+    if (!projectId) return;
+    let newValues = currentProjectFilters?.[key] ?? [];
 
-  const handleCurrentView = useCallback(
-    (_view: TCycleView) => {
-      setCycleTab(_view);
-      if (_view === "draft") handleCurrentLayout("list");
-    },
-    [handleCurrentLayout, setCycleTab]
-  );
+    if (!value) newValues = [];
+    else newValues = newValues.filter((val) => val !== value);
+
+    updateFilters(projectId.toString(), { [key]: newValues });
+  };
 
   if (!workspaceSlug || !projectId) return null;
 
@@ -89,101 +94,35 @@ const ProjectCyclesPage: NextPageWithLayout = observer(() => {
           <Tab.Group
             as="div"
             className="flex h-full flex-col overflow-hidden"
-            defaultIndex={CYCLE_TAB_LIST.findIndex((i) => i.key == cycleTab)}
-            selectedIndex={CYCLE_TAB_LIST.findIndex((i) => i.key == cycleTab)}
-            onChange={(i) => handleCurrentView(CYCLE_TAB_LIST[i]?.key ?? "active")}
+            defaultIndex={CYCLE_TABS_LIST.findIndex((i) => i.key == cycleTab)}
+            selectedIndex={CYCLE_TABS_LIST.findIndex((i) => i.key == cycleTab)}
+            onChange={(i) => {
+              if (!projectId) return;
+              const tab = CYCLE_TABS_LIST[i];
+              if (!tab) return;
+              updateDisplayFilters(projectId.toString(), {
+                active_tab: tab.key,
+              });
+            }}
           >
-            <div className="flex flex-col items-start justify-between gap-4 border-b border-custom-border-200 px-4 sm:flex-row sm:items-center sm:px-5 sm:pb-0">
-              <Tab.List as="div" className="flex items-center overflow-x-scroll">
-                {CYCLE_TAB_LIST.map((tab) => (
-                  <Tab
-                    key={tab.key}
-                    className={({ selected }) =>
-                      `border-b-2 p-4 text-sm font-medium outline-none ${
-                        selected ? "border-custom-primary-100 text-custom-primary-100" : "border-transparent"
-                      }`
-                    }
-                  >
-                    {tab.name}
-                  </Tab>
-                ))}
-              </Tab.List>
-              <div className="hidden sm:block">
-                {cycleTab !== "active" && (
-                  <div className="flex items-center self-end sm:self-center md:self-center lg:self-center gap-1 rounded bg-custom-background-80 p-1">
-                    {CYCLE_VIEW_LAYOUTS.map((layout) => {
-                      if (layout.key === "gantt" && cycleTab === "draft") return null;
-
-                      return (
-                        <Tooltip key={layout.key} tooltipContent={layout.title}>
-                          <button
-                            type="button"
-                            className={`group grid h-[22px] w-7 place-items-center overflow-hidden rounded transition-all hover:bg-custom-background-100 ${
-                              cycleLayout == layout.key ? "bg-custom-background-100 shadow-custom-shadow-2xs" : ""
-                            }`}
-                            onClick={() => handleCurrentLayout(layout.key as TCycleLayout)}
-                          >
-                            <layout.icon
-                              strokeWidth={2}
-                              className={`h-3.5 w-3.5 ${
-                                cycleLayout == layout.key ? "text-custom-text-100" : "text-custom-text-200"
-                              }`}
-                            />
-                          </button>
-                        </Tooltip>
-                      );
-                    })}
-                  </div>
-                )}
+            <CyclesViewHeader projectId={projectId.toString()} />
+            {calculateTotalFilters(currentProjectFilters ?? {}) !== 0 && (
+              <div className="border-b border-custom-border-200 px-5 py-3">
+                <CycleAppliedFiltersList
+                  appliedFilters={currentProjectFilters ?? {}}
+                  handleClearAllFilters={() => clearAllFilters(projectId.toString())}
+                  handleRemoveFilter={handleRemoveFilter}
+                />
               </div>
-            </div>
-
+            )}
             <Tab.Panels as={Fragment}>
-              <Tab.Panel as="div" className="h-full overflow-y-auto">
-                {cycleTab && cycleLayout && (
-                  <CyclesView
-                    filter="all"
-                    layout={cycleLayout}
-                    workspaceSlug={workspaceSlug.toString()}
-                    projectId={projectId.toString()}
-                    peekCycle={peekCycle?.toString()}
-                  />
-                )}
-              </Tab.Panel>
-
               <Tab.Panel as="div" className="h-full space-y-5 overflow-y-auto p-4 sm:p-5">
-                <ActiveCycleDetails workspaceSlug={workspaceSlug.toString()} projectId={projectId.toString()} />
+                <ActiveCycleRoot workspaceSlug={workspaceSlug.toString()} projectId={projectId.toString()} />
               </Tab.Panel>
-
               <Tab.Panel as="div" className="h-full overflow-y-auto">
                 {cycleTab && cycleLayout && (
                   <CyclesView
-                    filter="upcoming"
-                    layout={cycleLayout as TCycleLayout}
-                    workspaceSlug={workspaceSlug.toString()}
-                    projectId={projectId.toString()}
-                    peekCycle={peekCycle?.toString()}
-                  />
-                )}
-              </Tab.Panel>
-
-              <Tab.Panel as="div" className="h-full overflow-y-auto">
-                {cycleTab && cycleLayout && workspaceSlug && projectId && (
-                  <CyclesView
-                    filter="completed"
-                    layout={cycleLayout as TCycleLayout}
-                    workspaceSlug={workspaceSlug.toString()}
-                    projectId={projectId.toString()}
-                    peekCycle={peekCycle?.toString()}
-                  />
-                )}
-              </Tab.Panel>
-
-              <Tab.Panel as="div" className="h-full overflow-y-auto">
-                {cycleTab && cycleLayout && workspaceSlug && projectId && (
-                  <CyclesView
-                    filter="draft"
-                    layout={cycleLayout as TCycleLayout}
+                    layout={cycleLayout}
                     workspaceSlug={workspaceSlug.toString()}
                     projectId={projectId.toString()}
                     peekCycle={peekCycle?.toString()}
