e098cb1faa6ef68d01bb87ec2011351fc85e9b93
sriram veeraghanta <veeraghanta.sriram@gmail.com>
2024-03-11 12:58:35 +0530

    chore: file formatting

--- packages/editor/core/src/ui/components/editor-content.tsx
+++ packages/editor/core/src/ui/components/editor-content.tsx
@@ -1,5 +1,5 @@
 import { Editor, EditorContent } from "@tiptap/react";
-import { ReactNode } from "react";
+import { FC, ReactNode } from "react";
 import { ImageResizer } from "src/ui/extensions/image/image-resize";
 
 interface EditorContentProps {
@@ -9,21 +9,20 @@ interface EditorContentProps {
   tabIndex?: number;
 }
 
-export const EditorContentWrapper = ({
-  editor,
-  editorContentCustomClassNames = "",
-  tabIndex,
-  children,
-}: EditorContentProps) => (
-  <div
-    className={`contentEditor ${editorContentCustomClassNames}`}
-    tabIndex={tabIndex}
-    onFocus={() => {
-      editor?.chain().focus(undefined, { scrollIntoView: false }).run();
-    }}
-  >
-    <EditorContent editor={editor} />
-    {editor?.isActive("image") && editor?.isEditable && <ImageResizer editor={editor} />}
-    {children}
-  </div>
-);
+export const EditorContentWrapper: FC<EditorContentProps> = (props) => {
+  const { editor, editorContentCustomClassNames = "", tabIndex, children } = props;
+
+  return (
+    <div
+      className={`contentEditor ${editorContentCustomClassNames}`}
+      tabIndex={tabIndex}
+      onFocus={() => {
+        editor?.chain().focus(undefined, { scrollIntoView: false }).run();
+      }}
+    >
+      <EditorContent editor={editor} />
+      {editor?.isActive("image") && editor?.isEditable && <ImageResizer editor={editor} />}
+      {children}
+    </div>
+  );
+};
