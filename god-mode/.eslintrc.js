c1a29fe3d2a8e1db13f7e976348dff04ef67620a
sriram veeraghanta <veeraghanta.sriram@gmail.com>
2024-03-13 12:37:20 +0530

    fix: minor package changes

---
+++ god-mode/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    root: true,
+    extends: ["custom"],
+    parser: "@typescript-eslint/parser",
+    settings: {
+      "import/resolver": {
+        typescript: {},
+        node: {
+          moduleDirectory: ["node_modules", "."],
+        },
+      },
+    },
+}
