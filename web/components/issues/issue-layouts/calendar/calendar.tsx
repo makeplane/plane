d6fe45e64ba77fb3f555eb24d71b53339405d324
LAKHAN BAHETI <lakhanbaheti9@gmail.com>
2024-03-14 10:15:16 +0530

    style: added calendar layout responsiveness

--- web/components/issues/issue-layouts/calendar/calendar.tsx
+++ web/components/issues/issue-layouts/calendar/calendar.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import { observer } from "mobx-react-lite";
 // hooks
+import useSize from "hooks/use-window-size";
 // components
 // ui
 import { Spinner } from "@plane/ui";
-import { CalendarHeader, CalendarWeekDays, CalendarWeekHeader } from "components/issues";
+import {
+  CalendarHeader,
+  CalendarIssueBlock,
+  CalendarQuickAddIssueForm,
+  CalendarWeekDays,
+  CalendarWeekHeader,
+} from "components/issues";
 // types
 import {
   IIssueDisplayFilterOptions,
@@ -15,6 +23,9 @@ import {
   TIssueMap,
 } from "@plane/types";
 import { ICalendarWeek } from "./types";
+// helpers
+import { renderFormattedPayloadDate } from "helpers/date-time.helper";
+import { cn } from "helpers/common.helper";
 // constants
 import { EIssueFilterType, EIssuesStoreType } from "constants/issue";
 import { EUserProjectRoles } from "constants/project";
@@ -24,6 +35,7 @@ import { ICycleIssuesFilter } from "store/issue/cycle";
 import { IModuleIssuesFilter } from "store/issue/module";
 import { IProjectIssuesFilter } from "store/issue/project";
 import { IProjectViewIssuesFilter } from "store/issue/project-views";
+import { MONTHS_LIST } from "constants/calendar";
 
 type Props = {
   issuesFilterStore: IProjectIssuesFilter | IModuleIssuesFilter | ICycleIssuesFilter | IProjectViewIssuesFilter;
@@ -62,6 +74,8 @@ export const CalendarChart: React.FC<Props> = observer((props) => {
     updateFilters,
     readOnly = false,
   } = props;
+  // states
+  const [selectedDate, setSelectedDate] = useState<Date>(new Date());
   // store hooks
   const {
     issues: { viewFlags },
@@ -70,6 +84,7 @@ export const CalendarChart: React.FC<Props> = observer((props) => {
   const {
     membership: { currentProjectRole },
   } = useUser();
+  const [windowWidth] = useSize();
 
   const { enableIssueCreation } = viewFlags || {};
   const isEditingAllowed = !!currentProjectRole && currentProjectRole >= EUserProjectRoles.MEMBER;
@@ -78,18 +93,31 @@ export const CalendarChart: React.FC<Props> = observer((props) => {
 
   const allWeeksOfActiveMonth = issueCalendarView.allWeeksOfActiveMonth;
 
-  if (!calendarPayload)
+  const formattedDatePayload = renderFormattedPayloadDate(selectedDate) ?? undefined;
+
+  if (!calendarPayload || !formattedDatePayload)
     return (
       <div className="grid h-full w-full place-items-center">
         <Spinner />
       </div>
     );
 
+  const issueIdList = groupedIssueIds ? groupedIssueIds[formattedDatePayload] : null;
+
   return (
     <>
       <div className="flex h-full w-full flex-col overflow-hidden">
-        <CalendarHeader issuesFilterStore={issuesFilterStore} updateFilters={updateFilters} />
-        <div className="flex h-full w-full vertical-scrollbar scrollbar-lg flex-col">
+        <CalendarHeader
+          setSelectedDate={setSelectedDate}
+          issuesFilterStore={issuesFilterStore}
+          updateFilters={updateFilters}
+        />
+        <div
+          className={cn("flex md:h-full w-full flex-col", {
+            "vertical-scrollbar scrollbar-lg": windowWidth > 768,
+            "overflow-y-auto": windowWidth <= 768,
+          })}
+        >
           <CalendarWeekHeader isLoading={!issues} showWeekends={showWeekends} />
           <div className="h-full w-full">
             {layout === "month" && (
@@ -97,6 +125,8 @@ export const CalendarChart: React.FC<Props> = observer((props) => {
                 {allWeeksOfActiveMonth &&
                   Object.values(allWeeksOfActiveMonth).map((week: ICalendarWeek, weekIndex) => (
                     <CalendarWeekDays
+                      selectedDate={selectedDate}
+                      setSelectedDate={setSelectedDate}
                       issuesFilterStore={issuesFilterStore}
                       key={weekIndex}
                       week={week}
@@ -115,6 +145,8 @@ export const CalendarChart: React.FC<Props> = observer((props) => {
             )}
             {layout === "week" && (
               <CalendarWeekDays
+                selectedDate={selectedDate}
+                setSelectedDate={setSelectedDate}
                 issuesFilterStore={issuesFilterStore}
                 week={issueCalendarView.allDaysOfActiveWeek}
                 issues={issues}
@@ -129,6 +161,38 @@ export const CalendarChart: React.FC<Props> = observer((props) => {
               />
             )}
           </div>
+          <div className="md:hidden">
+            <p className="p-4 text-xl font-semibold">
+              {`${selectedDate.getDate()} ${
+                MONTHS_LIST[selectedDate.getMonth() + 1].title
+              }, ${selectedDate.getFullYear()}`}
+            </p>
+            {issueIdList &&
+              issueIdList?.length > 0 &&
+              issueIdList?.map((issueId) => {
+                if (!issues?.[issueId]) return null;
+                const issue = issues?.[issueId];
+                return (
+                  <div className="border-b border-custom-border-200 px-4">
+                    <CalendarIssueBlock issue={issue} quickActions={quickActions} />
+                  </div>
+                );
+              })}
+          </div>
+
+          {enableIssueCreation && isEditingAllowed && !readOnly && (
+            <div className="flex-shrink-0 px-2 border-b border-custom-border-200 !h-10 flex items-center md:hidden">
+              <CalendarQuickAddIssueForm
+                formKey="target_date"
+                groupId={formattedDatePayload}
+                prePopulatedData={{
+                  target_date: renderFormattedPayloadDate(selectedDate) ?? undefined,
+                }}
+                quickAddCallback={quickAddCallback}
+                viewId={viewId}
+              />
+            </div>
+          )}
         </div>
       </div>
     </>
