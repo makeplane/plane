2e1c63b0a8b084bc9250fa0742362f5d5b12adf6
Anmol Singh Bhatia <121005188+anmolsinghbhatia@users.noreply.github.com>
2024-03-26 19:54:47 +0530

    [WEB-829] fix: issue embed card (#154)
    * fix: issue embed card
    
    * fix: issue embed card

--- packages/editor/document-editor/src/ui/extensions/widgets/issue-embed-widget/issue-widget-card.tsx
+++ packages/editor/document-editor/src/ui/extensions/widgets/issue-embed-widget/issue-widget-card.tsx
@@ -6,7 +6,7 @@ import { Calendar, AlertTriangle } from "lucide-react";
 
 export const IssueWidgetCard = (props) => {
   const [loading, setLoading] = useState<number>(1);
-  const [issueDetails, setIssueDetails] = useState();
+  const [issueDetails, setIssueDetails] = useState<any>();
 
   useEffect(() => {
     const issue = props.issueEmbedConfig.fetchIssue(props.node.attrs.entity_identifier);
@@ -18,8 +18,10 @@ export const IssueWidgetCard = (props) => {
     }
   }, []);
 
+  if (!issueDetails) return <></>;
+
   const completeIssueEmbedAction = () => {
-    props.issueEmbedConfig.clickAction(issueDetails.id, props.node.attrs.title);
+    props.issueEmbedConfig.clickAction(issueDetails?.id, props.node.attrs.title);
   };
 
   return (
@@ -32,16 +34,16 @@ export const IssueWidgetCard = (props) => {
           } w-full cursor-pointer space-y-2 rounded-md border-[0.5px] border-custom-border-200 p-3 shadow-custom-shadow-2xs`}
         >
           <h5 className="text-xs text-custom-text-300">
-            {issueDetails.project_detail.identifier}-{issueDetails.sequence_id}
+            {issueDetails?.project_detail?.identifier}-{issueDetails?.sequence_id}
           </h5>
-          <h4 className="break-words text-sm font-medium">{issueDetails.name}</h4>
+          <h4 className="break-words text-sm font-medium">{issueDetails?.name}</h4>
           <div className="flex flex-wrap items-center gap-x-3 gap-y-2">
             <div>
-              <PriorityIcon priority={issueDetails.priority} />
+              <PriorityIcon priority={issueDetails?.priority} />
             </div>
             <div>
               <AvatarGroup size="sm">
-                {issueDetails.assignee_details.map((assignee, index) => {
+                {(issueDetails?.assignee_details).map((assignee, index) => {
                   if (assignee) {
                     return (
                       <Avatar key={index} name={assignee?.display_name} src={assignee?.avatar} className={"m-0"} />
@@ -50,10 +52,10 @@ export const IssueWidgetCard = (props) => {
                 })}
               </AvatarGroup>
             </div>
-            {issueDetails.target_date && (
+            {issueDetails?.target_date && (
               <div className="flex h-5 items-center gap-1 rounded border-[0.5px] border-custom-border-300 px-2.5 py-1 text-xs text-custom-text-100">
                 <Calendar className="h-3 w-3" strokeWidth={1.5} />
-                {new Date(issueDetails.target_date).toLocaleDateString()}
+                {new Date(issueDetails?.target_date).toLocaleDateString()}
               </div>
             )}
           </div>
