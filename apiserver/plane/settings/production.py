3c7fcfbed663873a94ec00929792f7764a506634
sriram veeraghanta <veeraghanta.sriram@gmail.com>
2024-04-05 14:55:01 +0530

    fix: production debug fixes

--- apiserver/plane/settings/production.py
+++ apiserver/plane/settings/production.py
@@ -6,7 +6,7 @@ from .common import *  # noqa
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", 0)) == 1
-DEBUG = True
+
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
