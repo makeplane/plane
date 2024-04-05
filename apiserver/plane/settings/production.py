b9ca69353f05f619b257cf9818a705bcc3118bc7
pablohashescobar <nikhilschacko@gmail.com>
2024-04-05 15:26:12 +0530

    dev: add logging configuration

--- apiserver/plane/settings/production.py
+++ apiserver/plane/settings/production.py
@@ -6,7 +6,7 @@ from .common import *  # noqa
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", 0)) == 1
-DEBUG = True
+
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
@@ -25,7 +25,7 @@ LOG_DIR = os.path.join(BASE_DIR, "logs")  # noqa
 if not os.path.exists(LOG_DIR):
     os.makedirs(LOG_DIR)
 
-
+# Logging configuration
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
