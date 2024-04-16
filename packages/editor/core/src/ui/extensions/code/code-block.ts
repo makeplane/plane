bca46ea8eb770e2f8bcc84c3492e7a84519a6a92
Palanikannan1437 <73993394+Palanikannan1437@users.noreply.github.com>
2024-04-16 17:29:21 +0530

    fix: some more error handling

--- packages/editor/core/src/ui/extensions/code/code-block.ts
+++ packages/editor/core/src/ui/extensions/code/code-block.ts
@@ -130,85 +130,100 @@ export const CodeBlock = Node.create<CodeBlockOptions>({
 
       // remove code block when at start of document or code block is empty
       Backspace: () => {
-        const { empty, $anchor } = this.editor.state.selection;
-        const isAtStart = $anchor.pos === 1;
+        try {
+          const { empty, $anchor } = this.editor.state.selection;
+          const isAtStart = $anchor.pos === 1;
 
-        if (!empty || $anchor.parent.type.name !== this.name) {
+          if (!empty || $anchor.parent.type.name !== this.name) {
+            return false;
+          }
+
+          if (isAtStart || !$anchor.parent.textContent.length) {
+            return this.editor.commands.clearNodes();
+          }
+
+          return false;
+        } catch (error) {
+          console.error("Error handling Backspace in code block:", error);
           return false;
         }
-
-        if (isAtStart || !$anchor.parent.textContent.length) {
-          return this.editor.commands.clearNodes();
-        }
-
-        return false;
       },
 
       // exit node on triple enter
       Enter: ({ editor }) => {
-        if (!this.options.exitOnTripleEnter) {
+        try {
+          if (!this.options.exitOnTripleEnter) {
+            return false;
+          }
+
+          const { state } = editor;
+          const { selection } = state;
+          const { $from, empty } = selection;
+
+          if (!empty || $from.parent.type !== this.type) {
+            return false;
+          }
+
+          const isAtEnd = $from.parentOffset === $from.parent.nodeSize - 2;
+          const endsWithDoubleNewline = $from.parent.textContent.endsWith("\n\n");
+
+          if (!isAtEnd || !endsWithDoubleNewline) {
+            return false;
+          }
+
+          return editor
+            .chain()
+            .command(({ tr }) => {
+              tr.delete($from.pos - 2, $from.pos);
+
+              return true;
+            })
+            .exitCode()
+            .run();
+        } catch (error) {
+          console.error("Error handling Enter in code block:", error);
           return false;
         }
-
-        const { state } = editor;
-        const { selection } = state;
-        const { $from, empty } = selection;
-
-        if (!empty || $from.parent.type !== this.type) {
-          return false;
-        }
-
-        const isAtEnd = $from.parentOffset === $from.parent.nodeSize - 2;
-        const endsWithDoubleNewline = $from.parent.textContent.endsWith("\n\n");
-
-        if (!isAtEnd || !endsWithDoubleNewline) {
-          return false;
-        }
-
-        return editor
-          .chain()
-          .command(({ tr }) => {
-            tr.delete($from.pos - 2, $from.pos);
-
-            return true;
-          })
-          .exitCode()
-          .run();
       },
 
       // exit node on arrow down
       ArrowDown: ({ editor }) => {
-        if (!this.options.exitOnArrowDown) {
+        try {
+          if (!this.options.exitOnArrowDown) {
+            return false;
+          }
+
+          const { state } = editor;
+          const { selection, doc } = state;
+          const { $from, empty } = selection;
+
+          if (!empty || $from.parent.type !== this.type) {
+            return false;
+          }
+
+          const isAtEnd = $from.parentOffset === $from.parent.nodeSize - 2;
+
+          if (!isAtEnd) {
+            return false;
+          }
+
+          const after = $from.after();
+
+          if (after === undefined) {
+            return false;
+          }
+
+          const nodeAfter = doc.nodeAt(after);
+
+          if (nodeAfter) {
+            return false;
+          }
+
+          return editor.commands.exitCode();
+        } catch (error) {
+          console.error("Error handling ArrowDown in code block:", error);
           return false;
         }
-
-        const { state } = editor;
-        const { selection, doc } = state;
-        const { $from, empty } = selection;
-
-        if (!empty || $from.parent.type !== this.type) {
-          return false;
-        }
-
-        const isAtEnd = $from.parentOffset === $from.parent.nodeSize - 2;
-
-        if (!isAtEnd) {
-          return false;
-        }
-
-        const after = $from.after();
-
-        if (after === undefined) {
-          return false;
-        }
-
-        const nodeAfter = doc.nodeAt(after);
-
-        if (nodeAfter) {
-          return false;
-        }
-
-        return editor.commands.exitCode();
       },
     };
   },
@@ -237,117 +252,122 @@ export const CodeBlock = Node.create<CodeBlockOptions>({
         key: new PluginKey("codeBlockVSCodeHandlerCustom"),
         props: {
           handlePaste: (view, event) => {
-            if (!event.clipboardData) {
-              return false;
-            }
-
-            if (this.editor.isActive(this.type.name)) {
-              return false;
-            }
-
-            if (this.editor.isActive("code")) {
-              // Check if it's an inline code block
-              event.preventDefault();
-              const text = event.clipboardData.getData("text/plain");
-
-              if (!text) {
-                console.error("Pasted text is empty.");
+            try {
+              if (!event.clipboardData) {
                 return false;
               }
 
-              const { tr } = view.state;
-              const { $from, $to } = tr.selection;
-
-              if ($from.pos > $to.pos) {
-                console.error("Invalid selection range.");
+              if (this.editor.isActive(this.type.name)) {
                 return false;
               }
 
-              const docSize = tr.doc.content.size;
-              if ($from.pos < 0 || $to.pos > docSize) {
-                console.error("Selection range is out of document bounds.");
-                return false;
-              }
+              if (this.editor.isActive("code")) {
+                // Check if it's an inline code block
+                event.preventDefault();
+                const text = event.clipboardData.getData("text/plain");
 
-              // Extend the current selection to replace it with the pasted text
-              // wrapped in an inline code mark
-              const codeMark = view.state.schema.marks.code.create();
-              tr.replaceWith($from.pos, $to.pos, view.state.schema.text(text, [codeMark]));
-              view.dispatch(tr);
-              return true;
-            }
+                if (!text) {
+                  console.error("Pasted text is empty.");
+                  return false;
+                }
 
-            event.preventDefault();
-            const text = event.clipboardData.getData("text/plain");
-            const vscode = event.clipboardData.getData("vscode-editor-data");
-            const vscodeData = vscode ? JSON.parse(vscode) : undefined;
-            const language = vscodeData?.mode;
+                const { tr } = view.state;
+                const { $from, $to } = tr.selection;
 
-            if (vscodeData && language) {
-              const { tr } = view.state;
-              const { $from } = tr.selection;
+                if ($from.pos > $to.pos) {
+                  console.error("Invalid selection range.");
+                  return false;
+                }
 
-              // Check if the current line is empty
-              const isCurrentLineEmpty = !$from.parent.textContent.trim();
+                const docSize = tr.doc.content.size;
+                if ($from.pos < 0 || $to.pos > docSize) {
+                  console.error("Selection range is out of document bounds.");
+                  return false;
+                }
 
-              let insertPos;
-
-              if (isCurrentLineEmpty) {
-                // If the current line is empty, use the current position
-                insertPos = $from.pos - 1;
-              } else {
-                // If the current line is not empty, insert below the current block node
-                insertPos = $from.end($from.depth) + 1;
-              }
-
-              // Ensure insertPos is within document bounds
-              if (insertPos < 0 || insertPos > tr.doc.content.size) {
-                console.error("Invalid insert position.");
-                return false;
-              }
-
-              // Create a new code block node with the pasted content
-              const textNode = view.state.schema.text(text.replace(/\r\n?/g, "\n"));
-              const codeBlock = this.type.create({ language }, textNode);
-              if (insertPos <= tr.doc.content.size) {
-                tr.insert(insertPos, codeBlock);
+                // Extend the current selection to replace it with the pasted text
+                // wrapped in an inline code mark
+                const codeMark = view.state.schema.marks.code.create();
+                tr.replaceWith($from.pos, $to.pos, view.state.schema.text(text, [codeMark]));
                 view.dispatch(tr);
                 return true;
               }
 
-              return false;
-            } else {
-              // // complicated paste logic, to be handled later
-              // const containsNewline = text.includes("\n");
-              // const isCurrentLineEmptyNode = isCurrentLineEmpty(view);
-              // if (!containsNewline) {
-              //   console.log("run contians n");
-              //   return false;
-              // }
-              //
-              // // Wrap the text in a div
-              // const html = `<div>${text.replace(/\r?\n/g, "<br>")}</div>`;
-              // let insertPos = view.state.selection.from;
-              //
-              // // Parse the HTML string to a ProseMirror document fragment
-              // const div = document.createElement("div");
-              // div.innerHTML = html;
-              // const domNode = div.firstChild;
-              // if (!domNode) {
-              //   return false;
-              // }
-              // const fragment = DOMParser.fromSchema(view.state.schema).parse(domNode);
-              //
-              // if (isCurrentLineEmptyNode) {
-              //   // If the current line is empty, use the current position
-              //   insertPos = view.state.selection.from - 1;
-              // }
-              //
-              // // Insert the fragment into the document
-              // const transaction = view.state.tr.insert(insertPos, fragment);
-              // view.dispatch(transaction);
-              //
-              // return true;
+              event.preventDefault();
+              const text = event.clipboardData.getData("text/plain");
+              const vscode = event.clipboardData.getData("vscode-editor-data");
+              const vscodeData = vscode ? JSON.parse(vscode) : undefined;
+              const language = vscodeData?.mode;
+
+              if (vscodeData && language) {
+                const { tr } = view.state;
+                const { $from } = tr.selection;
+
+                // Check if the current line is empty
+                const isCurrentLineEmpty = !$from.parent.textContent.trim();
+
+                let insertPos;
+
+                if (isCurrentLineEmpty) {
+                  // If the current line is empty, use the current position
+                  insertPos = $from.pos - 1;
+                } else {
+                  // If the current line is not empty, insert below the current block node
+                  insertPos = $from.end($from.depth) + 1;
+                }
+
+                // Ensure insertPos is within document bounds
+                if (insertPos < 0 || insertPos > tr.doc.content.size) {
+                  console.error("Invalid insert position.");
+                  return false;
+                }
+
+                // Create a new code block node with the pasted content
+                const textNode = view.state.schema.text(text.replace(/\r\n?/g, "\n"));
+                const codeBlock = this.type.create({ language }, textNode);
+                if (insertPos <= tr.doc.content.size) {
+                  tr.insert(insertPos, codeBlock);
+                  view.dispatch(tr);
+                  return true;
+                }
+
+                return false;
+              } else {
+                // // complicated paste logic, to be handled later
+                // const containsNewline = text.includes("\n");
+                // const isCurrentLineEmptyNode = isCurrentLineEmpty(view);
+                // if (!containsNewline) {
+                //   console.log("run contians n");
+                //   return false;
+                // }
+                //
+                // // Wrap the text in a div
+                // const html = `<div>${text.replace(/\r?\n/g, "<br>")}</div>`;
+                // let insertPos = view.state.selection.from;
+                //
+                // // Parse the HTML string to a ProseMirror document fragment
+                // const div = document.createElement("div");
+                // div.innerHTML = html;
+                // const domNode = div.firstChild;
+                // if (!domNode) {
+                //   return false;
+                // }
+                // const fragment = DOMParser.fromSchema(view.state.schema).parse(domNode);
+                //
+                // if (isCurrentLineEmptyNode) {
+                //   // If the current line is empty, use the current position
+                //   insertPos = view.state.selection.from - 1;
+                // }
+                //
+                // // Insert the fragment into the document
+                // const transaction = view.state.tr.insert(insertPos, fragment);
+                // view.dispatch(transaction);
+                //
+                // return true;
+                return false;
+              }
+            } catch (error) {
+              console.error("Error handling paste in CodeBlock extension:", error);
               return false;
             }
           },
