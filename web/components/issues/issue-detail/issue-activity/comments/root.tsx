3c384534f61c0b3ada7bae076b64dc8053b35074
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2024-04-16 12:41:38 +0530

    chore: issue comment empty state added

--- web/components/issues/issue-detail/issue-activity/comments/root.tsx
+++ web/components/issues/issue-detail/issue-activity/comments/root.tsx
@@ -1,5 +1,9 @@
 import { FC } from "react";
 import { observer } from "mobx-react-lite";
+// components
+import { EmptyState } from "@/components/empty-state";
+// hooks
+import { EmptyStateType } from "@/constants/empty-state";
 // hooks
 import { useIssueDetail } from "@/hooks/store";
 // components
@@ -28,18 +32,24 @@ export const IssueCommentRoot: FC<TIssueCommentRoot> = observer((props) => {
 
   return (
     <div>
-      {commentIds.map((commentId, index) => (
-        <IssueCommentCard
-          projectId={projectId}
-          key={commentId}
-          workspaceSlug={workspaceSlug}
-          commentId={commentId}
-          ends={index === 0 ? "top" : index === commentIds.length - 1 ? "bottom" : undefined}
-          activityOperations={activityOperations}
-          showAccessSpecifier={showAccessSpecifier}
-          disabled={disabled}
-        />
-      ))}
+      {commentIds.length > 0 ? (
+        commentIds.map((commentId, index) => (
+          <IssueCommentCard
+            projectId={projectId}
+            key={commentId}
+            workspaceSlug={workspaceSlug}
+            commentId={commentId}
+            ends={index === 0 ? "top" : index === commentIds.length - 1 ? "bottom" : undefined}
+            activityOperations={activityOperations}
+            showAccessSpecifier={showAccessSpecifier}
+            disabled={disabled}
+          />
+        ))
+      ) : (
+        <div className="flex items-center justify-center py-9">
+          <EmptyState type={EmptyStateType.ISSUE_COMMENT_EMPTY_STATE} layout="screen-simple" />
+        </div>
+      )}
     </div>
   );
 });
