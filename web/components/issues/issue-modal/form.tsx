a9a3e52e540650d8c3923ae8e5babb4571d052aa
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2024-04-23 12:27:37 +0530

    fix: preserve initial value on create more issues

--- web/components/issues/issue-modal/form.tsx
+++ web/components/issues/issue-modal/form.tsx
@@ -190,6 +190,7 @@ export const IssueFormRoot: FC<IssueFormProps> = observer((props) => {
 
     reset({
       ...defaultValues,
+      ...(isCreateMoreToggleEnabled ? { ...data } : {}),
       project_id: getValues("project_id"),
       description_html: data?.description_html ?? "<p></p>",
     });
