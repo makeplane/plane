b85d4e0bf91a4d9acd01b59b4de94fc0a89cc9c9
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2024-04-26 18:09:39 +0530

    fix: incomplete cycle issues auth

--- web/components/issues/issue-layouts/spreadsheet/roots/cycle-root.tsx
+++ web/components/issues/issue-layouts/spreadsheet/roots/cycle-root.tsx
@@ -1,22 +1,33 @@
 import React, { useCallback } from "react";
 import { observer } from "mobx-react-lite";
 import { useRouter } from "next/router";
-// mobx store
+// constants
 import { EIssuesStoreType } from "@/constants/issue";
-import { useCycle } from "@/hooks/store";
+import { EUserProjectRoles } from "@/constants/project";
+// hooks
+import { useCycle, useUser } from "@/hooks/store";
 // components
 import { CycleIssueQuickActions } from "../../quick-action-dropdowns";
 import { BaseSpreadsheetRoot } from "../base-spreadsheet-root";
 
 export const CycleSpreadsheetLayout: React.FC = observer(() => {
+  // router
   const router = useRouter();
   const { cycleId } = router.query;
+  // store hooks
   const { currentProjectCompletedCycleIds } = useCycle();
-
+  const {
+    membership: { currentProjectRole },
+  } = useUser();
+  // auth
   const isCompletedCycle =
     cycleId && currentProjectCompletedCycleIds ? currentProjectCompletedCycleIds.includes(cycleId.toString()) : false;
+  const isEditingAllowed = !!currentProjectRole && currentProjectRole >= EUserProjectRoles.MEMBER;
 
-  const canEditIssueProperties = useCallback(() => !isCompletedCycle, [isCompletedCycle]);
+  const canEditIssueProperties = useCallback(
+    () => !isCompletedCycle && isEditingAllowed,
+    [isCompletedCycle, isEditingAllowed]
+  );
 
   if (!cycleId) return null;
 
