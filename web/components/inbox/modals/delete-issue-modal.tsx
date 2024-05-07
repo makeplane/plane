20e7dc68e6534935a90fa974c4aed6567b5163a0
Aaryan Khandelwal <65252264+aaryan610@users.noreply.github.com>
2024-05-07 12:44:36 +0530

    [WEB-1127] style: create and delete modals' consistency (#4345)
    * style: update modals typography, alignment
    
    * style: made the modal separator full width
    
    * style: delete modals consistency
    
    * style: update the remaining delete modals
    
    * chore: delete modal secondary button text
    
    * style: update the remaining create modals
    
    * chore: update cancel button text
    
    * chore: created modal core
    
    * style: modals responsiveness

--- web/components/inbox/modals/delete-issue-modal.tsx
+++ web/components/inbox/modals/delete-issue-modal.tsx
@@ -1,14 +1,11 @@
 import React, { useState } from "react";
 import { observer } from "mobx-react";
-import { AlertTriangle } from "lucide-react";
-import { Dialog, Transition } from "@headlessui/react";
-// hooks
-// icons
-import type { TIssue } from "@plane/types";
-// ui
-import { Button } from "@plane/ui";
-import { useProject } from "@/hooks/store";
 // types
+import type { TIssue } from "@plane/types";
+// components
+import { AlertModalCore } from "@/components/core";
+// hooks
+import { useProject } from "@/hooks/store";
 
 type Props = {
   data: Partial<TIssue>;
@@ -20,79 +17,37 @@ type Props = {
 export const DeleteInboxIssueModal: React.FC<Props> = observer(({ isOpen, onClose, onSubmit, data }) => {
   // states
   const [isDeleting, setIsDeleting] = useState(false);
-
+  // store hooks
   const { getProjectById } = useProject();
+  // derived values
+  const projectDetails = data.project_id ? getProjectById(data?.project_id) : undefined;
 
   const handleClose = () => {
     setIsDeleting(false);
     onClose();
   };
 
-  const handleDelete = () => {
+  const handleDelete = async () => {
     setIsDeleting(true);
-    onSubmit().finally(() => handleClose());
+    await onSubmit().finally(() => handleClose());
   };
 
   return (
-    <Transition.Root show={isOpen} as={React.Fragment}>
-      <Dialog as="div" className="relative z-20" onClose={handleClose}>
-        <Transition.Child
-          as={React.Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-custom-backdrop transition-opacity" />
-        </Transition.Child>
-
-        <div className="fixed inset-0 z-10 overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <Transition.Child
-              as={React.Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-custom-background-100 text-left shadow-custom-shadow-md transition-all sm:my-8 sm:w-full sm:max-w-2xl">
-                <div className="flex flex-col gap-6 p-6">
-                  <div className="flex w-full items-center justify-start gap-6">
-                    <span className="place-items-center rounded-full bg-red-500/20 p-4">
-                      <AlertTriangle className="h-6 w-6 text-red-600" aria-hidden="true" />
-                    </span>
-                    <span className="flex items-center justify-start">
-                      <h3 className="text-xl font-medium 2xl:text-2xl">Delete Issue</h3>
-                    </span>
-                  </div>
-                  <span>
-                    <p className="text-sm text-custom-text-200">
-                      Are you sure you want to delete issue{" "}
-                      <span className="break-words font-medium text-custom-text-100">
-                        {(data && data?.project_id && getProjectById(data?.project_id)?.identifier) || ""}-
-                        {data?.sequence_id}
-                      </span>
-                      {""}? The issue will only be deleted from the inbox and this action cannot be undone.
-                    </p>
-                  </span>
-                  <div className="flex justify-end gap-2">
-                    <Button variant="neutral-primary" size="sm" onClick={handleClose}>
-                      Cancel
-                    </Button>
-                    <Button variant="danger" size="sm" tabIndex={1} onClick={handleDelete} loading={isDeleting}>
-                      {isDeleting ? "Deleting..." : "Delete Issue"}
-                    </Button>
-                  </div>
-                </div>
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
-        </div>
-      </Dialog>
-    </Transition.Root>
+    <AlertModalCore
+      handleClose={handleClose}
+      handleSubmit={handleDelete}
+      isDeleting={isDeleting}
+      isOpen={isOpen}
+      title="Delete Issue"
+      content={
+        <>
+          Are you sure you want to delete issue{" "}
+          <span className="break-words font-medium text-custom-text-100">
+            {projectDetails?.identifier}-{data?.sequence_id}
+          </span>
+          {""}? The issue will only be deleted from the inbox and this action cannot be undone.
+        </>
+      }
+    />
   );
 });
