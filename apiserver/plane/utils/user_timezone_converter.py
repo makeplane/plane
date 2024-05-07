335aff91fec69fa8cb376236cb84f72d71433aab
NarayanBavisetti <narayan3119@gmail.com>
2024-05-07 17:21:09 +0530

    chore: created by key changed

--- apiserver/plane/utils/user_timezone_converter.py
+++ apiserver/plane/utils/user_timezone_converter.py
@@ -8,14 +8,14 @@ def user_timezone_converter(queryset, datetime_fields, user_timezone):
     if isinstance(queryset, dict):
         queryset_values = [queryset]
     else:
-        queryset_values = list(queryset.values())
+        queryset_values = list(queryset)
 
     # Iterate over the dictionaries in the list
     for item in queryset_values:
         # Iterate over the datetime fields
         for field in datetime_fields:
             # Convert the datetime field to the user's timezone
-            if item[field]:
+            if field in item and item[field]:
                 item[field] = item[field].astimezone(user_tz)
 
     # If queryset was a single item, return a single item
