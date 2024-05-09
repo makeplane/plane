692f570258d0c1d20f38ba894c824e95dd0abb25
rahulramesha <71900764+rahulramesha@users.noreply.github.com>
2024-05-09 16:49:24 +0530

    [WEB-1134] fix: module link mutation issue (#4413)
    * fix module link mutation issue
    
    * reference moduleDetails?.link_module inside a prop of Disclosure instead of in a div

--- web/components/modules/sidebar.tsx
+++ web/components/modules/sidebar.tsx
@@ -644,7 +644,8 @@ export const ModuleDetailsSidebar: React.FC<Props> = observer((props) => {
           </div>
 
           <div className="flex w-full flex-col items-center justify-start gap-2 border-t border-custom-border-200 px-1.5 py-5">
-            <Disclosure>
+            {/* Accessing link outside the disclosure as mobx is not  considering the children inside Disclosure as part of the component hence not observing their state change*/}
+            <Disclosure defaultOpen={!!moduleDetails?.link_module?.length}>
               {({ open }) => (
                 <div className={`relative  flex  h-full w-full flex-col ${open ? "" : "flex-row"}`}>
                   <Disclosure.Button className="flex w-full items-center justify-between gap-2 p-1.5">
