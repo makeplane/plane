0ed71787ff6a1fc9af098d35f04c349b4734f834
pablohashescobar <nikhilschacko@gmail.com>
2024-05-10 16:45:21 +0530

    dev: fix app, space, admin redirection in docker setitngs

--- apiserver/plane/authentication/utils/host.py
+++ apiserver/plane/authentication/utils/host.py
@@ -8,13 +8,10 @@ from django.conf import settings
 def base_host(request, is_admin=False, is_space=False, is_app=False):
     """Utility function to return host / origin from the request"""
     # Calculate the base origin from request
-    base_origin = (
+    base_origin = str(
         request.META.get("HTTP_ORIGIN")
         or f"{urlsplit(request.META.get('HTTP_REFERER')).scheme}://{urlsplit(request.META.get('HTTP_REFERER')).netloc}"
         or f"""{"https" if request.is_secure() else "http"}://{request.get_host()}"""
-    )
-    print(
-        settings.ADMIN_BASE_URL, settings.SPACE_BASE_URL, settings.APP_BASE_URL
     )
 
     # Admin redirections
@@ -22,15 +19,16 @@ def base_host(request, is_admin=False, is_space=False, is_app=False):
         if settings.ADMIN_BASE_URL:
             return settings.ADMIN_BASE_URL
         else:
-            return base_origin + "/god-mode"
+            return base_origin + "/god-mode/"
 
     # Space redirections
     if is_space:
         if settings.SPACE_BASE_URL:
             return settings.SPACE_BASE_URL
         else:
-            return base_origin + "/god-mode"
+            return base_origin + "/spaces/"
 
+    # App Redirection
     if is_app:
         if settings.APP_BASE_URL:
             return settings.APP_BASE_URL
