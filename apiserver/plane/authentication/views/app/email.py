2f4cf47630ab61af772caca1de5c967868017d64
pablohashescobar <nikhilschacko@gmail.com>
2024-05-11 19:38:13 +0530

    dev: user account deactivation

--- apiserver/plane/authentication/views/app/email.py
+++ apiserver/plane/authentication/views/app/email.py
@@ -90,11 +90,29 @@ class SignInAuthEndpoint(View):
             )
             return HttpResponseRedirect(url)
 
-        if not User.objects.filter(email=email).exists():
+        existing_user = User.objects.filter(email=email).first()
+
+        if not existing_user:
             exc = AuthenticationException(
                 error_code=AUTHENTICATION_ERROR_CODES["USER_DOES_NOT_EXIST"],
                 error_message="USER_DOES_NOT_EXIST",
                 payload={"email": str(email)},
+            )
+            params = exc.get_error_dict()
+            if next_path:
+                params["next_path"] = str(next_path)
+            url = urljoin(
+                base_host(request=request, is_app=True),
+                "sign-in?" + urlencode(params),
+            )
+            return HttpResponseRedirect(url)
+
+        if not existing_user.is_active:
+            exc = AuthenticationException(
+                error_code=AUTHENTICATION_ERROR_CODES[
+                    "USER_ACCOUNT_DEACTIVATED"
+                ],
+                error_message="USER_ACCOUNT_DEACTIVATED",
             )
             params = exc.get_error_dict()
             if next_path:
@@ -197,7 +215,26 @@ class SignUpAuthEndpoint(View):
             )
             return HttpResponseRedirect(url)
 
-        if User.objects.filter(email=email).exists():
+        existing_user = User.objects.filter(email=email).first()
+
+        if existing_user:
+            # Existing User
+            if not existing_user.is_active:
+                exc = AuthenticationException(
+                    error_code=AUTHENTICATION_ERROR_CODES[
+                        "USER_ACCOUNT_DEACTIVATED"
+                    ],
+                    error_message="USER_ACCOUNT_DEACTIVATED",
+                )
+                params = exc.get_error_dict()
+                if next_path:
+                    params["next_path"] = str(next_path)
+                url = urljoin(
+                    base_host(request=request, is_app=True),
+                    "?" + urlencode(params),
+                )
+                return HttpResponseRedirect(url)
+
             exc = AuthenticationException(
                 error_code=AUTHENTICATION_ERROR_CODES["USER_ALREADY_EXIST"],
                 error_message="USER_ALREADY_EXIST",
