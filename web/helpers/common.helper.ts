2b196ba1f11d0fee097cd128155eea7076576a36
sriram veeraghanta <veeraghanta.sriram@gmail.com>
2024-05-14 22:51:07 +0530

    fix: window workflow build error

--- web/helpers/common.helper.ts
+++ web/helpers/common.helper.ts
@@ -6,7 +6,7 @@ export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "";
 export const ADMIN_BASE_URL = process.env.NEXT_PUBLIC_ADMIN_BASE_URL || "";
 export const ADMIN_BASE_PATH = process.env.NEXT_PUBLIC_ADMIN_BASE_PATH || "";
 
-export const SPACE_BASE_URL = process.env.NEXT_PUBLIC_SPACE_BASE_URL || window.location.origin;
+export const SPACE_BASE_URL = process.env.NEXT_PUBLIC_SPACE_BASE_URL || "";
 export const SPACE_BASE_PATH = process.env.NEXT_PUBLIC_SPACE_BASE_PATH || "";
 
 export const debounce = (func: any, wait: number, immediate: boolean = false) => {
