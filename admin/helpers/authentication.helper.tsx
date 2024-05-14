dbc0d7019b6a8b31eab40aa763618479f2c2aa2e
Nikhil <118773738+pablohashescobar@users.noreply.github.com>
2024-05-14 13:46:05 +0530

    [WEB - 1302]dev: nginx headers and auth error codes.  (#4449)
    * dev: add nginx headers
    
    * fix: handled error messages in admin
    
    ---------
    
    Co-authored-by: guru_sainath <gurusainath007@gmail.com>

--- admin/helpers/authentication.helper.tsx
+++ admin/helpers/authentication.helper.tsx
@@ -1,10 +1,12 @@
 import { ReactNode } from "react";
+import Link from "next/link";
 
 export enum EPageTypes {
-  "PUBLIC" = "PUBLIC",
-  "NON_AUTHENTICATED" = "NON_AUTHENTICATED",
-  "ONBOARDING" = "ONBOARDING",
-  "AUTHENTICATED" = "AUTHENTICATED",
+  PUBLIC = "PUBLIC",
+  NON_AUTHENTICATED = "NON_AUTHENTICATED",
+  SET_PASSWORD = "SET_PASSWORD",
+  ONBOARDING = "ONBOARDING",
+  AUTHENTICATED = "AUTHENTICATED",
 }
 
 export enum EAuthModes {
@@ -18,26 +20,24 @@ export enum EAuthSteps {
   UNIQUE_CODE = "UNIQUE_CODE",
 }
 
-export enum EAuthenticationErrorCodes {
-  INSTANCE_NOT_CONFIGURED = "5000",
-  // Admin
-  ADMIN_ALREADY_EXIST = "5029",
-  REQUIRED_ADMIN_EMAIL_PASSWORD_FIRST_NAME = "5030",
-  INVALID_ADMIN_EMAIL = "5031",
-  INVALID_ADMIN_PASSWORD = "5032",
-  REQUIRED_ADMIN_EMAIL_PASSWORD = "5033",
-  ADMIN_AUTHENTICATION_FAILED = "5034",
-  ADMIN_USER_ALREADY_EXIST = "5035",
-  ADMIN_USER_DOES_NOT_EXIST = "5036",
-}
-
 export enum EErrorAlertType {
   BANNER_ALERT = "BANNER_ALERT",
-  TOAST_ALERT = "TOAST_ALERT",
   INLINE_FIRST_NAME = "INLINE_FIRST_NAME",
   INLINE_EMAIL = "INLINE_EMAIL",
   INLINE_PASSWORD = "INLINE_PASSWORD",
   INLINE_EMAIL_CODE = "INLINE_EMAIL_CODE",
+}
+
+export enum EAuthenticationErrorCodes {
+  // Admin
+  ADMIN_ALREADY_EXIST = "5150",
+  REQUIRED_ADMIN_EMAIL_PASSWORD_FIRST_NAME = "5155",
+  INVALID_ADMIN_EMAIL = "5160",
+  INVALID_ADMIN_PASSWORD = "5165",
+  REQUIRED_ADMIN_EMAIL_PASSWORD = "5170",
+  ADMIN_AUTHENTICATION_FAILED = "5175",
+  ADMIN_USER_ALREADY_EXIST = "5180",
+  ADMIN_USER_DOES_NOT_EXIST = "5185",
 }
 
 export type TAuthErrorInfo = {
@@ -50,41 +50,54 @@ export type TAuthErrorInfo = {
 const errorCodeMessages: {
   [key in EAuthenticationErrorCodes]: { title: string; message: (email?: string | undefined) => ReactNode };
 } = {
-  [EAuthenticationErrorCodes.INSTANCE_NOT_CONFIGURED]: {
-    title: "Instance not configured",
-    message: () => "Please contact your administrator to configure the instance.",
-  },
+  // admin
   [EAuthenticationErrorCodes.ADMIN_ALREADY_EXIST]: {
-    title: "Admin already exists",
-    message: () => "Admin already exists. Please sign in.",
+    title: `Admin already exists`,
+    message: () => `Admin already exists. Please try again.`,
   },
   [EAuthenticationErrorCodes.REQUIRED_ADMIN_EMAIL_PASSWORD_FIRST_NAME]: {
-    title: "Required",
-    message: () => "Please enter email, password and first name.",
+    title: `Email, password and first name required`,
+    message: () => `Email, password and first name required. Please try again.`,
   },
   [EAuthenticationErrorCodes.INVALID_ADMIN_EMAIL]: {
-    title: "Invalid email",
-    message: () => "Please enter a valid email.",
+    title: `Invalid admin email`,
+    message: () => `Invalid admin email. Please try again.`,
   },
   [EAuthenticationErrorCodes.INVALID_ADMIN_PASSWORD]: {
-    title: "Invalid password",
-    message: () => "Password must be at least 8 characters long.",
+    title: `Invalid admin password`,
+    message: () => `Invalid admin password. Please try again.`,
   },
   [EAuthenticationErrorCodes.REQUIRED_ADMIN_EMAIL_PASSWORD]: {
-    title: "Required",
-    message: () => "Please enter email and password.",
+    title: `Email and password required`,
+    message: () => `Email and password required. Please try again.`,
   },
   [EAuthenticationErrorCodes.ADMIN_AUTHENTICATION_FAILED]: {
-    title: "Authentication failed",
-    message: () => "Please check your email and password and try again.",
+    title: `Authentication failed`,
+    message: () => `Authentication failed. Please try again.`,
   },
   [EAuthenticationErrorCodes.ADMIN_USER_ALREADY_EXIST]: {
-    title: "User already exists",
-    message: () => "User already exists. Please sign in.",
+    title: `Admin user already exists`,
+    message: () => (
+      <div>
+        Admin user already exists.&nbsp;
+        <Link className="underline underline-offset-4 font-medium hover:font-bold transition-all" href={`/admin`}>
+          Sign In
+        </Link>
+        &nbsp;now.
+      </div>
+    ),
   },
   [EAuthenticationErrorCodes.ADMIN_USER_DOES_NOT_EXIST]: {
-    title: "User does not exist",
-    message: () => "User does not exist. Please sign up.",
+    title: `Admin user does not exist`,
+    message: () => (
+      <div>
+        Admin user does not exist.&nbsp;
+        <Link className="underline underline-offset-4 font-medium hover:font-bold transition-all" href={`/admin`}>
+          Sign In
+        </Link>
+        &nbsp;now.
+      </div>
+    ),
   },
 };
 
@@ -92,27 +105,15 @@ export const authErrorHandler = (
   errorCode: EAuthenticationErrorCodes,
   email?: string | undefined
 ): TAuthErrorInfo | undefined => {
-  const toastAlertErrorCodes = [
-    EAuthenticationErrorCodes.ADMIN_ALREADY_EXIST,
+  const bannerAlertErrorCodes = [
+    EAuthenticationErrorCodes.REQUIRED_ADMIN_EMAIL_PASSWORD_FIRST_NAME,
     EAuthenticationErrorCodes.INVALID_ADMIN_EMAIL,
     EAuthenticationErrorCodes.INVALID_ADMIN_PASSWORD,
-    EAuthenticationErrorCodes.ADMIN_AUTHENTICATION_FAILED,
-  ];
-  const bannerAlertErrorCodes = [
-    EAuthenticationErrorCodes.INSTANCE_NOT_CONFIGURED,
-    EAuthenticationErrorCodes.REQUIRED_ADMIN_EMAIL_PASSWORD_FIRST_NAME,
     EAuthenticationErrorCodes.REQUIRED_ADMIN_EMAIL_PASSWORD,
+    EAuthenticationErrorCodes.ADMIN_AUTHENTICATION_FAILED,
     EAuthenticationErrorCodes.ADMIN_USER_ALREADY_EXIST,
     EAuthenticationErrorCodes.ADMIN_USER_DOES_NOT_EXIST,
   ];
-
-  if (toastAlertErrorCodes.includes(errorCode))
-    return {
-      type: EErrorAlertType.TOAST_ALERT,
-      code: errorCode,
-      title: errorCodeMessages[errorCode]?.title || "Error",
-      message: errorCodeMessages[errorCode]?.message(email) || "Something went wrong. Please try again.",
-    };
 
   if (bannerAlertErrorCodes.includes(errorCode))
     return {
