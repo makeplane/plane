865350a2df8503489cb109050774187240845c0c
guru_sainath <gurusainath007@gmail.com>
2024-05-15 22:08:42 +0530

    fix: issue peekoverview handled state and query parameters handlers

--- space/components/views/project-details.tsx
+++ space/components/views/project-details.tsx
@@ -55,7 +55,9 @@ export const ProjectDetailsView: FC<ProjectDetailsViewProps> = observer((props) 
 
   return (
     <div className="relative h-full w-full overflow-hidden">
-      {workspaceSlug && <IssuePeekOverview />}
+      {workspaceSlug && projectId && peekId && (
+        <IssuePeekOverview workspaceSlug={workspaceSlug} projectId={projectId} peekId={peekId} />
+      )}
 
       {issueStore?.loader && !issueStore.issues ? (
         <div className="py-10 text-center text-sm text-custom-text-100">Loading...</div>
