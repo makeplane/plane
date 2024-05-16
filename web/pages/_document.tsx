e6142d8247fb7fcbb06c4c864b6be2004e5eb383
sriram veeraghanta <veeraghanta.sriram@gmail.com>
2024-05-16 19:37:22 +0530

    fix: adding new apple touch icon

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -25,7 +25,7 @@ class MyDocument extends Document {
           <meta name="keywords" content={SITE_KEYWORDS} />
           <meta name="twitter:site" content={`@${TWITTER_USER_NAME}`} />
           <meta name="theme-color" content="#fff" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+          <link rel="apple-touch-icon" sizes="512x512" href="/plane-logos/plane-mobile-pwa.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
           <link rel="manifest" href="/site.webmanifest.json" />
