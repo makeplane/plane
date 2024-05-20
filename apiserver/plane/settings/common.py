14afa22f34068ab1c7edf4abbe59b803a006238d
pablohashescobar <nikhilschacko@gmail.com>
2024-05-20 11:03:49 +0530

    dev: add pg port to configuration

--- apiserver/plane/settings/common.py
+++ apiserver/plane/settings/common.py
@@ -151,6 +151,7 @@ else:
             "USER": os.environ.get("POSTGRES_USER"),
             "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
             "HOST": os.environ.get("POSTGRES_HOST"),
+            "PORT": os.environ.get("POSTGRES_PORT", "5432"),
         }
     }
 
