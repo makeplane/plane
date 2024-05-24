724f227842bc004a026cae8bf29f78341ac727ae
Prateek Shourya <prateekshourya29@gmail.com>
2024-05-24 16:59:49 +0530

    [WEB-1404] chore: admin app improvements (#4580)
    * [WEB-1404] fix: redirection to web app and issue with telemetry checkbox in setup form.
    
    * chore: add scrollbar in admin app.
    
    * chore: fix `workspaces_exist` logic in instance api.

--- admin/components/admin-sidebar/help-section.tsx
+++ admin/components/admin-sidebar/help-section.tsx
@@ -38,7 +38,7 @@ export const HelpSection: FC = observer(() => {
   // refs
   const helpOptionsRef = useRef<HTMLDivElement | null>(null);
 
-  const redirectionLink = encodeURI(WEB_BASE_URL + "/create-workspace");
+  const redirectionLink = encodeURI(WEB_BASE_URL + "/");
 
   return (
     <div
