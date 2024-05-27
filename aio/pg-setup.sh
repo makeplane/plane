b9cd2c5eb308412f4f8588ce0a5dd3d1dcfbd1ba
Manish Gupta <manish@mgupta.me>
2024-05-27 17:58:26 +0530

    fix: codacy suggestions implemented

--- aio/pg-setup.sh
+++ aio/pg-setup.sh
@@ -6,9 +6,9 @@ set -o allexport
 source plane.env set
 set +o allexport
 
-PGHOST=localhost
+export PGHOST=localhost
 
-sudo -u postgres /usr/lib/postgresql/${POSTGRES_VERSION}/bin/pg_ctl -D /var/lib/postgresql/data start
-sudo -u postgres /usr/lib/postgresql/${POSTGRES_VERSION}/bin/psql --command "CREATE USER $POSTGRES_USER WITH SUPERUSER PASSWORD '$POSTGRES_PASSWORD';" && \
-sudo -u postgres /usr/lib/postgresql/${POSTGRES_VERSION}/bin/createdb -O $POSTGRES_USER $POSTGRES_DB && \
-sudo -u postgres /usr/lib/postgresql/${POSTGRES_VERSION}/bin/pg_ctl -D /var/lib/postgresql/data stop
+sudo -u postgres "/usr/lib/postgresql/${POSTGRES_VERSION}/bin/pg_ctl" -D /var/lib/postgresql/data start
+sudo -u postgres "/usr/lib/postgresql/${POSTGRES_VERSION}/bin/psql" --command "CREATE USER $POSTGRES_USER WITH SUPERUSER PASSWORD '$POSTGRES_PASSWORD';" && \
+sudo -u postgres "/usr/lib/postgresql/${POSTGRES_VERSION}/bin/createdb" -O $POSTGRES_USER $POSTGRES_DB && \
+sudo -u postgres "/usr/lib/postgresql/${POSTGRES_VERSION}/bin/pg_ctl" -D /var/lib/postgresql/data stop
