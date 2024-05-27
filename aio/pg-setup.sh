68d6918cc75a5942db94e326adfdd18cf5d5d8ef
Manish Gupta <manish@mgupta.me>
2024-05-27 18:04:17 +0530

    fix: codacy fix

--- aio/pg-setup.sh
+++ aio/pg-setup.sh
@@ -10,5 +10,5 @@ export PGHOST=localhost
 
 sudo -u postgres "/usr/lib/postgresql/${POSTGRES_VERSION}/bin/pg_ctl" -D /var/lib/postgresql/data start
 sudo -u postgres "/usr/lib/postgresql/${POSTGRES_VERSION}/bin/psql" --command "CREATE USER $POSTGRES_USER WITH SUPERUSER PASSWORD '$POSTGRES_PASSWORD';" && \
-sudo -u postgres "/usr/lib/postgresql/${POSTGRES_VERSION}/bin/createdb" -O $POSTGRES_USER $POSTGRES_DB && \
+sudo -u postgres "/usr/lib/postgresql/${POSTGRES_VERSION}/bin/createdb" -O "$POSTGRES_USER" "$POSTGRES_DB" && \
 sudo -u postgres "/usr/lib/postgresql/${POSTGRES_VERSION}/bin/pg_ctl" -D /var/lib/postgresql/data stop
