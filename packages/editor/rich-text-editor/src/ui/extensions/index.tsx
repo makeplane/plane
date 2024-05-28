05807fe123ebc63c37be84e3f5758e5550c1932c
Anmol Singh Bhatia <121005188+anmolsinghbhatia@users.noreply.github.com>
2024-05-28 11:48:20 +0530

    [WEB-1067] chore: enter key entension added to editor package and issue modal description improvement (#4617)
    * chore: enter key extension added to RichTextEditorWithRef editor package
    
    * chore: enter to submit functionality added to issue and inbox issue modal description

--- packages/editor/rich-text-editor/src/ui/extensions/index.tsx
+++ packages/editor/rich-text-editor/src/ui/extensions/index.tsx
@@ -1,13 +1,21 @@
 import { UploadImage } from "@plane/editor-core";
 import { DragAndDrop, SlashCommand } from "@plane/editor-extensions";
+import { EnterKeyExtension } from "./enter-key-extension";
 
 type TArguments = {
   uploadFile: UploadImage;
   dragDropEnabled?: boolean;
   setHideDragHandle?: (hideDragHandlerFromDragDrop: () => void) => void;
+  onEnterKeyPress?: () => void;
 };
 
-export const RichTextEditorExtensions = ({ uploadFile, dragDropEnabled, setHideDragHandle }: TArguments) => [
+export const RichTextEditorExtensions = ({
+  uploadFile,
+  dragDropEnabled,
+  setHideDragHandle,
+  onEnterKeyPress,
+}: TArguments) => [
   SlashCommand(uploadFile),
   dragDropEnabled === true && DragAndDrop(setHideDragHandle),
+  EnterKeyExtension(onEnterKeyPress),
 ];
