3e942b98e54790e78fddb96b38138bf419cbd159
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2024-05-29 18:41:20 +0530

    chore: logo component updated

--- web/components/common/logo.tsx
+++ web/components/common/logo.tsx
@@ -1,17 +1,20 @@
 import { FC } from "react";
 // emoji-picker-react
 import { Emoji } from "emoji-picker-react";
+// import { icons } from "lucide-react";
 import { TLogoProps } from "@plane/types";
 // helpers
+import { LUCIDE_ICONS_LIST } from "@plane/ui";
 import { emojiCodeToUnicode } from "@/helpers/emoji.helper";
 
 type Props = {
   logo: TLogoProps;
   size?: number;
+  type?: "lucide" | "material";
 };
 
 export const Logo: FC<Props> = (props) => {
-  const { logo, size = 16 } = props;
+  const { logo, size = 16, type = "material" } = props;
 
   // destructuring the logo object
   const { in_use, emoji, icon } = logo;
@@ -19,6 +22,7 @@ export const Logo: FC<Props> = (props) => {
   // derived values
   const value = in_use === "emoji" ? emoji?.value : icon?.name;
   const color = icon?.color;
+  const lucideIcon = LUCIDE_ICONS_LIST.find((item) => item.name === value);
 
   // if no value, return empty fragment
   if (!value) return <></>;
@@ -31,16 +35,32 @@ export const Logo: FC<Props> = (props) => {
   // icon
   if (in_use === "icon") {
     return (
-      <span
-        className="material-symbols-rounded"
-        style={{
-          fontSize: size,
-          color: color,
-          scale: "115%",
-        }}
-      >
-        {value}
-      </span>
+      <>
+        {type === "lucide" ? (
+          <>
+            {lucideIcon && (
+              <lucideIcon.element
+                style={{
+                  color: color,
+                  height: size,
+                  width: size,
+                }}
+              />
+            )}
+          </>
+        ) : (
+          <span
+            className="material-symbols-rounded"
+            style={{
+              fontSize: size,
+              color: color,
+              scale: "115%",
+            }}
+          >
+            {value}
+          </span>
+        )}
+      </>
     );
   }
 
