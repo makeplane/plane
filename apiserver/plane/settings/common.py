60f0583af1bb57ca07d537e252d9ab9317e90874
pablohashescobar <nikhilschacko@gmail.com>
2024-05-30 11:44:14 +0530

    dev: disconnect web url and app base url configuration.

--- apiserver/plane/settings/common.py
+++ apiserver/plane/settings/common.py
@@ -351,4 +351,4 @@ CSRF_FAILURE_VIEW = "plane.authentication.views.common.csrf_failure"
 # Base URLs
 ADMIN_BASE_URL = os.environ.get("ADMIN_BASE_URL", None)
 SPACE_BASE_URL = os.environ.get("SPACE_BASE_URL", None)
-APP_BASE_URL = os.environ.get("APP_BASE_URL") or os.environ.get("WEB_URL")
+APP_BASE_URL = os.environ.get("APP_BASE_URL")
