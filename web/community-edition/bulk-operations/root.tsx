243c5f8d99dd1c8b729e887c3af8c674f3d6aac8
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2024-05-31 19:42:24 +0530

    style: bulk operations action bar

--- web/community-edition/bulk-operations/root.tsx
+++ web/community-edition/bulk-operations/root.tsx
@@ -1,9 +1,8 @@
 import { observer } from "mobx-react";
-import { Crown } from "lucide-react";
 // ui
 import { getButtonStyling } from "@plane/ui";
 // constants
-import { MARKETING_PRICING_PAGE_LINK } from "@/constants/common";
+import { MARKETING_PLANE_ONE_PAGE_LINK } from "@/constants/common";
 // helpers
 import { cn } from "@/helpers/common.helper";
 // hooks
@@ -21,20 +20,18 @@ export const IssueBulkOperationsRoot: React.FC<Props> = observer((props) => {
   if (!isSelectionActive) return null;
 
   return (
-    <div className={cn("sticky bottom-0 left-0 z-[2] h-14", className)}>
-      <div className="size-full bg-custom-background-100 border-t border-custom-border-200 py-4 px-3.5 flex items-center justify-between gap-2">
-        <p className="text-sm text-custom-text-300">
-          All features within bulk operations are available in our Pro plan and above. Upgrade now to boost
-          productivity.
+    <div className={cn("sticky bottom-0 left-0 h-20 z-[2] px-3.5 grid place-items-center", className)}>
+      <div className="h-14 w-full bg-custom-primary-100/10 border-[0.5px] border-custom-primary-100/50 py-4 px-3.5 flex items-center justify-between gap-2 rounded-md">
+        <p className="text-custom-primary-100 font-medium">
+          Change state, priority, and more for several issues at once. Save three minutes on an average per operation.
         </p>
         <a
-          href={MARKETING_PRICING_PAGE_LINK}
+          href={MARKETING_PLANE_ONE_PAGE_LINK}
           target="_blank"
           rel="noopener noreferrer"
           className={cn(getButtonStyling("primary", "sm"), "flex-shrink-0")}
         >
-          <Crown className="size-3" />
-          Upgrade
+          Upgrade to One
         </a>
       </div>
     </div>
