62174a3fdc2cf0c8afbbae4e69c90444868fe816
pablohashescobar <nikhilschacko@gmail.com>
2024-05-31 12:37:11 +0530

    dev: add trimming for file assets

--- apiserver/plane/db/models/asset.py
+++ apiserver/plane/db/models/asset.py
@@ -13,6 +13,7 @@ from .base import BaseModel
 
 def get_upload_path(instance, filename):
     if instance.workspace_id is not None:
+        filename = filename[:60]
         return f"{instance.workspace.id}/{uuid4().hex}-{filename}"
     return f"user-{uuid4().hex}-{filename}"
 
