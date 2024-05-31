3af2219de2fc2970b8e9e7151edde62bc2e37f4e
Prateek Shourya <prateekshourya29@gmail.com>
2024-05-31 15:10:38 +0530

    [WEB-1325] chore: refactor inbox issue store to avoid data loss. (#4640)
    * [WEB-1325] chore: refactor inbox issue store to avoid data loss.
    
    * chore: inbox store improvement.

--- web/components/inbox/sidebar/inbox-list.tsx
+++ web/components/inbox/sidebar/inbox-list.tsx
@@ -2,30 +2,28 @@ import { FC, Fragment } from "react";
 import { observer } from "mobx-react";
 // components
 import { InboxIssueListItem } from "@/components/inbox";
-// store
-import { IInboxIssueStore } from "@/store/inbox/inbox-issue.store";
 
 export type InboxIssueListProps = {
   workspaceSlug: string;
   projectId: string;
   projectIdentifier?: string;
-  inboxIssues: IInboxIssueStore[];
+  inboxIssueIds: string[];
   setIsMobileSidebar: (value: boolean) => void;
 };
 
 export const InboxIssueList: FC<InboxIssueListProps> = observer((props) => {
-  const { workspaceSlug, projectId, projectIdentifier, inboxIssues, setIsMobileSidebar } = props;
+  const { workspaceSlug, projectId, projectIdentifier, inboxIssueIds, setIsMobileSidebar } = props;
 
   return (
     <>
-      {inboxIssues.map((inboxIssue) => (
-        <Fragment key={inboxIssue.id}>
+      {inboxIssueIds.map((inboxIssueId) => (
+        <Fragment key={inboxIssueId}>
           <InboxIssueListItem
             setIsMobileSidebar={setIsMobileSidebar}
             workspaceSlug={workspaceSlug}
             projectId={projectId}
             projectIdentifier={projectIdentifier}
-            inboxIssue={inboxIssue}
+            inboxIssueId={inboxIssueId}
           />
         </Fragment>
       ))}
