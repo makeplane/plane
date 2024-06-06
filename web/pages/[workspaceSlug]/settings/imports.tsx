b3280401454253156896a12bd9c26aba6704c58d
Prateek Shourya <prateekshourya29@gmail.com>
2024-06-06 15:44:52 +0530

    chore: add importers and integrations empty state for plane one users. (#367)
    * chore: add importers and integrations empty state for plane one users.
    
    * chore: typo fix.

--- web/pages/[workspaceSlug]/settings/imports.tsx
+++ web/pages/[workspaceSlug]/settings/imports.tsx
@@ -1,12 +1,13 @@
 import { observer } from "mobx-react";
 // components
+import { ArrowUpDown } from "lucide-react";
 import { PageHead } from "@/components/core";
 import { WorkspaceSettingHeader } from "@/components/headers";
 import IntegrationGuide from "@/components/integration/guide";
 // constants
 import { EUserWorkspaceRoles } from "@/constants/workspace";
 // hooks
-import { useUser, useWorkspace } from "@/hooks/store";
+import { useInstance, useUser, useWorkspace } from "@/hooks/store";
 // layouts
 import { AppLayout } from "@/layouts/app-layout";
 import { WorkspaceSettingLayout } from "@/layouts/settings-layout";
@@ -15,6 +16,7 @@ import { NextPageWithLayout } from "@/lib/types";
 
 const ImportsPage: NextPageWithLayout = observer(() => {
   // store hooks
+  const { instance } = useInstance();
   const {
     membership: { currentWorkspaceRole },
   } = useUser();
@@ -32,6 +34,28 @@ const ImportsPage: NextPageWithLayout = observer(() => {
           <p className="text-sm text-custom-text-300">You are not authorized to access this page.</p>
         </div>
       </>
+    );
+
+  if (instance?.product === "plane-one")
+    return (
+      <div className="flex justify-center py-28 h-full w-full">
+        <div className="text-center flex flex-col gap-10 items-center">
+          <div className="flex items-center justify-center h-28 w-28 bg-custom-background-90 rounded-full">
+            <ArrowUpDown className="h-12 w-12 text-custom-text-400" />
+          </div>
+          <div className="flex flex-col gap-1">
+            <h3 className="text-lg font-medium text-custom-text-300 whitespace-pre-line">
+              Importers for Plane One will be coming soon. Stay tuned!!!
+            </h3>
+            <p className="text-base font-medium text-custom-text-400 whitespace-pre-line">
+              For requests on specific importer, reach out to{" "}
+              <a href="mailto:support@plane.so" className="text-custom-primary-200 hover:underline">
+                support@plane.so
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
     );
 
   return (
