af09f7e14c2877bb5ec0f474fc47041af5a6cf07
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2024-06-12 15:50:05 +0530

    chore: sidebar width reduced

--- web/app/[workspaceSlug]/(projects)/sidebar.tsx
+++ web/app/[workspaceSlug]/(projects)/sidebar.tsx
@@ -32,10 +32,9 @@ export const AppSidebar: FC<IAppSidebar> = observer(() => {
     <div
       className={`fixed inset-y-0 z-20 flex h-full flex-shrink-0 flex-grow-0 flex-col border-r border-custom-sidebar-border-200 bg-custom-sidebar-background-100
         duration-300 md:relative
-        ${sidebarCollapsed ? "-ml-[280px]" : ""}
-        sm:${sidebarCollapsed ? "-ml-[280px]" : ""}
-        md:ml-0 ${sidebarCollapsed ? "w-[80px]" : "w-[280px]"}
-        lg:ml-0 ${sidebarCollapsed ? "w-[80px]" : "w-[280px]"}
+        ${sidebarCollapsed ? "-ml-[250px]" : ""}
+        sm:${sidebarCollapsed ? "-ml-[250px]" : ""}
+        md:ml-0 ${sidebarCollapsed ? "w-[70px]" : "w-[250px]"}
       `}
     >
       <div ref={ref} className="flex h-full w-full flex-1 flex-col">
