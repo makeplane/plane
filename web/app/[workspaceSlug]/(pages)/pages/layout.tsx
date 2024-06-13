d9c8271f354ee1cdd05df2dfa613d98e2fdf5714
Prateek Shourya <prateekshourya29@gmail.com>
2024-06-13 14:49:59 +0530

    chore: build error fixes and code cleanup.  (#4800)
    * chore: add export for store.
    
    * chore: remove pages route group.
    
    * fix: upgrading turbo
    
    ---------
    
    Co-authored-by: sriram veeraghanta <veeraghanta.sriram@gmail.com>

--- web/app/[workspaceSlug]/(pages)/pages/layout.tsx
+++
@@ -1,8 +0,0 @@
-export default function WorkspacePagesLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div>
-      <h1>WorkspacePages Layout </h1>
-      {children}
-    </div>
-  );
-}
--- web/app/[workspaceSlug]/(pages)/pages/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function WorkspacePagesPage() {
-  return <div>WorkspacePagesPage</div>;
-}
