5bbb796e5e682fae92f6bb6322e6f26b7e74bf94
sriram veeraghanta <veeraghanta.sriram@gmail.com>
2024-06-14 17:59:45 +0530

    fix: migration order

--- apiserver/plane/db/migrations/0069_alter_account_provider_and_more.py
+++ apiserver/plane/db/migrations/0069_alter_account_provider_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('db', '0066_account_id_token_cycle_logo_props_module_logo_props'),
+        ('db', '0068_remove_pagelabel_project_remove_pagelog_project_and_more'),
     ]
 
     operations = [
