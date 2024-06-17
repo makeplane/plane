2b5288c838be31d0691b63805e790e42a98d2cae
sriram veeraghanta <veeraghanta.sriram@gmail.com>
2024-06-17 20:01:28 +0530

    fix: workspace subscription store added

--- web/ee/store/root.store.ts
+++ web/ee/store/root.store.ts
@@ -1,18 +1,25 @@
 // plane web store
 import { IWorkspacePageStore, WorkspacePageStore } from "@/plane-web/store/pages/workspace-page.store";
 // store
+import {
+  IWorkspaceSubscriptionStore,
+  WorkspaceSubscriptionStore,
+} from "@/plane-web/store/subscription/subscription.store";
 import { CoreRootStore } from "@/store/root.store";
 
 export class RootStore extends CoreRootStore {
   workspacePages: IWorkspacePageStore;
+  workspaceSubscription: IWorkspaceSubscriptionStore;
 
   constructor() {
     super();
     this.workspacePages = new WorkspacePageStore(this);
+    this.workspaceSubscription = new WorkspaceSubscriptionStore();
   }
 
   resetOnSignOut() {
     super.resetOnSignOut();
     this.workspacePages = new WorkspacePageStore(this);
+    this.workspaceSubscription = new WorkspaceSubscriptionStore();
   }
 }
