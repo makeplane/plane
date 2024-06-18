8cbad9051d65d2e6f71445f78a0ef8fb678f475f
gurusainath <gurusainath007@gmail.com>
2024-06-18 18:12:11 +0530

    chore: mutating issue estimation_point when we delete the eatimate point from the project

--- web/core/store/estimates/estimate.ts
+++ web/core/store/estimates/estimate.ts
@@ -163,24 +163,20 @@ export class Estimate implements IEstimate {
     projectId: string,
     payload: TEstimatePointsObject[]
   ): Promise<IEstimateType | undefined> => {
-    try {
-      if (!this.id || !payload) return;
+    if (!this.id || !payload) return;
 
-      const estimate = await estimateService.updateEstimate(workspaceSlug, projectId, this.id, {
-        estimate_points: payload,
-      });
-      runInAction(() => {
-        estimate?.points &&
-          estimate?.points.map((estimatePoint) => {
-            if (estimatePoint.id)
-              set(this.estimatePoints, [estimatePoint.id], new EstimatePoint(this.store, this.data, estimatePoint));
-          });
-      });
+    const estimate = await estimateService.updateEstimate(workspaceSlug, projectId, this.id, {
+      estimate_points: payload,
+    });
+    runInAction(() => {
+      estimate?.points &&
+        estimate?.points.map((estimatePoint) => {
+          if (estimatePoint.id)
+            set(this.estimatePoints, [estimatePoint.id], new EstimatePoint(this.store, this.data, estimatePoint));
+        });
+    });
 
-      return estimate;
-    } catch (error) {
-      throw error;
-    }
+    return estimate;
   };
 
   /**
@@ -195,29 +191,25 @@ export class Estimate implements IEstimate {
     projectId: string,
     payload: IEstimateFormData
   ): Promise<IEstimateType | undefined> => {
-    try {
-      if (!this.id || !payload) return;
+    if (!this.id || !payload) return;
 
-      const estimate = await estimateService.updateEstimate(workspaceSlug, projectId, this.id, payload);
-      if (estimate) {
-        runInAction(() => {
-          this.name = estimate?.name;
-          this.type = estimate?.type;
-          estimate?.points &&
-            estimate?.points.map((estimatePoint) => {
-              if (estimatePoint.id)
-                this.estimatePoints?.[estimatePoint.id]?.updateEstimatePointObject({
-                  key: estimatePoint.key,
-                  value: estimatePoint.value,
-                });
-            });
-        });
-      }
-
-      return estimate;
-    } catch (error) {
-      throw error;
+    const estimate = await estimateService.updateEstimate(workspaceSlug, projectId, this.id, payload);
+    if (estimate) {
+      runInAction(() => {
+        this.name = estimate?.name;
+        this.type = estimate?.type;
+        estimate?.points &&
+          estimate?.points.map((estimatePoint) => {
+            if (estimatePoint.id)
+              this.estimatePoints?.[estimatePoint.id]?.updateEstimatePointObject({
+                key: estimatePoint.key,
+                value: estimatePoint.value,
+              });
+          });
+      });
     }
+
+    return estimate;
   };
 
   /**
@@ -232,19 +224,15 @@ export class Estimate implements IEstimate {
     projectId: string,
     payload: Partial<IEstimatePointType>
   ): Promise<IEstimatePointType | undefined> => {
-    try {
-      if (!this.id || !payload) return;
+    if (!this.id || !payload) return;
 
-      const estimatePoint = await estimateService.createEstimatePoint(workspaceSlug, projectId, this.id, payload);
-      if (estimatePoint) {
-        runInAction(() => {
-          if (estimatePoint.id) {
-            set(this.estimatePoints, [estimatePoint.id], new EstimatePoint(this.store, this.data, estimatePoint));
-          }
-        });
-      }
-    } catch (error) {
-      throw error;
+    const estimatePoint = await estimateService.createEstimatePoint(workspaceSlug, projectId, this.id, payload);
+    if (estimatePoint) {
+      runInAction(() => {
+        if (estimatePoint.id) {
+          set(this.estimatePoints, [estimatePoint.id], new EstimatePoint(this.store, this.data, estimatePoint));
+        }
+      });
     }
   };
 
@@ -262,32 +250,37 @@ export class Estimate implements IEstimate {
     estimatePointId: string,
     newEstimatePointId: string | undefined
   ): Promise<IEstimatePointType[] | undefined> => {
-    try {
-      if (!this.id) return;
+    if (!this.id) return;
 
-      const deleteEstimatePoint = await estimateService.removeEstimatePoint(
-        workspaceSlug,
-        projectId,
-        this.id,
-        estimatePointId,
-        newEstimatePointId ? { new_estimate_id: newEstimatePointId } : undefined
-      );
+    const deleteEstimatePoint = await estimateService.removeEstimatePoint(
+      workspaceSlug,
+      projectId,
+      this.id,
+      estimatePointId,
+      newEstimatePointId ? { new_estimate_id: newEstimatePointId } : undefined
+    );
 
-      runInAction(() => {
-        unset(this.estimatePoints, [estimatePointId]);
+    const currentIssues = Object.values(this.store.issue.issues.issuesMap || {});
+    if (currentIssues && currentIssues.length > 0) {
+      currentIssues.map((issue) => {
+        if (issue.estimate_point === estimatePointId) {
+          this.store.issue.issues.updateIssue(issue.id, { estimate_point: newEstimatePointId });
+        }
       });
-      if (deleteEstimatePoint && deleteEstimatePoint.length > 0) {
-        runInAction(() => {
-          deleteEstimatePoint.map((estimatePoint) => {
-            if (estimatePoint.id)
-              set(this.estimatePoints, [estimatePoint.id], new EstimatePoint(this.store, this.data, estimatePoint));
-          });
-        });
-      }
-
-      return deleteEstimatePoint;
-    } catch (error) {
-      throw error;
     }
+
+    runInAction(() => {
+      unset(this.estimatePoints, [estimatePointId]);
+    });
+    if (deleteEstimatePoint && deleteEstimatePoint.length > 0) {
+      runInAction(() => {
+        deleteEstimatePoint.map((estimatePoint) => {
+          if (estimatePoint.id)
+            set(this.estimatePoints, [estimatePoint.id], new EstimatePoint(this.store, this.data, estimatePoint));
+        });
+      });
+    }
+
+    return deleteEstimatePoint;
   };
 }
