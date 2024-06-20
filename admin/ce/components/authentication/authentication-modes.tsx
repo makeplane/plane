58f85e4b7e2d181fd92124fab0f2ba0d4a036821
Prateek Shourya <prateekshourya29@gmail.com>
2024-06-20 17:39:49 +0530

    chore: update mobx-react-lite import to mobx-react.

--- admin/ce/components/authentication/authentication-modes.tsx
+++ admin/ce/components/authentication/authentication-modes.tsx
@@ -1,4 +1,4 @@
-import { observer } from "mobx-react-lite";
+import { observer } from "mobx-react";
 import Image from "next/image";
 import { useTheme } from "next-themes";
 import { KeyRound, Mails } from "lucide-react";
