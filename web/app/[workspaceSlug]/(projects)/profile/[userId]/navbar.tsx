8d3755c4332bff37cfb0704f1ea35bff8c17c824
Prateek Shourya <prateekshourya29@gmail.com>
2024-06-20 20:07:23 +0530

    [WEB-1670] style: fix tab highlights on user activity page. (#4898)

--- web/app/[workspaceSlug]/(projects)/profile/[userId]/navbar.tsx
+++ web/app/[workspaceSlug]/(projects)/profile/[userId]/navbar.tsx
@@ -28,7 +28,7 @@ const pathname = usePathname();
           <Link key={tab.route} href={`/${workspaceSlug}/profile/${userId}/${tab.route}`}>
             <span
               className={`flex whitespace-nowrap border-b-2 p-4 text-sm font-medium outline-none ${
-                pathname === `/${workspaceSlug}/profile/${userId}${tab.selected}`
+                pathname === `/${workspaceSlug}/profile/${userId}${tab.selected}/`
                   ? "border-custom-primary-100 text-custom-primary-100"
                   : "border-transparent"
               }`}
