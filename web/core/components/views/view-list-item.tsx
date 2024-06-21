a11dd0a18b7012866c76a2e5046157895d0161f9
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2024-06-21 18:32:39 +0530

    chore: logo picker removed from page and view list page

--- web/core/components/views/view-list-item.tsx
+++ web/core/components/views/view-list-item.tsx
@@ -1,20 +1,17 @@
 "use client";
 
-import { FC, useRef, useState } from "react";
+import { FC, useRef } from "react";
 import { observer } from "mobx-react";
 import { useParams } from "next/navigation";
 // types
-import { IProjectView, TLogoProps } from "@plane/types";
+import { IProjectView } from "@plane/types";
 // ui
-import { EmojiIconPicker, EmojiIconPickerTypes, PhotoFilterIcon, TOAST_TYPE, setToast } from "@plane/ui";
+import { PhotoFilterIcon } from "@plane/ui";
 // components
 import { Logo } from "@/components/common";
 import { ListItem } from "@/components/core/list";
 import { ViewListItemAction } from "@/components/views";
-// helpers
-import { convertHexEmojiToDecimal } from "@/helpers/emoji.helper";
 // hooks
-import { useProjectView } from "@/hooks/store";
 import { usePlatformOS } from "@/hooks/use-platform-os";
 
 type Props = {
@@ -25,78 +22,20 @@ export const ProjectViewListItem: FC<Props> = observer((props) => {
   const { view } = props;
   // refs
   const parentRef = useRef(null);
-  // state
-  const [isOpen, setIsOpen] = useState(false);
   // router
   const { workspaceSlug, projectId } = useParams();
   // store hooks
   const { isMobile } = usePlatformOS();
-  const { updateView } = useProjectView();
-
-  const handleViewLogoUpdate = async (data: TLogoProps) => {
-    if (!workspaceSlug || !projectId || !view.id || !data) return;
-
-    updateView(workspaceSlug.toString(), projectId.toString(), view.id.toString(), {
-      logo_props: data,
-    })
-      .then(() => {
-        setToast({
-          type: TOAST_TYPE.SUCCESS,
-          title: "Success!",
-          message: "Logo Updated successfully.",
-        });
-      })
-      .catch(() => {
-        setToast({
-          type: TOAST_TYPE.ERROR,
-          title: "Error!",
-          message: "Something went wrong. Please try again.",
-        });
-      });
-  };
 
   return (
     <ListItem
       prependTitleElement={
         <>
-          <EmojiIconPicker
-            isOpen={isOpen}
-            handleToggle={(val: boolean) => setIsOpen(val)}
-            className="flex items-center justify-center"
-            buttonClassName="flex items-center justify-center"
-            label={
-              <>
-                {view?.logo_props?.in_use ? (
-                  <Logo logo={view?.logo_props} size={16} type="lucide" />
-                ) : (
-                  <PhotoFilterIcon className="h-4 w-4 text-custom-text-300" />
-                )}
-              </>
-            }
-            onChange={(val) => {
-              let logoValue = {};
-
-              if (val?.type === "emoji")
-                logoValue = {
-                  value: convertHexEmojiToDecimal(val.value.unified),
-                  url: val.value.imageUrl,
-                };
-              else if (val?.type === "icon") logoValue = val.value;
-
-              handleViewLogoUpdate({
-                in_use: val?.type,
-                [val?.type]: logoValue,
-              }).finally(() => setIsOpen(false));
-            }}
-            defaultIconColor={
-              view?.logo_props?.in_use && view?.logo_props.in_use === "icon" ? view?.logo_props?.icon?.color : undefined
-            }
-            defaultOpen={
-              view?.logo_props?.in_use && view?.logo_props?.in_use === "emoji"
-                ? EmojiIconPickerTypes.EMOJI
-                : EmojiIconPickerTypes.ICON
-            }
-          />
+          {view?.logo_props?.in_use ? (
+            <Logo logo={view?.logo_props} size={16} type="lucide" />
+          ) : (
+            <PhotoFilterIcon className="h-4 w-4 text-custom-text-300" />
+          )}
         </>
       }
       title={view.name}
@@ -104,7 +43,6 @@ export const ProjectViewListItem: FC<Props> = observer((props) => {
       actionableItems={<ViewListItemAction parentRef={parentRef} view={view} />}
       isMobile={isMobile}
       parentRef={parentRef}
-      disableLink={isOpen}
     />
   );
 });
