146b63f1ad17de433271b12673013c466ca5cbce
pablohashescobar <nikhilschacko@gmail.com>
2024-06-24 20:20:37 +0530

    fix: external apis

--- apiserver/plane/api/serializers/intake.py
+++ apiserver/plane/api/serializers/intake.py
@@ -2,11 +2,13 @@
 from .base import BaseSerializer
 from .issue import IssueExpandSerializer
 from plane.db.models import IntakeIssue
+from rest_framework import serializers
 
 
 class IntakeIssueSerializer(BaseSerializer):
 
     issue_detail = IssueExpandSerializer(read_only=True, source="issue")
+    inbox = serializers.UUIDField(source="intake.id", read_only=True)
 
     class Meta:
         model = IntakeIssue
