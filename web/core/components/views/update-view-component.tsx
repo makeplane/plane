b53c26b68248ba863bee2e7a2a30a71b7cfb1489
rahulramesha <rahulramesham@gmail.com>
2024-06-25 16:11:51 +0530

    add feedback for update View button

--- web/core/components/views/update-view-component.tsx
+++ web/core/components/views/update-view-component.tsx
@@ -1,4 +1,4 @@
-import { SetStateAction } from "react";
+import { SetStateAction, useEffect, useState } from "react";
 import { Button } from "@plane/ui";
 import { LockedComponent } from "../icons/locked-component";
 
@@ -22,6 +22,34 @@ export const UpdateViewComponent = (props: Props) => {
     handleUpdateView,
     lockedTooltipContent,
   } = props;
+
+  const [isUpdating, setIsUpdating] = useState(false);
+
+  useEffect(() => {
+    if (areFiltersEqual) {
+      setIsUpdating(false);
+    }
+  }, [areFiltersEqual]);
+
+  // Change state while updating view to have a feedback
+  const updateButton = isUpdating ? (
+    <Button variant="primary" size="sm" className="flex-shrink-0">
+      Updating...
+    </Button>
+  ) : (
+    <Button
+      variant="primary"
+      size="sm"
+      className="flex-shrink-0"
+      onClick={() => {
+        setIsUpdating(true);
+        handleUpdateView();
+      }}
+    >
+      Update view
+    </Button>
+  );
+
   return (
     <div className="flex gap-2">
       {isLocked ? (
@@ -33,11 +61,7 @@ export const UpdateViewComponent = (props: Props) => {
             <Button variant="outline-primary" size="sm" className="flex-shrink-0" onClick={() => setIsModalOpen(true)}>
               Save as
             </Button>
-            {isOwner && (
-              <Button variant="primary" size="sm" className="flex-shrink-0" onClick={handleUpdateView}>
-                Update view
-              </Button>
-            )}
+            {isOwner && <>{updateButton}</>}
           </>
         )
       )}
