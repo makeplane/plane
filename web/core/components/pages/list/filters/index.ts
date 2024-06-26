505c72d734d0265587e9967f6f985ebf329af726
rahulramesha <rahulramesham@gmail.com>
2024-06-26 22:00:09 +0530

    View with filters and changes required for advanced views

--- web/core/components/pages/list/filters/index.ts
+++ web/core/components/pages/list/filters/index.ts
@@ -1,3 +1,1 @@
-export * from "./created-at";
-export * from "./created-by";
 export * from "./root";
--- web/core/components/views/view-list-header.tsx
+++ web/core/components/views/view-list-header.tsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useRef, useState } from "react";
 import { observer } from "mobx-react";
 // icons
-import { Search, X } from "lucide-react";
+import { ListFilter, Search, X } from "lucide-react";
 // helpers
 import { cn } from "@/helpers/common.helper";
 // hooks
-import { useProjectView } from "@/hooks/store";
+import { useMember, useProjectView } from "@/hooks/store";
 import useOutsideClickDetector from "@/hooks/use-outside-click-detector";
+import { FiltersDropdown } from "../issues";
+import { ViewFiltersSelection } from "./filters/filter-selection";
+import { ViewOrderByDropdown } from "./filters/order-by";
 
 export const ViewListHeader = observer(() => {
   // states
@@ -14,13 +17,17 @@ export const ViewListHeader = observer(() => {
   // refs
   const inputRef = useRef<HTMLInputElement>(null);
   // store hooks
-  const { searchQuery, updateSearchQuery } = useProjectView();
+  const { filters, updateFilters } = useProjectView();
+  const {
+    project: { projectMemberIds },
+  } = useMember();
 
   // handlers
   const handleInputKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Escape") {
-      if (searchQuery && searchQuery.trim() !== "") updateSearchQuery("");
-      else {
+      if (filters?.searchQuery && filters?.searchQuery.trim() !== "") {
+        updateFilters("searchQuery", "");
+      } else {
         setIsSearchOpen(false);
         inputRef.current?.blur();
       }
@@ -29,12 +36,12 @@ export const ViewListHeader = observer(() => {
 
   // outside click detector hook
   useOutsideClickDetector(inputRef, () => {
-    if (isSearchOpen && searchQuery.trim() === "") setIsSearchOpen(false);
+    if (isSearchOpen && filters?.searchQuery.trim() === "") setIsSearchOpen(false);
   });
 
   useEffect(() => {
-    if (searchQuery.trim() !== "") setIsSearchOpen(true);
-  }, [searchQuery]);
+    if (filters?.searchQuery.trim() !== "") setIsSearchOpen(true);
+  }, [filters?.searchQuery]);
 
   return (
     <div className="h-full flex items-center gap-2">
@@ -64,8 +71,8 @@ export const ViewListHeader = observer(() => {
             ref={inputRef}
             className="w-full max-w-[234px] border-none bg-transparent text-sm text-custom-text-100 placeholder:text-custom-text-400 focus:outline-none"
             placeholder="Search"
-            value={searchQuery}
-            onChange={(e) => updateSearchQuery(e.target.value)}
+            value={filters?.searchQuery}
+            onChange={(e) => updateFilters("searchQuery", e.target.value)}
             onKeyDown={handleInputKeyDown}
           />
           {isSearchOpen && (
@@ -73,7 +80,7 @@ export const ViewListHeader = observer(() => {
               type="button"
               className="grid place-items-center"
               onClick={() => {
-                updateSearchQuery("");
+                updateFilters("searchQuery", "");
                 setIsSearchOpen(false);
               }}
             >
@@ -82,6 +89,26 @@ export const ViewListHeader = observer(() => {
           )}
         </div>
       </div>
+      <ViewOrderByDropdown
+        sortBy={filters.sortBy}
+        sortKey={filters.sortKey}
+        onChange={(val) => {
+          if (val.key) updateFilters("sortKey", val.key);
+          if (val.order) updateFilters("sortBy", val.order);
+        }}
+      />
+      <FiltersDropdown
+        icon={<ListFilter className="h-3 w-3" />}
+        title="Filters"
+        placement="bottom-end"
+        isFiltersApplied={false}
+      >
+        <ViewFiltersSelection
+          filters={filters}
+          handleFiltersUpdate={updateFilters}
+          memberIds={projectMemberIds ?? undefined}
+        />
+      </FiltersDropdown>
     </div>
   );
 });
