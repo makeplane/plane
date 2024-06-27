2513ce1eb11c8b427a25c556b58c49a23693e22b
Dan Byler <dannybyler@gmail.com>
2024-06-27 15:47:07 -0400

    responsive styles for recent-collborators

--- web/core/components/dashboard/widgets/recent-collaborators/root.tsx
+++ web/core/components/dashboard/widgets/recent-collaborators/root.tsx
@@ -15,14 +15,14 @@ export const RecentCollaboratorsWidget: React.FC<WidgetProps> = (props) => {
 
   return (
     <div className="w-full rounded-xl border-[0.5px] border-custom-border-200 bg-custom-background-100 duration-300 hover:shadow-custom-shadow-4xl">
-      <div className="flex items-start justify-between px-7 pt-6">
+      <div className="flex flex-col sm:flex-row items-start justify-between px-7 pt-6">
         <div>
           <h4 className="text-lg font-semibold text-custom-text-300">Collaborators</h4>
           <p className="mt-2 text-xs font-medium text-custom-text-300">
             View and find all members you collaborate with across projects
           </p>
         </div>
-        <div className="flex min-w-72 items-center justify-start gap-2 rounded-md border border-custom-border-200 px-2.5 py-1.5 placeholder:text-custom-text-400">
+        <div className="mt-5 sm:mt-0 flex min-w-72 items-center justify-start gap-2 rounded-md border border-custom-border-200 px-2.5 py-1.5 placeholder:text-custom-text-400">
           <Search className="h-3.5 w-3.5 text-custom-text-400" />
           <input
             className="w-full border-none bg-transparent text-sm focus:outline-none"
