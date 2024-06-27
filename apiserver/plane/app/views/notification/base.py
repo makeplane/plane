a0ed354874ebb01d7ff57795b17a6ae646d2f8a3
NarayanBavisetti <narayan3119@gmail.com>
2024-06-27 17:03:50 +0530

    chore: notifications filter changed

--- apiserver/plane/app/views/notification/base.py
+++ apiserver/plane/app/views/notification/base.py
@@ -43,8 +43,9 @@ class NotificationViewSet(BaseViewSet, BasePaginator):
         # Get query parameters
         snoozed = request.GET.get("snoozed", "false")
         archived = request.GET.get("archived", "false")
-        read = request.GET.get("read", "true")
+        read = request.GET.get("read", None)
         type = request.GET.get("type", "all")
+        q_filters = Q()
 
         notifications = (
             Notification.objects.filter(
@@ -74,8 +75,12 @@ class NotificationViewSet(BaseViewSet, BasePaginator):
         if read == "false":
             notifications = notifications.filter(read_at__isnull=True)
 
+        if read == "true":
+            notifications = notifications.filter(read_at__isnull=False)
+
+        type = type.split(",")
         # Subscribed issues
-        if type == "watching":
+        if "subscribed" in type:
             issue_ids = (
                 IssueSubscriber.objects.filter(
                     workspace__slug=slug, subscriber_id=request.user.id
@@ -97,35 +102,32 @@ class NotificationViewSet(BaseViewSet, BasePaginator):
                 .filter(created=False, assigned=False)
                 .values_list("issue_id", flat=True)
             )
-            notifications = notifications.filter(
-                entity_identifier__in=issue_ids,
-            )
+            q_filters |= Q(entity_identifier__in=issue_ids)
 
         # Assigned Issues
-        if type == "assigned":
+        if "assigned" in type:
             issue_ids = IssueAssignee.objects.filter(
                 workspace__slug=slug, assignee_id=request.user.id
             ).values_list("issue_id", flat=True)
-            notifications = notifications.filter(
-                entity_identifier__in=issue_ids
-            )
+            q_filters |= Q(entity_identifier__in=issue_ids)
 
         # Created issues
-        if type == "created":
+        if "created" in type:
             if WorkspaceMember.objects.filter(
                 workspace__slug=slug,
                 member=request.user,
                 role__lt=15,
                 is_active=True,
             ).exists():
-                notifications = Notification.objects.none()
+                notifications = notifications.none()
             else:
                 issue_ids = Issue.objects.filter(
                     workspace__slug=slug, created_by=request.user
                 ).values_list("pk", flat=True)
-                notifications = notifications.filter(
-                    entity_identifier__in=issue_ids
-                )
+                q_filters |= Q(entity_identifier__in=issue_ids)
+
+        # Apply the combined Q object filters
+        notifications = notifications.filter(q_filters)
 
         # Pagination
         if request.GET.get("per_page", False) and request.GET.get(
