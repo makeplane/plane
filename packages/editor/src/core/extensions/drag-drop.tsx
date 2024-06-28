181c9e51f53e78f08c066b288ddcc2cb7e5381ce
Palanikannan M <akashmalinimurugu@gmail.com>
2024-06-28 12:12:58 +0530

    fix: drag drop fixed for task lists

--- packages/editor/src/core/extensions/drag-drop.tsx
+++ packages/editor/src/core/extensions/drag-drop.tsx
@@ -71,8 +71,8 @@ function nodeDOMAtCoords(coords: { x: number; y: number }) {
     "blockquote",
     "img",
     "h1, h2, h3, h4, h5, h6",
-    ".table-wrapper",
     "[data-type=horizontalRule]",
+    ".table-wrapper",
   ].join(", ");
 
   for (const elem of elements) {
@@ -120,7 +120,7 @@ function calcNodePos(pos: number, view: EditorView, node: Element) {
   const $pos = view.state.doc.resolve(safePos);
 
   if ($pos.depth > 1) {
-    if (node.matches("ul:not([data-type=taskList]) li, ol li")) {
+    if (node.matches("ul li, ol li")) {
       // only for nested lists
       const newPos = $pos.before($pos.depth);
       return Math.max(0, Math.min(newPos, maxPos));
@@ -160,7 +160,6 @@ function DragHandle(options: DragHandleOptions) {
     if (nodePos.node().type.name === "doc") differentNodeSelected = true;
     else {
       const nodeSelection = NodeSelection.create(view.state.doc, nodePos.before());
-      // Check if the node where the drag event started is part of the current selection
       differentNodeSelected = !(
         draggedNodePos + 1 >= nodeSelection.$from.pos && draggedNodePos <= nodeSelection.$to.pos
       );
