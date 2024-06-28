f29bd21af7a7da57f620931e6536c175fade361a
NarayanBavisetti <narayan3119@gmail.com>
2024-06-28 19:07:27 +0530

    chore: preventing race condition

--- apiserver/plane/app/views/view/base.py
+++ apiserver/plane/app/views/view/base.py
@@ -14,6 +14,7 @@ from django.db.models.functions import Coalesce
 from django.utils.decorators import method_decorator
 from django.views.decorators.gzip import gzip_page
 from rest_framework import status
+from django.db import transaction
 
 # Third party imports
 from rest_framework.response import Response
@@ -76,32 +77,37 @@ class WorkspaceViewViewSet(BaseViewSet):
         )
 
     def partial_update(self, request, slug, pk):
-        workspace_view = IssueView.objects.get(
-            pk=pk,
-            workspace__slug=slug,
-        )
-
-        if workspace_view.is_locked:
-            return Response(
-                {"error": "view is locked"},
-                status=status.HTTP_400_BAD_REQUEST,
+        with transaction.atomic():
+            workspace_view = IssueView.objects.select_for_update().get(
+                pk=pk,
+                workspace__slug=slug,
             )
 
-        # Only update the view if owner is updating
-        if workspace_view.owned_by_id != request.user.id:
-            return Response(
-                {"error": "Only the owner of the view can update the view"},
-                status=status.HTTP_400_BAD_REQUEST,
+            if workspace_view.is_locked:
+                return Response(
+                    {"error": "view is locked"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            # Only update the view if owner is updating
+            if workspace_view.owned_by_id != request.user.id:
+                return Response(
+                    {
+                        "error": "Only the owner of the view can update the view"
+                    },
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            serializer = IssueViewSerializer(
+                workspace_view, data=request.data, partial=True
             )
 
-        serializer = IssueViewSerializer(
-            workspace_view, data=request.data, partial=True
-        )
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
 
 
 class WorkspaceViewIssuesViewSet(BaseViewSet):
@@ -344,31 +350,36 @@ class IssueViewViewSet(BaseViewSet):
         return Response(views, status=status.HTTP_200_OK)
 
     def partial_update(self, request, slug, project_id, pk):
-        issue_view = IssueView.objects.get(
-            pk=pk, workspace__slug=slug, project_id=project_id
-        )
-
-        if issue_view.is_locked:
-            return Response(
-                {"error": "view is locked"},
-                status=status.HTTP_400_BAD_REQUEST,
+        with transaction.atomic():
+            issue_view = IssueView.objects.select_for_update().get(
+                pk=pk, workspace__slug=slug, project_id=project_id
             )
 
-        # Only update the view if owner is updating
-        if issue_view.owned_by_id != request.user.id:
-            return Response(
-                {"error": "Only the owner of the view can update the view"},
-                status=status.HTTP_400_BAD_REQUEST,
+            if issue_view.is_locked:
+                return Response(
+                    {"error": "view is locked"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            # Only update the view if owner is updating
+            if issue_view.owned_by_id != request.user.id:
+                return Response(
+                    {
+                        "error": "Only the owner of the view can update the view"
+                    },
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            serializer = IssueViewSerializer(
+                issue_view, data=request.data, partial=True
             )
 
-        serializer = IssueViewSerializer(
-            issue_view, data=request.data, partial=True
-        )
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
 
 
 class IssueViewFavoriteViewSet(BaseViewSet):
