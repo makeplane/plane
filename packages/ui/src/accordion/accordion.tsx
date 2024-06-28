68d30f502f38e9b3bb3bd0683bdbae4413bb1f2a
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2024-06-28 12:24:40 +0530

    chore: accordion conponent updated

--- packages/ui/src/accordion/accordion.tsx
+++ packages/ui/src/accordion/accordion.tsx
@@ -1,21 +1,39 @@
-import React, { FC } from "react";
-// headless ui
+import React, { FC, useState, useEffect, useCallback } from "react";
 import { Disclosure, Transition } from "@headlessui/react";
 
 export type TAccordionProps = {
   title: string | React.ReactNode;
   children: React.ReactNode;
-  isOpen: boolean;
-  handleToggle: () => void;
+  buttonClassName?: string;
+  isOpen?: boolean;
+  handleToggle?: () => void;
+  defaultOpen?: boolean;
 };
 
 export const Accordion: FC<TAccordionProps> = (props) => {
-  const { isOpen, handleToggle, title, children } = props;
+  const { title, children, buttonClassName, isOpen, handleToggle, defaultOpen } = props;
+  // state
+  const [localIsOpen, setLocalIsOpen] = useState<boolean>(isOpen || defaultOpen ? true : false);
+
+  useEffect(() => {
+    if (isOpen !== undefined) {
+      setLocalIsOpen(isOpen);
+    }
+  }, [isOpen]);
+
+  // handlers
+  const handleOnClick = useCallback(() => {
+    setLocalIsOpen((prev) => !prev);
+    if (handleToggle) handleToggle();
+  }, [handleToggle]);
+
   return (
     <Disclosure>
-      <Disclosure.Button onClick={handleToggle}>{title}</Disclosure.Button>
+      <Disclosure.Button className={buttonClassName} onClick={handleOnClick}>
+        {title}
+      </Disclosure.Button>
       <Transition
-        show={isOpen}
+        show={localIsOpen}
         className="overflow-hidden"
         enter="transition-max-height duration-400 ease-in-out"
         enterFrom="max-h-0"
