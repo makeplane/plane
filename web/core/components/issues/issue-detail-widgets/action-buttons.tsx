ce835b375e2c4a6b0ee7cf265b2e61a8da0f2089
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2024-07-05 16:40:58 +0530

    fix: build error

--- web/core/components/issues/issue-detail-widgets/action-buttons.tsx
+++ web/core/components/issues/issue-detail-widgets/action-buttons.tsx
@@ -1,8 +1,6 @@
 "use client";
 import React, { FC } from "react";
 import { Layers, Link, Paperclip, Waypoints } from "lucide-react";
-// ui
-import { RelationsIcon } from "@plane/ui";
 // components
 import {
   IssueAttachmentActionButton,
