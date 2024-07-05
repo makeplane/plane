db67bf2c2a8adab76783d864f99b91b519abc3c0
gurusainath <gurusainath007@gmail.com>
2024-07-05 20:11:36 +0530

    chore: implemente component in project-states

--- web/app/[workspaceSlug]/(projects)/projects/(detail)/[projectId]/settings/states/page.tsx
+++ web/app/[workspaceSlug]/(projects)/projects/(detail)/[projectId]/settings/states/page.tsx
@@ -18,7 +18,7 @@ const StatesSettingsPage = observer(() => {
   return (
     <>
       <PageHead title={pageTitle} />
-      <div className="w-full gap-10 overflow-y-auto py-8 pr-9">
+      <div className="py-8 pr-9">
         <div className="flex items-center border-b border-custom-border-100 py-3.5">
           <h3 className="text-xl font-medium">States</h3>
         </div>
