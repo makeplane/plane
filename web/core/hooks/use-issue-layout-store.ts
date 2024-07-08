ff6b2dab46b7f253772e0a90230e38041077fd0b
rahulramesha <rahulramesham@gmail.com>
2024-07-08 16:14:50 +0530

    fix issue creation by using appropriate stores

--- web/core/hooks/use-issue-layout-store.ts
+++ web/core/hooks/use-issue-layout-store.ts
@@ -1,13 +1,30 @@
 import { createContext, useContext } from "react";
+import { useParams } from "next/navigation";
 import { EIssuesStoreType } from "@/constants/issue";
 import { useIssues } from "./store";
 
-export const IssuesStoreContext = createContext<EIssuesStoreType>(EIssuesStoreType.PROJECT);
+export const IssuesStoreContext = createContext<EIssuesStoreType | undefined>(undefined);
 
 export const useIssueStoreType = () => {
   const storeType = useContext(IssuesStoreContext);
 
-  return storeType;
+  const { globalViewId, viewId, projectId, cycleId, moduleId, userId } = useParams();
+
+  if (storeType) return storeType;
+
+  if (globalViewId) return EIssuesStoreType.GLOBAL;
+
+  if (userId) return EIssuesStoreType.PROFILE;
+
+  if (viewId) return EIssuesStoreType.PROJECT_VIEW;
+
+  if (cycleId) return EIssuesStoreType.CYCLE;
+
+  if (moduleId) return EIssuesStoreType.MODULE;
+
+  if (projectId) return EIssuesStoreType.PROJECT;
+
+  return EIssuesStoreType.PROJECT;
 };
 
 export const useIssuesStore = () => {
