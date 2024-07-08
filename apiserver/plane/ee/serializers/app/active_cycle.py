528edc2b0eb2b9069c25800f19e71cdc841f0103
Bavisetti Narayan <72156168+NarayanBavisetti@users.noreply.github.com>
2024-07-08 15:20:39 +0530

    [WEB-1854] chore: active cycle estimates (#573)
    * chore: estimate points float field
    
    * chore: active cycle estimate point

--- apiserver/plane/ee/serializers/app/active_cycle.py
+++ apiserver/plane/ee/serializers/app/active_cycle.py
@@ -20,6 +20,12 @@ class WorkspaceActiveCycleSerializer(BaseSerializer):
     unstarted_issues = serializers.IntegerField(read_only=True)
     backlog_issues = serializers.IntegerField(read_only=True)
 
+    backlog_estimate_points = serializers.FloatField(read_only=True)
+    unstarted_estimate_points = serializers.FloatField(read_only=True)
+    started_estimate_points = serializers.FloatField(read_only=True)
+    cancelled_estimate_points = serializers.FloatField(read_only=True)
+    total_estimate_points = serializers.FloatField(read_only=True)
+
     # active | draft | upcoming | completed
     status = serializers.CharField(read_only=True)
 
@@ -54,5 +60,10 @@ class WorkspaceActiveCycleSerializer(BaseSerializer):
             "backlog_issues",
             "status",
             "project_detail",
+            "backlog_estimate_points",
+            "unstarted_estimate_points",
+            "started_estimate_points",
+            "cancelled_estimate_points",
+            "total_estimate_points",
         ]
         read_only_fields = fields
