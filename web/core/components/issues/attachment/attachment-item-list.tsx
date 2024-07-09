42f6abfab86f717b1eff91b9b2ab61ced0259ce4
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2024-07-09 14:44:59 +0530

    fix: issue attachment delete modal fix and code refactor

--- web/core/components/issues/attachment/attachment-item-list.tsx
+++ web/core/components/issues/attachment/attachment-item-list.tsx
@@ -9,6 +9,7 @@ import { useInstance, useIssueDetail } from "@/hooks/store";
 // components
 import { IssueAttachmentsListItem } from "./attachment-list-item";
 // types
+import { IssueAttachmentDeleteModal } from "./delete-attachment-modal";
 import { TAttachmentOperations } from "./root";
 
 type TAttachmentOperationsRemoveModal = Exclude<TAttachmentOperations, "create">;
@@ -28,6 +29,8 @@ export const IssueAttachmentItemList: FC<TIssueAttachmentItemList> = observer((p
   const { config } = useInstance();
   const {
     attachment: { getAttachmentsByIssueId },
+    attachmentDeleteModalId,
+    toggleDeleteAttachmentModal,
   } = useIssueDetail();
   // derived values
   const issueAttachments = getAttachmentsByIssueId(issueId);
@@ -65,29 +68,34 @@ export const IssueAttachmentItemList: FC<TIssueAttachmentItemList> = observer((p
   if (!issueAttachments) return <></>;
 
   return (
-    <div
-      {...getRootProps()}
-      className={`relative flex flex-col ${isDragActive && issueAttachments.length < 3 ? "min-h-[200px]" : ""} ${disabled ? "cursor-not-allowed" : "cursor-pointer"}`}
-    >
-      <input {...getInputProps()} />
-      {isDragActive && (
-        <div className="absolute flex items-center justify-center left-0 top-0 h-full w-full bg-custom-background-90/75 z-30 ">
-          <div className="flex items-center justify-center p-1 rounded-md bg-custom-background-100">
-            <div className="flex flex-col justify-center items-center px-5 py-6 rounded-md border border-dashed border-custom-border-300">
-              <UploadCloud className="size-7" />
-              <span className="text-sm text-custom-text-300">Drag and drop anywhere to upload</span>
+    <>
+      {attachmentDeleteModalId && (
+        <IssueAttachmentDeleteModal
+          isOpen={Boolean(attachmentDeleteModalId)}
+          onClose={() => toggleDeleteAttachmentModal(null)}
+          handleAttachmentOperations={handleAttachmentOperations}
+          attachmentId={attachmentDeleteModalId}
+        />
+      )}
+      <div
+        {...getRootProps()}
+        className={`relative flex flex-col ${isDragActive && issueAttachments.length < 3 ? "min-h-[200px]" : ""} ${disabled ? "cursor-not-allowed" : "cursor-pointer"}`}
+      >
+        <input {...getInputProps()} />
+        {isDragActive && (
+          <div className="absolute flex items-center justify-center left-0 top-0 h-full w-full bg-custom-background-90/75 z-30 ">
+            <div className="flex items-center justify-center p-1 rounded-md bg-custom-background-100">
+              <div className="flex flex-col justify-center items-center px-5 py-6 rounded-md border border-dashed border-custom-border-300">
+                <UploadCloud className="size-7" />
+                <span className="text-sm text-custom-text-300">Drag and drop anywhere to upload</span>
+              </div>
             </div>
           </div>
-        </div>
-      )}
-      {issueAttachments?.map((attachmentId) => (
-        <IssueAttachmentsListItem
-          key={attachmentId}
-          attachmentId={attachmentId}
-          disabled={disabled}
-          handleAttachmentOperations={handleAttachmentOperations}
-        />
-      ))}
-    </div>
+        )}
+        {issueAttachments?.map((attachmentId) => (
+          <IssueAttachmentsListItem key={attachmentId} attachmentId={attachmentId} disabled={disabled} />
+        ))}
+      </div>
+    </>
   );
 });
