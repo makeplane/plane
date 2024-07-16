4c8713ec39521cde02deb62c82bd8846a86dec42
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2024-07-16 13:57:55 +0530

    chore: intake icon added

--- packages/ui/src/icons/intake.tsx
+++ packages/ui/src/icons/intake.tsx
@@ -9,7 +9,7 @@ export const Intake: React.FC<ISvgIcons> = ({ className = "text-current", ...res
     stroke="currentColor"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    strokeWidth="1.5"
+    strokeWidth="1.25"
     strokeLinecap="round"
     strokeLinejoin="round"
     {...rest}
