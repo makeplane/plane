aaba67915bf2e86637ba4d8c0a912be91a8a4792
Aaryan Khandelwal <65252264+aaryan610@users.noreply.github.com>
2024-07-16 18:15:36 +0530

    fix: issue embed file (#636)

--- space/app/pages/[anchor]/page.tsx
+++ space/app/pages/[anchor]/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import useSWR from "swr";
+// hooks
+import { useStates } from "@/hooks/store";
 // components
 import { PageDetailsMainContent } from "@/plane-web/components/pages";
 
@@ -11,5 +14,12 @@ type Props = {
 
 export default function PageDetailsPage(props: Props) {
   const { params } = props;
+  // params
+  const { anchor } = params;
+  // store
+  const { fetchStates } = useStates();
+
+  useSWR(anchor ? `PUBLIC_STATES_${anchor}` : null, anchor ? () => fetchStates(anchor) : null);
+
   return <PageDetailsMainContent anchor={params.anchor.toString()} />;
 }
