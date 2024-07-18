cfc70622d6200dbdc280ea61d0ac2a45c75d5a7a
Prateek Shourya <prateekshourya29@gmail.com>
2024-07-18 15:45:37 +0530

    [WEB-1960]: chore: upgrade to plane paid plans modal. (#5149)

--- packages/ui/src/modals/modal-core.tsx
+++ packages/ui/src/modals/modal-core.tsx
@@ -11,9 +11,17 @@ type Props = {
   isOpen: boolean;
   position?: EModalPosition;
   width?: EModalWidth;
+  className?: string;
 };
 export const ModalCore: React.FC<Props> = (props) => {
-  const { children, handleClose, isOpen, position = EModalPosition.CENTER, width = EModalWidth.XXL } = props;
+  const {
+    children,
+    handleClose,
+    isOpen,
+    position = EModalPosition.CENTER,
+    width = EModalWidth.XXL,
+    className = "",
+  } = props;
 
   return (
     <Transition.Root show={isOpen} as={Fragment}>
@@ -44,7 +52,8 @@ export const ModalCore: React.FC<Props> = (props) => {
               <Dialog.Panel
                 className={cn(
                   "relative transform rounded-lg bg-custom-background-100 text-left shadow-custom-shadow-md transition-all w-full",
-                  width
+                  width,
+                  className
                 )}
               >
                 {children}
