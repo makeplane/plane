438e44d3ec18f1d9237d91e12960c2338459c59e
guru_sainath <gurusainath007@gmail.com>
2024-07-19 17:18:56 +0530

    chore: removed time tracking on enterprise (#669)

--- web/ee/constants/project/settings/features.tsx
+++ web/ee/constants/project/settings/features.tsx
@@ -1,1 +1,11 @@
-export * from "ce/constants/project/settings/features";
+import {
+  TFeatureList,
+  TProjectFeatures,
+  PROJECT_FEATURES_LIST as PROJECT_FEATURES_LIST_CE,
+} from "ce/constants/project/settings/features";
+
+export const PROJECT_FEATURES_LIST: TProjectFeatures = {
+  project_features: PROJECT_FEATURES_LIST_CE.project_features,
+};
+
+export type { TFeatureList, TProjectFeatures };
