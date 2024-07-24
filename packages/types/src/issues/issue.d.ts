b1637934f21f0a6c0b60c97ce735092d163c596c
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2024-07-24 17:42:23 +0530

    fix: build error

--- packages/types/src/issues/issue.d.ts
+++ packages/types/src/issues/issue.d.ts
@@ -1,7 +1,7 @@
-import {TIssuePriorities} from "../issues";
-import {TIssueAttachment} from "./issue_attachment";
-import {TIssueLink} from "./issue_link";
-import {TIssueReaction} from "./issue_reaction";
+import { TIssuePriorities } from "../issues";
+import { TIssueAttachment } from "./issue_attachment";
+import { TIssueLink } from "./issue_link";
+import { TIssueReaction } from "./issue_reaction";
 
 // new issue structure types
 
@@ -42,7 +42,7 @@ export type TBaseIssue = {
 export type TIssue = TBaseIssue & {
   description_html?: string;
   is_subscribed?: boolean;
-  parent?: Partial<TIssue>;
+  parent?: partial<TIssue>;
   issue_reactions?: TIssueReaction[];
   issue_attachment?: TIssueAttachment[];
   issue_link?: TIssueLink[];
