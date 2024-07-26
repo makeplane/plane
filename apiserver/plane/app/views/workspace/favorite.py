d8f13b3dd307582f116379bbdad16510d2bcbd8e
NarayanBavisetti <narayan3119@gmail.com>
2024-07-26 14:22:58 +0530

    chore: updated the workspace group favorite filter

--- apiserver/plane/app/views/workspace/favorite.py
+++ apiserver/plane/app/views/workspace/favorite.py
@@ -72,8 +72,13 @@ class WorkspaceFavoriteGroupEndpoint(BaseAPIView):
             user=request.user,
             workspace__slug=slug,
             parent_id=favorite_id,
-            project__project_projectmember__member=request.user,
-            project__project_projectmember__is_active=True,
+        ).filter(
+            Q(project__isnull=True)
+            | (
+                Q(project__isnull=False)
+                & Q(project__project_projectmember__member=request.user)
+                & Q(project__project_projectmember__is_active=True)
+            )
         )
         serializer = UserFavoriteSerializer(favorites, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
