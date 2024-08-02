400438aa3b81faba9cc2323cf7827f73442c825c
Akshita Goyal <36129505+gakshita@users.noreply.github.com>
2024-08-02 18:26:44 +0530

    fix: advanced bulk ops feature (#740)

--- web/ee/components/issues/bulk-operations/properties.tsx
+++ web/ee/components/issues/bulk-operations/properties.tsx
@@ -237,7 +237,7 @@ export const IssueBulkOperationsProperties: React.FC<Props> = observer((props) =
             )}
           />
         )}
-        {!isAdvancedBulkOpsEnabled && (
+        {isAdvancedBulkOpsEnabled && (
           <>
             {projectId && (
               <Controller
