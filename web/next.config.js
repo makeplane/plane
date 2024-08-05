019aeebb17305757b4d1ff9f5dcb9dd96becee70
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2024-08-05 13:25:42 +0530

    fix: yjs duplicate import error

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import("next").NextConfig} */
 require("dotenv").config({ path: ".env" });
+const path = require("path");
+
 const { withSentryConfig } = require("@sentry/nextjs");
 const withPWA = require("next-pwa")({
   dest: "public",
@@ -33,6 +35,13 @@ const nextConfig = {
       },
     ],
     unoptimized: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Ensure that all imports of 'yjs' resolve to the same instance
+      config.resolve.alias["yjs"] = path.resolve(__dirname, "node_modules/yjs");
+    }
+    return config;
   },
   async redirects() {
     return [
