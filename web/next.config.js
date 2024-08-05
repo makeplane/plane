33927b90e204aefd6b9d1ec60176e3828680d00f
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2024-08-05 13:49:35 +0530

    chore: disable pwa updates on development mode

--- web/next.config.js
+++ web/next.config.js
@@ -6,6 +6,7 @@ const path = require("path");
 const { withSentryConfig } = require("@sentry/nextjs");
 const withPWA = require("next-pwa")({
   dest: "public",
+  disable: process.env.NODE_ENV === "development",
 });
 
 const nextConfig = {
