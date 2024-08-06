9210da03dbf9173cfa24ce8a4fa99420218ce1d9
Henit Chobisa <chobisa.henit@gmail.com>
2024-08-06 13:03:01 +0530

    feat: added endpoint for creating service tokens

--- apiserver/plane/app/views/api.py
+++ apiserver/plane/app/views/api.py
@@ -61,6 +61,7 @@ class ApiTokenEndpoint(BaseAPIView):
             workspace__slug=slug,
             user=request.user,
             pk=pk,
+            is_service=False,
         )
         api_token.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
@@ -78,3 +79,45 @@ class ApiTokenEndpoint(BaseAPIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ServiceApiTokenEndpoint(BaseAPIView):
+    permission_classes = [
+        WorkspaceOwnerPermission,
+    ]
+
+    def post(self, request, slug):
+        workspace = Workspace.objects.get(slug=slug)
+
+        api_token = APIToken.objects.filter(
+            workspace=workspace,
+            is_service=True,
+        ).first()
+
+        if api_token:
+            return Response(
+                {
+                    "token": str(api_token.token),
+                },
+                status=status.HTTP_200_OK,
+            )
+        else:
+            # Check the user type
+            user_type = 1 if request.user.is_bot else 0
+
+            api_token = APIToken.objects.create(
+                label=str(uuid4().hex),
+                description="Service Token",
+                user=request.user,
+                workspace=workspace,
+                user_type=user_type,
+                is_service=True,
+            )
+            # Token will be only visible while creating
+            return Response(
+                {
+                    "token": str(api_token.token),
+                },
+                status=status.HTTP_201_CREATED,
+            )
+
