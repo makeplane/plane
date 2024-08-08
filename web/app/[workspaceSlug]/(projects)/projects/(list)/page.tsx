3c0574644ccd84461a0e55fd5caec62666c55c50
gakshita <akshitagoyal1516@gmail.com>
2024-08-08 16:50:51 +0530

    fix: project page root moved

--- web/app/[workspaceSlug]/(projects)/projects/(list)/page.tsx
+++ web/app/[workspaceSlug]/(projects)/projects/(list)/page.tsx
@@ -1,11 +1,4 @@
-"use client";
-import { useParams } from "next/navigation";
-import { useWorkspace } from "@/hooks/store";
-import ProjectsPageRoot from "@/plane-web/components/projects/page";
+import ProjectPageRoot from "@/plane-web/components/projects/page";
 
-const ProjectsPage = () => {
-  const { currentWorkspace } = useWorkspace();
-  const { workspaceSlug } = useParams();
-  return <ProjectsPageRoot currentWorkspace={currentWorkspace} workspaceSlug={workspaceSlug.toString()} />;
-};
+const ProjectsPage = () => <ProjectPageRoot />;
 export default ProjectsPage;
