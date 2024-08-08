30fc6cf914c3c1af1ea6fcec48fd8c5b2478bf0c
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2024-08-08 13:27:38 +0530

    chore: user favorites helper function added

---
+++ web/core/components/workspace/sidebar/favorites/favorite-items/common/helper.tsx
@@ -0,0 +1,51 @@
+"use client";
+// lucide
+import { Briefcase, FileText, Layers } from "lucide-react";
+// types
+import { IFavorite, TLogoProps } from "@plane/types";
+// ui
+import { ContrastIcon, DiceIcon, FavoriteFolderIcon } from "@plane/ui";
+import { Logo } from "@/components/common";
+
+const iconClassName = `flex-shrink-0 size-4 stroke-[1.5] m-auto`;
+
+export const FAVORITE_ITEM: Record<string, JSX.Element> = {
+  page: <FileText className={iconClassName} />,
+  project: <Briefcase className={iconClassName} />,
+  view: <Layers className={iconClassName} />,
+  module: <DiceIcon className={iconClassName} />,
+  cycle: <ContrastIcon className={iconClassName} />,
+  folder: <FavoriteFolderIcon className={iconClassName} />,
+};
+
+export const getFavoriteItemIcon = (type: string, logo?: TLogoProps | undefined) => (
+  <>
+    <div className="hidden group-hover:flex items-center justify-center size-5">
+      {FAVORITE_ITEM[type] || <FileText />}
+    </div>
+    <div className="flex items-center justify-center size-5 group-hover:hidden">
+      {logo?.in_use ? (
+        <Logo logo={logo} size={16} type={type === "project" ? "material" : "lucide"} />
+      ) : (
+        FAVORITE_ITEM[type] || <FileText />
+      )}
+    </div>
+  </>
+);
+
+export const generateFavoriteItemLink = (workspaceSlug: string, favorite: IFavorite) => {
+  switch (favorite.entity_type) {
+    case "project":
+      return `/${workspaceSlug}/projects/${favorite.project_id}/issues`;
+    case "cycle":
+      return `/${workspaceSlug}/projects/${favorite.project_id}/cycles/${favorite.entity_identifier}`;
+    case "module":
+      return `/${workspaceSlug}/projects/${favorite.project_id}/modules/${favorite.entity_identifier}`;
+    case "view":
+      return `/${workspaceSlug}/projects/${favorite.project_id}/views/${favorite.entity_identifier}`;
+    case "page":
+      return `/${workspaceSlug}/projects/${favorite.project_id}/pages/${favorite.entity_identifier}`;
+    default:
+      return `/${workspaceSlug}`;
+  }
+};
