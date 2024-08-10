0ff7f12b63bf8385c74b50088a62f9982b6c0923
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2024-08-10 16:30:44 +0530

    chore: workspace setting members and exports page permission validation added

--- web/app/[workspaceSlug]/(projects)/settings/exports/page.tsx
+++ web/app/[workspaceSlug]/(projects)/settings/exports/page.tsx
@@ -2,34 +2,24 @@
 
 import { observer } from "mobx-react";
 // components
+import { NotAuthorizedView } from "@/components/auth-screens";
 import { PageHead } from "@/components/core";
 import ExportGuide from "@/components/exporter/guide";
-// constants
-import { EUserWorkspaceRoles } from "@/constants/workspace";
 // hooks
 import { useUser, useWorkspace } from "@/hooks/store";
 
 const ExportsPage = observer(() => {
   // store hooks
-  const {
-    membership: { currentWorkspaceRole },
-  } = useUser();
+  const { canPerformWorkspaceViewerActions } = useUser();
   const { currentWorkspace } = useWorkspace();
 
   // derived values
-  const hasPageAccess =
-    currentWorkspaceRole && [EUserWorkspaceRoles.ADMIN, EUserWorkspaceRoles.MEMBER].includes(currentWorkspaceRole);
   const pageTitle = currentWorkspace?.name ? `${currentWorkspace.name} - Exports` : undefined;
 
-  if (!hasPageAccess)
-    return (
-      <>
-        <PageHead title={pageTitle} />
-        <div className="mt-10 flex h-full w-full justify-center p-4">
-          <p className="text-sm text-custom-text-300">You are not authorized to access this page.</p>
-        </div>
-      </>
-    );
+  // if user is not authorized to view this page
+  if (!canPerformWorkspaceViewerActions) {
+    return <NotAuthorizedView section="general" />;
+  }
 
   return (
     <>
--- web/app/[workspaceSlug]/(projects)/settings/members/page.tsx
+++ web/app/[workspaceSlug]/(projects)/settings/members/page.tsx
@@ -9,11 +9,11 @@ import { IWorkspaceBulkInviteFormData } from "@plane/types";
 // ui
 import { Button, TOAST_TYPE, setToast } from "@plane/ui";
 // components
+import { NotAuthorizedView } from "@/components/auth-screens";
 import { PageHead } from "@/components/core";
 import { SendWorkspaceInvitationModal, WorkspaceMembersList } from "@/components/workspace";
 // constants
 import { MEMBER_INVITED } from "@/constants/event-tracker";
-import { EUserWorkspaceRoles } from "@/constants/workspace";
 // helpers
 import { getUserRole } from "@/helpers/user.helper";
 // hooks
@@ -27,9 +27,7 @@ const WorkspaceMembersSettingsPage = observer(() => {
   const { workspaceSlug } = useParams();
   // store hooks
   const { captureEvent } = useEventTracker();
-  const {
-    membership: { currentWorkspaceRole },
-  } = useUser();
+  const { canPerformWorkspaceAdminActions, canPerformWorkspaceViewerActions } = useUser();
   const {
     workspace: { inviteMembersToWorkspace },
   } = useMember();
@@ -79,8 +77,12 @@ const WorkspaceMembersSettingsPage = observer(() => {
   };
 
   // derived values
-  const isAdmin = currentWorkspaceRole && [EUserWorkspaceRoles.ADMIN].includes(currentWorkspaceRole);
   const pageTitle = currentWorkspace?.name ? `${currentWorkspace.name} - Members` : undefined;
+
+  // if user is not authorized to view this page
+  if (!canPerformWorkspaceViewerActions) {
+    return <NotAuthorizedView section="general" />;
+  }
 
   return (
     <>
@@ -103,13 +105,13 @@ const WorkspaceMembersSettingsPage = observer(() => {
               onChange={(e) => setSearchQuery(e.target.value)}
             />
           </div>
-          {isAdmin && (
+          {canPerformWorkspaceAdminActions && (
             <Button variant="primary" size="sm" onClick={() => setInviteModal(true)}>
               Add member
             </Button>
           )}
         </div>
-        <WorkspaceMembersList searchQuery={searchQuery} isAdmin={isAdmin ?? false} />
+        <WorkspaceMembersList searchQuery={searchQuery} isAdmin={canPerformWorkspaceAdminActions} />
       </section>
     </>
   );
