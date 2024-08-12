9ac1d1793fcc08c06edfce159b4714f789125385
gakshita <akshitagoyal1516@gmail.com>
2024-08-12 18:15:27 +0530

    fix: build error

--- web/core/components/workspace/sidebar/workspace-menu.tsx
+++ web/core/components/workspace/sidebar/workspace-menu.tsx
@@ -61,6 +61,12 @@ export const SidebarWorkspaceMenu = observer(() => {
   }, [sidebarCollapsed, toggleWorkspaceMenu]);
   useOutsideClickDetector(actionSectionRef, () => setIsMenuActive(false));
 
+  const indicatorElement = (
+    <div className="flex-shrink-0">
+      <UpgradeBadge />
+    </div>
+  );
+
   return (
     <Disclosure as="div" defaultOpen>
       {!sidebarCollapsed && (
@@ -171,11 +177,7 @@ export const SidebarWorkspaceMenu = observer(() => {
                           />
                           {!sidebarCollapsed && <p className="text-sm leading-5 font-medium">{link.label}</p>}
                         </div>
-                        {!sidebarCollapsed && link.key === "active-cycles" && (
-                          <div className="flex-shrink-0">
-                            <UpgradeBadge flag="WORKSPACE_ACTIVE_CYCLES" />
-                          </div>
-                        )}
+                        {!sidebarCollapsed && link.key === "active-cycles" && indicatorElement}
                       </SidebarNavItem>
                     </Link>
                   </Tooltip>
