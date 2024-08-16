24412c055b5ee8f83ddaf7446c7377f4e73fe1a0
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2024-08-16 15:11:41 +0530

    chore: remove unused tasks

--- web/ce/constants/ai.ts
+++ web/ce/constants/ai.ts
@@ -1,21 +1,9 @@
 export enum AI_EDITOR_TASKS {
-  PARAPHRASE = "PARAPHRASE",
-  SIMPLIFY = "SIMPLIFY",
-  ELABORATE = "ELABORATE",
-  SUMMARIZE = "SUMMARIZE",
-  GET_TITLE = "GET_TITLE",
-  TONE = "TONE",
   ASK_ANYTHING = "ASK_ANYTHING",
 }
 
 export const LOADING_TEXTS: {
   [key in AI_EDITOR_TASKS]: string;
 } = {
-  [AI_EDITOR_TASKS.PARAPHRASE]: "Pi is paraphrasing",
-  [AI_EDITOR_TASKS.SIMPLIFY]: "Pi is simplifying",
-  [AI_EDITOR_TASKS.ELABORATE]: "Pi is elaborating",
-  [AI_EDITOR_TASKS.SUMMARIZE]: "Pi is summarizing",
-  [AI_EDITOR_TASKS.GET_TITLE]: "Pi is getting title",
-  [AI_EDITOR_TASKS.TONE]: "Pi is adjusting tone",
   [AI_EDITOR_TASKS.ASK_ANYTHING]: "Pi is generating response",
 };
