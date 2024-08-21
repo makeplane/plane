d90c69137a9c3ad296ea74d20eeb690df0d87272
mohmed6002090 <mohmed6002090@gmail.com>
2024-08-21 05:44:27 +0000

    chore: add python, postgres and redis to the idx config

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,8 +6,11 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     pkgs.nodejs_20
+    pkgs.python3
   ];
 
   services.docker.enable = true;
+  services.postgres.enable = true;
+  services.redis.enable = true;
 
 }
