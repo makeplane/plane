95edaca381638430b1ad76fa3daa0ae301e14d1d
mohmed6002090 <mohmed6002090@gmail.com>
2024-08-21 05:37:11 +0000

    chore: add IDX configuration so anyone can edit the project from idx.google.com

---
+++ .idx/dev.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }: {
+
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_20
+  ];
+
+  services.docker.enable = true;
+
+}
