287b6b88beadd50de4325ba565a631162f8310ba
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2024-09-03 13:12:29 +0530

    refactor: page helpers for document transformation

--- packages/editor/src/ce/extensions/document-extensions.tsx
+++ packages/editor/src/ce/extensions/document-extensions.tsx
@@ -1,14 +1,17 @@
+import { HocuspocusProvider } from "@hocuspocus/provider";
 import { Extensions } from "@tiptap/core";
 import { SlashCommand } from "@/extensions";
 // plane editor types
 import { TIssueEmbedConfig } from "@/plane-editor/types";
 // types
-import { TExtensions, TFileHandler } from "@/types";
+import { TExtensions, TFileHandler, TUserDetails } from "@/types";
 
 type Props = {
   disabledExtensions?: TExtensions[];
   fileHandler: TFileHandler;
   issueEmbedConfig: TIssueEmbedConfig | undefined;
+  provider: HocuspocusProvider;
+  userDetails: TUserDetails;
 };
 
 export const DocumentEditorAdditionalExtensions = (props: Props) => {
