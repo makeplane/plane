eea6ceaec486b90015efa53c1e5c6dfb2d94bd11
Anmol Singh Bhatia <121005188+anmolsinghbhatia@users.noreply.github.com>
2024-09-04 20:15:46 +0530

    fix: pwa intake issue comment section z-index (#5522)

--- web/core/components/issues/issue-detail/issue-activity/comments/comment-block.tsx
+++ web/core/components/issues/issue-detail/issue-activity/comments/comment-block.tsx
@@ -26,7 +26,7 @@ export const IssueCommentBlock: FC<TIssueCommentBlock> = observer((props) => {
   return (
     <div className={`relative flex gap-3 ${ends === "top" ? `pb-2` : ends === "bottom" ? `pt-2` : `py-2`}`}>
       <div className="absolute left-[13px] top-0 bottom-0 w-0.5 bg-custom-background-80" aria-hidden />
-      <div className="flex-shrink-0 relative w-7 h-7 rounded-full flex justify-center items-center z-10 bg-gray-500 text-white border border-white uppercase font-medium">
+      <div className="flex-shrink-0 relative w-7 h-7 rounded-full flex justify-center items-center z-[3] bg-gray-500 text-white border border-white uppercase font-medium">
         {comment.actor_detail.avatar && comment.actor_detail.avatar !== "" ? (
           <img
             src={comment.actor_detail.avatar}
