223dd702183dea23874e5ed0d19cb4d1f3d73685
Akshita Goyal <36129505+gakshita@users.noreply.github.com>
2024-09-11 13:13:53 +0530

    fix: projects scope dropdown (#1071)

--- web/ee/components/projects/root.tsx
+++ web/ee/components/projects/root.tsx
@@ -4,10 +4,11 @@ import { FC, useEffect } from "react";
 import { observer } from "mobx-react";
 // plane web components
 import { usePathname, useSearchParams } from "next/navigation";
+import { useProject } from "@/hooks/store";
 import { ProjectLayoutRoot } from "@/plane-web/components/projects";
 // plane web hooks
 import { useProjectFilter } from "@/plane-web/hooks/store";
-import { EProjectScope } from "@/plane-web/types/workspace-project-filters";
+import { EProjectFilters, EProjectScope } from "@/plane-web/types/workspace-project-filters";
 
 type TWorkspaceProjectsRoot = {
   workspaceSlug: string;
@@ -21,18 +22,23 @@ export const WorkspaceProjectsRoot: FC<TWorkspaceProjectsRoot> = observer((props
   const searchParams = useSearchParams();
   const showAllProjects = searchParams.get("show-all-projects");
   const pathname = usePathname();
+  const { loader } = useProject();
 
   useEffect(() => {
-    if (workspaceSlug)
+    if (workspaceSlug) {
+      let filtersToInit = [EProjectFilters.LAYOUT, EProjectFilters.ATTRIBUTES, EProjectFilters.DISPLAY_FILTERS];
+      filtersToInit = loader ? filtersToInit : [EProjectFilters.SCOPE, ...filtersToInit];
       initWorkspaceFilters(
         workspaceSlug,
         typeof showAllProjects === "string"
           ? showAllProjects === "true"
             ? EProjectScope.ALL_PROJECTS
             : EProjectScope.MY_PROJECTS
-          : undefined
+          : undefined,
+        filtersToInit
       );
-  }, [workspaceSlug, initWorkspaceFilters, pathname, showAllProjects]);
+    }
+  }, [workspaceSlug, initWorkspaceFilters, pathname, showAllProjects, loader]);
 
   return <ProjectLayoutRoot />;
 });
