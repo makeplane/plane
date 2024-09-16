a1bc348a4e3964e6654b6398031d30d59b2d438b
gakshita <akshitagoyal1516@gmail.com>
2024-09-16 16:40:43 +0530

    fix: progress donut

--- web/core/components/cycles/active-cycle/progress-donut.tsx
+++ web/core/components/cycles/active-cycle/progress-donut.tsx
@@ -1,40 +1,54 @@
-import { useState } from "react";
+import { use, useEffect, useRef, useState } from "react";
 import { CircularProgressIndicator, Row } from "@plane/ui";
 import { TProgress } from "./types";
+import { set } from "lodash";
 
 type Props = {
   progress: TProgress;
 };
-const progress = { percentage: 10, pendingIssues: 60, completedIssues: 10, daysLeft: 9 };
+const progress = { percentage: 10, pendingIssues: 60, completedIssues: 100, daysLeft: 9 };
 
 const ProgressDonut = (props: Props) => {
   const [hoverStep, setHoverStep] = useState<number>(0);
-  //   const { progress } = props;
-  {
-    /* TODO: fix the stroke color */
-  }
+  const intervalId = useRef<NodeJS.Timer | null>(null);
+
+  useEffect(() => {
+    if (hoverStep === 1) {
+      intervalId.current = setInterval(() => {
+        setHoverStep((state) => state + 1);
+      }, 1000);
+    }
+    if (hoverStep === 4) {
+      intervalId.current && clearInterval(intervalId.current);
+      setHoverStep(0);
+    }
+  }, [hoverStep]);
   return (
-    // <Row
-    //   className="group flex items-center justify-between py-1"
-    //   //   onMouseEnter={() => setHoverStep(1)}
-    //   //   onMouseDown={() => setHoverStep(0)}
-    // >
-    <CircularProgressIndicator size={70} percentage={50} strokeWidth={2} strokeColor={"text-green-100"}>
-      <span className="text-lg text-custom-primary-200 font-medium">{`${hoverStep === 3 ? progress.daysLeft : progress.percentage}%`}</span>
-      {hoverStep > 0 && (
-        <div className="text-custom-primary-200 text-[7px]">
-          {hoverStep < 3 && (
-            <span className="font-semibold">{hoverStep === 1 ? progress.completedIssues : progress.pendingIssues}</span>
-          )}
-          <div className="uppercase">
-            {hoverStep === 1 && "Issues done"}
-            {hoverStep === 2 && "Issues pending"}
-            {hoverStep === 3 && "Days left"}
+    <div
+      className="group flex items-center justify-between py-1"
+      onMouseEnter={() => setHoverStep(1)}
+      onMouseDown={() => setHoverStep(0)}
+    >
+      <CircularProgressIndicator size={82} percentage={50} strokeWidth={2} strokeColor={"text-green-100"}>
+        <span className="text-lg text-custom-primary-200 font-medium">{`${hoverStep === 3 ? progress.daysLeft : progress.percentage}%`}</span>
+
+        {hoverStep === 1 && (
+          <div className="text-custom-primary-200 text-[8px] uppercase whitespace-nowrap">
+            <span className="font-semibold">{progress.completedIssues}</span> Issues <br /> done
           </div>
-        </div>
-      )}
-    </CircularProgressIndicator>
-    // </Row>
+        )}
+        {hoverStep === 2 && (
+          <div className="text-custom-primary-200 text-[8px] uppercase whitespace-nowrap">
+            <span className="font-semibold">{progress.pendingIssues}</span> Issues <br /> pending
+          </div>
+        )}
+        {hoverStep === 3 && (
+          <div className="text-custom-primary-200 text-[8px] uppercase whitespace-nowrap">
+            Days <br /> left
+          </div>
+        )}
+      </CircularProgressIndicator>
+    </div>
   );
 };
 export default ProgressDonut;
