d94046e69abd0e911ae0115820401c12ed517a17
Palanikannan M <akashmalinimurugu@gmail.com>
2024-09-18 20:55:48 +0530

    fix: added preview image for upload

--- packages/editor/src/core/extensions/custom-image/components/image-uploader.tsx
+++ packages/editor/src/core/extensions/custom-image/components/image-uploader.tsx
@@ -1,6 +1,8 @@
-import { ChangeEvent, useCallback, useEffect, useRef } from "react";
+import { ChangeEvent, useCallback, useEffect, useRef, useState } from "react";
 import { Editor } from "@tiptap/core";
 import { ImageIcon } from "lucide-react";
+import { Spinner } from "@plane/ui";
+
 // helpers
 import { cn } from "@/helpers/common";
 // hooks
@@ -29,8 +31,22 @@ export const CustomImageUploader = (props: {
   const { loading, uploadFile } = useUploader({ onUpload, editor });
   const { handleUploadClick, ref: internalRef } = useFileUpload();
   const { draggedInside, onDrop, onDragEnter, onDragLeave } = useDropZone({ uploader: uploadFile });
+  const imageRef = useRef<HTMLImageElement>(null);
 
   const localRef = useRef<HTMLInputElement | null>(null);
+  const [previewUrl, setPreviewUrl] = useState<string | null>(null);
+  const [width, setWidth] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (previewUrl) {
+      const closestEditorContainer = imageRef.current?.closest(".editor-container");
+      if (closestEditorContainer) {
+        const editorWidth = closestEditorContainer?.clientWidth;
+        const initialWidth = Math.max(editorWidth * 0.35, 100);
+        setWidth(initialWidth);
+      }
+    }
+  }, [previewUrl, imageRef.current]);
 
   const onFileChange = useCallback(
     (e: ChangeEvent<HTMLInputElement>) => {
@@ -38,54 +54,88 @@ export const CustomImageUploader = (props: {
       if (file) {
         if (isFileValid(file)) {
           editor.storage.image.uploadInProgress = true;
+          const reader = new FileReader();
+          reader.onload = () => {
+            setPreviewUrl(reader.result as string);
+          };
+          reader.readAsDataURL(file);
           uploadFile(file);
         }
       }
     },
-    [uploadFile]
+    [uploadFile, editor.storage.image]
   );
 
   useEffect(() => {
-    // no need to validate as the file is already validated before the drop onto
-    // the editor
     if (existingFile) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setPreviewUrl(reader.result as string);
+      };
+      reader.readAsDataURL(existingFile);
       uploadFile(existingFile);
     }
   }, [existingFile, uploadFile]);
 
   return (
-    <div
-      className={cn(
-        "image-upload-component flex items-center justify-start gap-2 py-3 px-2 rounded-lg text-custom-text-300 hover:text-custom-text-200 bg-custom-background-90 hover:bg-custom-background-80 border border-dashed border-custom-border-300 cursor-pointer transition-all duration-200 ease-in-out",
-        {
-          "bg-custom-background-80 text-custom-text-200": draggedInside,
-        },
-        {
-          "text-custom-primary-200 bg-custom-primary-100/10": selected,
-        }
+    <>
+      {!previewUrl ? (
+        <div
+          className={cn(
+            "image-upload-component flex items-center justify-start gap-2 py-3 px-2 rounded-lg text-custom-text-300 hover:text-custom-text-200 bg-custom-background-90 hover:bg-custom-background-80 border border-dashed border-custom-border-300 cursor-pointer transition-all duration-200 ease-in-out",
+            {
+              "bg-custom-background-80 text-custom-text-200": draggedInside,
+            },
+            {
+              "text-custom-primary-200 bg-custom-primary-100/10": selected,
+            }
+          )}
+          onDrop={onDrop}
+          onDragOver={onDragEnter}
+          onDragLeave={onDragLeave}
+          contentEditable={false}
+          onClick={handleUploadClick}
+        >
+          <ImageIcon className="size-4" />
+          <div className="text-base font-medium">
+            {loading
+              ? "Uploading..."
+              : draggedInside
+                ? "Drop image here"
+                : existingFile
+                  ? "Uploading..."
+                  : "Add an image"}
+          </div>
+          <input
+            className="size-0 overflow-hidden"
+            ref={(element) => {
+              localRef.current = element;
+              assignRef(fileInputRef, element);
+              assignRef(internalRef as RefType, element);
+            }}
+            hidden
+            type="file"
+            accept=".jpg,.jpeg,.png,.webp"
+            onChange={onFileChange}
+          />
+        </div>
+      ) : (
+        <div className="relative" style={{ width: `${width}px` }}>
+          <img
+            ref={imageRef}
+            src={previewUrl}
+            alt="Preview"
+            className={cn("w-full h-auto rounded-md blur-50", {
+              "blur-sm opacity-80": loading,
+            })}
+          />
+          {loading && (
+            <div className="absolute inset-0 flex items-center justify-center">
+              <Spinner height="20px" width="20px" />
+            </div>
+          )}
+        </div>
       )}
-      onDrop={onDrop}
-      onDragOver={onDragEnter}
-      onDragLeave={onDragLeave}
-      contentEditable={false}
-      onClick={handleUploadClick}
-    >
-      <ImageIcon className="size-4" />
-      <div className="text-base font-medium">
-        {loading ? "Uploading..." : draggedInside ? "Drop image here" : existingFile ? "Uploading..." : "Add an image"}
-      </div>
-      <input
-        className="size-0 overflow-hidden"
-        ref={(element) => {
-          localRef.current = element;
-          assignRef(fileInputRef, element);
-          assignRef(internalRef as RefType, element);
-        }}
-        hidden
-        type="file"
-        accept=".jpg,.jpeg,.png,.webp"
-        onChange={onFileChange}
-      />
-    </div>
+    </>
   );
 };
