85824bdd201c29437cfa9b6893c0783fcbf0f7f0
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2024-09-19 14:19:05 +0530

    chore: remove unused function

--- packages/editor/src/core/extensions/slash-commands/command-menu.tsx
+++ packages/editor/src/core/extensions/slash-commands/command-menu.tsx
@@ -10,20 +10,6 @@ type Props = {
   range: any;
 };
 
-const updateScrollView = (container: HTMLElement, item: HTMLElement) => {
-  const containerHeight = container.offsetHeight;
-  const itemHeight = item ? item.offsetHeight : 0;
-
-  const top = item.offsetTop;
-  const bottom = top + itemHeight;
-
-  if (top < container.scrollTop) {
-    container.scrollTop -= container.scrollTop - top + 5;
-  } else if (bottom > containerHeight + container.scrollTop) {
-    container.scrollTop += bottom - containerHeight - container.scrollTop + 5;
-  }
-};
-
 export const SlashCommandsMenu = (props: Props) => {
   const { items: sections, command } = props;
   // states
