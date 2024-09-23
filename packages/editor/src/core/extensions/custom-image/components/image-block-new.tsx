8303acb7234b38627331e59fe6292ee5e1377de4
Palanikannan M <akashmalinimurugu@gmail.com>
2024-09-23 19:01:15 +0530

    fix: loading of images

--- packages/editor/src/core/extensions/custom-image/components/image-block-new.tsx
+++ packages/editor/src/core/extensions/custom-image/components/image-block-new.tsx
@@ -300,40 +300,39 @@ export const CustomImageBlockNew = (props: CustomImageBlockProps) => {
         )}
       </div>
       {/* if there is no src (remote or local), show the upload button */}
-      {!displayedSrc ||
-        (isRemoteImageBeingUploaded && uploadEntity?.event === "insert" && (
-          <div
-            className={cn(
-              "image-upload-component flex items-center justify-start gap-2 py-3 px-2 rounded-lg text-custom-text-300 hover:text-custom-text-200 bg-custom-background-90 hover:bg-custom-background-80 border border-dashed border-custom-border-300 cursor-pointer transition-all duration-200 ease-in-out",
-              {
-                "bg-custom-background-80 text-custom-text-200": draggedInside,
-              },
-              {
-                "text-custom-primary-200 bg-custom-primary-100/10": selected,
-              }
-            )}
-            onDrop={onDrop}
-            onDragOver={onDragEnter}
-            onDragLeave={onDragLeave}
-            contentEditable={false}
-            onClick={handleUploadClick}
-          >
-            <ImageIcon className="size-4" />
-            <div className="text-base font-medium">{draggedInside ? "Drop image here" : "Add an image"}</div>
-            <input
-              className="size-0 overflow-hidden"
-              ref={(element) => {
-                localRef.current = element;
-                assignRef(fileInputRef, element);
-                assignRef(internalRef as RefType, element);
-              }}
-              hidden
-              type="file"
-              accept=".jpg,.jpeg,.png,.webp"
-              onChange={onFileChange}
-            />
-          </div>
-        ))}
+      {(!displayedSrc || (isRemoteImageBeingUploaded && uploadEntity?.event === "insert")) && (
+        <div
+          className={cn(
+            "image-upload-component flex items-center justify-start gap-2 py-3 px-2 rounded-lg text-custom-text-300 hover:text-custom-text-200 bg-custom-background-90 hover:bg-custom-background-80 border border-dashed border-custom-border-300 cursor-pointer transition-all duration-200 ease-in-out",
+            {
+              "bg-custom-background-80 text-custom-text-200": draggedInside,
+            },
+            {
+              "text-custom-primary-200 bg-custom-primary-100/10": selected,
+            }
+          )}
+          onDrop={onDrop}
+          onDragOver={onDragEnter}
+          onDragLeave={onDragLeave}
+          contentEditable={false}
+          onClick={handleUploadClick}
+        >
+          <ImageIcon className="size-4" />
+          <div className="text-base font-medium">{draggedInside ? "Drop image here" : "Add an image"}</div>
+          <input
+            className="size-0 overflow-hidden"
+            ref={(element) => {
+              localRef.current = element;
+              assignRef(fileInputRef, element);
+              assignRef(internalRef as RefType, element);
+            }}
+            hidden
+            type="file"
+            accept=".jpg,.jpeg,.png,.webp"
+            onChange={onFileChange}
+          />
+        </div>
+      )}
     </>
   );
 };
