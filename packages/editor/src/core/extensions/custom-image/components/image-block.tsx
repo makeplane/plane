cf1bd9e435e8b43d84ec4aa1031b7447e2459e76
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2024-09-24 14:01:50 +0530

    fix: pass update image size

--- packages/editor/src/core/extensions/custom-image/components/image-block.tsx
+++ packages/editor/src/core/extensions/custom-image/components/image-block.tsx
@@ -158,8 +158,8 @@ export const CustomImageBlock: React.FC<CustomImageNodeViewProps> = (props) => {
         containerClassName="absolute top-1 right-1 z-20 bg-black/40 rounded opacity-0 pointer-events-none group-hover/image-component:opacity-100 group-hover/image-component:pointer-events-auto transition-opacity"
         image={{
           src,
-          height,
-          width,
+          height: size.height,
+          width: size.width,
         }}
       />
       {editor.isEditable && selected && <div className="absolute inset-0 size-full bg-custom-primary-500/30" />}
