54036fd746dc9e802a00d61840e466ccd5a28065
sharma01ketan <sharma01ketan@gmail.com>
2024-09-30 17:00:29 +0530

    change in requirements

--- web/core/store/workspace-draft.store.ts
+++ web/core/store/workspace-draft.store.ts
@@ -1,116 +1,180 @@
-import { action, makeObservable, observable } from "mobx";
-import { ViewFlags, TLoader, TIssue } from "@plane/types";
-import { WorkspaceDraftService } from "@/services/workspace-draft.service";
+import { action, makeObservable, runInAction } from "mobx";
+// base class
+import {
+  IssuePaginationOptions,
+  TBulkOperationsPayload,
+  TIssue,
+  TIssuesResponse,
+  TLoader,
+  ViewFlags,
+} from "@plane/types";
+// services
+import { WorkspaceService } from "@/plane-web/services";
+import { IBaseIssuesStore, BaseIssuesStore } from "./issue/helpers/base-issues.store";
+import { IIssueRootStore } from "./issue/root.store";
+import { IWorkspaceIssuesFilter } from "./issue/workspace";
+import { IWorkspaceDraftsFilter } from "./workspace-draft_filter.store";
+// types
 
-export interface IWorkspaceDraftStore {
+export interface IWorkspaceDrafts extends IBaseIssuesStore {
+  // observable
   viewFlags: ViewFlags;
-  drafts: TIssue[]
-  createWorkspaceDraft(workspaceSlug: string, data: Partial<TIssue>): Promise<TIssue>;
-  getWorkspaceDrafts(workspaceSlug: string) : Promise<TIssue[]>;
-  getWorkspaceDraftById: (workspaceSlug: string, draftId: string, loadType?: TLoader) => Promise<TIssue>;
-  deleteWorkspaceDraft: (workspaceSlug: string, draftId: string) => Promise<void>;
+  // actions
+  fetchIssues: (
+    workspaceSlug: string,
+    viewId: string,
+    loadType: TLoader,
+    options: IssuePaginationOptions//can i just use this as it is or do i need to implement a separate one
+  ) => Promise<TIssuesResponse | undefined>; //the TIssuesResponse expects TBaseIssue
+  fetchIssuesWithExistingPagination: (
+    workspaceSlug: string,
+    viewId: string,
+    loadType: TLoader
+  ) => Promise<TIssuesResponse | undefined>;//this again expects TBaseIssue
+  fetchNextIssues: (
+    workspaceSlug: string,
+    viewId: string,
+    groupId?: string,
+    subGroupId?: string
+  ) => Promise<TIssuesResponse | undefined>;//this again expects TBaseIssue
+
+  // createIssue: (workspaceSlug: string, projectId: string, data: Partial<TIssue>) => Promise<Partial<TIssue>>;//Partial, return value
+  // updateIssue: (workspaceSlug: string, projectId: string, issueId: string, data: Partial<TIssue>) => Promise<void>;//Partial
+  createIssue: (workspaceSlug: string, projectId: string, data: Partial<TIssue>) => Promise<TIssue>;
+  updateIssue: (workspaceSlug: string, projectId: string, issueId: string, data: Partial<TIssue>) => Promise<void>;
+  quickAddIssue: undefined;
+  clear(): void;
 }
 
-export class WorkspaceDraftStore implements IWorkspaceDraftStore {
-  drafts: TIssue[] = [];
+export class WorkspaceDrafts extends BaseIssuesStore implements IWorkspaceDrafts {
   viewFlags = {
-      enableQuickAdd: false,
-      enableIssueCreation: true,
-      enableInlineEditing: true,
+    enableQuickAdd: true,
+    enableIssueCreation: true,
+    enableInlineEditing: true,
   };
-  workspaceDraftService: WorkspaceDraftService;
+  // service
+  workspaceService;
+  // filterStore
+  draftFilterStore;
 
-  constructor() {
+  constructor(_rootStore: IIssueRootStore, draftFilterStore: IWorkspaceDraftsFilter) {
+    super(_rootStore, draftFilterStore);
+
     makeObservable(this, {
-      drafts: observable,
-      createWorkspaceDraft: action,
-      getWorkspaceDrafts: action,
-      getWorkspaceDraftById: action,
-      deleteWorkspaceDraft: action,
+      // action
+      fetchIssues: action,
+      fetchNextIssues: action,
+      fetchIssuesWithExistingPagination: action,
     });
-    this.workspaceDraftService = new WorkspaceDraftService();
+    // services
+    this.workspaceService = new WorkspaceService();
+    // filter store
+    this.draftFilterStore = draftFilterStore;
   }
 
+  fetchParentStats = () => {};
 
-  addDraft = (draft: TIssue) => {
-    if(draft && !this.drafts.some(existingDraft => existingDraft.id === draft.id))
-      this.drafts.push(draft);
-  }
+  /** */
+  updateParentStats = () => {};
 
-  removeDraft = (draftId: string) => {
-    if (draftId) {
-      const index = this.drafts.findIndex(existingDraft => existingDraft.id === draftId);
-      if (index !== -1) {
-        this.drafts.splice(index, 1);
-      }
+  /**
+   * This method is called to fetch the first issues of pagination
+   * @param workspaceSlug
+   * @param viewId
+   * @param loadType
+   * @param options
+   * @returns
+   */
+  fetchIssues = async (
+    workspaceSlug: string,
+    viewId: string,
+    loadType: TLoader,
+    options: IssuePaginationOptions,
+    isExistingPaginationOptions: boolean = false
+  ) => {
+    try {
+      // set loader and clear store
+      runInAction(() => {
+        this.setLoader(loadType);
+      });
+      this.clear(!isExistingPaginationOptions);// #### clear the store
+
+      // get params from pagination options
+      const params = this.draftFilterStore?.getFilterParams(options, viewId, undefined, undefined, undefined);// #### get the params for the filters applied
+      // call the fetch issues API with the params
+
+      //response is TIssueResponse, which expects TBaseIssue
+      const response = await this.workspaceService.getViewIssues(workspaceSlug, params, {//this needs to be getDraftIssues
+        signal: this.controller.signal,//what is the use of this signal
+      });
+
+      // after fetching issues, call the base method to process the response further
+      this.onfetchIssues(response, options, workspaceSlug, undefined, undefined, !isExistingPaginationOptions);
+      return response;
+    } catch (error) {
+      // set loader to undefined if errored out
+      this.setLoader(undefined);
+      throw error;
     }
   };
 
-  createWorkspaceDraft = async (
-    workspaceSlug: string,
-    data: Partial<TIssue>,
-  ) => {
-   try{
-     //perform an API call
-     const response = await this.workspaceDraftService.createDraftIssue(workspaceSlug,data);
+  /**
+   * This method is called subsequent pages of pagination
+   * if groupId/subgroupId is provided, only that specific group's next page is fetched
+   * else all the groups' next page is fetched
+   * @param workspaceSlug
+   * @param viewId
+   * @param groupId
+   * @param subGroupId
+   * @returns
+   */
+  fetchNextIssues = async (workspaceSlug: string, viewId: string, groupId?: string, subGroupId?: string) => {
+    const cursorObject = this.getPaginationData(groupId, subGroupId);
+    // if there are no pagination options and the next page results do not exist the return
+    if (!this.paginationOptions || (cursorObject && !cursorObject?.nextPageResults)) return;
+    try {
+      // set Loader
+      this.setLoader("pagination", groupId, subGroupId);
 
-     // add Issue to Store
-     this.addDraft(response);
+      // get params from stored pagination options
+      const params = this.draftFilterStore?.getFilterParams(
+        this.paginationOptions,
+        viewId,
+        this.getNextCursor(groupId, subGroupId),
+        groupId,
+        subGroupId
+      );
+      // call the fetch issues API with the params for next page in issues
+      const response = await this.workspaceService.getViewIssues(workspaceSlug, params);
 
-     return response;
-
-   } catch(error){
-    console.error(error)
-    throw error;
-   }
-  }
-
-  getWorkspaceDrafts = async (
-    workspaceSlug: string,
-  ) => {
-    try{
-      // call the fetch issues API with the params
-      const response = await this.workspaceDraftService.getDraftIssues(workspaceSlug);
-      if(Array.isArray(response))
-        response.forEach((draft)=>{
-          this.addDraft(draft);
-        })
-      // after fetching issues, call the base method to process the response further
+      // after the next page of issues are fetched, call the base method to process the response
+      this.onfetchNexIssues(response, groupId, subGroupId);
       return response;
-    } catch (error){
-      console.error(error)
+    } catch (error) {
+      // set Loader as undefined if errored out
+      this.setLoader(undefined, groupId, subGroupId);
       throw error;
     }
-  }
+  };
 
-  getWorkspaceDraftById = async (
-    workspaceSlug: string,
-    draftId: string,
-  ) => {
-    try{
-      // call the fetch issues API with the params
-      const response = await this.workspaceDraftService.getDraftIssueById(workspaceSlug, draftId);
-      this.addDraft(response);
-      return response;
-    } catch (error){
-      console.error(error)
-      throw error;
-    }
-  }
+  /**
+   * This Method exists to fetch the first page of the issues with the existing stored pagination
+   * This is useful for refetching when filters, groupBy, orderBy etc changes
+   * @param workspaceSlug
+   * @param viewId
+   * @param loadType
+   * @returns
+   */
+  fetchIssuesWithExistingPagination = async (workspaceSlug: string, viewId: string, loadType: TLoader) => {
+    if (!this.paginationOptions) return;
+    return await this.fetchIssues(workspaceSlug, viewId, loadType, this.paginationOptions, true);
+  };
 
-  deleteWorkspaceDraft = async (workspaceSlug: string, draftId: string) => {
-    //Make API call
-    try{
-      await this.workspaceDraftService.deleteDraftIssue(workspaceSlug,draftId);
-      this.removeDraft(draftId);
-    } catch (error){
-      console.error(error)
-      throw error;
-    }
-  }
+  // Using aliased names as they cannot be overridden in other stores
+  archiveBulkIssues = this.bulkArchiveIssues;
+  updateIssue = this.issueUpdate;
+  archiveIssue = this.issueArchive;
 
-  // TODO IMPLEMENT UPDATE
-  // async updateWorkspaceDraft(){
-  // }
-
+  // Setting them as undefined as they can not performed on workspace issues
+  quickAddIssue = undefined;
 }
