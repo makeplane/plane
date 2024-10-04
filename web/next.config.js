0f4e8762ff7828b6ff46e80332c7a9b63e1de6ce
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2024-10-04 17:29:31 +0530

    chore: update next.config

--- web/next.config.js
+++ web/next.config.js
@@ -10,22 +10,22 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   output: "standalone",
-  // async headers() {
-  //   return [
-  //     {
-  //       source: "/(.*)?",
-  //       headers: [
-  //         { key: "X-Frame-Options", value: "SAMEORIGIN" },
-  //         {
-  //           key: "Referrer-Policy",
-  //           value: "origin-when-cross-origin",
-  //         },
-  //         { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
-  //         { key: "Cross-Origin-Embedder-Policy", value: "credentialless" },
-  //       ],
-  //     },
-  //   ];
-  // },
+  async headers() {
+    return [
+      {
+        source: "/(.*)?",
+        headers: [
+          { key: "X-Frame-Options", value: "SAMEORIGIN" },
+          // {
+          //   key: "Referrer-Policy",
+          //   value: "origin-when-cross-origin",
+          // },
+          // { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
+          // { key: "Cross-Origin-Embedder-Policy", value: "credentialless" },
+        ],
+      },
+    ];
+  },
   images: {
     unoptimized: true,
   },
