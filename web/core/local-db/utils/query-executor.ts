84956cdb49d8bb33d32d26059a37f932d5924c53
Satish Gandham <satish.iitg@gmail.com>
2024-10-04 16:07:31 +0530

    Handle cases when DB intilization failed

--- web/core/local-db/utils/query-executor.ts
+++ web/core/local-db/utils/query-executor.ts
@@ -1,9 +1,7 @@
-// import { SQL } from "./sqlite";
-
 import { persistence } from "../storage.sqlite";
 
 export const runQuery = async (sql: string) => {
-  const data = await persistence.db.exec({
+  const data = await persistence.db?.exec({
     sql,
     rowMode: "object",
     returnValue: "resultRows",
