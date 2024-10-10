d17f2e8989f1925c32b640bd322766f31a4db680
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2024-10-10 14:00:41 +0530

    chore: update constants

--- packages/editor/src/core/constants/common.ts
+++ packages/editor/src/core/constants/common.ts
@@ -1,61 +1,61 @@
 export const COLORS_LIST: {
   key: string;
   label: string;
-  textColorVariable: string;
-  backgroundColorVariable: string;
+  textColor: string;
+  backgroundColor: string;
 }[] = [
   {
     key: "gray",
     label: "Gray",
-    textColorVariable: "--editor-colors-gray-text",
-    backgroundColorVariable: "--editor-colors-gray-background",
+    textColor: "var(--editor-colors-gray-text)",
+    backgroundColor: "var(--editor-colors-gray-background)",
   },
   {
     key: "peach",
     label: "Peach",
-    textColorVariable: "--editor-colors-peach-text",
-    backgroundColorVariable: "--editor-colors-peach-background",
+    textColor: "var(--editor-colors-peach-text)",
+    backgroundColor: "var(--editor-colors-peach-background)",
   },
   {
     key: "pink",
     label: "Pink",
-    textColorVariable: "--editor-colors-pink-text",
-    backgroundColorVariable: "--editor-colors-pink-background",
+    textColor: "var(--editor-colors-pink-text)",
+    backgroundColor: "var(--editor-colors-pink-background)",
   },
   {
     key: "orange",
     label: "Orange",
-    textColorVariable: "--editor-colors-orange-text",
-    backgroundColorVariable: "--editor-colors-orange-background",
+    textColor: "var(--editor-colors-orange-text)",
+    backgroundColor: "var(--editor-colors-orange-background)",
   },
   {
     key: "green",
     label: "Green",
-    textColorVariable: "--editor-colors-green-text",
-    backgroundColorVariable: "--editor-colors-green-background",
+    textColor: "var(--editor-colors-green-text)",
+    backgroundColor: "var(--editor-colors-green-background)",
   },
   {
     key: "light-blue",
     label: "Light blue",
-    textColorVariable: "--editor-colors-light-blue-text",
-    backgroundColorVariable: "--editor-colors-light-blue-background",
+    textColor: "var(--editor-colors-light-blue-text)",
+    backgroundColor: "var(--editor-colors-light-blue-background)",
   },
   {
     key: "dark-blue",
     label: "Dark blue",
-    textColorVariable: "--editor-colors-dark-blue-text",
-    backgroundColorVariable: "--editor-colors-dark-blue-background",
+    textColor: "var(--editor-colors-dark-blue-text)",
+    backgroundColor: "var(--editor-colors-dark-blue-background)",
   },
   {
     key: "purple",
     label: "Purple",
-    textColorVariable: "--editor-colors-purple-text",
-    backgroundColorVariable: "--editor-colors-purple-background",
+    textColor: "var(--editor-colors-purple-text)",
+    backgroundColor: "var(--editor-colors-purple-background)",
   },
   // {
   //   key: "pink-blue-gradient",
   //   label: "Pink blue gradient",
-  //   textColorVariable: "--editor-colors-pink-blue-gradient-text",
-  //   backgroundColorVariable: "--editor-colors-pink-blue-gradient-background",
+  //   textColor: "var(--editor-colors-pink-blue-gradient-text)",
+  //   backgroundColor: "var(--editor-colors-pink-blue-gradient-background)",
   // },
 ];
