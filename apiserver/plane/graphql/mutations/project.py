0ecd2b1b604f6662277750f9eb4c56272893f66a
guru_sainath <gurusainath007@gmail.com>
2024-10-12 02:18:39 +0530

    chore: updated project create mutation (#1453)

--- apiserver/plane/graphql/mutations/project.py
+++ apiserver/plane/graphql/mutations/project.py
@@ -16,12 +16,18 @@ from plane.graphql.permissions.project import (
     ProjectAdminPermission,
 )
 from plane.graphql.types.project import ProjectType
-from plane.db.models import Workspace, Project, ProjectMember, UserFavorite
+from plane.db.models import (
+    Workspace,
+    Project,
+    ProjectMember,
+    UserFavorite,
+    IssueUserProperty,
+    State,
+)
 
 
 @strawberry.type
 class ProjectMutation:
-
     @strawberry.mutation(
         extensions=[
             PermissionExtension(permissions=[WorkspaceBasePermission()])
@@ -33,26 +39,99 @@ class ProjectMutation:
         slug: str,
         name: str,
         identifier: str,
-        network: int,
         description: Optional[str] = "",
+        network: int = 2,
         project_lead: Optional[str] = None,
         logo_props: Optional[JSON] = {},
+        page_view: Optional[bool] = False,
     ) -> ProjectType:
         workspace = await sync_to_async(Workspace.objects.get)(slug=slug)
         project = await sync_to_async(Project.objects.create)(
             name=name,
             identifier=identifier,
-            network=network,
             description=description,
-            project_lead=project_lead,
+            network=network,
             logo_props=logo_props,
+            page_view=page_view,
             workspace=workspace,
         )
+
         # add the user as a admin of the project
         _ = await sync_to_async(ProjectMember.objects.create)(
             project=project,
             member=info.context.user,
             role=20,
+        )
+        # creating the issue property for the user
+        _ = await sync_to_async(IssueUserProperty.objects.create)(
+            project_id=project.id,
+            user_id=info.context.user.id,
+        )
+
+        # if lead was passed we can add the user as a lead
+        if project_lead:
+            # add the user as a admin of the project
+            _ = await sync_to_async(ProjectMember.objects.create)(
+                project=project,
+                member_id=project_lead,
+                role=20,
+            )
+            # creating the issue property for the user
+            _ = await sync_to_async(IssueUserProperty.objects.create)(
+                project_id=project.id,
+                user_id=project_lead,
+            )
+
+        # Default states
+        states = [
+            {
+                "name": "Backlog",
+                "color": "#A3A3A3",
+                "sequence": 15000,
+                "group": "backlog",
+                "default": True,
+            },
+            {
+                "name": "Todo",
+                "color": "#3A3A3A",
+                "sequence": 25000,
+                "group": "unstarted",
+            },
+            {
+                "name": "In Progress",
+                "color": "#F59E0B",
+                "sequence": 35000,
+                "group": "started",
+            },
+            {
+                "name": "Done",
+                "color": "#16A34A",
+                "sequence": 45000,
+                "group": "completed",
+            },
+            {
+                "name": "Cancelled",
+                "color": "#EF4444",
+                "sequence": 55000,
+                "group": "cancelled",
+            },
+        ]
+
+        # creating the default states for the project
+        _ = await sync_to_async(State.objects.bulk_create)(
+            [
+                State(
+                    name=state["name"],
+                    color=state["color"],
+                    project=project,
+                    sequence=state["sequence"],
+                    workspace=workspace,
+                    group=state["group"],
+                    default=state.get("default", False),
+                    created_by=info.context.user,
+                )
+                for state in states
+            ]
         )
 
         return project
@@ -65,16 +144,27 @@ class ProjectMutation:
     async def updateProject(
         self,
         id: strawberry.ID,
-        name: str,
         slug: str,
-        organizationSize: str,
-        ownerId: str,
+        name: Optional[str] = None,
+        identifier: Optional[str] = None,
+        description: Optional[str] = None,
+        network: Optional[int] = None,
+        logo_props: Optional[JSON] = None,
+        page_view: Optional[bool] = None,
     ) -> ProjectType:
         project = await sync_to_async(Project.objects.get)(id=id)
-        project.name = name
-        project.slug = slug
-        project.organization_size = organizationSize
-        project.owner_id = ownerId
+        if name is not None:
+            project.name = name
+        if identifier is not None:
+            project.identifier = identifier
+        if description is not None:
+            project.description = description
+        if network is not None:
+            project.network = network
+        if logo_props is not None:
+            project.logo_props = logo_props
+        if page_view is not None:
+            project.page_view = page_view
         await sync_to_async(project.save)()
         return project
 
@@ -91,7 +181,6 @@ class ProjectMutation:
 
 @strawberry.type
 class ProjectInviteMutation:
-
     @strawberry.mutation(
         extensions=[
             PermissionExtension(permissions=[ProjectMemberPermission()])
@@ -115,7 +204,6 @@ class ProjectInviteMutation:
 
 @strawberry.type
 class ProjectFavoriteMutation:
-
     @strawberry.mutation(
         extensions=[PermissionExtension(permissions=[ProjectBasePermission()])]
     )
