37817a4445c12d061034a23a35e61b6d5d7f742f
violet <167108906+avioletheart@users.noreply.github.com>
2024-11-04 08:08:30 +0900

    feat: allow binding apiserver on ipv6

--- apiserver/bin/docker-entrypoint-api.sh
+++ apiserver/bin/docker-entrypoint-api.sh
@@ -32,4 +32,4 @@ python manage.py create_bucket
 # Clear Cache before starting to remove stale values
 python manage.py clear_cache
 
-exec gunicorn -w "$GUNICORN_WORKERS" -k uvicorn.workers.UvicornWorker plane.asgi:application --bind 0.0.0.0:"${PORT:-8000}" --max-requests 1200 --max-requests-jitter 1000 --access-logfile -
+exec gunicorn -w "$GUNICORN_WORKERS" -k uvicorn.workers.UvicornWorker plane.asgi:application --bind "${BIND_HOST:-[::]}":"${PORT:-8000}" --max-requests 1200 --max-requests-jitter 1000 --access-logfile -
