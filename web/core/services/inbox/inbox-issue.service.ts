eed2ca77ef2a00a46aaff806606d4bbafc18695b
Akshita Goyal <36129505+gakshita@users.noreply.github.com>
2024-11-05 16:07:27 +0530

    fix: added workspaceslug in renderChildren of project settings (#5951)
    * fix: added workspaceslug in renderChildren of project settings
    
    * fix: updated apis
    
    * fix: types
    
    * fix: added editor
    
    * fix: handled avatar for intake

--- web/core/services/inbox/inbox-issue.service.ts
+++ web/core/services/inbox/inbox-issue.service.ts
@@ -77,17 +77,15 @@ export class InboxIssueService extends APIService {
   }
 
   async retrievePublishForm(workspaceSlug: string, projectId: string): Promise<TInboxForm> {
-    return this.get(`/api/workspaces/${workspaceSlug}/projects/${projectId}/publish-intake/`)
+    return this.get(`/api/workspaces/${workspaceSlug}/projects/${projectId}/intake-settings/`)
       .then((response) => response?.data)
       .catch((error) => {
         throw error?.response?.data;
       });
   }
 
-  async updatePublishForm(workspaceSlug: string, projectId: string, is_disabled: boolean): Promise<TInboxIssue> {
-    return this.patch(`/api/workspaces/${workspaceSlug}/projects/${projectId}/publish-intake/`, {
-      is_disabled,
-    })
+  async updatePublishForm(workspaceSlug: string, projectId: string, data: Partial<TInboxForm>): Promise<TInboxForm> {
+    return this.patch(`/api/workspaces/${workspaceSlug}/projects/${projectId}/intake-settings/`, data)
       .then((response) => response?.data)
       .catch((error) => {
         throw error?.response?.data;
