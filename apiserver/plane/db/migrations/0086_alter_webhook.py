a2425a19d472854cdc420f92edda573a3ae0afad
pablohashescobar <nikhilschacko@gmail.com>
2024-11-18 16:35:08 +0530

    chore: subscription logs

--- apiserver/plane/db/migrations/0086_alter_webhook.py
+++ apiserver/plane/db/migrations/0086_alter_webhook.py
@@ -2,6 +2,9 @@
 
 from django.db import migrations, models
 import plane.db.models.webhook
+from django.conf import settings
+import django.db.models.deletion
+import uuid
 
 
 class Migration(migrations.Migration):
@@ -85,5 +88,179 @@ class Migration(migrations.Migration):
         ),
         migrations.DeleteModel(
             name="TeamPage",
+        ),
+        migrations.CreateModel(
+            name="IssueVersion",
+            fields=[
+                (
+                    "created_at",
+                    models.DateTimeField(
+                        auto_now_add=True, verbose_name="Created At"
+                    ),
+                ),
+                (
+                    "updated_at",
+                    models.DateTimeField(
+                        auto_now=True, verbose_name="Last Modified At"
+                    ),
+                ),
+                (
+                    "deleted_at",
+                    models.DateTimeField(
+                        blank=True, null=True, verbose_name="Deleted At"
+                    ),
+                ),
+                (
+                    "id",
+                    models.UUIDField(
+                        db_index=True,
+                        default=uuid.uuid4,
+                        editable=False,
+                        primary_key=True,
+                        serialize=False,
+                        unique=True,
+                    ),
+                ),
+                ("parent", models.UUIDField(blank=True, null=True)),
+                ("state", models.UUIDField(blank=True, null=True)),
+                ("estimate_point", models.UUIDField(blank=True, null=True)),
+                (
+                    "name",
+                    models.CharField(
+                        max_length=255, verbose_name="Issue Name"
+                    ),
+                ),
+                ("description", models.JSONField(blank=True, default=dict)),
+                (
+                    "description_html",
+                    models.TextField(blank=True, default="<p></p>"),
+                ),
+                (
+                    "description_stripped",
+                    models.TextField(blank=True, null=True),
+                ),
+                ("description_binary", models.BinaryField(null=True)),
+                (
+                    "priority",
+                    models.CharField(
+                        choices=[
+                            ("urgent", "Urgent"),
+                            ("high", "High"),
+                            ("medium", "Medium"),
+                            ("low", "Low"),
+                            ("none", "None"),
+                        ],
+                        default="none",
+                        max_length=30,
+                        verbose_name="Issue Priority",
+                    ),
+                ),
+                ("start_date", models.DateField(blank=True, null=True)),
+                ("target_date", models.DateField(blank=True, null=True)),
+                (
+                    "sequence_id",
+                    models.IntegerField(
+                        default=1, verbose_name="Issue Sequence ID"
+                    ),
+                ),
+                ("sort_order", models.FloatField(default=65535)),
+                ("completed_at", models.DateTimeField(null=True)),
+                ("archived_at", models.DateField(null=True)),
+                ("is_draft", models.BooleanField(default=False)),
+                (
+                    "external_source",
+                    models.CharField(blank=True, max_length=255, null=True),
+                ),
+                (
+                    "external_id",
+                    models.CharField(blank=True, max_length=255, null=True),
+                ),
+                ("type", models.UUIDField(blank=True, null=True)),
+                (
+                    "last_saved_at",
+                    models.DateTimeField(default=django.utils.timezone.now),
+                ),
+                ("owned_by", models.UUIDField()),
+                (
+                    "assignees",
+                    django.contrib.postgres.fields.ArrayField(
+                        base_field=models.UUIDField(),
+                        blank=True,
+                        default=list,
+                        size=None,
+                    ),
+                ),
+                (
+                    "labels",
+                    django.contrib.postgres.fields.ArrayField(
+                        base_field=models.UUIDField(),
+                        blank=True,
+                        default=list,
+                        size=None,
+                    ),
+                ),
+                ("cycle", models.UUIDField(blank=True, null=True)),
+                (
+                    "modules",
+                    django.contrib.postgres.fields.ArrayField(
+                        base_field=models.UUIDField(),
+                        blank=True,
+                        default=list,
+                        size=None,
+                    ),
+                ),
+                ("properties", models.JSONField(default=dict)),
+                ("meta", models.JSONField(default=dict)),
+                (
+                    "created_by",
+                    models.ForeignKey(
+                        null=True,
+                        on_delete=django.db.models.deletion.SET_NULL,
+                        related_name="%(class)s_created_by",
+                        to=settings.AUTH_USER_MODEL,
+                        verbose_name="Created By",
+                    ),
+                ),
+                (
+                    "issue",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="versions",
+                        to="db.issue",
+                    ),
+                ),
+                (
+                    "project",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="project_%(class)s",
+                        to="db.project",
+                    ),
+                ),
+                (
+                    "updated_by",
+                    models.ForeignKey(
+                        null=True,
+                        on_delete=django.db.models.deletion.SET_NULL,
+                        related_name="%(class)s_updated_by",
+                        to=settings.AUTH_USER_MODEL,
+                        verbose_name="Last Modified By",
+                    ),
+                ),
+                (
+                    "workspace",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="workspace_%(class)s",
+                        to="db.workspace",
+                    ),
+                ),
+            ],
+            options={
+                "verbose_name": "Issue Version",
+                "verbose_name_plural": "Issue Versions",
+                "db_table": "issue_versions",
+                "ordering": ("-created_at",),
+            },
         ),
     ]
