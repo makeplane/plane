fa2e60101f5df3d2fda4772b089cb0b6eda49397
Vamsi Krishna <46787868+mathalav55@users.noreply.github.com>
2024-11-26 19:15:21 +0530

    [WEB-2774] Chore: re-ordering functionality for entities in favorites. (#6078)
    * fixed re order for favorites
    
    * fixed lint errors
    
    * added reorder
    
    * fixed reorder inside folder
    
    * fixed lint issues
    
    * memoized reorder
    
    * removed unnecessary comments
    
    * seprated duplicate logic to a common file
    
    * removed code comments

--- web/core/components/workspace/sidebar/projects-list-item.tsx
+++ web/core/components/workspace/sidebar/projects-list-item.tsx
@@ -298,12 +298,13 @@ export const SidebarProjectsListItem: React.FC<Props> = observer((props) => {
     <>
       <PublishProjectModal isOpen={publishModalOpen} project={project} onClose={() => setPublishModal(false)} />
       <LeaveProjectModal project={project} isOpen={leaveProjectModalOpen} onClose={() => setLeaveProjectModal(false)} />
-      <Disclosure key={`${project.id}_${URLProjectId}`} ref={projectRef} defaultOpen={isProjectListOpen} as="div">
+      <Disclosure key={`${project.id}_${URLProjectId}`}  defaultOpen={isProjectListOpen} as="div">
         <div
           id={`sidebar-${projectId}-${projectListType}`}
           className={cn("relative", {
             "bg-custom-sidebar-background-80 opacity-60": isDragging,
           })}
+          ref={projectRef}
         >
           <DropIndicator classNames="absolute top-0" isVisible={instruction === "DRAG_OVER"} />
           <div
