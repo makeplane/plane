8634733506ee265416600a938ae2e9310252ae0d
sriram veeraghanta <veeraghanta.sriram@gmail.com>
2024-12-04 17:10:00 +0530

    fix: build errors on space app

--- space/ee/services/intake.service.ts
+++ space/ee/services/intake.service.ts
@@ -1,5 +1,5 @@
 // types
-import { API_BASE_URL } from "@/helpers/common.helper";
+import { API_BASE_URL } from "@plane/constants";
 import { APIService } from "@/services/api.service";
 import { TIntakeIssueForm } from "@/types/intake";
 // helpers
