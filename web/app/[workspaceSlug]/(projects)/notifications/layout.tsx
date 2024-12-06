b296cd0784d40549d159a4144f3dcc9db16bcebd
vamsi krishna <matalav55@gmail.com>
2024-12-06 13:39:52 +0530

    added feature flag

--- web/app/[workspaceSlug]/(projects)/notifications/layout.tsx
+++ web/app/[workspaceSlug]/(projects)/notifications/layout.tsx
@@ -6,7 +6,7 @@ import { NotificationsSidebarRoot } from "@/components/workspace-notifications";
 export default function ProjectInboxIssuesLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="relative w-full h-full overflow-hidden flex items-center">
-      <NotificationsSidebar />
+      <NotificationsSidebarRoot />
       <div className="w-full h-full overflow-hidden overflow-y-auto">{children}</div>
     </div>
   );
