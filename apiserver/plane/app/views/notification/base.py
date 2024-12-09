1f5ae035a302e3d93bbd5d5712814cf310d7838d
Bavisetti Narayan <72156168+NarayanBavisetti@users.noreply.github.com>
2024-12-09 12:58:06 +0530

    chore: grouped archive and snoozed notifications (#1891)

--- apiserver/plane/app/views/notification/base.py
+++ apiserver/plane/app/views/notification/base.py
@@ -168,6 +168,27 @@ class NotificationViewSet(BaseViewSet, BasePaginator):
         allowed_roles=[ROLE.ADMIN, ROLE.MEMBER, ROLE.GUEST], level="WORKSPACE"
     )
     def partial_update(self, request, slug, pk):
+
+        notification_ids = request.data.get("notification_ids")
+        if notification_ids:
+            notifications = Notification.objects.filter(
+                workspace__slug=slug,
+                receiver=request.user,
+                id__in=request.data.get("notification_ids"),
+            )
+            for notification in notifications:
+                notification_data = {
+                    "snoozed_till": request.data.get("snoozed_till", None)
+                }
+                serializer = NotificationSerializer(
+                    notification, data=notification_data, partial=True
+                )
+                if serializer.is_valid():
+                    serializer.save()
+                else:
+                    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            serializer = NotificationSerializer(notifications, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         notification = Notification.objects.get(
             workspace__slug=slug, pk=pk, receiver=request.user
         )
@@ -186,6 +207,18 @@ class NotificationViewSet(BaseViewSet, BasePaginator):
         allowed_roles=[ROLE.ADMIN, ROLE.MEMBER, ROLE.GUEST], level="WORKSPACE"
     )
     def mark_read(self, request, slug, pk):
+        notification_ids = request.data.get("notification_ids")
+        if notification_ids:
+            notifications = Notification.objects.filter(
+                workspace__slug=slug,
+                receiver=request.user,
+                id__in=request.data.get("notification_ids"),
+            )
+            for notification in notifications:
+                notification.read_at = timezone.now()
+                notification.save()
+            serializer = NotificationSerializer(notifications, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         notification = Notification.objects.get(
             receiver=request.user, workspace__slug=slug, pk=pk
         )
@@ -198,6 +231,18 @@ class NotificationViewSet(BaseViewSet, BasePaginator):
         allowed_roles=[ROLE.ADMIN, ROLE.MEMBER, ROLE.GUEST], level="WORKSPACE"
     )
     def mark_unread(self, request, slug, pk):
+        notification_ids = request.data.get("notification_ids")
+        if notification_ids:
+            notifications = Notification.objects.filter(
+                workspace__slug=slug,
+                receiver=request.user,
+                id__in=request.data.get("notification_ids"),
+            )
+            for notification in notifications:
+                notification.read_at = None
+                notification.save()
+            serializer = NotificationSerializer(notifications, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         notification = Notification.objects.get(
             receiver=request.user, workspace__slug=slug, pk=pk
         )
@@ -210,6 +255,18 @@ class NotificationViewSet(BaseViewSet, BasePaginator):
         allowed_roles=[ROLE.ADMIN, ROLE.MEMBER, ROLE.GUEST], level="WORKSPACE"
     )
     def archive(self, request, slug, pk):
+        notification_ids = request.data.get("notification_ids")
+        if notification_ids:
+            notifications = Notification.objects.filter(
+                workspace__slug=slug,
+                receiver=request.user,
+                id__in=request.data.get("notification_ids"),
+            )
+            for notification in notifications:
+                notification.archived_at = timezone.now()
+                notification.save()
+            serializer = NotificationSerializer(notifications, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         notification = Notification.objects.get(
             receiver=request.user, workspace__slug=slug, pk=pk
         )
@@ -222,6 +279,18 @@ class NotificationViewSet(BaseViewSet, BasePaginator):
         allowed_roles=[ROLE.ADMIN, ROLE.MEMBER, ROLE.GUEST], level="WORKSPACE"
     )
     def unarchive(self, request, slug, pk):
+        notification_ids = request.data.get("notification_ids")
+        if notification_ids:
+            notifications = Notification.objects.filter(
+                workspace__slug=slug,
+                receiver=request.user,
+                id__in=request.data.get("notification_ids"),
+            )
+            for notification in notifications:
+                notification.archived_at = None
+                notification.save()
+            serializer = NotificationSerializer(notifications, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         notification = Notification.objects.get(
             receiver=request.user, workspace__slug=slug, pk=pk
         )
