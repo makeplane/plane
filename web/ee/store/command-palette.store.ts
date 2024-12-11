73305e9dd187fc773e2730f5fa69290d4c1a371c
Prateek Shourya <prateekshourya29@gmail.com>
2024-12-11 20:27:33 +0530

    refactor: enhance command palette modularity (#1909)

--- web/ee/store/command-palette.store.ts
+++ web/ee/store/command-palette.store.ts
@@ -1,1 +1,12 @@
-export * from "ce/store/command-palette.store";
+import { makeObservable } from "mobx";
+// types / constants
+import { BaseCommandPaletteStore, IBaseCommandPaletteStore } from "@/store/base-command-palette.store";
+
+export type ICommandPaletteStore = IBaseCommandPaletteStore;
+
+export class CommandPaletteStore extends BaseCommandPaletteStore implements ICommandPaletteStore {
+  constructor() {
+    super();
+    makeObservable(this, {});
+  }
+}
