024a870e50590b0cd77eac42d6583e1a73fe11aa
pablohashescobar <nikhilschacko@gmail.com>
2024-12-20 11:41:49 +0530

    chore: optimize worker startups

--- apiserver/plane/bgtasks/recent_visited_task.py
+++ apiserver/plane/bgtasks/recent_visited_task.py
@@ -8,11 +8,18 @@ from celery import shared_task
 # Module imports
 from plane.db.models import UserRecentVisit, Workspace
 from plane.utils.exception_logger import log_exception
+from plane.settings.redis import redis_instance
 
 
 @shared_task(queue=settings.TASK_LOW_QUEUE)
 def recent_visited_task(entity_name, entity_identifier, user_id, project_id, slug):
     try:
+        ri = redis_instance()
+        # Check if the same entity is set in redis for the user
+        if ri.exists(f"recent_visited:{user_id}:{entity_name}:{entity_identifier}"):
+            return
+
+        # Check if the same entity is set in redis for the workspace
         workspace = Workspace.objects.get(slug=slug)
         recent_visited = UserRecentVisit.objects.filter(
             entity_name=entity_name,
@@ -51,6 +58,10 @@ def recent_visited_task(entity_name, entity_identifier, user_id, project_id, slu
             recent_activity.updated_by_id = user_id
             recent_activity.save(update_fields=["created_by_id", "updated_by_id"])
 
+        # Set in redis
+        ri.set(
+            f"recent_visited:{user_id}:{entity_name}:{entity_identifier}", 1, ex=60 * 10
+        )
         return
     except Exception as e:
         log_exception(e)
