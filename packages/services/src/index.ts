d57a242b5e245c0c076d9c39ace6be5dd945dc16
sriram veeraghanta <veeraghanta.sriram@gmail.com>
2024-12-23 01:13:22 +0530

    fix: import fixes

--- packages/services/src/index.ts
+++ packages/services/src/index.ts
@@ -4,7 +4,6 @@ export * from "./developer";
 export * from "./auth";
 export * from "./cycle";
 export * from "./dashboard";
-export * from "./favorite";
 export * from "./instance";
 export * from "./intake";
 export * from "./module";
