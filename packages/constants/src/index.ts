043f4eaa5e93e81f6e386f7c88c08ed76b4c98b4
sriram veeraghanta <veeraghanta.sriram@gmail.com>
2024-12-23 01:51:30 +0530

    chore: common services package (#6255)
    * fix: initial services package setup
    
    * fix: services packages updates
    
    * fix: services changes
    
    * fix: merge conflicts
    
    * chore: file structuring
    
    * fix: import fixes

--- packages/constants/src/index.ts
+++ packages/constants/src/index.ts
@@ -1,3 +1,4 @@
+export * from "./ai";
 export * from "./auth";
 export * from "./endpoints";
 export * from "./file";
---
+++ packages/services/src/workspace/index.ts
@@ -0,0 +1,5 @@
+export * from "./invitation.service";
+export * from "./member.service";
+export * from "./notification.service";
+export * from "./view.service";
+export * from "./workspace.service";
