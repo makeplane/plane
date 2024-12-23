d54c1bae0388ae76e7449bf345a1dfb6239a23df
Aaryan Khandelwal <65252264+aaryan610@users.noreply.github.com>
2024-12-23 15:25:40 +0530

    [PE-93] regression: mention users highlight color, reomve bot users from search list (#6258)
    * chore: remove bot users in mention
    
    * fix: user highlight color
    
    ---------
    
    Co-authored-by: NarayanBavisetti <narayan3119@gmail.com>

--- space/core/components/editor/embeds/mentions/user.tsx
+++ space/core/components/editor/embeds/mentions/user.tsx
@@ -26,9 +26,12 @@ export const EditorUserMention: React.FC<Props> = observer((props) => {
 
   return (
     <div
-      className={cn("not-prose inline px-1 py-0.5 rounded bg-yellow-500/20 text-yellow-500 no-underline", {
-        "bg-custom-primary-100/20 text-custom-primary-100": id === currentUser?.id,
-      })}
+      className={cn(
+        "not-prose inline px-1 py-0.5 rounded bg-custom-primary-100/20 text-custom-primary-100 no-underline",
+        {
+          "bg-yellow-500/20 text-yellow-500": id === currentUser?.id,
+        }
+      )}
     >
       @{userDetails?.member__display_name}
     </div>
--- web/core/components/editor/embeds/mentions/user.tsx
+++ web/core/components/editor/embeds/mentions/user.tsx
@@ -58,9 +58,9 @@ export const EditorUserMention: React.FC<Props> = observer((props) => {
   return (
     <div
       className={cn(
-        "not-prose group/user-mention inline px-1 py-0.5 rounded bg-yellow-500/20 text-yellow-500 no-underline",
+        "not-prose group/user-mention inline px-1 py-0.5 rounded bg-custom-primary-100/20 text-custom-primary-100 no-underline",
         {
-          "bg-custom-primary-100/20 text-custom-primary-100": id === currentUser?.id,
+          "bg-yellow-500/20 text-yellow-500": id === currentUser?.id,
         }
       )}
     >
