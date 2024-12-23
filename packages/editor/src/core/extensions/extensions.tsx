20260f0720761143fd394539c94c72a990e9d697
M. Palanikannan <73993394+Palanikannan1437@users.noreply.github.com>
2024-12-23 20:04:34 +0530

    [PE-101] feat: smooth scrolling in editor while dragging and dropping nodes (#6233)
    * fix: smoother drag scrolling
    
    * fix: refactoring out common fns
    
    * fix: moved to mouse events instead of drag
    
    * fix: improving the drag preview
    
    * fix: added better selection logic
    
    * fix: drag handle new way almost working
    
    * fix: drag-handle old behaviour with better scrolling
    
    * fix: remove experiments
    
    * fix: better scroll thresholds
    
    * fix: transition to drop cursor added
    
    * fix: drag handling speed
    
    * fix: cleaning up listeners
    
    * fix: common out selection and dragging logic
    
    * fix: scroll threshold logic fixed

--- packages/editor/src/core/extensions/extensions.tsx
+++ packages/editor/src/core/extensions/extensions.tsx
@@ -82,7 +82,8 @@ export const CoreEditorExtensions = (args: TArguments): Extensions => {
         },
       },
       dropcursor: {
-        class: "text-custom-text-300",
+        class:
+          "text-custom-text-300 transition-all motion-reduce:transition-none motion-reduce:hover:transform-none duration-200 ease-[cubic-bezier(0.165, 0.84, 0.44, 1)]",
       },
       ...(enableHistory ? {} : { history: false }),
     }),
