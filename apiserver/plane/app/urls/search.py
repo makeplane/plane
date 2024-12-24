6c25ed3a54cbc409bcf10dbb1bd4968749c4ca02
NarayanBavisetti <narayan3119@gmail.com>
2024-12-24 19:12:42 +0530

    chore: workspace entity search endpoint

--- apiserver/plane/app/urls/search.py
+++ apiserver/plane/app/urls/search.py
@@ -16,7 +16,7 @@ urlpatterns = [
         name="project-issue-search",
     ),
     path(
-        "workspaces/<str:slug>/projects/<uuid:project_id>/entity-search/",
+        "workspaces/<str:slug>/entity-search/",
         SearchEndpoint.as_view(),
         name="entity-search",
     ),
