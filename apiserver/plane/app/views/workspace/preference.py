1d147b030b3281e07f8bd4040e5f89f98b3aac57
sangeethailango <sangeethailango21@gmail.com>
2025-01-03 17:09:38 +0530

    Create home preference if not exist

--- apiserver/plane/app/views/workspace/preference.py
+++ apiserver/plane/app/views/workspace/preference.py
@@ -1,8 +1,8 @@
 # Module imports
 from ..base import BaseAPIView
 from plane.db.models.workspace import WorkspaceHomePreference
-from plane.app.permissions import (allow_permission, ROLE)
-from plane.db.models.workspace import WorkspaceHomePreference   
+from plane.app.permissions import allow_permission, ROLE
+from plane.db.models.workspace import WorkspaceHomePreference
 from plane.db.models import Workspace
 
 # Third party imports
@@ -12,32 +12,42 @@ from rest_framework import status
 
 class WorkspacePreferenceViewSet(BaseAPIView):
     model = WorkspaceHomePreference
-    
+
     @allow_permission([ROLE.ADMIN, ROLE.MEMBER, ROLE.GUEST], level="WORKSPACE")
     def get(self, request, slug):
         workspace = Workspace.objects.get(slug=slug, owner=request.user)
-    
-        quick_links = WorkspaceHomePreference.objects.filter(user=request.user, workspace_id=workspace.id, key=WorkspaceHomePreference.HomeWidgetKeys.QUICK_LINKS).exists()
-        recents = WorkspaceHomePreference.objects.filter(user=request.user, workspace_id=workspace.id, key=WorkspaceHomePreference.HomeWidgetKeys.RECENTS).exists()
-        my_stickies = WorkspaceHomePreference.objects.filter(user=request.user, workspace_id=workspace.id, key=WorkspaceHomePreference.HomeWidgetKeys.MY_STICKIES).exists()
-        new_at_plane = WorkspaceHomePreference.objects.filter(user=request.user, workspace_id=workspace.id, key=WorkspaceHomePreference.HomeWidgetKeys.NEW_AT_PLANE).exists()
-        quick_tutorial = WorkspaceHomePreference.objects.filter(user=request.user, workspace_id=workspace.id, key=WorkspaceHomePreference.HomeWidgetKeys.QUICK_TUTORIAL).exists()
 
-        if quick_links and recents and my_stickies and new_at_plane and quick_tutorial:
-            return Response(WorkspaceHomePreference.objects.filter(user=request.user, workspace_id=workspace.id).values("user", "workspace", "sort_order", "is_enabled", "key"), status=status.HTTP_201_CREATED)  
+        preference_keys = WorkspaceHomePreference.objects.filter(
+            user=request.user, workspace_id=workspace.id
+        ).values_list("key", flat=True)
 
-        home_preferences = [
-            WorkspaceHomePreference.HomeWidgetKeys.QUICK_LINKS, 
-            WorkspaceHomePreference.HomeWidgetKeys.RECENTS, 
-            WorkspaceHomePreference.HomeWidgetKeys.MY_STICKIES,
-            WorkspaceHomePreference.HomeWidgetKeys.NEW_AT_PLANE,
-            WorkspaceHomePreference.HomeWidgetKeys.QUICK_TUTORIAL
-        ]
+        create_preference_keys = []
 
-        for preference in home_preferences:
-            WorkspaceHomePreference.objects.create(user_id=request.user.id, workspace_id=workspace.id, key=preference)
+        print(WorkspaceHomePreference.HomeWidgetKeys.choices, "Print Choices")
 
-        return Response({"Created"}, status=status.HTTP_201_CREATED)
-    
+        keys = [key for key, _ in WorkspaceHomePreference.HomeWidgetKeys.choices]
 
-    
+        for preference in keys:
+            if preference not in preference_keys:
+                create_preference_keys.append(preference)
+
+        WorkspaceHomePreference.objects.bulk_create(
+            [
+                WorkspaceHomePreference(
+                    key=key,
+                    user=request.user,
+                    workspace=workspace,
+                    sort_order=(index + 1) * 1000,
+                )
+                for index, key in enumerate(create_preference_keys)
+            ],
+            batch_size=10,
+            ignore_conflicts=True,
+        )
+
+        workspace_user_home_preferences = WorkspaceHomePreference.objects.filter(
+            workspace=workspace, user=request.user
+        ).values("key", "is_enabled", "sort_order", "config")
+
+        # for preference in home_preferences:
+        return Response(workspace_user_home_preferences, status=status.HTTP_200_OK)
