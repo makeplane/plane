837c2b326c529b6bd6eac6fcf8c64e9898f931f3
Vamsi krishna <matalav55@gmail.com>
2025-01-06 20:30:07 +0530

    fix: added tooltips for truncated favorites

--- web/core/components/workspace/sidebar/favorites/favorite-items/common/favorite-item-title.tsx
+++ web/core/components/workspace/sidebar/favorites/favorite-items/common/favorite-item-title.tsx
@@ -3,6 +3,7 @@ import React, { FC } from "react";
 import { observer } from "mobx-react";
 import Link from "next/link";
 import { Tooltip } from "@plane/ui";
+import { cn } from "@plane/utils";
 import { useAppTheme } from "@/hooks/store";
 import { usePlatformOS } from "@/hooks/use-platform-os";
 
@@ -28,7 +29,7 @@ export const FavoriteItemTitle: FC<Props> = observer((props) => {
   };
 
   return (
-    <Tooltip tooltipContent={title} isMobile={isMobile} disabled={!isSidebarCollapsed} position="right">
+    <Tooltip tooltipContent={title} isMobile={isMobile} position="right" className={cn(!isSidebarCollapsed && "ml-8")}>
       <Link href={href} className={isSidebarCollapsed ? collapsedClass : linkClass} draggable onClick={handleOnClick}>
         <span className="flex items-center justify-center size-5">{icon}</span>
         {!isSidebarCollapsed && <span className="text-sm leading-5 font-medium flex-1 truncate">{title}</span>}
