45b1a9e6c3ddc9d831b6dfca190349e56b67a342
Vamsi krishna <matalav55@gmail.com>
2025-01-06 15:12:54 +0530

    chore: added issue activity filters to local storage chore: added sort order to local storage

--- web/core/components/issues/issue-detail/issue-activity/activity-comment-root.tsx
+++ web/core/components/issues/issue-detail/issue-activity/activity-comment-root.tsx
@@ -1,5 +1,7 @@
 import { FC } from "react";
 import { observer } from "mobx-react";
+// constants
+import { TSORT_ORDER } from "@/constants/common";
 // hooks
 import { useIssueDetail } from "@/hooks/store";
 // plane web components
@@ -21,18 +23,27 @@ type TIssueActivityCommentRoot = {
   activityOperations: TActivityOperations;
   showAccessSpecifier?: boolean;
   disabled?: boolean;
+  sortOrder: TSORT_ORDER;
 };
 
 export const IssueActivityCommentRoot: FC<TIssueActivityCommentRoot> = observer((props) => {
-  const { workspaceSlug, issueId, selectedFilters, activityOperations, showAccessSpecifier, projectId, disabled } =
-    props;
+  const {
+    workspaceSlug,
+    issueId,
+    selectedFilters,
+    activityOperations,
+    showAccessSpecifier,
+    projectId,
+    disabled,
+    sortOrder,
+  } = props;
   // hooks
   const {
     activity: { getActivityCommentByIssueId },
     comment: {},
   } = useIssueDetail();
 
-  const activityComments = getActivityCommentByIssueId(issueId);
+  const activityComments = getActivityCommentByIssueId(issueId, sortOrder);
 
   if (!activityComments || (activityComments && activityComments.length <= 0)) return <></>;
 
