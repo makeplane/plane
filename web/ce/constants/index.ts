d3d3bf79f9bb41d90c16a1740bc5f60d7f9bd0e2
Aaryan Khandelwal <65252264+aaryan610@users.noreply.github.com>
2025-01-06 20:31:07 +0530

    refactor: page actions (#6327)

--- web/ce/constants/index.ts
+++ web/ce/constants/index.ts
@@ -2,6 +2,7 @@ export * from "./ai";
 export * from "./estimates";
 export * from "./gantt-chart";
 export * from "./issues";
+export * from "./page";
 export * from "./project";
 export * from "./user-permissions";
 export * from "./workspace";
