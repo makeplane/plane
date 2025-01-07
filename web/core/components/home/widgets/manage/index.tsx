8012c111113d8c120353d7c2df77f95a8a53b973
gakshita <akshitagoyal1516@gmail.com>
2025-01-07 15:40:43 +0530

    fix: remvoved btn

--- web/core/components/home/widgets/manage/index.tsx
+++ web/core/components/home/widgets/manage/index.tsx
@@ -4,7 +4,7 @@ import { FC } from "react";
 import { observer } from "mobx-react";
 // plane types
 // plane ui
-import { Button, EModalWidth, ModalCore } from "@plane/ui";
+import { EModalWidth, ModalCore } from "@plane/ui";
 import { WidgetList } from "./widget-list";
 
 export type TProps = {
@@ -22,14 +22,6 @@ export const ManageWidgetsModal: FC<TProps> = observer((props) => {
       <div className="p-4">
         <div className="font-medium text-xl">Manage widgets</div>
         <WidgetList workspaceSlug={workspaceSlug} />
-        <div className="px-5 py-4 flex items-center justify-end gap-2 border-t-[0.5px] border-custom-border-200">
-          <Button variant="neutral-primary" size="md" onClick={handleOnClose}>
-            Cancel
-          </Button>
-          <Button variant="primary" size="md" type="submit">
-            Save changes
-          </Button>
-        </div>
       </div>
     </ModalCore>
   );
