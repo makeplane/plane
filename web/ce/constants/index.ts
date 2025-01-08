ce4a375b6209f427274f2b01271c5a9f785337a3
Aaryan Khandelwal <65252264+aaryan610@users.noreply.github.com>
2025-01-08 16:01:31 +0530

    fix: remove invalid export (#6355)

--- web/ce/constants/index.ts
+++ web/ce/constants/index.ts
@@ -2,7 +2,6 @@ export * from "./ai";
 export * from "./estimates";
 export * from "./gantt-chart";
 export * from "./issues";
-export * from "./page";
 export * from "./project";
 export * from "./user-permissions";
 export * from "./workspace";
