e962770a5f4d327ba59a09d216dbcdbbf39edc54
sriram veeraghanta <veeraghanta.sriram@gmail.com>
2025-01-14 16:50:41 +0530

    fix: adding propel ui kit to packages

---
+++ packages/propel/tailwind.config.ts
@@ -0,0 +1,7 @@
+import type { Config } from "tailwindcss";
+// import sharedConfig from "@plane/tailwind-config-custom/tailwind.config";
+
+export default {
+  //   ...sharedConfig,
+  content: ["./src/**/*.{ts,tsx}"],
+} satisfies Config;
