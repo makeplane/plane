ebdc5e48823781841fea0657c2bd7106e4598654
Vamsi krishna <matalav55@gmail.com>
2025-01-24 20:47:56 +0530

    chore: migrated filters.ts to packages

--- web/core/components/modules/applied-filters/date.tsx
+++ web/core/components/modules/applied-filters/date.tsx
@@ -1,8 +1,8 @@
 import { observer } from "mobx-react";
 // icons
 import { X } from "lucide-react";
+import { DATE_AFTER_FILTER_OPTIONS } from "@plane/constants";
 // helpers
-import { DATE_AFTER_FILTER_OPTIONS } from "@/constants/filters";
 import { renderFormattedDate } from "@/helpers/date-time.helper";
 import { capitalizeFirstLetter } from "@/helpers/string.helper";
 // constants
