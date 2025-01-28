f04ee31235909aa2239c748b6b28e05b8fec9dd9
gakshita <akshitagoyal1516@gmail.com>
2025-01-28 15:53:35 +0530

    feat: ln support for notifications

--- web/core/components/workspace-notifications/sidebar/filters/menu/root.tsx
+++ web/core/components/workspace-notifications/sidebar/filters/menu/root.tsx
@@ -27,7 +27,7 @@ export const NotificationFilter: FC = observer(() => {
     <PopoverMenu
       data={translatedFilterTypeOptions}
       button={
-        <Tooltip tooltipContent="Inbox Filters" isMobile={isMobile} position="bottom">
+        <Tooltip tooltipContent={t("notification.options.filters")} isMobile={isMobile} position="bottom">
           <div className="flex-shrink-0 w-5 h-5 flex justify-center items-center overflow-hidden cursor-pointer transition-all hover:bg-custom-background-80 rounded-sm outline-none">
             <ListFilter className="h-3 w-3" />
           </div>
