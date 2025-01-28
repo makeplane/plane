88b4d3222080af9d0401460d6c791122ae1cead6
Aaryan Khandelwal <65252264+aaryan610@users.noreply.github.com>
2025-01-28 16:15:18 +0530

    [WEB-3237, 3238] dev: date picker enhancements (#6470)
    * [WEB-3238] dev: datepicker with month and year selection dropdowns (#6391)
    
    * feat: react-day-picker upgrade and caption dropdowns
    
    * style fixes
    
    * style: css and autofocus improved
    
    * fix: fixed weeks for datepicker to ensure static height
    
    ---------
    
    Co-authored-by: Vineet K <55555696+vineetk13@users.noreply.github.com>

--- web/core/components/dropdowns/date-range.tsx
+++ web/core/components/dropdowns/date-range.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useRef, useState } from "react";
 import { Placement } from "@popperjs/core";
-import { DateRange, DayPicker, Matcher } from "react-day-picker";
+import { DateRange, DayPicker, Matcher, getDefaultClassNames } from "react-day-picker";
 import { usePopper } from "react-popper";
 import { ArrowRight, CalendarCheck2, CalendarDays } from "lucide-react";
 import { Combobox } from "@headlessui/react";
@@ -51,6 +51,8 @@ type Props = {
   renderByDefault?: boolean;
   renderPlaceholder?: boolean;
 };
+
+const defaultClassNames = getDefaultClassNames();
 
 export const DateRangeDropdown: React.FC<Props> = (props) => {
   const {
@@ -198,12 +200,14 @@ export const DateRangeDropdown: React.FC<Props> = (props) => {
       {isOpen && (
         <Combobox.Options className="fixed z-10" static>
           <div
-            className="my-1 bg-custom-background-100 shadow-custom-shadow-rg rounded-md overflow-hidden p-3"
+            className="my-1 bg-custom-background-100 shadow-custom-shadow-rg overflow-hidden"
             ref={setPopperElement}
             style={styles.popper}
             {...attributes.popper}
           >
             <DayPicker
+              captionLayout="dropdown"
+              classNames={{ root: `${defaultClassNames.root} p-3 rounded-md` }}
               selected={dateRange}
               onSelect={(val) => {
                 // if both the dates are not required, immediately call onSelect
@@ -216,7 +220,8 @@ export const DateRangeDropdown: React.FC<Props> = (props) => {
               mode="range"
               disabled={disabledDays}
               showOutsideDays
-              initialFocus
+              autoFocus
+              fixedWeeks
               footer={
                 bothRequired && (
                   <div className="grid grid-cols-2 items-center gap-3.5 pt-6 relative">
