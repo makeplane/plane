8a42a24ac4ffda6e4a73fce0eaaee2ce872331ba
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2025-01-30 19:47:10 +0530

    chore: handle undefined status

--- packages/editor/src/core/extensions/custom-image/components/upload-status.tsx
+++ packages/editor/src/core/extensions/custom-image/components/upload-status.tsx
@@ -14,6 +14,8 @@ export const ImageUploadStatus: React.FC<Props> = (props) => {
     selector: ({ editor }) => editor.storage.imageComponent.assetsUploadStatus[nodeId],
   });
 
+  if (uploadStatus === undefined) return null;
+
   return (
     <div className="absolute top-1 right-1 z-20 bg-black/60 rounded text-xs font-medium w-10 text-center">
       {uploadStatus}%
