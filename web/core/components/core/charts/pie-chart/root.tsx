f8f64ccc3cbd85fe9797e71cd439c4178d60f069
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2025-02-07 03:35:56 +0530

    chore: separate out pie chart tooltip

--- web/core/components/core/charts/pie-chart/root.tsx
+++ web/core/components/core/charts/pie-chart/root.tsx
@@ -5,19 +5,10 @@ import React, { useMemo } from "react";
 import { Cell, PieChart as CorePieChart, Pie, ResponsiveContainer, Tooltip } from "recharts";
 // plane imports
 import { TPieChartProps } from "@plane/types";
-import { Card, ECardSpacing } from "@plane/ui";
-import { cn } from "@plane/utils";
+import { CustomPieChartTooltip } from "./tooltip";
 
 export const PieChart = React.memo(<K extends string, T extends string>(props: TPieChartProps<K, T>) => {
   const { data, dataKey, cells, className = "w-full h-96", innerRadius, outerRadius, showTooltip = true } = props;
-  // derived values
-  const itemDotClassNames = useMemo(
-    () => cells.reduce((acc, cell) => ({ ...acc, [cell.key]: cell.dotClassName }), {}),
-    [cells]
-  );
-
-  console.log("cells", cells);
-  console.log("itemDotClassNames", itemDotClassNames);
 
   const renderCells = useMemo(
     () => cells.map((cell) => <Cell key={cell.key} className={cell.className} style={cell.style} />),
@@ -48,20 +39,7 @@ export const PieChart = React.memo(<K extends string, T extends string>(props: T
                 if (!active || !payload || !payload.length) return null;
                 const cellData = cells.find((c) => c.key === payload[0].name);
                 if (!cellData) return null;
-                return (
-                  <Card className="flex flex-col" spacing={ECardSpacing.SM}>
-                    <p className="text-xs text-custom-text-100 font-medium border-b border-custom-border-200 pb-2 capitalize">
-                      {dataKey}
-                    </p>
-                    {payload?.map((item) => (
-                      <div key={item?.dataKey} className="flex items-center gap-2 text-xs capitalize">
-                        <div className={cn("size-2 rounded-full", cellData.dotClassName)} />
-                        <span className="text-custom-text-300">{item?.name}:</span>
-                        <span className="font-medium text-custom-text-200">{item?.value}</span>
-                      </div>
-                    ))}
-                  </Card>
-                );
+                return <CustomPieChartTooltip dotClassName={cellData.dotClassName} label={dataKey} payload={payload} />;
               }}
             />
           )}
