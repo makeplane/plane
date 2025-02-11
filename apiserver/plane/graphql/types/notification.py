d99195f81c47095fa29c5ea62f13b266e9b4937b
guru_sainath <gurusainath007@gmail.com>
2025-02-11 16:16:12 +0530

    fix: handled the html tags rendering in the notificatio comment (#2455)

--- apiserver/plane/graphql/types/notification.py
+++ apiserver/plane/graphql/types/notification.py
@@ -1,6 +1,7 @@
 # Python imports
 from typing import Optional
 from datetime import datetime
+from asgiref.sync import sync_to_async
 
 # Strawberry imports
 import strawberry
@@ -12,6 +13,7 @@ from plane.graphql.utils.timezone import user_timezone_converter
 from plane.db.models import Notification
 from plane.graphql.types.users import UserType
 from plane.graphql.types.project import ProjectType
+from plane.graphql.utils.issue_activity import issue_activity_comment_string
 
 
 @strawberry_django.type(Notification)
@@ -43,7 +45,6 @@ class NotificationType:
     def receiver(self) -> int:
         return self.receiver_id
 
-    @strawberry.field
     def created_at(self, info) -> Optional[datetime]:
         converted_date = user_timezone_converter(info.context.user, self.created_at)
         return converted_date
@@ -52,3 +53,47 @@ class NotificationType:
     def updated_at(self, info) -> Optional[datetime]:
         converted_date = user_timezone_converter(info.context.user, self.updated_at)
         return converted_date
+
+    @strawberry.field
+    async def data(self) -> JSON:
+        # handle the html and custom mention tags in comment and issue_activity
+        @sync_to_async
+        def process_data() -> JSON:
+            issue_activity = self.data.get("issue_activity")
+            if issue_activity is not None:
+                field = issue_activity.get("field")
+                old_value = issue_activity.get("old_value")
+                new_value = issue_activity.get("new_value")
+
+                if field == "comment":
+                    # handling the old value
+                    if (
+                        old_value is not None
+                        and old_value != ""
+                        and old_value != "None"
+                    ):
+                        stripped_old_value = issue_activity_comment_string(
+                            old_value
+                        ).get("content")
+                        if stripped_old_value is not None:
+                            self.data["issue_activity"]["old_value"] = (
+                                stripped_old_value
+                            )
+
+                    # handling the new value
+                    if (
+                        new_value is not None
+                        and new_value != ""
+                        and new_value != "None"
+                    ):
+                        stripped_new_value = issue_activity_comment_string(
+                            new_value
+                        ).get("content")
+                        if stripped_new_value is not None:
+                            self.data["issue_activity"]["new_value"] = (
+                                stripped_new_value
+                            )
+
+            return self.data
+
+        return await process_data()
