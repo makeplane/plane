6a3ccafe354cb189e618a85d7310f7476f9025a1
Akshita Goyal <36129505+gakshita@users.noreply.github.com>
2025-02-12 17:15:40 +0530

    fix: code splitting for workspace delete modal (#6581)
    * fix: code splitting for delete modal
    
    * fix: redirected to profile post deletion
    
    * fix: translations

--- web/core/components/workspace/index.ts
+++ web/core/components/workspace/index.ts
@@ -3,6 +3,5 @@ export * from "./sidebar";
 export * from "./views";
 export * from "./confirm-workspace-member-remove";
 export * from "./create-workspace-form";
-export * from "./delete-workspace-modal";
 export * from "./logo";
 export * from "./invite-modal";
