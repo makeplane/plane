54b57e471b6b633a1898c9232c595016a606ae39
Anmol Singh Bhatia <121005188+anmolsinghbhatia@users.noreply.github.com>
2025-02-14 19:25:07 +0530

    [WEB-3396] chore: work items parent select improvement and fix epic parent mutation (#2507)
    * chore: work items parent select improvements
    
    * fix: parent change operation mutation
    
    * chore: code refactor

--- web/core/components/issues/issue-detail/sidebar.tsx
+++ web/core/components/issues/issue-detail/sidebar.tsx
@@ -16,7 +16,7 @@ import {
   StateDropdown,
 } from "@/components/dropdowns";
 import { ButtonAvatars } from "@/components/dropdowns/member/avatar";
-import { IssueCycleSelect, IssueLabel, IssueModuleSelect, IssueParentSelect } from "@/components/issues";
+import { IssueCycleSelect, IssueLabel, IssueModuleSelect } from "@/components/issues";
 // helpers
 import { cn } from "@/helpers/common.helper";
 import { getDate, renderFormattedPayloadDate } from "@/helpers/date-time.helper";
@@ -25,7 +25,7 @@ import { shouldHighlightIssueDueDate } from "@/helpers/issue.helper";
 import { useProjectEstimates, useIssueDetail, useProject, useProjectState, useMember } from "@/hooks/store";
 // plane web components
 import { IssueAdditionalPropertyValuesUpdate } from "@/plane-web/components/issue-types/values";
-import { IssueWorklogProperty } from "@/plane-web/components/issues";
+import { IssueParentSelectRoot, IssueWorklogProperty } from "@/plane-web/components/issues";
 // components
 import type { TIssueOperations } from "./root";
 
@@ -261,7 +261,7 @@ export const IssueDetailsSidebar: React.FC<Props> = observer((props) => {
                 <LayoutPanelTop className="h-4 w-4 flex-shrink-0" />
                 <span>{t("common.parent")}</span>
               </div>
-              <IssueParentSelect
+              <IssueParentSelectRoot
                 className="h-full w-3/5 flex-grow"
                 workspaceSlug={workspaceSlug}
                 projectId={projectId}
