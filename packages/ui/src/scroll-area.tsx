4bcb0635d2ec04fa1ef1353f36afec06d4a80e46
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2025-02-17 01:30:22 +0530

    chore-scroll-area-component-improvement

--- packages/ui/src/scroll-area.tsx
+++ packages/ui/src/scroll-area.tsx
@@ -7,25 +7,59 @@ type TScrollAreaProps = {
   type?: "auto" | "always" | "scroll" | "hover";
   className?: string;
   scrollHideDelay?: number;
+  size?: "sm" | "md" | "lg";
   children: React.ReactNode;
 };
 
 export const ScrollArea: FC<TScrollAreaProps> = (props) => {
-  const { type = "always", className = "", scrollHideDelay = 600, children } = props;
+  const { type = "always", className = "", scrollHideDelay = 600, size = "md", children } = props;
+
+  const sizeStyles = {
+    sm: "p-[0.112rem] data-[orientation=vertical]:w-2.5 data-[orientation=horizontal]:h-2.5",
+    md: "p-[0.152rem] data-[orientation=vertical]:w-3 data-[orientation=horizontal]:h-3",
+    lg: "p-[0.225rem] data-[orientation=vertical]:w-4 data-[orientation=horizontal]:h-4",
+  };
+
+  const thumbSizeStyles = {
+    sm: "before:absolute before:left-1/2 before:top-1/2 before:size-full before:min-h-11 before:min-w-11 before:-translate-x-1/2 before:-translate-y-1/2",
+    md: "before:absolute before:left-1/2 before:top-1/2 before:size-full before:min-h-14 before:min-w-14 before:-translate-x-1/2 before:-translate-y-1/2",
+    lg: "before:absolute before:left-1/2 before:top-1/2 before:size-full before:min-h-17 before:min-w-17 before:-translate-x-1/2 before:-translate-y-1/2",
+  };
+
   return (
-    <RadixScrollArea.Root type={type} className={cn("overflow-hidden", className)} scrollHideDelay={scrollHideDelay}>
+    <RadixScrollArea.Root
+      type={type}
+      className={cn("group overflow-hidden", className)}
+      scrollHideDelay={scrollHideDelay}
+    >
       <RadixScrollArea.Viewport className="size-full">{children}</RadixScrollArea.Viewport>
       <RadixScrollArea.Scrollbar
-        className="group flex touch-none select-none bg-transparent p-0.5 transition-colors duration-[160ms] ease-out data-[orientation=horizontal]:h-2.5 data-[orientation=vertical]:w-2.5 data-[orientation=horizontal]:flex-col"
+        className={cn(
+          "group/track flex touch-none select-none bg-transparent transition-colors duration-[160ms] ease-out",
+          sizeStyles[size]
+        )}
         orientation="vertical"
       >
-        <RadixScrollArea.Thumb className="relative flex-1 rounded-[10px] bg-custom-scrollbar-neutral group-hover:bg-custom-scrollbar-hover group-active:bg-custom-scrollbar-active before:absolute before:left-1/2 before:top-1/2 before:size-full before:min-h-11 before:min-w-11 before:-translate-x-1/2 before:-translate-y-1/2" />
+        <RadixScrollArea.Thumb
+          className={cn(
+            "relative flex-1 rounded-[10px]  bg-custom-scrollbar-neutral group-hover:bg-custom-scrollbar-hover group-active/track:bg-custom-scrollbar-active",
+            thumbSizeStyles[size]
+          )}
+        />
       </RadixScrollArea.Scrollbar>
       <RadixScrollArea.Scrollbar
-        className="group flex touch-none select-none bg-transparent p-0.5 transition-colors duration-[160ms] ease-out data-[orientation=horizontal]:h-2.5 data-[orientation=vertical]:w-2.5 data-[orientation=horizontal]:flex-col"
+        className={cn(
+          "group/track flex touch-none select-none bg-transparent transition-colors duration-[160ms] ease-out",
+          sizeStyles[size]
+        )}
         orientation="horizontal"
       >
-        <RadixScrollArea.Thumb className="relative flex-1 rounded-[10px] bg-custom-scrollbar-neutral group-hover:bg-custom-scrollbar-hover group-active:bg-custom-scrollbar-active before:absolute before:left-1/2 before:top-1/2 before:size-full before:min-h-[44px] before:min-w-[44px] before:-translate-x-1/2 before:-translate-y-1/2" />
+        <RadixScrollArea.Thumb
+          className={cn(
+            "relative flex-1 rounded-[10px] bg-custom-scrollbar-neutral group-hover:bg-custom-scrollbar-hover group-active/track:bg-custom-scrollbar-active",
+            thumbSizeStyles[size]
+          )}
+        />
       </RadixScrollArea.Scrollbar>
     </RadixScrollArea.Root>
   );
