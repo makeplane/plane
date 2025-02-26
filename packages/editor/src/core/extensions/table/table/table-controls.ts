ce19996d4f4dec51c16854757e838f400678e9f6
VipinDevelops <vipinchaudhary1809@gmail.com>
2025-02-26 21:02:43 +0530

    handle triple click in cell

--- packages/editor/src/core/extensions/table/table/table-controls.ts
+++ packages/editor/src/core/extensions/table/table/table-controls.ts
@@ -16,6 +16,20 @@ export function tableControls() {
       },
     },
     props: {
+      handleTripleClickOn(view, pos, node, nodePos, event, direct) {
+        if (node.type.name === 'tableCell') {
+          event.preventDefault();
+          const start = nodePos + 1;
+          const end = nodePos + node.nodeSize - 1;
+          const tr = view.state.tr.setSelection(
+            TextSelection.create(view.state.doc, start, end)
+          );
+          view.dispatch(tr);
+          return true;
+        }
+
+        return false;
+      },
       handleDOMEvents: {
         mousemove: (view, event) => {
           const pluginState = key.getState(view.state);
