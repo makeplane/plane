cf411000031db80e22806bfb1bdc1b0703a023cb
VipinDevelops <vipinchaudhary1809@gmail.com>
2025-02-28 00:21:48 +0530

    handle multi types in table

--- packages/editor/src/core/extensions/clipboard.ts
+++ packages/editor/src/core/extensions/clipboard.ts
@@ -22,12 +22,14 @@ export const MarkdownClipboard = Extension.create({
             const isTableContent =
               slice.content.childCount === 1 &&
               slice.content.firstChild?.type.name === "table";
+            const isTableRow = slice.content.firstChild?.type.name === 'tableRow'
 
             const hasMultipleContentBlocks = slice.content.childCount > 1;
 
             const isWholeSelectionCopied = slice.openStart === 0 && slice.openEnd === 0;
 
             const listTypes = ["bulletList", "orderedList", "taskList"]
+
             const isListContent =
               slice.content.childCount === 1 &&
               listTypes.includes(
@@ -42,15 +44,23 @@ export const MarkdownClipboard = Extension.create({
               if (tableNode.content.firstChild && tableNode.content.firstChild.content.firstChild) {
                 const firstTableCell = tableNode.content.firstChild.content.firstChild;
                 const cellContainsMultipleBlocks = firstTableCell.content.childCount > 1;
-
                 let containsComplexList = false;
                 const listHasMultipleItemsInTable = firstTableCell.content.firstChild!.content!.childCount > 1;
                 const cellContainsList = firstTableCell.content.content.some(
                   (node) => listTypes.includes(node.type.name)
                 );
 
+                const allNodes = firstTableCell.content.content
+
+                const hasMultipleTypes = new Set(allNodes.map(n => n.type.name)).size > 1;
+
+                if (hasMultipleTypes) {
+                  return markdownSerializer.serialize(slice.content.firstChild.content.firstChild?.content.firstChild?.content)
+                }
+
                 if (cellContainsList && listHasMultipleItemsInTable) {
                   containsComplexList = true;
+                  return markdownSerializer.serialize(slice.content.firstChild.content.firstChild?.content.firstChild?.content)
                 }
 
                 const cellContainsOnlyParagraphs = firstTableCell.content.content.every(
