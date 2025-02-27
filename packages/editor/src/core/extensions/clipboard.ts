d2a5faf54b4ec3c5465781a85162a04fe10a378a
VipinDevelops <vipinchaudhary1809@gmail.com>
2025-02-27 15:59:32 +0530

    lists fixed

--- packages/editor/src/core/extensions/clipboard.ts
+++ packages/editor/src/core/extensions/clipboard.ts
@@ -3,14 +3,12 @@ import { Plugin, PluginKey } from "@tiptap/pm/state";
 
 export const MarkdownClipboard = Extension.create({
   name: "markdownClipboard",
-
   addOptions() {
     return {
       transformPastedText: false,
       transformCopiedText: true,
     };
   },
-
   addProseMirrorPlugins() {
     return [
       new Plugin({
@@ -21,12 +19,14 @@ export const MarkdownClipboard = Extension.create({
             const isTable =
               slice.content.childCount === 1 &&
               slice.content.firstChild?.type.name === "table";
-            const hasMultipleBlocks = slice.content.childCount > 1;
+            const hasMultipleBlocks = slice.content.childCount > 2;
             const selectWhole = slice.openStart === 0 && slice.openEnd === 0;
-            const islist =
-              slice.content.childCount === 1 &&
-              ["bulletList", "orderedList", "taskList"].includes(slice.content.firstChild?.type.name ?? "");
-            const isBlockQuote = slice.content.firstChild?.type.name === "blockquote"
+            const isList =
+              slice.content.childCount === 2 &&
+              ["bulletList", "orderedList", "taskList"].includes(
+                slice.content.firstChild?.type.name ?? ""
+              );
+
             let handleMarkdown = false;
 
             if (isTable && !selectWhole) {
@@ -37,27 +37,41 @@ export const MarkdownClipboard = Extension.create({
               ) {
                 const firstCell = tableNode.content.firstChild.content.firstChild;
                 const cellHasMultipleBlocks = firstCell.content.childCount > 1;
+
+                let handleList = false;
+                const listhasmultiintabl = firstCell.content.firstChild!.content!.childCount > 1;
+
+                const containsLists = firstCell.content.content.some(
+                  (node) =>
+                    ["bulletList", "orderedList", "taskList"].includes(node.type.name)
+                );
+
+                if (containsLists && listhasmultiintabl) {
+                  handleList = true;
+                }
+
                 const allChildrenAreParagraphs = firstCell.content.content.every(
                   (node) => node.type.name === "paragraph"
                 );
-                handleMarkdown = this.options.transformCopiedText && !allChildrenAreParagraphs && cellHasMultipleBlocks;
+
+                console.log(allChildrenAreParagraphs, cellHasMultipleBlocks, handleList)
+                handleMarkdown =
+                  this.options.transformCopiedText &&
+                  (!allChildrenAreParagraphs && cellHasMultipleBlocks || handleList);
               }
-            } else if (islist) {
-              console.log('list')
+            } else if (isList) {
               const firstCell = slice.content.firstChild;
               if (firstCell && firstCell.content.firstChild) {
-                const listHasMultipleBlocks = firstCell.content.childCount > 1;
+                const listHasMultipleBlocks = firstCell.content.childCount > 2;
                 handleMarkdown = this.options.transformCopiedText && listHasMultipleBlocks;
               }
             } else {
               console.log('else')
               handleMarkdown =
-                this.options.transformCopiedText && (hasMultipleBlocks || selectWhole || isBlockQuote);
+                this.options.transformCopiedText && (hasMultipleBlocks || selectWhole);
             }
 
-
-            console.log(handleMarkdown, 'u4', slice)
-
+            console.log(handleMarkdown, 'U2', slice);
             return handleMarkdown
               ? serializer.serialize(slice.content)
               : slice.content.textBetween(0, slice.content.size, "\n");
