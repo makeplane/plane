1bdc1e845a288d697217301333b172d0a20023b6
VipinDevelops <vipinchaudhary1809@gmail.com>
2025-03-05 01:45:18 +0530

    fix code rabbit  suggestions

--- packages/editor/src/core/extensions/clipboard.ts
+++ packages/editor/src/core/extensions/clipboard.ts
@@ -20,7 +20,7 @@ export const MarkdownClipboard = Extension.create({
         props: {
           clipboardTextSerializer: (slice) => {
             const markdownSerializer = this.editor.storage.markdown.serializer;
-            const isTableRow = slice.content.firstChild?.type.name === 'tableRow';
+            const isTableRow = slice.content.firstChild?.type?.name === 'tableRow';
             const nodeSelect = slice.openStart === 0 && slice.openEnd === 0;
             if (nodeSelect) {
               return markdownSerializer.serialize(slice.content);
@@ -28,9 +28,9 @@ export const MarkdownClipboard = Extension.create({
 
             const processTableContent = (tableNode: Node | Fragment) => {
               let result = '';
-              tableNode.content.forEach((tableRowNode: Node | Fragment) => {
-                tableRowNode.content.forEach((cell: Node) => {
-                  const cellContent = markdownSerializer.serialize(cell.content);
+              tableNode.content?.forEach?.((tableRowNode: Node | Fragment) => {
+                tableRowNode.content?.forEach?.((cell: Node) => {
+                  const cellContent = cell.content ? markdownSerializer.serialize(cell.content) : '';
                   result += cellContent + '\n';
                 });
               });
@@ -38,8 +38,8 @@ export const MarkdownClipboard = Extension.create({
             };
 
             if (isTableRow) {
-              const rowsCount = slice.content.childCount;
-              const cellsCount = slice.content.firstChild.content.childCount;
+              const rowsCount = slice.content?.childCount || 0
+              const cellsCount = slice.content?.firstChild?.content?.childCount || 0;
               if (rowsCount === 1 || cellsCount === 1) {
                 return processTableContent(slice.content)
               } else {
@@ -51,15 +51,14 @@ export const MarkdownClipboard = Extension.create({
               if (!node || depth <= 1 || !node.content?.firstChild) {
                 return parent;
               }
-              if (node.content.childCount > 1) {
-                if (node.content.firstChild.type.name === 'listItem') {
+              if (node.content?.childCount > 1) {
+                if (node.content.firstChild?.type?.name === 'listItem') {
                   return parent;
                 } else {
                   return node.content;
                 }
-
               }
-              return traverseToParentOfLeaf(node.content.firstChild, node, depth - 1);
+              return traverseToParentOfLeaf(node.content?.firstChild || null, node, depth - 1);
             };
 
             const targetNode = traverseToParentOfLeaf(slice.content.firstChild, slice.content, slice.openStart);
