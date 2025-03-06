44af90dc6c043bcc3d209b99f2a55f611c19045b
Akshita Goyal <36129505+gakshita@users.noreply.github.com>
2025-03-06 13:44:37 +0530

    fix: issue stats refactor (#6705)
    * fix: issue stats refactor
    
    * fix: refactor
    
    * fix: ui color
    
    * fix: translation key

--- web/ce/components/issues/issue-layouts/issue-stats.tsx
+++ web/ce/components/issues/issue-layouts/issue-stats.tsx
@@ -4,9 +4,10 @@ import React, { FC } from "react";
 
 type Props = {
   issueId: string;
+  className?: string;
+  size?: number;
+  showProgressText?: boolean;
+  showLabel?: boolean;
 };
 
-export const IssueStats: FC<Props> = (props) => {
-  const { issueId } = props;
-  return <></>;
-};
+export const IssueStats: FC<Props> = (props) => <></>;
