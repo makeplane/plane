edeeb70efb27331592409c51ec7cdf4b8dca787e
Vamsi krishna <matalav55@gmail.com>
2025-03-07 20:40:31 +0530

    chore: issue properties refactor

---
+++ web/ce/components/issues/issue-layouts/additional-properties.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+import { IIssueDisplayProperties, TIssue } from "@plane/types";
+
+type TProps = {
+  displayProperties: IIssueDisplayProperties;
+  issue: TIssue;
+};
+
+export const IssueAdditionalProperties: FC<TProps> = (props) => <></>;
