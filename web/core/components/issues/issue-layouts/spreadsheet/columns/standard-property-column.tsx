b82176c61c4cc86131c88a2db801fa4b7bad1a48
naman-agrawal-shipsy <naman.agrawal@shipsy.io>
2025-03-20 18:20:36 +0530

    changes in custom prop column component

--- web/core/components/issues/issue-layouts/spreadsheet/columns/standard-property-column.tsx
+++ web/core/components/issues/issue-layouts/spreadsheet/columns/standard-property-column.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { observer } from "mobx-react";
 // types
 import { TIssue } from "@plane/types";
-import { Row } from "@plane/ui";
+import { Row, Tooltip } from "@plane/ui";
 
 type Props = {
   issue: TIssue;
@@ -12,8 +12,10 @@ export const SpreadsheetStandardPropertyColumn: React.FC<Props> = observer((prop
   const { issue, property } = props;
 
   return (
-    <Row className="flex h-11 w-full items-center border-b-[0.5px] border-custom-border-200 py-1 text-xs hover:bg-custom-background-80 group-[.selected-issue-row]:bg-custom-primary-100/5 group-[.selected-issue-row]:hover:bg-custom-primary-100/10">
-      {issue?.[property]}
-    </Row>
+    <Tooltip tooltipContent={issue?.[property]}>
+      <Row className="h-11 truncate border-b-[0.5px] border-custom-border-200 pt-[1.25em] text-xs hover:bg-custom-background-80">
+          {issue?.[property]}
+      </Row>
+    </Tooltip>
   );
 });
