2b96095dfdc1b556575fa9d4af80344341c6b811
abu-siddique-shipsy <abu.s@shipsy.io>
2025-03-21 07:36:22 +0000

    hide tooltip when no content

--- web/core/components/issues/issue-layouts/spreadsheet/columns/standard-property-column.tsx
+++ web/core/components/issues/issue-layouts/spreadsheet/columns/standard-property-column.tsx
@@ -12,7 +12,7 @@ export const SpreadsheetStandardPropertyColumn: React.FC<Props> = observer((prop
   const { issue, property } = props;
 
   return (
-    <Tooltip tooltipContent={issue?.[property]}>
+    <Tooltip tooltipContent={issue?.[property]} disabled={!issue?.[property]}>
       <Row className="h-11 truncate border-b-[0.5px] border-custom-border-200 pt-[1.25em] text-xs hover:bg-custom-background-80">
           {issue?.[property]}
       </Row>
