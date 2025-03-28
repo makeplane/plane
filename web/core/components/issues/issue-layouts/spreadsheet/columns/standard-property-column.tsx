7e62b20a488ac959a60072a4898d50610c50f5f4
abu-siddique-shipsy <abu.s@shipsy.io>
2025-03-28 14:35:56 +0000

    Fix issue With tool tip

--- web/core/components/issues/issue-layouts/spreadsheet/columns/standard-property-column.tsx
+++ web/core/components/issues/issue-layouts/spreadsheet/columns/standard-property-column.tsx
@@ -3,7 +3,6 @@ import { observer } from "mobx-react";
 // types
 import { TIssue } from "@plane/types";
 import { Row, Tooltip } from "@plane/ui";
-import { Row, Tooltip } from "@plane/ui";
 
 type Props = {
   issue: TIssue;
@@ -13,10 +12,10 @@ export const SpreadsheetStandardPropertyColumn: React.FC<Props> = observer((prop
   const { issue, property } = props;
 
   return (
-    <Tooltip tooltipContent={issue?.[property]}>
-      <Row className="h-11 truncate border-b-[0.5px] border-custom-border-200 pt-[1.25em] text-xs hover:bg-custom-background-80">
-          {issue?.[property]}
-      </Row>
-    </Tooltip>
+    <Tooltip tooltipContent={issue && property ? issue[property] : "No data available"}>
+  <Row className="h-11 truncate border-b-[0.5px] border-custom-border-200 pt-[1.25em] text-xs hover:bg-custom-background-80">
+    {issue && property ? issue[property] : "N/A"}
+  </Row>
+</Tooltip>
   );
 });
