fbc86e4896141c104c2d7ea51f21867116fe3843
pablohashescobar <nikhilschacko@gmail.com>
2025-04-07 15:13:06 +0530

    chore: update the script to use transaction

--- apiserver/plane/db/management/commands/fix_duplicate_sequences.py
+++ apiserver/plane/db/management/commands/fix_duplicate_sequences.py
@@ -1,7 +1,7 @@
 # Django imports
 from django.core.management.base import BaseCommand, CommandError
 from django.db.models import Max
-from django.db import connection
+from django.db import connection, transaction
 
 # Module imports
 from plane.db.models import Project, Issue, IssueSequence
@@ -49,9 +49,6 @@ class Command(BaseCommand):
 
             # Get the issues
             issues = Issue.objects.filter(project=project, sequence_id=issue_sequence)
-            issue_sequences = IssueSequence.objects.filter(
-                project=project, sequence=issue_sequence
-            )
             # Check if there are duplicate issues
             if not issues.count() > 1:
                 raise CommandError(
@@ -63,42 +60,42 @@ class Command(BaseCommand):
                     f"{issues.count()} issues found with identifier {issue_identifier}"
                 )
             )
+            with transaction.atomic():
+                # This ensures only one transaction per project can execute this code at a time
+                lock_key = convert_uuid_to_integer(project.id)
 
-            # This ensures only one transaction per project can execute this code at a time
-            lock_key = convert_uuid_to_integer(project.id)
+                # Acquire an exclusive lock using the project ID as the lock key
+                with connection.cursor() as cursor:
+                    # Get an exclusive lock using the project ID as the lock key
+                    cursor.execute("SELECT pg_advisory_xact_lock(%s)", [lock_key])
 
-            # Acquire an exclusive lock using the project ID as the lock key
-            with connection.cursor() as cursor:
-                # Get an exclusive lock using the project ID as the lock key
-                cursor.execute("SELECT pg_advisory_xact_lock(%s)", [lock_key])
+                # Get the maximum sequence ID for the project
+                last_sequence = IssueSequence.objects.filter(project=project).aggregate(
+                    largest=Max("sequence")
+                )["largest"]
 
-            # Get the maximum sequence ID for the project
-            last_sequence = IssueSequence.objects.filter(project=project).aggregate(
-                largest=Max("sequence")
-            )["largest"]
+                bulk_issues = []
+                bulk_issue_sequences = []
 
-            bulk_issues = []
-            bulk_issue_sequences = []
+                issue_sequence_map = {
+                    isq.issue_id: isq
+                    for isq in IssueSequence.objects.filter(project=project)
+                }
 
-            issue_sequence_map = {
-                isq.issue_id: isq
-                for isq in IssueSequence.objects.filter(project=project)
-            }
+                # change the ids of duplicate issues
+                for index, issue in enumerate(issues[1:]):
+                    updated_sequence_id = last_sequence + index + 1
+                    issue.sequence_id = updated_sequence_id
+                    bulk_issues.append(issue)
 
-            # change the ids of duplicate issues
-            for index, issue in enumerate(issues[1:]):
-                updated_sequence_id = last_sequence + index + 1
-                issue.sequence_id = updated_sequence_id
-                bulk_issues.append(issue)
+                    # Find the same issue sequence instance from the above queryset
+                    sequence_identifier = issue_sequence_map.get(issue.id)
+                    if sequence_identifier:
+                        sequence_identifier.sequence = updated_sequence_id
+                        bulk_issue_sequences.append(sequence_identifier)
 
-                # Find the same issue sequence instance from the above queryset
-                sequence_identifier = issue_sequence_map.get(issue.id)
-                if sequence_identifier:
-                    sequence_identifier.sequence = updated_sequence_id
-                    bulk_issue_sequences.append(sequence_identifier)
-
-            Issue.objects.bulk_update(bulk_issues, ["sequence_id"])
-            IssueSequence.objects.bulk_update(bulk_issue_sequences, ["sequence"])
+                Issue.objects.bulk_update(bulk_issues, ["sequence_id"])
+                IssueSequence.objects.bulk_update(bulk_issue_sequences, ["sequence"])
 
             self.stdout.write(self.style.SUCCESS("Sequence IDs updated successfully"))
         except Exception as e:
