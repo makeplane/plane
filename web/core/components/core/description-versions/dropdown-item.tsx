08702a53813f61f327331471ad54e65ffbf97368
Aaryan Khandelwal <65252264+aaryan610@users.noreply.github.com>
2025-04-08 18:05:14 +0530

    [WEB-3766] fix: user avatar in description version history dropdown item (#6888)
    * fix: avatar url
    
    * chore: update version modal width

--- web/core/components/core/description-versions/dropdown-item.tsx
+++ web/core/components/core/description-versions/dropdown-item.tsx
@@ -2,7 +2,7 @@ import { observer } from "mobx-react";
 // plane imports
 import { TDescriptionVersion } from "@plane/types";
 import { Avatar, CustomMenu } from "@plane/ui";
-import { calculateTimeAgo } from "@plane/utils";
+import { calculateTimeAgo, getFileURL } from "@plane/utils";
 // hooks
 import { useMember } from "@/hooks/store";
 
@@ -21,7 +21,7 @@ export const DescriptionVersionsDropdownItem: React.FC<Props> = observer((props)
   return (
     <CustomMenu.MenuItem key={version.id} className="flex items-center gap-1" onClick={() => onClick(version.id)}>
       <span className="flex-shrink-0">
-        <Avatar name={versionCreator?.display_name} size="sm" src={versionCreator?.avatar_url} />
+        <Avatar name={versionCreator?.display_name} size="sm" src={getFileURL(versionCreator?.avatar_url ?? "")} />
       </span>
       <p className="text-xs text-custom-text-200 flex items-center gap-1.5">
         <span className="font-medium">{versionCreator?.display_name}</span>
