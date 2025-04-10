f9dc37dff0a5e33b1a653f96393c163639c09609
vamsikrishnamathala <matalav55@gmail.com>
2025-04-10 19:40:15 +0530

    chore: estimate activity refactor

--- web/core/components/issues/issue-detail/issue-activity/activity/actions/estimate.tsx
+++ web/core/components/issues/issue-detail/issue-activity/activity/actions/estimate.tsx
@@ -2,10 +2,9 @@ import { FC } from "react";
 import { observer } from "mobx-react";
 import { Triangle } from "lucide-react";
 // hooks
-import { EEstimateSystem } from "@plane/types/src/enums";
-import { convertMinutesToHoursMinutesString } from "@/helpers/date-time.helper";
-import { useIssueDetail, useProjectEstimates } from "@/hooks/store";
+import { useIssueDetail } from "@/hooks/store";
 // components
+import { renderEstimate } from "@/plane-web/components/issues/issue-details";
 import { IssueActivityBlockComponent, IssueLink } from "./";
 
 type TIssueEstimateActivity = { activityId: string; showIssue?: boolean; ends: "top" | "bottom" | undefined };
@@ -16,17 +15,8 @@ export const IssueEstimateActivity: FC<TIssueEstimateActivity> = observer((props
   const {
     activity: { getActivityById },
   } = useIssueDetail();
-  const { currentActiveEstimate } = useProjectEstimates();
 
   const activity = getActivityById(activityId);
-
-  const renderValue = (value: string) => {
-    const isTinmeEstimate = currentActiveEstimate?.type === EEstimateSystem.TIME;
-    if (isTinmeEstimate) {
-      return convertMinutesToHoursMinutesString(Number(value));
-    }
-    return value;
-  };
 
   if (!activity) return <></>;
 
@@ -38,7 +28,9 @@ export const IssueEstimateActivity: FC<TIssueEstimateActivity> = observer((props
     >
       <>
         {activity.new_value ? `set the estimate to ` : `removed the estimate `}
-        {activity.new_value ? renderValue(activity.new_value) : renderValue(activity?.old_value || "")}
+        {activity.new_value
+          ? renderEstimate(activity, activity.new_value)
+          : renderEstimate(activity, activity?.old_value || "")}
         {showIssue && (activity.new_value ? ` to ` : ` from `)}
         {showIssue && <IssueLink activityId={activityId} />}.
       </>
