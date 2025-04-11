5070c927adb700412eb80e5df0f698a07d510a69
gakshita <akshitagoyal1516@gmail.com>
2025-04-11 19:03:00 +0530

    fix: refactor

--- web/ce/components/inbox/source-pill.tsx
+++ web/ce/components/inbox/source-pill.tsx
@@ -1,6 +1,7 @@
 import { EInboxIssueSource } from "@plane/constants";
 
-type TInboxSourcePill = {
+export type TInboxSourcePill = {
   source: EInboxIssueSource;
 };
+
 export const InboxSourcePill = (props: TInboxSourcePill) => <></>;
