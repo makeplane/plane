57560c214d7c1d3ffedc36c2ac97062efd4879a5
gakshita <akshitagoyal1516@gmail.com>
2025-04-25 15:55:24 +0530

    fix: handled no project

--- web/app/[workspaceSlug]/(settings)/settings/project/layout.tsx
+++ web/app/[workspaceSlug]/(settings)/settings/project/layout.tsx
@@ -1,32 +1,46 @@
 "use client";
 
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
+import { observer } from "mobx-react";
 import { useParams, usePathname } from "next/navigation";
 // components
 import { CommandPalette } from "@/components/command-palette";
 import { SettingsContentWrapper } from "@/components/settings";
 import { ProjectSettingsSidebar } from "@/components/settings/project/sidebar";
+import { useProject } from "@/hooks/store";
+import { useAppRouter } from "@/hooks/use-app-router";
 import { ProjectAuthWrapper } from "@/plane-web/layouts/project-wrapper";
 
 type Props = {
   children: ReactNode;
 };
 
-export default function ProjectSettingsLayout(props: Props) {
+const ProjectSettingsLayout = observer((props: Props) => {
   const { children } = props;
   // router
+  const router = useAppRouter();
   const pathname = usePathname();
   const { workspaceSlug, projectId } = useParams();
+  const { joinedProjectIds } = useProject();
+
+  useEffect(() => {
+    if (projectId) return;
+    if (joinedProjectIds.length > 0) {
+      router.push(`/${workspaceSlug}/settings/project/${joinedProjectIds[0]}`);
+    }
+  }, [joinedProjectIds, router, workspaceSlug, projectId]);
 
   return (
     <>
       <CommandPalette />
       <ProjectAuthWrapper workspaceSlug={workspaceSlug?.toString()} projectId={projectId?.toString()}>
         <div className="relative flex h-full w-full">
-          <ProjectSettingsSidebar workspaceSlug={workspaceSlug.toString()} pathname={pathname} />
+          {projectId && <ProjectSettingsSidebar workspaceSlug={workspaceSlug.toString()} pathname={pathname} />}
           <SettingsContentWrapper>{children}</SettingsContentWrapper>
         </div>
       </ProjectAuthWrapper>
     </>
   );
-}
+});
+
+export default ProjectSettingsLayout;
