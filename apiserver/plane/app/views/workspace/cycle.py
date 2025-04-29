190300bc6c572da38a432315415101f066bb2f25
Bavisetti Narayan <72156168+NarayanBavisetti@users.noreply.github.com>
2025-04-29 14:00:54 +0530

    [WEB-3877] chore: changed the logic to end cycle (#6971)
    * chore: changed the logic to end cycle
    
    * chore: added issue deleted filter
    
    * chore: added check for progress snapshot

--- apiserver/plane/app/views/workspace/cycle.py
+++ apiserver/plane/app/views/workspace/cycle.py
@@ -29,6 +29,7 @@ class WorkspaceCyclesEndpoint(BaseAPIView):
                         issue_cycle__issue__archived_at__isnull=True,
                         issue_cycle__issue__is_draft=False,
                         issue_cycle__deleted_at__isnull=True,
+                        issue_cycle__issue__deleted_at__isnull=True,
                     ),
                 )
             )
