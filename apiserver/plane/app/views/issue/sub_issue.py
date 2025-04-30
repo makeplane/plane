97faf12029aff379cad03e2ad6b419530941abd6
sangeethailango <sangeethailango21@gmail.com>
2025-04-30 17:02:00 +0530

    feat: filters for sub work items

--- apiserver/plane/app/views/issue/sub_issue.py
+++ apiserver/plane/app/views/issue/sub_issue.py
@@ -24,6 +24,8 @@ from plane.utils.timezone_converter import user_timezone_converter
 from collections import defaultdict
 from plane.utils.host import base_host
 from plane.utils.order_queryset import order_issue_queryset
+from plane.utils.issue_filters import issue_filters
+
 
 class SubIssuesEndpoint(BaseAPIView):
     permission_classes = [ProjectEntityPermission]
@@ -112,6 +114,10 @@ class SubIssuesEndpoint(BaseAPIView):
                 sub_issues, order_by_param
             )
 
+        # Filtering
+        filters = issue_filters(request.query_params, "GET")
+        sub_issues = sub_issues.filter(**filters)
+
         # create's a dict with state group name with their respective issue id's
         result = defaultdict(list)
         for sub_issue in sub_issues:
@@ -148,6 +154,7 @@ class SubIssuesEndpoint(BaseAPIView):
         sub_issues = user_timezone_converter(
             sub_issues, datetime_fields, request.user.user_timezone
         )
+
         # Grouping
         if group_by:
             result_dict = defaultdict(list)
@@ -168,6 +175,7 @@ class SubIssuesEndpoint(BaseAPIView):
                 {"sub_issues": result_dict, "state_distribution": result},
                 status=status.HTTP_200_OK,
             )
+
         return Response(
             {"sub_issues": sub_issues, "state_distribution": result},
             status=status.HTTP_200_OK,
