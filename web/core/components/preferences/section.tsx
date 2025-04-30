3f3c581306f5c149672e83227d12d8d4e29a5f35
gakshita <akshitagoyal1516@gmail.com>
2025-04-30 19:46:38 +0530

    fix: css

--- web/core/components/preferences/section.tsx
+++ web/core/components/preferences/section.tsx
@@ -5,7 +5,7 @@ interface SettingsSectionProps {
 }
 
 export const PreferencesSection = ({ title, description, control }: SettingsSectionProps) => (
-  <div className="flex w-full justify-between gap-4 py-6 sm:gap-16">
+  <div className="flex w-full justify-between gap-4 sm:gap-16">
     <div className="col-span-12 sm:col-span-6">
       <h4 className="text-base font-medium text-custom-text-100">{title}</h4>
       <p className="text-sm text-custom-text-200">{description}</p>
