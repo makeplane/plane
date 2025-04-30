44b09cd662b1006a8b193f20f8e9205b7bd88e92
gakshita <akshitagoyal1516@gmail.com>
2025-04-30 18:00:52 +0530

    fix: css

--- web/core/components/exporter/prev-exports.tsx
+++ web/core/components/exporter/prev-exports.tsx
@@ -112,7 +112,7 @@ export const PrevExports = observer((props: Props) => {
                   tHeadClassName="border-b border-custom-border-100"
                   thClassName="text-left font-medium divide-x-0 text-custom-text-400"
                   tBodyClassName="divide-y-0"
-                  tBodyTrClassName="divide-x-0 p-4 h-[50px] text-custom-text-200"
+                  tBodyTrClassName="divide-x-0 p-4 h-[40px] text-custom-text-200"
                   tHeadTrClassName="divide-x-0"
                 />
               </div>
