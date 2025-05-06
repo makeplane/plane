2c2dd2ab596a9d2272b6708dc6be71584eabe27f
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2025-05-06 17:26:14 +0530

    chore: code refactor

--- web/app/[workspaceSlug]/(projects)/projects/(detail)/[projectId]/inbox/page.tsx
+++ web/app/[workspaceSlug]/(projects)/projects/(detail)/[projectId]/inbox/page.tsx
@@ -1,12 +1,3 @@
-import { redirect } from "next/navigation";
-
-interface PageProps {
-  params: {
-    workspaceSlug: string;
-    projectId: string;
-  };
-}
-
-export default async function InboxPage({ params: { workspaceSlug, projectId } }: PageProps) {
-  return redirect(`/${workspaceSlug}/projects/${projectId}/intake/`);
+export default function InboxPage() {
+  return null;
 }
