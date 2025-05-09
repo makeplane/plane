02bccb44d6ab4c5c4bac9f1417d8add8de9c53c5
sriram veeraghanta <veeraghanta.sriram@gmail.com>
2025-05-09 21:07:24 +0530

    chore: adding robots txt file for not indexing the server

--- apiserver/plane/urls.py
+++ apiserver/plane/urls.py
@@ -2,12 +2,10 @@
 
 from django.conf import settings
 from django.urls import include, path, re_path
-from django.views.generic import TemplateView
 
 handler404 = "plane.app.views.error_404.custom_404_view"
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="index.html")),
     path("api/", include("plane.app.urls")),
     path("api/public/", include("plane.space.urls")),
     path("api/instances/", include("plane.license.urls")),
