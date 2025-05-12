ba62fc2ca63dd17a43653eb8a967c8b54dfe6228
JayashTripathy <jayashtripathy371@gmail.com>
2025-05-12 15:04:01 +0530

    modified relevant variables to observable.ref

--- web/core/store/analytics-v2.store.ts
+++ web/core/store/analytics-v2.store.ts
@@ -3,67 +3,66 @@ import { ANALYTICS_V2_DURATION_FILTER_OPTIONS, PROJECT_CREATED_AT_FILTER_OPTIONS
 import { TAnalyticsTabsV2Base } from "@plane/types";
 import { CoreRootStore } from "./root.store";
 
-
-type DurationType = typeof PROJECT_CREATED_AT_FILTER_OPTIONS[number]['value']
+type DurationType = (typeof PROJECT_CREATED_AT_FILTER_OPTIONS)[number]["value"];
 
 export interface IAnalyticsStoreV2 {
-    //observables
-    currentTab: TAnalyticsTabsV2Base
-    selectedProjects: string[]
-    selectedDuration: DurationType,
+  //observables
+  currentTab: TAnalyticsTabsV2Base;
+  selectedProjects: string[];
+  selectedDuration: DurationType;
 
-    //computed
-    selectedDurationLabel: string | null,
+  //computed
+  selectedDurationLabel: string | null;
 
-    //actions
-    updateSelectedProjects: (projects: string[]) => void,
-    updateSelectedDuration: (duration: DurationType) => void,
+  //actions
+  updateSelectedProjects: (projects: string[]) => void;
+  updateSelectedDuration: (duration: DurationType) => void;
 }
 
 export class AnalyticsStoreV2 implements IAnalyticsStoreV2 {
-    //observables
-    currentTab: TAnalyticsTabsV2Base = "overview";
-    selectedProjects: string[] = [];
-    selectedDuration: typeof ANALYTICS_V2_DURATION_FILTER_OPTIONS[number]['value'] = "last_30_days";
+  //observables
+  currentTab: TAnalyticsTabsV2Base = "overview";
+  selectedProjects: string[] = [];
+  selectedDuration: (typeof ANALYTICS_V2_DURATION_FILTER_OPTIONS)[number]["value"] = "last_30_days";
 
-    constructor(_rootStore: CoreRootStore) {
-        makeObservable(this, {
-            // observables
-            currentTab: observable,
-            selectedDuration: observable,
-            selectedProjects: observable,
-            // computed
-            selectedDurationLabel: computed,
-            // actions
-            updateSelectedProjects: action,
-            updateSelectedDuration: action
-        })
-    }
+  constructor(_rootStore: CoreRootStore) {
+    makeObservable(this, {
+      // observables
+      currentTab: observable.ref,
+      selectedDuration: observable.ref,
+      selectedProjects: observable.ref,
+      // computed
+      selectedDurationLabel: computed,
+      // actions
+      updateSelectedProjects: action,
+      updateSelectedDuration: action,
+    });
+  }
 
-    get selectedDurationLabel() {
-        return ANALYTICS_V2_DURATION_FILTER_OPTIONS.find(item => item.value === this.selectedDuration)?.name ?? null
-    }
+  get selectedDurationLabel() {
+    return ANALYTICS_V2_DURATION_FILTER_OPTIONS.find((item) => item.value === this.selectedDuration)?.name ?? null;
+  }
 
-    updateSelectedProjects = (projects: string[]) => {
-        const initialState = this.selectedProjects;
-        try {
-            runInAction(() => {
-                this.selectedProjects = projects;
-            })
-        } catch (error) {
-            console.error("Failed to update selected project");
-            throw error;
-        }
+  updateSelectedProjects = (projects: string[]) => {
+    const initialState = this.selectedProjects;
+    try {
+      runInAction(() => {
+        this.selectedProjects = projects;
+      });
+    } catch (error) {
+      console.error("Failed to update selected project");
+      throw error;
     }
+  };
 
-    updateSelectedDuration = (duration: DurationType) => {
-        try {
-            runInAction(() => {
-                this.selectedDuration = duration;
-            })
-        } catch (error) {
-            console.error("Failed to update selected duration");
-            throw error;
-        }
+  updateSelectedDuration = (duration: DurationType) => {
+    try {
+      runInAction(() => {
+        this.selectedDuration = duration;
+      });
+    } catch (error) {
+      console.error("Failed to update selected duration");
+      throw error;
     }
+  };
 }
