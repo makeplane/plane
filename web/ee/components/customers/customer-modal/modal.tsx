9dc105f85c86090506a43f612f950fd464f05c74
Anmol Singh Bhatia <121005188+anmolsinghbhatia@users.noreply.github.com>
2025-05-19 14:57:08 +0530

    [WEB-4041] chore: modal outside click behaviour #3212

--- web/ee/components/customers/customer-modal/modal.tsx
+++ web/ee/components/customers/customer-modal/modal.tsx
@@ -2,6 +2,8 @@ import React, { FC } from "react";
 import { observer } from "mobx-react";
 // plane ui
 import { EModalPosition, EModalWidth, ModalCore } from "@plane/ui";
+// hooks
+import useKeypress from "@/hooks/use-keypress";
 // plane web components
 import { CustomerForm, CustomerModalProvider } from "@/plane-web/components/customers/customer-modal";
 import { useCustomers } from "@/plane-web/hooks/store";
@@ -15,10 +17,15 @@ export const CreateUpdateCustomerModal: FC<Props> = observer((props) => {
   // hooks
   const { getCustomerById } = useCustomers();
   const customerDetails = customerId ? getCustomerById(customerId) : undefined;
+
+  useKeypress("Escape", () => {
+    if (isOpen) onClose();
+  });
+
   return (
     isOpen && (
       <CustomerModalProvider>
-        <ModalCore isOpen={isOpen} handleClose={onClose} position={EModalPosition.TOP} width={EModalWidth.XXXL}>
+        <ModalCore isOpen={isOpen} position={EModalPosition.TOP} width={EModalWidth.XXXL}>
           <CustomerForm data={customerDetails} handleModalClose={onClose} />
         </ModalCore>
       </CustomerModalProvider>
