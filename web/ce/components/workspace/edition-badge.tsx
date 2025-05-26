4a0b00913eeec3321d5b6d87ce748df3b1382f01
gakshita <akshitagoyal1516@gmail.com>
2025-05-26 15:08:06 +0530

    fix: revert

--- web/ce/components/workspace/edition-badge.tsx
+++ web/ce/components/workspace/edition-badge.tsx
@@ -4,33 +4,26 @@ import packageJson from "package.json";
 // ui
 import { Button, Tooltip } from "@plane/ui";
 // hooks
-import { cn } from "@plane/utils";
 import { usePlatformOS } from "@/hooks/use-platform-os";
 // local components
 import { PaidPlanUpgradeModal } from "../license";
 
-export const WorkspaceEditionBadge = observer((props: { className?: string; isEditable?: boolean }) => {
-  const { className, isEditable = true } = props;
+export const WorkspaceEditionBadge = observer(() => {
   const { isMobile } = usePlatformOS();
   // states
   const [isPaidPlanPurchaseModalOpen, setIsPaidPlanPurchaseModalOpen] = useState(false);
 
   return (
     <>
-      {isEditable && (
-        <PaidPlanUpgradeModal
-          isOpen={isPaidPlanPurchaseModalOpen}
-          handleClose={() => setIsPaidPlanPurchaseModalOpen(false)}
-        />
-      )}
+      <PaidPlanUpgradeModal
+        isOpen={isPaidPlanPurchaseModalOpen}
+        handleClose={() => setIsPaidPlanPurchaseModalOpen(false)}
+      />
       <Tooltip tooltipContent={`Version: v${packageJson.version}`} isMobile={isMobile}>
         <Button
           tabIndex={-1}
           variant="accent-primary"
-          className={cn(
-            "w-fit min-w-24 cursor-pointer rounded-2xl px-2 py-1 text-center text-sm font-medium outline-none",
-            className
-          )}
+          className="w-fit min-w-24 cursor-pointer rounded-2xl px-2 py-1 text-center text-sm font-medium outline-none"
           onClick={() => setIsPaidPlanPurchaseModalOpen(true)}
         >
           Community
