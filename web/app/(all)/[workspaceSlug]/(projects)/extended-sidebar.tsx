a3a580923c43c7550efeb623886911f15dd7560a
Aaryan Khandelwal <65252264+aaryan610@users.noreply.github.com>
2025-05-28 00:58:22 +0530

    [WEB-4166] chore: projects app sidebar accessibility (#7115)
    * chore: add ARIA attributes
    
    * chore: add missing translations
    
    * chore: add accessibility translations for multiple languages and configured store according to it
    
    * chore: refactor translation file handling and introduce TranslationFiles enum
    
    * fix: accessibility issues in workspace sidebar
    
    ---------
    
    Co-authored-by: JayashTripathy <jayashtripathy371@gmail.com>
    Co-authored-by: Prateek Shourya <prateekshourya29@gmail.com>

--- web/app/(all)/[workspaceSlug]/(projects)/extended-sidebar.tsx
+++ web/app/(all)/[workspaceSlug]/(projects)/extended-sidebar.tsx
@@ -96,7 +96,7 @@ export const ExtendedAppSidebar = observer(() => {
 
   useExtendedSidebarOutsideClickDetector(
     extendedSidebarRef,
-    () => toggleExtendedSidebar(false),
+    () => toggleExtendedSidebar(true),
     "extended-sidebar-toggle"
   );
 
@@ -106,8 +106,8 @@ export const ExtendedAppSidebar = observer(() => {
       className={cn(
         "absolute top-0 h-full z-[19] flex flex-col gap-0.5 w-[300px] transform transition-all duration-300 ease-in-out bg-custom-sidebar-background-100 border-r border-custom-sidebar-border-200 p-4 shadow-md pb-6",
         {
-          "translate-x-0 opacity-100 pointer-events-auto": extendedSidebarCollapsed,
-          "-translate-x-full opacity-0 pointer-events-none": !extendedSidebarCollapsed,
+          "-translate-x-full opacity-0 pointer-events-none": extendedSidebarCollapsed,
+          "translate-x-0 opacity-100 pointer-events-auto": !extendedSidebarCollapsed,
           "left-[70px]": sidebarCollapsed,
           "left-[250px]": !sidebarCollapsed,
         }
