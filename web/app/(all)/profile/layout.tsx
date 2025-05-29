b4eab66e3dc531abdd064bcad949d4d0fd1b5c79
Aaron Reisman <aaron.reisman@plane.so>
2025-05-28 20:19:46 -0700

    refactor: revert unintentional layout changes

--- web/app/(all)/profile/layout.tsx
+++ web/app/(all)/profile/layout.tsx
@@ -6,7 +6,7 @@ import { ProfileLayoutSidebar } from "./sidebar";
 
 // Dynamically import heavy components
 const CommandPalette = dynamic(
-  () => import("@/components/command-palette").then(module => ({ default: module.CommandPalette })),
+  () => import("@/components/command-palette").then((module) => ({ default: module.CommandPalette })),
   {
     ssr: false, // Command palette doesn't need SSR
     loading: () => null,
@@ -15,14 +15,16 @@ const CommandPalette = dynamic(
 
 export default function ProfileLayout({ children }: { children: React.ReactNode }) {
   return (
-    <AuthenticationWrapper>
+    <>
       <CommandPalette />
-      <div className="relative flex h-screen w-full overflow-hidden">
-        <ProfileLayoutSidebar />
-        <main className="relative flex h-full w-full flex-col overflow-hidden bg-custom-background-100">
-          {children}
-        </main>
-      </div>
-    </AuthenticationWrapper>
+      <AuthenticationWrapper>
+        <div className="relative flex h-full w-full overflow-hidden">
+          <ProfileLayoutSidebar />
+          <main className="relative flex h-full w-full flex-col overflow-hidden bg-custom-background-100">
+            <div className="h-full w-full overflow-hidden">{children}</div>
+          </main>
+        </div>
+      </AuthenticationWrapper>
+    </>
   );
 }
