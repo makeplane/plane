55e54f64ee9443ca17435693c8cd9310da39b787
JayashTripathy <jayashtripathy371@gmail.com>
2025-05-29 14:40:08 +0530

    refactor: remove isEpic state from WorkItemsModalMainContent component

--- web/core/components/analytics-v2/work-items/modal/content.tsx
+++ web/core/components/analytics-v2/work-items/modal/content.tsx
@@ -17,13 +17,11 @@ type Props = {
   projectDetails: IProject | undefined;
   cycleDetails: ICycle | undefined;
   moduleDetails: IModule | undefined;
-  isEpic?: boolean;
 };
 
 export const WorkItemsModalMainContent: React.FC<Props> = observer((props) => {
-  const { projectDetails, cycleDetails, moduleDetails, fullScreen, isEpic } = props;
-  const { updateSelectedProjects, updateSelectedCycle, updateSelectedModule, updateIsPeekView, updateIsEpic } =
-    useAnalyticsV2();
+  const { projectDetails, cycleDetails, moduleDetails, fullScreen } = props;
+  const { updateSelectedProjects, updateSelectedCycle, updateSelectedModule, updateIsPeekView } = useAnalyticsV2();
   const [isModalConfigured, setIsModalConfigured] = useState(false);
 
   useEffect(() => {
@@ -43,11 +41,6 @@ export const WorkItemsModalMainContent: React.FC<Props> = observer((props) => {
     if (moduleDetails?.id) {
       updateSelectedModule(moduleDetails.id);
     }
-
-    if (isEpic) {
-      updateIsEpic(true);
-    }
-
     setIsModalConfigured(true);
 
     // Cleanup fields
@@ -56,7 +49,6 @@ export const WorkItemsModalMainContent: React.FC<Props> = observer((props) => {
       updateSelectedCycle("");
       updateSelectedModule("");
       updateIsPeekView(false);
-      updateIsEpic(false);
     };
   }, [
     projectDetails?.id,
@@ -65,8 +57,6 @@ export const WorkItemsModalMainContent: React.FC<Props> = observer((props) => {
     updateSelectedProjects,
     updateSelectedCycle,
     updateSelectedModule,
-    isEpic,
-    updateIsEpic,
     updateIsPeekView,
   ]);
 
