63687eca7760120cbf6eb62dfb541ea52c374828
sriram veeraghanta <veeraghanta.sriram@gmail.com>
2025-05-30 15:02:07 +0530

    fix: remove build process from utils package

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,19 +1,16 @@
 export * from "./array";
 export * from "./attachment";
 export * from "./auth";
-export * from "./datetime";
 export * from "./color";
 export * from "./common";
 export * from "./datetime";
 export * from "./emoji";
 export * from "./file";
+export * from "./get-icon-for-link";
 export * from "./issue";
 export * from "./state";
 export * from "./string";
-export * from "./theme";
-export * from "./workspace";
-export * from "./work-item";
-
-export * from "./get-icon-for-link";
-
 export * from "./subscription";
+export * from "./theme";
+export * from "./work-item";
+export * from "./workspace";
