0e369f87acc01d65a9e788da28fc87a7138cc828
NarayanBavisetti <narayan3119@gmail.com>
2025-06-05 18:23:17 +0530

    chore: change the type in url

--- apiserver/plane/app/urls/asset.py
+++ apiserver/plane/app/urls/asset.py
@@ -91,7 +91,7 @@ urlpatterns = [
         name="asset-check",
     ),
     path(
-        "assets/v2/workspaces/<slug:slug>/duplicate-assets/",
+        "assets/v2/workspaces/<str:slug>/duplicate-assets/",
         DuplicateAssetEndpoint.as_view(),
         name="duplicate-assets",
     ),
