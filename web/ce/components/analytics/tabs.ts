790b36517c4f80a64b0513cccfe57213a27ddd89
JayashTripathy <jayashtripathy371@gmail.com>
2025-06-10 16:42:32 +0530

    chore: updated label for epics

--- web/ce/components/analytics/tabs.ts
+++ web/ce/components/analytics/tabs.ts
@@ -5,6 +5,7 @@ export const ANALYTICS_TABS: {
   key: TAnalyticsTabsBase;
   i18nKey: string;
   content: React.FC;
+  isExtended?: boolean;
 }[] = [
   { key: "overview", i18nKey: "common.overview", content: Overview },
   { key: "work-items", i18nKey: "sidebar.work_items", content: WorkItems },
