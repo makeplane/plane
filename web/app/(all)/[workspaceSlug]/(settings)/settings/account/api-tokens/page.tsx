95355891f27e63f20c23e079856dd607a923af7b
gakshita <akshitagoyal1516@gmail.com>
2025-06-17 13:59:02 +0530

    chore: improved pat permissions

--- web/app/(all)/[workspaceSlug]/(settings)/settings/account/api-tokens/page.tsx
+++ web/app/(all)/[workspaceSlug]/(settings)/settings/account/api-tokens/page.tsx
@@ -5,18 +5,16 @@ import { observer } from "mobx-react";
 import { useParams } from "next/navigation";
 import useSWR from "swr";
 // plane imports
-import { EUserPermissions, EUserPermissionsLevel } from "@plane/constants";
 import { useTranslation } from "@plane/i18n";
 // component
 import { ApiTokenListItem, CreateApiTokenModal } from "@/components/api-token";
-import { NotAuthorizedView } from "@/components/auth-screens";
 import { PageHead } from "@/components/core";
 import { DetailedEmptyState } from "@/components/empty-state";
 import { SettingsHeading } from "@/components/settings";
 import { APITokenSettingsLoader } from "@/components/ui";
 import { API_TOKENS_LIST } from "@/constants/fetch-keys";
 // store hooks
-import { useUserPermissions, useWorkspace } from "@/hooks/store";
+import { useWorkspace } from "@/hooks/store";
 import { useResolvedAssetPath } from "@/hooks/use-resolved-asset-path";
 // services
 import { APITokenService } from "@/services/api_token.service";
@@ -32,24 +30,16 @@ const ApiTokensPage = observer(() => {
   const { t } = useTranslation();
   // store hooks
   const { currentWorkspace } = useWorkspace();
-  const { workspaceUserInfo, allowPermissions } = useUserPermissions();
   // derived values
-  const canPerformWorkspaceAdminActions = allowPermissions([EUserPermissions.ADMIN], EUserPermissionsLevel.WORKSPACE);
   const resolvedPath = useResolvedAssetPath({ basePath: "/empty-state/workspace-settings/api-tokens" });
 
-  const { data: tokens } = useSWR(
-    workspaceSlug && canPerformWorkspaceAdminActions ? API_TOKENS_LIST(workspaceSlug.toString()) : null,
-    () =>
-      workspaceSlug && canPerformWorkspaceAdminActions ? apiTokenService.getApiTokens(workspaceSlug.toString()) : null
+  const { data: tokens } = useSWR(workspaceSlug ? API_TOKENS_LIST(workspaceSlug.toString()) : null, () =>
+    workspaceSlug ? apiTokenService.getApiTokens(workspaceSlug.toString()) : null
   );
 
   const pageTitle = currentWorkspace?.name
     ? `${currentWorkspace.name} - ${t("workspace_settings.settings.api_tokens.title")}`
     : undefined;
-
-  if (workspaceUserInfo && !canPerformWorkspaceAdminActions) {
-    return <NotAuthorizedView section="settings" className="h-auto" />;
-  }
 
   if (!tokens) {
     return <APITokenSettingsLoader />;
