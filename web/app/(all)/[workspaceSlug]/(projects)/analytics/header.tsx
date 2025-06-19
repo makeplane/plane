d0c849749b7b5df9f07445a4a46b5f20412da310
JayashTripathy <jayashtripathy371@gmail.com>
2025-06-19 15:06:25 +0530

    refactor: removed unused things from header

--- web/app/(all)/[workspaceSlug]/(projects)/analytics/header.tsx
+++ web/app/(all)/[workspaceSlug]/(projects)/analytics/header.tsx
@@ -1,38 +1,15 @@
 "use client";
 
-import { useEffect } from "react";
 import { observer } from "mobx-react";
-import { useSearchParams } from "next/navigation";
-import { BarChart2, PanelRight } from "lucide-react";
+import { BarChart2 } from "lucide-react";
 // plane imports
 import { useTranslation } from "@plane/i18n";
 import { Breadcrumbs, Header } from "@plane/ui";
-import { cn } from "@plane/utils";
 // components
 import { BreadcrumbLink } from "@/components/common/breadcrumb-link";
-// hooks
-import { useAppTheme } from "@/hooks/store";
 
 export const WorkspaceAnalyticsHeader = observer(() => {
   const { t } = useTranslation();
-  // store hooks
-  const { workspaceAnalyticsSidebarCollapsed, toggleWorkspaceAnalyticsSidebar } = useAppTheme();
-
-  useEffect(() => {
-    const handleToggleWorkspaceAnalyticsSidebar = () => {
-      if (window && window.innerWidth < 768) {
-        toggleWorkspaceAnalyticsSidebar(true);
-      }
-      if (window && workspaceAnalyticsSidebarCollapsed && window.innerWidth >= 768) {
-        toggleWorkspaceAnalyticsSidebar(false);
-      }
-    };
-
-    window.addEventListener("resize", handleToggleWorkspaceAnalyticsSidebar);
-    handleToggleWorkspaceAnalyticsSidebar();
-    return () => window.removeEventListener("resize", handleToggleWorkspaceAnalyticsSidebar);
-  }, [toggleWorkspaceAnalyticsSidebar, workspaceAnalyticsSidebarCollapsed]);
-
   return (
     <Header>
       <Header.LeftItem>
