4b9ec8911a61565f69dee2a96a12fa948c98ae80
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2025-06-20 16:16:36 +0530

    chore: code refactor

--- web/core/components/global/product-updates/loader.tsx
+++ web/core/components/global/product-updates/loader.tsx
@@ -1,8 +1,17 @@
 import React from "react";
-import { LogoSpinner } from "@/components/common";
+import { Loader } from "@plane/ui";
 
 export const ChangeLogLoader = () => (
-  <div className="flex justify-center items-center h-full">
-    <LogoSpinner />
-  </div>
+  <Loader className="flex flex-col gap-3 h-full w-full ">
+    {/* header */}
+    <div className="flex items-center gap-2 mb-3">
+      <Loader.Item height="44px" width="500px" />
+    </div>
+    {/* body */}
+    <Loader.Item height="36px" width="300px" />
+    <Loader.Item height="26px" width="60%" />
+    <Loader.Item height="26px" width="100%" />
+    <Loader.Item height="26px" width="75%" />
+    <Loader.Item height="26px" width="80%" />
+  </Loader>
 );
