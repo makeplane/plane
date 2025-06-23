b1c2987c6de84d4de9217a11da8c02da16165999
akshat5302 <akshatjain9782@gmail.com>
2025-06-23 17:19:37 +0530

    Enhance Caddy configuration and update environment variables
    * Added note in caddy.sh regarding the persistence of SITE_ADDRESS in Docker.
    * Updated docker-compose.yml to use dynamic CERT_ACME_CA and added CERT_ACME_DNS variable.
    * Documented CERT_ACME_DNS usage in variables.env for DNS challenge certificate generation.

--- caddy/caddy.sh
+++ caddy/caddy.sh
@@ -8,4 +8,7 @@ else
     export SITE_ADDRESS="http://${APP_DOMAIN}:${LISTEN_HTTP_PORT}"
 fi
 
+# Note: SITE_ADDRESS is set for the Caddy process but won't persist in new shell sessions
+# when you exec into the container later. This is normal Docker behavior.
+
 exec caddy run --config /etc/caddy/Caddyfile
