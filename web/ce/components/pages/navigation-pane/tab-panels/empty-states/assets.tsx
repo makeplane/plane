b5f41eb5600e75bf8482d1bc830ab43b38391552
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2025-06-24 15:56:21 +0530

    fix: empty state asset path

--- web/ce/components/pages/navigation-pane/tab-panels/empty-states/assets.tsx
+++ web/ce/components/pages/navigation-pane/tab-panels/empty-states/assets.tsx
@@ -6,7 +6,7 @@ import { useResolvedAssetPath } from "@/hooks/use-resolved-asset-path";
 
 export const PageNavigationPaneAssetsTabEmptyState = () => {
   // asset resolved path
-  const resolvedPath = useResolvedAssetPath({ basePath: "/empty-state/pages/navigation-pane/outline" });
+  const resolvedPath = useResolvedAssetPath({ basePath: "/empty-state/pages/navigation-pane/assets" });
   // translation
   const { t } = useTranslation();
 
