db4bc010d0500c435a2bb4ce14c6ce499bab8968
sriramveeraghanta <veeraghanta.sriram@gmail.com>
2025-06-27 13:55:34 +0530

    fix: fixing circular dependecies and import errors

--- web/ce/components/issues/issue-modal/provider.tsx
+++ web/ce/components/issues/issue-modal/provider.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { observer } from "mobx-react-lite";
+import { observer } from "mobx-react";
 // plane imports
 import { ISearchIssueResponse, TIssue } from "@plane/types";
 // components
