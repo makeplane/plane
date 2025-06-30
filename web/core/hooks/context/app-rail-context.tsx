cc0e2cf88746653112290a6bbee90ceac341cef2
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2025-06-30 17:21:40 +0530

    chore: workspace content wrapper and apprail context

---
+++ web/core/hooks/context/app-rail-context.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import React, { createContext, ReactNode, useEffect } from "react";
+import { observer } from "mobx-react";
+import { useParams } from "next/navigation";
+// hooks
+import useLocalStorage from "@/hooks/use-local-storage";
+
+export interface AppRailContextType {
+  isEnabled: boolean;
+  shouldRenderAppRail: boolean;
+  toggleAppRail: (value?: boolean) => void;
+}
+
+const AppRailContext = createContext<AppRailContextType | undefined>(undefined);
+
+export { AppRailContext };
+
+interface AppRailProviderProps {
+  children: ReactNode;
+}
+
+export const AppRailProvider = observer(({ children }: AppRailProviderProps) => {
+  const { workspaceSlug } = useParams();
+  const { storedValue: isAppRailVisible, setValue: setIsAppRailVisible } = useLocalStorage<boolean>(
+    `APP_RAIL_${workspaceSlug}`,
+    false
+  );
+
+  const isEnabled = false;
+
+  useEffect(() => {
+    if (!isEnabled && isAppRailVisible) {
+      setIsAppRailVisible(false);
+    }
+  }, [workspaceSlug]);
+
+  const toggleAppRail = (value?: boolean) => {
+    if (value === undefined) {
+      setIsAppRailVisible(!isAppRailVisible);
+    } else {
+      setIsAppRailVisible(value);
+    }
+  };
+
+  const contextValue: AppRailContextType = {
+    isEnabled,
+    shouldRenderAppRail: !!isAppRailVisible,
+    toggleAppRail,
+  };
+
+  return <AppRailContext.Provider value={contextValue}>{children}</AppRailContext.Provider>;
+});
