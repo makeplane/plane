d9bfedd81d236964375189f09157cd738fa29401
Bavisetti Narayan <72156168+NarayanBavisetti@users.noreply.github.com>
2025-07-03 15:06:11 +0530

    [WEB-4435] bug: trigger event for work item comment #7331

--- apiserver/plane/app/views/issue/comment.py
+++ apiserver/plane/app/views/issue/comment.py
@@ -18,7 +18,10 @@ from plane.app.permissions import allow_permission, ROLE
 from plane.db.models import IssueComment, ProjectMember, CommentReaction, Project, Issue
 from plane.bgtasks.issue_activities_task import issue_activity
 from plane.utils.host import base_host
-from plane.ee.utils.check_user_teamspace_member import check_if_current_user_is_teamspace_member
+from plane.ee.utils.check_user_teamspace_member import (
+    check_if_current_user_is_teamspace_member,
+)
+from plane.bgtasks.webhook_task import model_activity
 
 
 class IssueCommentViewSet(BaseViewSet):
@@ -67,7 +70,9 @@ class IssueCommentViewSet(BaseViewSet):
             ).exists()
             and not project.guest_view_all_features
             and not issue.created_by == request.user
-            and not check_if_current_user_is_teamspace_member(request.user.id, slug, project_id)
+            and not check_if_current_user_is_teamspace_member(
+                request.user.id, slug, project_id
+            )
         ):
             return Response(
                 {"error": "You are not allowed to comment on the issue"},
@@ -87,6 +92,16 @@ class IssueCommentViewSet(BaseViewSet):
                 current_instance=None,
                 epoch=int(timezone.now().timestamp()),
                 notification=True,
+                origin=base_host(request=request, is_app=True),
+            )
+            # Send the model activity
+            model_activity.delay(
+                model_name="issue_comment",
+                model_id=str(serializer.data["id"]),
+                requested_data=request.data,
+                current_instance=None,
+                actor_id=request.user.id,
+                slug=slug,
                 origin=base_host(request=request, is_app=True),
             )
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -121,6 +136,16 @@ class IssueCommentViewSet(BaseViewSet):
                 current_instance=current_instance,
                 epoch=int(timezone.now().timestamp()),
                 notification=True,
+                origin=base_host(request=request, is_app=True),
+            )
+            # Send the model activity
+            model_activity.delay(
+                model_name="issue_comment",
+                model_id=str(pk),
+                requested_data=request.data,
+                current_instance=current_instance,
+                actor_id=request.user.id,
+                slug=slug,
                 origin=base_host(request=request, is_app=True),
             )
             return Response(serializer.data, status=status.HTTP_200_OK)
