cf91c782806872f4a98d5343a57a5f302769d974
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2025-07-09 13:36:38 +0530

    fix: content wrapper overflow

--- apps/web/ce/components/workspace/content-wrapper.tsx
+++ apps/web/ce/components/workspace/content-wrapper.tsx
@@ -4,6 +4,6 @@ import { observer } from "mobx-react";
 
 export const WorkspaceContentWrapper = observer(({ children }: { children: React.ReactNode }) => (
   <div className="flex relative size-full overflow-hidden bg-custom-background-90 rounded-lg transition-all ease-in-out duration-300">
-    <div className="size-full p-2 flex-grow transition-all ease-in-out duration-300">{children}</div>
+    <div className="size-full p-2 flex-grow transition-all ease-in-out duration-30 overflow-hidden">{children}</div>
   </div>
 ));
