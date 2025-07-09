3ade9ee64134d3fa327ae2a97c139ddbfd1a746a
Prateek Shourya <prateekshourya29@gmail.com>
2025-07-09 17:36:05 +0530

    [WEB-4462] improvement: UI enhancements for desktop app to support side rail (#3608)
    Co-authored-by: sriram veeraghanta <veeraghanta.sriram@gmail.com>

--- apps/web/ee/components/desktop/root.tsx
+++ apps/web/ee/components/desktop/root.tsx
@@ -7,24 +7,20 @@ import { isDesktopApp } from "@todesktop/client-core/platform/todesktop";
 import { observer } from "mobx-react";
 // helpers
 import { cn } from "@plane/utils";
-// hooks
-import { useAppTheme } from "@/hooks/store";
 // desktop app components
 import { SidebarToggle, DesktopAppNavigation, useDesktopApp, DesktopAppProvider } from "@/plane-web/components/desktop";
 
 const DesktopAppRoot: FC = observer(() => {
   // store hooks
-  const { sidebarCollapsed } = useAppTheme();
   const { pageTitle, isFullScreen } = useDesktopApp();
 
   return (
     <>
-      <div className="header fixed top-0 left-0 flex gap-4 items-center w-full h-9">
+      <div className="header fixed top-0 left-0 flex gap-4 items-center w-full h-8">
         <div
           className={cn(
             "flex flex-shrink-0 gap-0.5 items-center justify-end w-[160px] transition-all duration-300 ease-in-out",
             {
-              "md:w-[250px]": !sidebarCollapsed,
               "pl-3 justify-start": isFullScreen,
             }
           )}
