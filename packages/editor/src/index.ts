40587622c412f46ffa450bf6193690eaa3ad2498
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2025-07-10 18:46:29 +0530

    chore: remove unnecessary exports

--- packages/editor/src/index.ts
+++ packages/editor/src/index.ts
@@ -15,24 +15,12 @@ export {
   RichTextEditorWithRef,
 } from "@/components/editors";
 
-export { isCellSelection } from "@/extensions/table/table/utilities/is-cell-selection";
-
 // constants
 export * from "@/constants/common";
 
 // helpers
 export * from "@/helpers/common";
-export * from "@/helpers/editor-commands";
 export * from "@/helpers/yjs-utils";
-export * from "@/extensions/table/table";
-
-// components
-export * from "@/components/menus";
-
-// hooks
-export { useEditor } from "@/hooks/use-editor";
-export { type IMarking, useEditorMarkings } from "@/hooks/use-editor-markings";
-export { useReadOnlyEditor } from "@/hooks/use-read-only-editor";
 
 export { CORE_EXTENSIONS } from "@/constants/extension";
 export { ADDITIONAL_EXTENSIONS } from "@/plane-editor/constants/extensions";
