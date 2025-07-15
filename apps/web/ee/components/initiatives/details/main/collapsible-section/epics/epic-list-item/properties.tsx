7da8cc7e646e0dee2a8013683cdc9d3a8c4d593b
Anmol Singh Bhatia <121005188+anmolsinghbhatia@users.noreply.github.com>
2025-07-16 01:15:23 +0530

    [WEB-4496] chore: initiative items enhancements #3678

--- apps/web/ee/components/initiatives/details/main/collapsible-section/epics/epic-list-item/properties.tsx
+++ apps/web/ee/components/initiatives/details/main/collapsible-section/epics/epic-list-item/properties.tsx
@@ -5,10 +5,17 @@ import { observer } from "mobx-react";
 import { CalendarCheck2, CalendarClock } from "lucide-react";
 // Plane
 import { TIssuePriorities } from "@plane/constants";
+import { useTranslation } from "@plane/i18n";
 import { EIssueServiceType, EIssuesStoreType, IIssueDisplayProperties } from "@plane/types";
 // components
 import { getDate, renderFormattedPayloadDate } from "@plane/utils";
-import { DateDropdown, PriorityDropdown, MemberDropdown, StateDropdown } from "@/components/dropdowns";
+import {
+  DateDropdown,
+  PriorityDropdown,
+  MemberDropdown,
+  StateDropdown,
+  DateRangeDropdown,
+} from "@/components/dropdowns";
 // helpers
 import { WithDisplayPropertiesHOC } from "@/components/issues/issue-layouts/properties/with-display-properties-HOC";
 // hooks
@@ -35,14 +42,10 @@ export const EpicProperties: FC<Props> = observer((props) => {
   const { isMobile } = usePlatformOS();
 
   // hooks
+  const { t } = useTranslation();
   const { updateIssue } = useIssuesActions(EIssuesStoreType.EPIC);
   const {
-    initiative: {
-      fetchInitiativeAnalytics,
-      epics: {
-        filters: { getInitiativeEpicsFiltersById },
-      },
-    },
+    initiative: { fetchInitiativeAnalytics },
   } = useInitiatives();
 
   // derived values
@@ -88,18 +91,22 @@ export const EpicProperties: FC<Props> = observer((props) => {
   const maxDate = getDate(epic.target_date);
   maxDate?.setDate(maxDate.getDate());
 
+  const isDateRangeEnabled: boolean = Boolean(
+    epic.start_date && epic.target_date && displayProperties?.start_date && displayProperties?.due_date
+  );
+
   return (
     <div className="flex items-center gap-2">
       <WithDisplayPropertiesHOC displayProperties={displayProperties} displayPropertyKey="state">
-        <div className="h-5" onFocus={handleEventPropagation} onClick={handleEventPropagation}>
+        <div className="h-5 flex-shrink-0">
           <StateDropdown
-            buttonContainerClassName="truncate max-w-40"
             value={epic.state_id}
-            onChange={handleState}
             projectId={epic.project_id}
+            onChange={handleState}
             disabled={disabled}
-            buttonVariant="border-with-text"
-            renderByDefault={isMobile}
+            buttonVariant="transparent-without-text"
+            buttonClassName="hover:bg-transparent px-0"
+            iconSize="size-5"
             showTooltip
           />
         </div>
@@ -120,39 +127,74 @@ export const EpicProperties: FC<Props> = observer((props) => {
         </div>
       </WithDisplayPropertiesHOC>
 
-      <WithDisplayPropertiesHOC displayProperties={displayProperties} displayPropertyKey="start_date">
-        {/* start date */}
+      {/* merged dates */}
+      <WithDisplayPropertiesHOC
+        displayProperties={displayProperties}
+        displayPropertyKey={["start_date", "due_date"]}
+        shouldRenderProperty={() => isDateRangeEnabled}
+      >
         <div className="h-5" onFocus={handleEventPropagation} onClick={handleEventPropagation}>
+          <DateRangeDropdown
+            value={{
+              from: getDate(epic.start_date) || undefined,
+              to: getDate(epic.target_date) || undefined,
+            }}
+            onSelect={(range) => {
+              handleStartDate(range?.from ?? null);
+              handleTargetDate(range?.to ?? null);
+            }}
+            hideIcon={{
+              from: false,
+            }}
+            isClearable
+            mergeDates
+            buttonVariant={epic.start_date || epic.target_date ? "border-with-text" : "border-without-text"}
+            disabled={!disabled}
+            showTooltip
+            customTooltipHeading="Date Range"
+            renderPlaceholder={false}
+          />
+        </div>
+      </WithDisplayPropertiesHOC>
+
+      {/* start date */}
+      <WithDisplayPropertiesHOC
+        displayProperties={displayProperties}
+        displayPropertyKey="start_date"
+        shouldRenderProperty={() => !isDateRangeEnabled}
+      >
+        <div className="h-5">
           <DateDropdown
             value={epic.start_date ?? null}
             onChange={handleStartDate}
             maxDate={maxDate}
-            placeholder="Start date"
+            placeholder={t("common.order_by.start_date")}
             icon={<CalendarClock className="h-3 w-3 flex-shrink-0" />}
             buttonVariant={epic.start_date ? "border-with-text" : "border-without-text"}
             optionsClassName="z-10"
             disabled={disabled}
-            renderByDefault={isMobile}
             showTooltip
           />
         </div>
       </WithDisplayPropertiesHOC>
 
-      <WithDisplayPropertiesHOC displayProperties={displayProperties} displayPropertyKey="due_date">
-        {/* target/due date */}
-        <div className="h-5" onFocus={handleEventPropagation} onClick={handleEventPropagation}>
+      {/* target/due date */}
+      <WithDisplayPropertiesHOC
+        displayProperties={displayProperties}
+        displayPropertyKey="due_date"
+        shouldRenderProperty={() => !isDateRangeEnabled}
+      >
+        <div className="h-5">
           <DateDropdown
             value={epic?.target_date ?? null}
             onChange={handleTargetDate}
             minDate={minDate}
-            placeholder="Due date"
+            placeholder={t("common.order_by.due_date")}
             icon={<CalendarCheck2 className="h-3 w-3 flex-shrink-0" />}
             buttonVariant={epic.target_date ? "border-with-text" : "border-without-text"}
-            // buttonClassName={shouldHighlightIssueDueDate(epic.target_date, stateDetails?.group) ? "text-red-500" : ""}
-            clearIconClassName="!text-custom-text-100"
+            clearIconClassName="text-custom-text-100"
             optionsClassName="z-10"
             disabled={disabled}
-            renderByDefault={isMobile}
             showTooltip
           />
         </div>
