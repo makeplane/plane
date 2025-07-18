1e712ed0fae14f71cfeac6b134f7bd21cbcf5944
Vamsi Krishna <46787868+vamsikrishnamathala@users.noreply.github.com>
2025-07-18 17:32:27 +0530

    [WEB-4529]fix: positioning for state dropdown in project create modal #3701

--- apps/web/ee/components/projects/create/attributes.tsx
+++ apps/web/ee/components/projects/create/attributes.tsx
@@ -45,7 +45,6 @@ const ProjectAttributes: React.FC<Props> = (props) => {
               workspaceId={currentWorkspace.id}
               buttonClassName="h-7"
               disabled={false}
-              optionsClassName="z-20"
             />
           )}
         />
