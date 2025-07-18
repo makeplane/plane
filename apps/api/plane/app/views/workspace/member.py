d7d1545801158776f2867ac4399b34ba64c17025
Vamsi Krishna <46787868+vamsikrishnamathala@users.noreply.github.com>
2025-07-18 20:08:59 +0530

    [WEB-4192]fix: inactive member is hidden in created by #7435

--- apps/api/plane/app/views/workspace/member.py
+++ apps/api/plane/app/views/workspace/member.py
@@ -33,7 +33,7 @@ class WorkSpaceMemberViewSet(BaseViewSet):
         return self.filter_queryset(
             super()
             .get_queryset()
-            .filter(workspace__slug=self.kwargs.get("slug"), is_active=True)
+            .filter(workspace__slug=self.kwargs.get("slug"))
             .select_related("member", "member__avatar_asset")
         )
 
