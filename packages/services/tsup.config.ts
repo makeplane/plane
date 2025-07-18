9f9b612bcd9bec9fb609985c7be9950d91d92294
sriramveeraghanta <veeraghanta.sriram@gmail.com>
2025-07-18 15:01:06 +0530

    chore: update tsup build packages

--- packages/services/tsup.config.ts
+++ packages/services/tsup.config.ts
@@ -9,5 +9,5 @@ export default defineConfig({
   minify: true,
   splitting: true,
   treeshake: true,
-  external: ["winston", "winston-daily-rotate-file"],
+  external: ["axios"],
 });
--- packages/types/tsup.config.ts
+++ packages/types/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   outDir: "dist",
-  format: ["esm"],
+  format: ["esm", "cjs"],
   dts: true,
   clean: true,
   minify: true,
