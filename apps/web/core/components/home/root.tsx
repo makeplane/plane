9007ec37094894dd2284b11e22ed4a489329cb89
Akshita Goyal <36129505+gakshita@users.noreply.github.com>
2025-07-23 10:11:18 +0530

    [WEB-4546] chore: home update #7456

--- apps/web/core/components/home/root.tsx
+++ apps/web/core/components/home/root.tsx
@@ -60,7 +60,9 @@ export const WorkspaceHomeView = observer(() => {
       )}
       <>
         <HomePeekOverviewsRoot />
-        <ContentWrapper className={cn("gap-6 bg-custom-background-100 max-w-[750px] mx-auto scrollbar-hide")}>
+        <ContentWrapper
+          className={cn("gap-6 bg-custom-background-100 max-w-[800px] mx-auto scrollbar-hide px-page-x lg:px-0")}
+        >
           {currentUser && <UserGreetingsView user={currentUser} />}
           <DashboardWidgets />
         </ContentWrapper>
