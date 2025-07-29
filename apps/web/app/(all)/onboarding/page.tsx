ea82f15bfabfc930a1b921de12fce246cd226f71
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2025-07-29 19:24:20 +0530

    fix: build error

--- apps/web/app/(all)/onboarding/page.tsx
+++ apps/web/app/(all)/onboarding/page.tsx
@@ -41,19 +41,21 @@ const OnboardingPage = observer(() => {
   );
 
   return (
-    <WorkspaceContentWrapper>
-      <AuthenticationWrapper pageType={EPageTypes.ONBOARDING}>
-        <div className="relative flex flex-col h-full w-full overflow-hidden rounded-lg bg-custom-background-100 shadow-md border border-custom-border-200">
-          {user && !invitationsLoader ? (
-            <OnboardingRoot invitations={invitations ?? []} />
-          ) : (
-            <div className="grid h-full w-full place-items-center">
-              <LogoSpinner />
-            </div>
-          )}
+    <AuthenticationWrapper pageType={EPageTypes.ONBOARDING}>
+      <div className="flex relative size-full overflow-hidden bg-custom-background-90 rounded-lg transition-all ease-in-out duration-300">
+        <div className="size-full p-2 flex-grow transition-all ease-in-out duration-300 overflow-hidden">
+          <div className="relative flex flex-col h-full w-full overflow-hidden rounded-lg bg-custom-background-100 shadow-md border border-custom-border-200">
+            {user && !invitationsLoader ? (
+              <OnboardingRoot invitations={invitations ?? []} />
+            ) : (
+              <div className="grid h-full w-full place-items-center">
+                <LogoSpinner />
+              </div>
+            )}
+          </div>
         </div>
-      </AuthenticationWrapper>
-    </WorkspaceContentWrapper>
+      </div>
+    </AuthenticationWrapper>
   );
 });
 
