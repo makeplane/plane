ae43fff4316b4789d11a26bc51cac543ce154fb9
naaa760 <neh6a683@gmail.com>
2025-07-31 12:45:21 +0530

    replaced blue dot with filter icon badge for display filters

--- apps/web/core/components/issues/issue-detail-widgets/sub-issues/display-filters.tsx
+++ apps/web/core/components/issues/issue-detail-widgets/sub-issues/display-filters.tsx
@@ -43,7 +43,13 @@ export const SubIssueDisplayFilters: FC<TSubIssueDisplayFiltersProps> = observer
                 isFilterApplied && "bg-custom-primary-60/20"
               )}
             >
-              {isFilterApplied && <span className="p-1 rounded-full bg-custom-primary-100 absolute -top-1 -right-1" />}
+              {isFilterApplied && (
+                <span className="absolute -right-1 -top-1 flex h-4 w-4 items-center justify-center rounded-full bg-custom-primary-100 text-[10px] font-medium text-white">
+                  <svg className="h-2.5 w-2.5" fill="currentColor" viewBox="0 0 20 20">
+                    <path fillRule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clipRule="evenodd" />
+                  </svg>
+                </span>
+              )}
               <SlidersHorizontal className="h-3.5 w-3.5 text-custom-text-100" />
             </div>
           }
