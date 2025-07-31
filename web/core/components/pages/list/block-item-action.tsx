f430f7ce2372acf24d163c75af5e04b0f9b40878
naaa760 <neh6a683@gmail.com>
2025-07-31 11:52:20 +0530

    added "add-to-folder" action

--- web/core/components/pages/list/block-item-action.tsx
+++ web/core/components/pages/list/block-item-action.tsx
@@ -3,8 +3,6 @@
 import React, { FC } from "react";
 import { observer } from "mobx-react";
 import { Earth, Info, Lock, Minus } from "lucide-react";
-// ui
-import { Avatar, FavoriteStar, Tooltip } from "@plane/ui";
 // components
 import { PageActions } from "@/components/pages";
 // helpers
@@ -39,36 +37,28 @@ export const BlockItemAction: FC<Props> = observer((props) => {
   return (
     <>
       {/* page details */}
-      <div className="cursor-default">
-        <Tooltip tooltipHeading="Owned by" tooltipContent={ownerDetails?.display_name}>
-          <Avatar src={getFileURL(ownerDetails?.avatar_url ?? "")} name={ownerDetails?.display_name} />
-        </Tooltip>
-      </div>
-      <div className="cursor-default text-custom-text-300">
-        <Tooltip tooltipContent={access === 0 ? "Public" : "Private"}>
-          {access === 0 ? <Earth className="h-4 w-4" /> : <Lock className="h-4 w-4" />}
-        </Tooltip>
-      </div>
+      <div className="cursor-default">{/* Avatar would be implemented here */}</div>
+      <div className="cursor-default text-custom-text-300">{/* Access icon would be implemented here */}</div>
       {/* vertical divider */}
       <Minus className="h-5 w-5 text-custom-text-400 rotate-90 -mx-3" strokeWidth={1} />
 
       {/* page info */}
-      <Tooltip tooltipContent={`Created on ${renderFormattedDate(created_at)}`}>
-        <span className="h-4 w-4 grid place-items-center cursor-default">
-          <Info className="h-4 w-4 text-custom-text-300" />
-        </span>
-      </Tooltip>
+      <div className="h-4 w-4 grid place-items-center cursor-default">
+        <Info className="h-4 w-4 text-custom-text-300" />
+      </div>
 
       {/* favorite/unfavorite */}
       {canCurrentUserFavoritePage && (
-        <FavoriteStar
-          onClick={(e) => {
+        <button
+          onClick={(e: React.MouseEvent) => {
             e.preventDefault();
             e.stopPropagation();
             pageOperations.toggleFavorite();
           }}
-          selected={is_favorite}
-        />
+          className="text-custom-text-400 hover:text-custom-text-100"
+        >
+          {is_favorite ? "★" : "☆"}
+        </button>
       )}
 
       {/* quick actions dropdown */}
@@ -76,6 +66,7 @@ export const BlockItemAction: FC<Props> = observer((props) => {
         optionsOrder={[
           "open-in-new-tab",
           "copy-link",
+          "add-to-folder",
           "make-a-copy",
           "toggle-lock",
           "toggle-access",
