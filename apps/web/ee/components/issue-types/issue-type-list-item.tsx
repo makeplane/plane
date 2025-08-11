58bb85496e81f1db0ef995af2194f472c6bde807
Vamsi Krishna <46787868+vamsikrishnamathala@users.noreply.github.com>
2025-08-11 20:25:10 +0530

    [WEB-4643] fix: build errors for event trackers #3887

--- apps/web/ee/components/issue-types/issue-type-list-item.tsx
+++ apps/web/ee/components/issue-types/issue-type-list-item.tsx
@@ -2,7 +2,6 @@
 import { observer } from "mobx-react";
 import { ChevronRight } from "lucide-react";
 // plane imports
-import { WORK_ITEM_TYPE_TRACKER_ELEMENTS, WORK_ITEM_TYPE_TRACKER_EVENTS } from "@plane/constants";
 import { useTranslation } from "@plane/i18n";
 import { TLoader, IIssueType } from "@plane/types";
 import { Collapsible } from "@plane/ui";
@@ -126,18 +125,6 @@ export const IssueTypeListItem = observer((props: TIssueTypeListItem) => {
               issueTypeId={issueTypeId}
               propertiesLoader={propertiesLoader}
               getWorkItemTypeById={getWorkItemTypeById}
-              trackers={{
-                create: {
-                  button: WORK_ITEM_TYPE_TRACKER_ELEMENTS.CREATE_PROPERTY_BUTTON,
-                  eventName: WORK_ITEM_TYPE_TRACKER_EVENTS.CREATE_PROPERTY,
-                },
-                update: {
-                  eventName: WORK_ITEM_TYPE_TRACKER_EVENTS.UPDATE,
-                },
-                delete: {
-                  eventName: WORK_ITEM_TYPE_TRACKER_EVENTS.DELETE,
-                },
-              }}
             />
           </div>
         </Collapsible>
