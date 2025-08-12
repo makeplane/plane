a5b39fd767743213fe323275845171119e13a9b6
sangeethailango <sangeethailango21@gmail.com>
2025-08-12 18:38:15 +0530

    fix: unsubscribed WI on your work

--- apps/api/plane/app/views/workspace/user.py
+++ apps/api/plane/app/views/workspace/user.py
@@ -97,9 +97,15 @@ class WorkspaceUserProfileIssuesEndpoint(BaseAPIView):
         order_by_param = request.GET.get("order_by", "-created_at")
         issue_queryset = (
             Issue.issue_objects.filter(
-                Q(assignees__in=[user_id])
+                Q(
+                    assignees__in=[user_id],
+                    issue_assignee__deleted_at__isnull=True,
+                )
                 | Q(created_by_id=user_id)
-                | Q(issue_subscribers__subscriber_id=user_id),
+                | Q(
+                    issue_subscribers__subscriber_id=user_id,
+                    issue_subscribers__deleted_at__isnull=True,
+                ),
                 workspace__slug=slug,
                 project__project_projectmember__member=request.user,
                 project__project_projectmember__is_active=True,
