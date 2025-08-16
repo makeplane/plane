0d2433d577acabd58f002c6e39e794438c692ae1
Sriram Veeraghanta <veeraghanta.sriram@gmail.com>
2025-08-16 15:01:18 +0530

    feat: adding baseui components

--- packages/propel/.eslintrc.js
+++ packages/propel/.eslintrc.js
@@ -3,4 +3,33 @@ module.exports = {
   root: true,
   extends: ["@plane/eslint-config/library.js"],
   parser: "@typescript-eslint/parser",
+  rules: {
+    "import/order": [
+      "warn",
+      {
+        groups: ["builtin", "external", "internal", "parent", "sibling"],
+        pathGroups: [
+          {
+            pattern: "react",
+            group: "external",
+            position: "before",
+          },
+          {
+            pattern: "@plane/**",
+            group: "external",
+            position: "after",
+          },
+          {
+            pattern: "@/**",
+            group: "internal",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["builtin", "internal", "react"],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
 };
