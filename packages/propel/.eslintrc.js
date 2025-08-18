9c21fd320c7bcbc78894cb13d1cbd2c5ff102ac2
sriram veeraghanta <veeraghanta.sriram@gmail.com>
2025-08-18 19:35:34 +0530

    feat: adding baseui components to propel package (#7585)
    * feat: adding baseui components
    
    * fix: export from the package.json

--- packages/propel/.eslintrc.js
+++ packages/propel/.eslintrc.js
@@ -3,4 +3,29 @@ module.exports = {
   root: true,
   extends: ["@plane/eslint-config/library.js"],
   parser: "@typescript-eslint/parser",
+  rules: {
+    "import/order": [
+      "warn",
+      {
+        groups: ["builtin", "external", "internal", "parent", "sibling"],
+        pathGroups: [
+          {
+            pattern: "react",
+            group: "external",
+            position: "before",
+          },
+          {
+            pattern: "@plane/**",
+            group: "external",
+            position: "after",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["builtin", "internal", "react"],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
 };
