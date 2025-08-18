fda8c728b5f016e2a94e659234db6db4eb8c407a
gakshita <akshitagoyal1516@gmail.com>
2025-08-18 19:47:21 +0530

    chore: replaced listbox with our custom implementation

--- apps/space/core/components/issues/peek-overview/header.tsx
+++ apps/space/core/components/issues/peek-overview/header.tsx
@@ -3,9 +3,8 @@
 import React from "react";
 import { observer } from "mobx-react";
 import { Link2, MoveRight } from "lucide-react";
-import { Listbox, Transition } from "@headlessui/react";
 // ui
-import { CenterPanelIcon, FullScreenPanelIcon, setToast, SidePanelIcon, TOAST_TYPE } from "@plane/ui";
+import { CenterPanelIcon, CustomSelect, FullScreenPanelIcon, setToast, SidePanelIcon, TOAST_TYPE } from "@plane/ui";
 // helpers
 import { copyTextToClipboard } from "@/helpers/string.helper";
 // hooks
@@ -66,49 +65,29 @@ export const PeekOverviewHeader: React.FC<Props> = observer((props) => {
               <MoveRight className="size-4" />
             </button>
           )}
-          <Listbox
-            as="div"
+          <CustomSelect
             value={peekMode}
-            onChange={(val) => setPeekMode(val)}
+            onChange={(val: any) => setPeekMode(val)}
             className="relative flex-shrink-0 text-left"
+            customButtonClassName={`grid place-items-center text-custom-text-300 hover:text-custom-text-200 ${peekMode === "full" ? "rotate-45" : ""}`}
+            customButton={<Icon className="h-4 w-4 text-custom-text-300 hover:text-custom-text-200" />}
+            optionsClassName="absolute left-0 z-10 mt-1 min-w-[12rem] origin-top-left overflow-y-auto whitespace-nowrap rounded-md border border-custom-border-300 bg-custom-background-90 text-xs shadow-lg focus:outline-none"
           >
-            <Listbox.Button
-              className={`grid place-items-center text-custom-text-300 hover:text-custom-text-200 ${peekMode === "full" ? "rotate-45" : ""}`}
-            >
-              <Icon className="h-4 w-4 text-custom-text-300 hover:text-custom-text-200" />
-            </Listbox.Button>
-
-            <Transition
-              as={React.Fragment}
-              enter="transition ease-out duration-100"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
-            >
-              <Listbox.Options className="absolute left-0 z-10 mt-1 min-w-[12rem] origin-top-left overflow-y-auto whitespace-nowrap rounded-md border border-custom-border-300 bg-custom-background-90 text-xs shadow-lg focus:outline-none">
-                <div className="space-y-1 p-2">
-                  {PEEK_MODES.map((mode) => (
-                    <Listbox.Option
-                      key={mode.key}
-                      value={mode.key}
-                      className={({ active, selected }) =>
-                        `cursor-pointer select-none truncate rounded px-1 py-1.5 ${
-                          active ? "bg-custom-background-80" : ""
-                        } ${selected ? "text-custom-text-100" : "text-custom-text-200"}`
-                      }
-                    >
-                      <div className="flex items-center gap-1.5">
-                        <mode.icon className="-my-1 h-4 w-4 flex-shrink-0" />
-                        {mode.label}
-                      </div>
-                    </Listbox.Option>
-                  ))}
-                </div>
-              </Listbox.Options>
-            </Transition>
-          </Listbox>
+            <div className="space-y-1 p-2">
+              {PEEK_MODES.map((mode) => (
+                <CustomSelect.Option
+                  key={mode.key}
+                  value={mode.key}
+                  className="cursor-pointer select-none truncate rounded px-1 py-1.5"
+                >
+                  <div className="flex items-center gap-1.5">
+                    <mode.icon className="-my-1 h-4 w-4 flex-shrink-0" />
+                    {mode.label}
+                  </div>
+                </CustomSelect.Option>
+              ))}
+            </div>
+          </CustomSelect>
         </div>
         {isClipboardWriteAllowed && (peekMode === "side" || peekMode === "modal") && (
           <div className="flex flex-shrink-0 items-center gap-2">
