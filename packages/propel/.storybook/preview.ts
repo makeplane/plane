4914fb33996d766549d656691c073d5309644773
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2025-08-21 05:42:14 +0530

    chore: storybook init

---
+++ packages/propel/.storybook/preview.ts
@@ -0,0 +1,15 @@
+import type { Preview } from '@storybook/react-vite'
+import "@plane/tailwind-config/global.css"
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+       color: /(background|color)$/i,
+       date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
