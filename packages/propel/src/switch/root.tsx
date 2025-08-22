8a4e4af66db50cfd29ff030df38f5a256fcab9a3
Jayash Tripathy <76092296+JayashTripathy@users.noreply.github.com>
2025-08-22 19:48:20 +0530

    refactor: styles

--- packages/propel/src/switch/root.tsx
+++ packages/propel/src/switch/root.tsx
@@ -22,7 +22,11 @@ const Switch: React.FC<IToggleSwitchProps> = ({ value, onChange, label, size = "
       // size
       size === "sm" ? "h-4 w-6" : size === "md" ? "h-5 w-8" : "h-6 w-10",
       // state
-      disabled ? "cursor-not-allowed bg-custom-background-80" : value ? "bg-custom-primary-100" : "bg-gray-700",
+      disabled
+        ? "cursor-not-allowed bg-custom-background-80"
+        : value
+          ? "cursor-pointer bg-custom-primary-100"
+          : "cursor-pointer bg-custom-background-90",
       className
     )}
   >
@@ -30,16 +34,19 @@ const Switch: React.FC<IToggleSwitchProps> = ({ value, onChange, label, size = "
     <BaseSwitch.Thumb
       aria-hidden="true"
       className={cn(
-        "inline-block self-center h-4 w-4 transform rounded-full shadow ring-0 transition duration-200 ease-in-out",
-        {
-          "translate-x-5 bg-white": value,
-          "h-2 w-2": size === "sm",
-          "h-3 w-3": size === "md",
-          "translate-x-3": value && size === "sm",
-          "translate-x-4": value && size === "md",
-          "translate-x-0.5 bg-custom-background-90": !value,
-          "cursor-not-allowed bg-custom-background-90": disabled,
-        }
+        "inline-block self-center rounded-full shadow ring-0 transition-transform duration-200 ease-in-out",
+        // size
+        size === "sm" ? "h-3 w-3" : size === "md" ? "h-4 w-4" : "h-5 w-5",
+        // position + color by state
+        value
+          ? size === "sm"
+            ? "translate-x-3 bg-white"
+            : size === "md"
+              ? "translate-x-4 bg-white"
+              : "translate-x-5 bg-white"
+          : "translate-x-0.5 bg-custom-background-90",
+        // disabled
+        disabled && "cursor-not-allowed bg-custom-background-90"
       )}
     />
   </BaseSwitch.Root>
