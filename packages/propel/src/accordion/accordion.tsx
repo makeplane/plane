0fe7536617bed45000a7d995c44d6a6ea62d44bf
vamsikrishnamathala <matalav55@gmail.com>
2025-08-25 16:51:30 +0530

    fix: lint errors

--- packages/propel/src/accordion/accordion.tsx
+++ packages/propel/src/accordion/accordion.tsx
@@ -28,38 +28,34 @@ export const Accordion: React.FC<AccordionProps> = ({
   itemClassName = "",
   triggerClassName = "",
   panelClassName = "",
-  icon = (
-    <PlusIcon className="mr-2 size-3 shrink-0 transition-all ease-out group-data-[panel-open]:scale-110 group-data-[panel-open]:rotate-45" />
-  ),
-}) => {
-  return (
-    <BaseAccordion.Root
-      defaultValue={defaultValue}
-      openMultiple={allowMultiple}
-      className={`flex flex-col justify-center text-gray-900 ${className}`}
-    >
-      {items.map((item) => (
-        <BaseAccordion.Item
-          key={item.id}
-          value={item.id}
-          disabled={item.disabled}
-          className={`border-b border-gray-200 last:border-0 ${itemClassName}`}
-        >
-          <BaseAccordion.Header>
-            <BaseAccordion.Trigger
-              className={`group relative flex w-full items-center justify-between gap-4 bg-gray-50 py-2 pr-1 pl-3 text-left font-medium hover:bg-gray-100 focus-visible:z-1 focus-visible:outline focus-visible:outline-2 focus-visible:outline-blue-800 disabled:cursor-not-allowed disabled:opacity-50 ${triggerClassName}`}
-            >
-              {item.title}
-              {icon}
-            </BaseAccordion.Trigger>
-          </BaseAccordion.Header>
-          <BaseAccordion.Panel
-            className={`h-[var(--accordion-panel-height)] overflow-hidden text-base text-gray-600 transition-[height] ease-out data-[ending-style]:h-0 data-[starting-style]:h-0 ${panelClassName}`}
+  icon = <PlusIcon className="mr-2 size-3 shrink-0 transition-all ease-out group-data-[panel-open]:scale-110 group-data-[panel-open]:rotate-45" />,
+}) => (
+  <BaseAccordion.Root
+    defaultValue={defaultValue}
+    openMultiple={allowMultiple}
+    className={`flex flex-col justify-center text-gray-900 ${className}`}
+  >
+    {items.map((item) => (
+      <BaseAccordion.Item
+        key={item.id}
+        value={item.id}
+        disabled={item.disabled}
+        className={`border-b border-gray-200 last:border-0 ${itemClassName}`}
+      >
+        <BaseAccordion.Header>
+          <BaseAccordion.Trigger
+            className={`group relative flex w-full items-center justify-between gap-4 bg-gray-50 py-2 pr-1 pl-3 text-left font-medium hover:bg-gray-100 focus-visible:z-1 focus-visible:outline focus-visible:outline-2 focus-visible:outline-blue-800 disabled:cursor-not-allowed disabled:opacity-50 ${triggerClassName}`}
           >
-            <div className="p-3">{item.content}</div>
-          </BaseAccordion.Panel>
-        </BaseAccordion.Item>
-      ))}
-    </BaseAccordion.Root>
-  );
-};
+            {item.title}
+            {icon}
+          </BaseAccordion.Trigger>
+        </BaseAccordion.Header>
+        <BaseAccordion.Panel
+          className={`h-[var(--accordion-panel-height)] overflow-hidden text-base text-gray-600 transition-[height] ease-out data-[ending-style]:h-0 data-[starting-style]:h-0 ${panelClassName}`}
+        >
+          <div className="p-3">{item.content}</div>
+        </BaseAccordion.Panel>
+      </BaseAccordion.Item>
+    ))}
+  </BaseAccordion.Root>
+);
