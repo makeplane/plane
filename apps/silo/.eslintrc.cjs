ac8bd2ae922304b3dadc68302c9304c837d54bc9
Prateek Shourya <prateekshourya29@gmail.com>
2025-08-28 13:27:01 +0530

    [WEB-4790] fix: moved typescript parser to the base eslint config (#4021)

--- apps/silo/.eslintrc.cjs
+++ apps/silo/.eslintrc.cjs
@@ -1,8 +1,6 @@
-/** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
   extends: ["@plane/eslint-config/library.js"],
-  parser: "@typescript-eslint/parser",
   env: {
     jest: true,
   },
