542591413f8caec5d78640e1b58649ac45b45f40
sriramveeraghanta <veeraghanta.sriram@gmail.com>
2025-08-29 21:18:12 +0530

    feat: migrat to tsdown to tsup

--- packages/constants/tsdown.config.ts
+++ packages/constants/tsdown.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["src/index.ts"],
   outDir: "dist",
   format: ["esm", "cjs"],
-  dts: true,
-  clean: true,
-  minify: true,
 });
