eabcb8d1118903543eed82ffabba787c92555871
sriramveeraghanta <veeraghanta.sriram@gmail.com>
2025-08-29 15:19:48 +0530

    fix: live server build error

--- apps/live/src/core/controller-registry.ts
+++ apps/live/src/core/controller-registry.ts
@@ -1,4 +1,4 @@
 import { CONTROLLERS } from "@/plane-live/controllers";
 
 // Helper to get all REST controllers
-export const getAllControllers = () => [...CONTROLLERS.CORE, ...CONTROLLERS.DOCUMENT, ...CONTROLLERS.WEBSOCKET];
+export const getAllControllers: any = () => [...CONTROLLERS.CORE, ...CONTROLLERS.DOCUMENT, ...CONTROLLERS.WEBSOCKET];
