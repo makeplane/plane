b49faa622687ba6eaea6b908c803c7105b5a7966
Prateek Shourya <prateekshourya29@gmail.com>
2025-09-02 02:24:08 +0530

    [WEB-4829] refactor: remove `start us` CTA from header

--- apps/web/app/(all)/[workspaceSlug]/(projects)/header.tsx
+++ apps/web/app/(all)/[workspaceSlug]/(projects)/header.tsx
@@ -1,26 +1,17 @@
 "use client";
 
 import { observer } from "mobx-react";
-import Image from "next/image";
-import { useTheme } from "next-themes";
 import { Home, Shapes } from "lucide-react";
-// images
-import githubBlackImage from "/public/logos/github-black.png";
-import githubWhiteImage from "/public/logos/github-white.png";
-// ui
-import { GITHUB_REDIRECTED_TRACKER_EVENT, HEADER_GITHUB_ICON } from "@plane/constants";
+// plane imports
 import { useTranslation } from "@plane/i18n";
 import { Breadcrumbs, Button, Header } from "@plane/ui";
 // components
 import { BreadcrumbLink } from "@/components/common/breadcrumb-link";
-// constants
 // hooks
-import { captureElementAndEvent } from "@/helpers/event-tracker.helper";
 import { useHome } from "@/hooks/store/use-home";
 
 export const WorkspaceDashboardHeader = observer(() => {
   // hooks
-  const { resolvedTheme } = useTheme();
   const { toggleWidgetSettings } = useHome();
   const { t } = useTranslation();
 
@@ -48,31 +39,6 @@ export const WorkspaceDashboardHeader = observer(() => {
             <Shapes size={16} />
             <div className="hidden text-xs font-medium sm:hidden md:block">{t("home.manage_widgets")}</div>
           </Button>
-          <a
-            onClick={() =>
-              captureElementAndEvent({
-                element: {
-                  elementName: HEADER_GITHUB_ICON,
-                },
-                event: {
-                  eventName: GITHUB_REDIRECTED_TRACKER_EVENT,
-                  state: "SUCCESS",
-                },
-              })
-            }
-            className="flex flex-shrink-0 items-center gap-1.5 rounded bg-custom-background-80 px-3 py-1.5"
-            href="https://github.com/makeplane/plane"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src={resolvedTheme === "dark" ? githubWhiteImage : githubBlackImage}
-              height={16}
-              width={16}
-              alt="GitHub Logo"
-            />
-            <span className="hidden text-xs font-medium sm:hidden md:block">{t("home.star_us_on_github")}</span>
-          </a>
         </Header.RightItem>
       </Header>
     </>
