31f0f500b4b6abec70ef3ea96a0f752e80738dde
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2025-09-01 16:47:53 +0530

    chore: code refactor

--- packages/propel/src/emoji-icon-picker/emoji-picker.stories.tsx
+++ packages/propel/src/emoji-icon-picker/emoji-picker.stories.tsx
@@ -1,5 +1,5 @@
-import type { Meta, StoryObj } from "@storybook/react-vite";
 import { useState } from "react";
+import type { Meta, StoryObj } from "@storybook/react-vite";
 import { EmojiPicker } from "./emoji-picker";
 import { EmojiIconPickerTypes } from "./helper";
 
