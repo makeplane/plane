6644052970cd920c52b44a9ba9c179a1f1577d0f
Prateek Shourya <prateekshourya29@gmail.com>
2025-09-02 14:21:02 +0530

    [WEB-4829] refactor: remove star us CTA from wiki header (#4068)
    Co-authored-by: sriramveeraghanta <veeraghanta.sriram@gmail.com>

--- packages/constants/src/event-tracker/core.ts
+++ packages/constants/src/event-tracker/core.ts
@@ -6,9 +6,6 @@ import { EProductSubscriptionEnum } from "@plane/types";
  * ===========================================================================
  */
 export const GROUP_WORKSPACE_TRACKER_EVENT = "workspace_metrics";
-export const GITHUB_REDIRECTED_TRACKER_EVENT = "github_redirected";
-export const HEADER_GITHUB_ICON = "header_github_icon";
-export const PAGE_HEADER_NAVBAR_TRACKER_ELEMENT = "navbar";
 
 /**
  * ===========================================================================
