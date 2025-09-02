3037ca1606a79ad213ab47c9a95dd22a274bcb13
Aaron Reisman <aaron.reisman@plane.so>
2025-09-02 15:01:02 -0700

    fix: tsdown configs

--- packages/hooks/tsdown.config.ts
+++ packages/hooks/tsdown.config.ts
@@ -4,4 +4,8 @@ export default defineConfig({
   entry: ["src/index.ts"],
   outDir: "dist",
   format: ["esm", "cjs"],
+  external: ["react", "react-dom"],
+  dts: true,
+  sourcemap: true,
+  clean: true,
 });
