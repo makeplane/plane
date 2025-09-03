b99ddc24e742e82c32d60a5604dd45ebac8a0b00
sriram veeraghanta <veeraghanta.sriram@gmail.com>
2025-09-03 14:01:57 +0530

    [WEB-4810] feat: migrate to tsdown from tsup (#7679)
    * feat: migrat to tsdown to tsup
    
    * fix: build scripts
    
    * fix: lock file fixes
    
    * fix: adding build process to i18n and propel packages
    
    * fix: lint warning
    
    * chore: update services module entry points
    
    * fix: lock file
    
    * fix: lock file
    
    * fix: remove tsc from build
    
    * fix: tsdown configs
    
    * fix: remove tsc step from build process
    
    ---------
    
    Co-authored-by: Aaryan Khandelwal <aaryankhandu123@gmail.com>
    Co-authored-by: Aaron Reisman <aaron.reisman@plane.so>

--- packages/hooks/tsdown.config.ts
+++ packages/hooks/tsdown.config.ts
@@ -1,13 +1,11 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["src/index.ts"],
   outDir: "dist",
   format: ["esm", "cjs"],
+  external: ["react", "react-dom"],
   dts: true,
+  sourcemap: true,
   clean: true,
-  minify: true,
-  splitting: true,
-  treeshake: true,
-  external: ["react"],
 });
