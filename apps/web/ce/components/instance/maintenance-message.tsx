6f58683fc99cf5e5b5de4c006ce3d9cbcd686b5d
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2025-09-05 12:53:11 +0530

    chore: maintenance mode empty state updated

--- apps/web/ce/components/instance/maintenance-message.tsx
+++ apps/web/ce/components/instance/maintenance-message.tsx
@@ -6,11 +6,10 @@ export const MaintenanceMessage = observer(() => {
   const { t } = useTranslation();
 
   return (
-    <h1 className="text-xl font-medium text-custom-text-100 text-center md:text-left">
+    <h1 className="text-base font-medium text-custom-text-100 text-left">
       {t(
         "self_hosted_maintenance_message.plane_didnt_start_up_this_could_be_because_one_or_more_plane_services_failed_to_start"
-      )}
-      <br />
+      )}{" "}
       {t("self_hosted_maintenance_message.choose_view_logs_from_setup_sh_and_docker_logs_to_be_sure")}
     </h1>
   );
--- apps/web/core/components/instance/maintenance-view.tsx
+++ apps/web/core/components/instance/maintenance-view.tsx
@@ -2,6 +2,7 @@
 
 import { FC } from "react";
 import Image from "next/image";
+import { useTheme } from "next-themes";
 // ui
 import { Button } from "@plane/ui";
 // layouts
@@ -9,26 +10,33 @@ import DefaultLayout from "@/layouts/default-layout";
 // components
 import { MaintenanceMessage } from "@/plane-web/components/instance";
 // images
-import maintenanceModeImage from "@/public/maintenance-mode.webp";
+import maintenanceModeDarkModeImage from "@/public/instance/maintenance-mode-dark.svg";
+import maintenanceModeLightModeImage from "@/public/instance/maintenance-mode-light.svg";
 
-export const MaintenanceView: FC = () => (
-  <DefaultLayout>
-    <div className="relative container mx-auto h-full w-full flex flex-col md:flex-row gap-2 items-center justify-center gap-y-5 bg-custom-background-100 text-center">
-      <div className="relative w-full">
-        <Image
-          src={maintenanceModeImage}
-          height="176"
-          width="288"
-          alt="ProjectSettingImg"
-          className="w-full h-full object-fill object-center"
-        />
+export const MaintenanceView: FC = () => {
+  // hooks
+  const { resolvedTheme } = useTheme();
+  // derived values
+  const maintenanceModeImage = resolvedTheme === "dark" ? maintenanceModeDarkModeImage : maintenanceModeLightModeImage;
+  return (
+    <DefaultLayout>
+      <div className="relative container mx-auto h-full w-full max-w-xl flex flex-col gap-2 items-center justify-center gap-y-5 bg-custom-background-100 text-center">
+        <div className="relative w-full">
+          <Image
+            src={maintenanceModeImage}
+            height="176"
+            width="288"
+            alt="ProjectSettingImg"
+            className="w-full h-full object-fill object-center"
+          />
+        </div>
+        <div className="w-full space-y-4 relative flex flex-col">
+          <MaintenanceMessage />
+          <Button variant="outline-primary" className="w-min" onClick={() => window.location.reload()}>
+            Reload
+          </Button>
+        </div>
       </div>
-      <div className="w-full space-y-4 relative flex flex-col justify-center md:justify-start items-center md:items-start">
-        <MaintenanceMessage />
-        <Button variant="outline-primary" onClick={() => window.location.reload()}>
-          Reload
-        </Button>
-      </div>
-    </div>
-  </DefaultLayout>
-);
+    </DefaultLayout>
+  );
+};
