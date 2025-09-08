ebab0234d5bb4fe588be693a698892cd2a867880
vamsikrishnamathala <matalav55@gmail.com>
2025-09-08 15:41:42 +0530

    chore: updated import paths

--- packages/propel/src/charts/components/tooltip.tsx
+++ packages/propel/src/charts/components/tooltip.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { NameType, Payload, ValueType } from "recharts/types/component/DefaultTooltipContent";
-// plane imports
-import { cn } from "@plane/utils";
 import { Card, ECardSpacing } from "../../card";
-
+import { cn } from "../../utils/classname";
 
 type Props = {
   active: boolean | undefined;
