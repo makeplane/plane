d4cdb1fd7115e16f646975cef746b403dce72a5d
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2025-09-09 09:46:25 +0530

    chore: propel config updated

--- packages/propel/tsdown.config.ts
+++ packages/propel/tsdown.config.ts
@@ -11,6 +11,8 @@ export default defineConfig({
     "src/command/index.ts",
     "src/dialog/index.ts",
     "src/emoji-icon-picker/index.ts",
+    "src/emoji-reaction/index.ts",
+    "src/emoji-reaction-picker/index.ts",
     "src/icons/index.ts",
     "src/menu/index.ts",
     "src/popover/index.ts",
