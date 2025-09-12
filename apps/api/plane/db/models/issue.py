c4f40bc9cf6af3e0c12d48366e5cf10d253e53a7
NarayanBavisetti <narayan3119@gmail.com>
2025-09-12 15:00:15 +0530

    chore: added issue relation and page sort order

--- apps/api/plane/db/models/issue.py
+++ apps/api/plane/db/models/issue.py
@@ -284,6 +284,7 @@ class IssueRelationChoices(models.TextChoices):
     BLOCKED_BY = "blocked_by", "Blocked By"
     START_BEFORE = "start_before", "Start Before"
     FINISH_BEFORE = "finish_before", "Finish Before"
+    IMPLEMENTED_BY = "implemented_by", "Implemented By"
 
 
 class IssueRelation(ProjectBaseModel):
@@ -295,7 +296,6 @@ class IssueRelation(ProjectBaseModel):
     )
     relation_type = models.CharField(
         max_length=20,
-        choices=IssueRelationChoices.choices,
         verbose_name="Issue Relation Type",
         default=IssueRelationChoices.BLOCKED_BY,
     )
