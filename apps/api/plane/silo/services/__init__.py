37360a5ad275eadf676e9f732e55fd1f2c2bc2e9
Saurabh Kumar <70131915+Saurabhkmr98@users.noreply.github.com>
2025-09-17 17:02:30 +0530

    [SILO-474] Data Migrations for existing customers Github new flow (#4110)
    * data migration and management command for github v1
    
    * add check to support already splitted entity connections
    
    * fix return and method name in migration
    
    * add entity connection id arg and fixes
    
    * remove log params

--- apps/api/plane/silo/services/__init__.py
+++ apps/api/plane/silo/services/__init__.py
@@ -1,1 +1,5 @@
 from .generate_application import create_applications
+from .split_entity_connections import (
+    split_github_entity_connections,
+    get_github_entity_connections_ids_split_and_not_split,
+)
