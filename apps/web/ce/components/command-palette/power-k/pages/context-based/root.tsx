21f7a1cff3a434ec03555bc8b05f53a03fe88f55
Prateek Shourya <prateekshourya29@gmail.com>
2025-10-06 18:50:16 +0530

    chore: state menu items for work item context based commands

--- apps/web/ce/components/command-palette/power-k/pages/context-based/root.tsx
+++ apps/web/ce/components/command-palette/power-k/pages/context-based/root.tsx
@@ -2,7 +2,7 @@
 import type { TPowerKCommandConfig } from "@/components/power-k/core/types";
 import type { ContextBasedActionsProps, TContextEntityMap } from "@/components/power-k/ui/pages/context-based";
 // local imports
-import type { TPowerKContextTypeExtended } from "../types";
+import { TPowerKContextTypeExtended } from "../../types";
 
 export const CONTEXT_ENTITY_MAP_EXTENDED: Record<TPowerKContextTypeExtended, TContextEntityMap> = {};
 
