edc198e919fa38ba2872814a1b1fa9b195b441a4
Prateek Shourya <prateekshourya29@gmail.com>
2025-10-14 19:19:40 +0530

    refactor: remove deprecated views layout and page components

--- apps/space/app/routes.ts
+++ apps/space/app/routes.ts
@@ -5,7 +5,6 @@ export default [
   index("./page.tsx"),
   route(":workspaceSlug/:projectId", "./[workspaceSlug]/[projectId]/page.tsx"),
   layout("./issues/[anchor]/client-layout.tsx", [route("issues/:anchor", "./issues/[anchor]/page.tsx")]),
-  layout("./views/[anchor]/layout.tsx", [route("views/:anchor", "./views/[anchor]/page.tsx")]),
   // Catch-all route for 404 handling
   route("*", "./not-found.tsx"),
 ] satisfies RouteConfig;
