64ee7feed6f0de571cf558a9e4e6a2686d0388d1
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2025-10-15 17:27:24 +0530

    chore: project search empty state implementation

--- apps/web/core/components/project/card-list.tsx
+++ apps/web/core/components/project/card-list.tsx
@@ -1,5 +1,4 @@
 import { observer } from "mobx-react";
-import Image from "next/image";
 // plane imports
 import { EUserPermissionsLevel, EUserPermissions, PROJECT_TRACKER_ELEMENTS } from "@plane/constants";
 import { useTranslation } from "@plane/i18n";
@@ -13,7 +12,6 @@ import { useCommandPalette } from "@/hooks/store/use-command-palette";
 import { useProject } from "@/hooks/store/use-project";
 import { useProjectFilter } from "@/hooks/store/use-project-filter";
 import { useUserPermissions } from "@/hooks/store/user";
-import { useResolvedAssetPath } from "@/hooks/use-resolved-asset-path";
 // local imports
 import { ProjectCard } from "./card";
 
@@ -35,18 +33,8 @@ export const ProjectCardList = observer((props: TProjectCardListProps) => {
     filteredProjectIds: storeFilteredProjectIds,
     getProjectById,
   } = useProject();
-  const { searchQuery, currentWorkspaceDisplayFilters } = useProjectFilter();
+  const { currentWorkspaceDisplayFilters } = useProjectFilter();
   const { allowPermissions } = useUserPermissions();
-
-  // helper hooks
-  const resolvedFiltersImage = useResolvedAssetPath({
-    basePath: "/empty-state/project/all-filters",
-    extension: "svg",
-  });
-  const resolvedNameFilterImage = useResolvedAssetPath({
-    basePath: "/empty-state/project/name-filter",
-    extension: "svg",
-  });
 
   // derived values
   const workspaceProjectIds = totalProjectIdsProps ?? storeWorkspaceProjectIds;
@@ -84,21 +72,12 @@ export const ProjectCardList = observer((props: TProjectCardListProps) => {
 
   if (filteredProjectIds.length === 0)
     return (
-      <div className="grid h-full w-full place-items-center">
-        <div className="text-center">
-          <Image
-            src={searchQuery.trim() === "" ? resolvedFiltersImage : resolvedNameFilterImage}
-            className="mx-auto h-36 w-36 sm:h-48 sm:w-48"
-            alt="No matching projects"
-          />
-          <h5 className="mb-1 mt-7 text-xl font-medium">{t("workspace_projects.empty_state.filter.title")}</h5>
-          <p className="whitespace-pre-line text-base text-custom-text-400">
-            {searchQuery.trim() === ""
-              ? t("workspace_projects.empty_state.filter.description")
-              : t("workspace_projects.empty_state.search.description")}
-          </p>
-        </div>
-      </div>
+      <EmptyStateDetailed
+        title={t("common.search.title")}
+        description={t("common.search.description")}
+        assetKey="project"
+        assetClassName="size-40"
+      />
     );
 
   return (
