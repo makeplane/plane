92f0d20b1f331ce2cb2cb89710c089c7283f75d5
sriram veeraghanta <veeraghanta.sriram@gmail.com>
2025-10-23 20:09:00 +0530

    chore: initial setup

---
+++ packages/oxlint-config/library.js
@@ -0,0 +1,17 @@
+module.exports = {
+  rules: {
+    "no-useless-escape": "off",
+    "prefer-const": "error",
+    "no-irregular-whitespace": "error",
+    "no-trailing-spaces": "error",
+    "no-duplicate-imports": "error",
+    "no-useless-catch": "warn",
+    "no-case-declarations": "error",
+    "no-undef": "error",
+    "no-unreachable": "error",
+    "arrow-body-style": ["error", "as-needed"],
+    "@next/next/no-html-link-for-pages": "off",
+    "@next/next/no-img-element": "off",
+    "react/jsx-key": "error",
+  },
+};
