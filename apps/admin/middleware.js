bd092a531ecc95df1e9ab44810c70a0ffe75ae4c
Prateek Shourya <prateekshourya29@gmail.com>
2025-11-03 15:11:49 +0530

    fix: update X-Frame-Options header in admin middleware to SAMEORIGIN

--- apps/admin/middleware.js
+++ apps/admin/middleware.js
@@ -4,7 +4,7 @@ export default function middleware() {
   return next({
     headers: {
       'Referrer-Policy': 'origin-when-cross-origin',
-      'X-Frame-Options': 'DENY',
+      'X-Frame-Options': 'SAMEORIGIN',
       'X-Content-Type-Options': 'nosniff',
       'X-DNS-Prefetch-Control': 'on',
       'Strict-Transport-Security':
