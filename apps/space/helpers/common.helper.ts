ed816e371c3c937bd50c1415143593e5deac534e
sriramveeraghanta <veeraghanta.sriram@gmail.com>
2025-11-12 18:33:39 +0530

    chore: remove unused imports

--- apps/space/helpers/common.helper.ts
+++ apps/space/helpers/common.helper.ts
@@ -1,4 +1,2 @@
-export const SUPPORT_EMAIL = process.env.VITE_SUPPORT_EMAIL || "";
-
 export const resolveGeneralTheme = (resolvedTheme: string | undefined) =>
   resolvedTheme?.includes("light") ? "light" : resolvedTheme?.includes("dark") ? "dark" : "system";
