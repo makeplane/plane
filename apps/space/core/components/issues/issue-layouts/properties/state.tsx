09f4e3d4ae70837375dff6145311df0de9715bf7
Aaryan Khandelwal <65252264+aaryan610@users.noreply.github.com>
2025-11-24 21:26:48 +0530

    [WIKI-774] refactor: space app mentions #8152
    Co-authored-by: sriramveeraghanta <veeraghanta.sriram@gmail.com>

--- apps/space/core/components/issues/issue-layouts/properties/state.tsx
+++ apps/space/core/components/issues/issue-layouts/properties/state.tsx
@@ -1,31 +1,47 @@
+"use client";
+
 import { observer } from "mobx-react";
 // plane ui
 import { StateGroupIcon } from "@plane/propel/icons";
 import { Tooltip } from "@plane/propel/tooltip";
+import type { TStateGroups } from "@plane/types";
 // plane utils
 import { cn } from "@plane/utils";
 //hooks
 import { useStates } from "@/hooks/store/use-state";
 
 type Props = {
-  stateId: string | undefined;
   shouldShowBorder?: boolean;
-};
-export const IssueBlockState = observer(function IssueBlockState({ stateId, shouldShowBorder = true }: Props) {
-  const { getStateById } = useStates();
+} & (
+  | {
+      stateDetails: {
+        name: string;
+        group: TStateGroups;
+      };
+    }
+  | {
+      stateId: string;
+    }
+);
 
-  const state = getStateById(stateId);
+export const IssueBlockState: React.FC<Props> = observer((props) => {
+  const { shouldShowBorder = true } = props;
+  // store hooks
+  const { getStateById } = useStates();
+  // derived values
+  const state = "stateId" in props ? getStateById(props.stateId) : props.stateDetails;
+  if (!state) return null;
 
   return (
-    <Tooltip tooltipHeading="State" tooltipContent={state?.name ?? "State"}>
+    <Tooltip tooltipHeading="State" tooltipContent={state.name}>
       <div
         className={cn("flex h-full w-full items-center justify-between gap-1 rounded px-2.5 py-1 text-xs", {
           "border-[0.5px] border-custom-border-300": shouldShowBorder,
         })}
       >
         <div className="flex w-full items-center gap-1.5">
-          <StateGroupIcon stateGroup={state?.group ?? "backlog"} color={state?.color} />
-          <div className="text-xs">{state?.name ?? "State"}</div>
+          <StateGroupIcon stateGroup={state.group} />
+          <div className="text-xs">{state.name}</div>
         </div>
       </div>
     </Tooltip>
