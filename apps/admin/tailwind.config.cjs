17c50f725b6b40bc234e42c99654197d3b4e36cd
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2025-12-02 19:07:24 +0530

    chore: update all configs

--- apps/admin/tailwind.config.cjs
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-const sharedConfig = require("@plane/tailwind-config/tailwind.config.js");
-
-module.exports = {
-  presets: [sharedConfig],
-};
