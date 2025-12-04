bc96d34cb76a6cbc03955a268730e925e4e4370b
Prateek Shourya <prateekshourya29@gmail.com>
2025-12-04 15:36:39 +0530

    [WEB-5576] fix: remove optionsClassName from email and workspace forms (#8240)

--- apps/admin/app/(all)/(dashboard)/workspace/create/form.tsx
+++ apps/admin/app/(all)/(dashboard)/workspace/create/form.tsx
@@ -177,7 +177,6 @@ export function WorkspaceCreateForm() {
                   }
                   buttonClassName="!border-[0.5px] !border-custom-border-200 !shadow-none"
                   input
-                  optionsClassName="w-full"
                 >
                   {ORGANIZATION_SIZE.map((item) => (
                     <CustomSelect.Option key={item} value={item}>
