7183906e671ae9bc2b3002b577517d9f3d5fd0ce
Vipin Chaudhary <VipinChaudhary1809@gmail.com>
2025-12-10 20:05:00 +0530

    Chore update editor design system (#8299)
    * refactor: update styles for callout, color selector, logo selector, and image uploader
    
    * refactor:fix image

--- packages/editor/src/core/extensions/code/code-block-node-view.tsx
+++ packages/editor/src/core/extensions/code/code-block-node-view.tsx
@@ -53,7 +53,7 @@ export function CodeBlockComponent({ node }: Props) {
         </button>
       </Tooltip>
 
-      <pre className="bg-surface-2 text-primary rounded-lg p-4 my-2">
+      <pre className="bg-layer-2 text-primary rounded-lg p-4 my-2">
         <NodeViewContent as="code" className="whitespace-pre-wrap" />
       </pre>
     </NodeViewWrapper>
