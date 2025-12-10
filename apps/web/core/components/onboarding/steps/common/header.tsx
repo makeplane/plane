917b8a6e335a472130945d570d3d6f14d8f25420
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2025-12-10 15:27:53 +0530

    chore: auth and onboarding design token updated

--- apps/web/core/components/onboarding/steps/common/header.tsx
+++ apps/web/core/components/onboarding/steps/common/header.tsx
@@ -8,8 +8,8 @@ type Props = {
 export function CommonOnboardingHeader({ title, description }: Props) {
   return (
     <div className="text-left space-y-2">
-      <h1 className="text-20 font-semibold text-secondary">{title}</h1>
-      <p className="text-14 text-tertiary">{description}</p>
+      <h1 className="text-h4-semibold text-primary">{title}</h1>
+      <p className="text-body-md-regular text-tertiary">{description}</p>
     </div>
   );
 }
