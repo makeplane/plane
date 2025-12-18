17993e67745131790eef1c15b2be85b05e1b7e56
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2025-12-18 18:34:15 +0530

    fix: space app default background

--- apps/space/app/not-found.tsx
+++ apps/space/app/not-found.tsx
@@ -3,9 +3,9 @@ import SomethingWentWrongImage from "@/app/assets/something-went-wrong.svg?url";
 
 function NotFound() {
   return (
-    <div className="h-screen w-screen grid place-items-center">
+    <div className="h-screen w-screen grid place-items-center bg-surface-1">
       <div className="text-center">
-        <div className="mx-auto size-32 md:size-52 grid place-items-center rounded-full bg-layer-1">
+        <div className="mx-auto size-32 md:size-52 grid place-items-center rounded-full">
           <div className="size-16 md:size-32 grid place-items-center">
             <img src={SomethingWentWrongImage} alt="Something went wrong" width={128} height={128} />
           </div>
