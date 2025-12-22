1fc18e70110769853d13bb6bed6778c8e7e7b756
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2025-12-22 15:13:39 +0530

    fix: editor image full screen modal

--- packages/editor/src/core/extensions/custom-image/components/toolbar/alignment.tsx
+++ packages/editor/src/core/extensions/custom-image/components/toolbar/alignment.tsx
@@ -34,7 +34,7 @@ export function ImageAlignmentAction(props: Props) {
       <Tooltip disabled={isTouchDevice} tooltipContent="Align">
         <button
           type="button"
-          className="h-full flex items-center gap-1 text-on-color/60 hover:text-on-color transition-colors"
+          className="h-full flex items-center gap-1 text-alpha-white-800 hover:text-alpha-white-1200 transition-colors"
           onClick={() => setIsDropdownOpen((prev) => !prev)}
         >
           {activeAlignmentDetails && <activeAlignmentDetails.icon className="flex-shrink-0 size-3" />}
@@ -47,7 +47,7 @@ export function ImageAlignmentAction(props: Props) {
             <Tooltip disabled={isTouchDevice} key={option.value} tooltipContent={option.label}>
               <button
                 type="button"
-                className="flex-shrink-0 h-full grid place-items-center text-on-color/60 hover:text-on-color transition-colors"
+                className="flex-shrink-0 h-full grid place-items-center text-alpha-white-800 hover:text-alpha-white-1200 transition-colors"
                 onClick={() => {
                   handleChange(option.value);
                   setIsDropdownOpen(false);
