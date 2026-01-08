2e380dc38bb59485b8f181ae618c5f731a43c256
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2026-01-08 15:51:44 +0530

    chore: members dropdown updated

--- apps/web/core/components/dropdowns/member/base.tsx
+++ apps/web/core/components/dropdowns/member/base.tsx
@@ -1,3 +1,16 @@
+/**
+ * SPDX-FileCopyrightText: 2023-present Plane Software, Inc.
+ * SPDX-License-Identifier: LicenseRef-Plane-Commercial
+ *
+ * Licensed under the Plane Commercial License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * https://plane.so/legals/eula
+ *
+ * DO NOT remove or modify this notice.
+ * NOTICE: Proprietary and confidential. Unauthorized use or distribution is prohibited.
+ */
+
 import { useRef, useState } from "react";
 import { observer } from "mobx-react";
 import type { LucideIcon } from "lucide-react";
@@ -177,6 +190,7 @@ export const MemberDropdownBase = observer(function MemberDropdownBase(props: TM
           optionsClassName={optionsClassName}
           placement={placement}
           referenceElement={referenceElement}
+          value={value}
         />
       )}
     </ComboDropDown>
--- apps/web/core/components/dropdowns/member/member-options.tsx
+++ apps/web/core/components/dropdowns/member/member-options.tsx
@@ -11,7 +11,7 @@ import { CheckIcon, SearchIcon, SuspendedUserIcon } from "@plane/propel/icons";
 import { EPillSize, EPillVariant, Pill } from "@plane/propel/pill";
 import type { IUserLite } from "@plane/types";
 import { Avatar } from "@plane/ui";
-import { cn, getFileURL } from "@plane/utils";
+import { cn, getFileURL, sortByCurrentUserThenSelected } from "@plane/utils";
 // hooks
 import { useMember } from "@/hooks/store/use-member";
 import { useUser } from "@/hooks/store/user";
@@ -26,6 +26,7 @@ interface Props {
   optionsClassName?: string;
   placement: Placement | undefined;
   referenceElement: HTMLButtonElement | null;
+  value?: string[] | string | null;
 }
 
 export const MemberOptions = observer(function MemberOptions(props: Props) {
@@ -37,6 +38,7 @@ export const MemberOptions = observer(function MemberOptions(props: Props) {
     optionsClassName = "",
     placement,
     referenceElement,
+    value,
   } = props;
   // router
   const { workspaceSlug } = useParams();
@@ -111,8 +113,11 @@ export const MemberOptions = observer(function MemberOptions(props: Props) {
     })
     .filter((o) => !!o);
 
-  const filteredOptions =
-    query === "" ? options : options?.filter((o) => o?.query.toLowerCase().includes(query.toLowerCase()));
+  const filteredOptions = sortByCurrentUserThenSelected(
+    query === "" ? options : options?.filter((o) => o?.query.toLowerCase().includes(query.toLowerCase())),
+    value,
+    currentUser?.id
+  );
 
   return createPortal(
     <Combobox.Options data-prevent-outside-click static>
