0460b3c00bba9aafa81960a0b4af9d616f61b206
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2026-01-15 18:47:29 +0530

    refactor: removed unnecessary line breaks

--- packages/types/src/settings.ts
+++ packages/types/src/settings.ts
@@ -5,7 +5,6 @@ import type { EUserWorkspaceRoles } from "./workspace";
 export type TProfileSettingsTabs = "general" | "preferences" | "activity" | "notifications" | "security" | "api-tokens";
 
 export type TWorkspaceSettingsTabs = "general" | "members" | "billing-and-plans" | "export" | "webhooks";
-
 export type TWorkspaceSettingsItem = {
   key: TWorkspaceSettingsTabs;
   i18n_label: string;
@@ -22,7 +21,6 @@ export type TProjectSettingsTabs =
   | "labels"
   | "estimates"
   | "automations";
-
 export type TProjectSettingsItem = {
   key: TProjectSettingsTabs;
   i18n_label: string;
