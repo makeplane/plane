2c9ae1eaf1aeff7a8935fd549f1039fe7d3d405b
Aaryan Khandelwal <aaryankhandu123@gmail.com>
2026-01-21 14:40:24 +0530

    refactor: settings headings

--- apps/web/core/components/settings/heading.tsx
+++ apps/web/core/components/settings/heading.tsx
@@ -13,7 +13,7 @@ export function SettingsHeading({ className, control, description, title, varian
   return (
     <div className={cn("flex flex-col md:flex-row gap-4 items-start md:items-center justify-between", className)}>
       <div className="flex flex-col items-start gap-1">
-        {typeof title === "string" ? (
+        {title && (
           <h3
             className={cn("text-h3-medium text-primary", {
               "text-h3-medium": variant === "h3",
@@ -23,8 +23,6 @@ export function SettingsHeading({ className, control, description, title, varian
           >
             {title}
           </h3>
-        ) : (
-          title
         )}
         {description && <p className="text-body-xs-regular text-tertiary">{description}</p>}
       </div>
--- apps/web/core/components/settings/profile/heading.tsx
+++ apps/web/core/components/settings/profile/heading.tsx
@@ -12,7 +12,7 @@ export function ProfileSettingsHeading({ className, control, description, title 
   return (
     <div className={cn("flex flex-col md:flex-row gap-4 items-start md:items-center justify-between", className)}>
       <div className="flex flex-col items-start gap-1">
-        {typeof title === "string" ? <h6 className="text-h6-medium text-primary">{title}</h6> : title}
+        {title && <h6 className="text-h6-medium text-primary">{title}</h6>}
         {description && <p className="text-body-xs-regular text-tertiary">{description}</p>}
       </div>
       {control}
