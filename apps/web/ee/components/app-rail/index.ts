22b1aad649ca96e0de715c48d10f11743d2e59b7
sriramveeraghanta <veeraghanta.sriram@gmail.com>
2026-02-10 19:31:00 +0530

    fix: remove ee folder from web

--- apps/web/ee/components/app-rail/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Copyright (c) 2023-present Plane Software, Inc. and contributors
- * SPDX-License-Identifier: AGPL-3.0-only
- * See the LICENSE file for details.
- */
-
-export * from "ce/components/app-rail";
