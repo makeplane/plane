f6492389a7743861577fad25fe0d5f21d702f4bb
sriram veeraghanta <veeraghanta.sriram@gmail.com>
2026-02-16 15:43:23 +0530

    chore: copy cloud implement to ee (#5894)

--- apps/web/app/(all)/sign-up/page.tsx
+++ apps/web/app/(all)/sign-up/page.tsx
@@ -11,6 +11,8 @@
  * NOTICE: Proprietary and confidential. Unauthorized use or distribution is prohibited.
  */
 
+import { GOOGLE_ANALYTICS_ID } from "@plane/constants";
+
 // components
 import { AuthBase } from "@/components/auth-screens/auth-base";
 // helpers
@@ -24,6 +26,27 @@ export const clientMiddleware = [redirectIfUserIsAuthenticated];
 function SignUpPage() {
   return (
     <DefaultLayout>
+      {GOOGLE_ANALYTICS_ID && (
+        <>
+          <script src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`} id="google-analytics" />
+          <script
+            async
+            src="https://tag.clearbitscripts.com/v1/pk_12bcecff2ad52af4201b104045511543/tags.js"
+            referrerPolicy="strict-origin-when-cross-origin"
+          />
+          <script
+            id="google-analytics-config"
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GOOGLE_ANALYTICS_ID}');
+          `,
+            }}
+          />
+        </>
+      )}
       <AuthBase authType={EAuthModes.SIGN_UP} />
     </DefaultLayout>
   );
