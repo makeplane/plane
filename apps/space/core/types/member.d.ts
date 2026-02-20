8c23fdd1d865f9bf3fd7edc7c7f2a60828523216
sriram veeraghanta <veeraghanta.sriram@gmail.com>
2026-02-20 18:33:45 +0530

    fix: Member Information Disclosure via Public Endpoint #8646

--- apps/space/core/types/member.d.ts
+++ apps/space/core/types/member.d.ts
@@ -1,10 +1,6 @@
 export type TPublicMember = {
   id: string;
   member: string;
-  member__avatar: string;
-  member__first_name: string;
-  member__last_name: string;
   member__display_name: string;
-  project: string;
-  workspace: string;
+  member__avatar: string;
 };
