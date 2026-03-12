9406a9cef7893c786c107a6fddb39b0f2b049f8a
Anmol Singh Bhatia <anmolsinghbhatia@plane.so>
2026-03-12 22:28:24 +0530

    chore: code refactoring

--- apps/web/core/components/instance/not-ready-view.tsx
+++ apps/web/core/components/instance/not-ready-view.tsx
@@ -1,14 +1,7 @@
 /**
- * SPDX-FileCopyrightText: 2023-present Plane Software, Inc.
- * SPDX-License-Identifier: LicenseRef-Plane-Commercial
- *
- * Licensed under the Plane Commercial License (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * https://plane.so/legals/eula
- *
- * DO NOT remove or modify this notice.
- * NOTICE: Proprietary and confidential. Unauthorized use or distribution is prohibited.
+ * Copyright (c) 2023-present Plane Software, Inc. and contributors
+ * SPDX-License-Identifier: AGPL-3.0-only
+ * See the LICENSE file for details.
  */
 
 import Link from "next/link";
