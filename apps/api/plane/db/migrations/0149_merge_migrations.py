6cb9b78f9bd08dd63bd33bd61348de6bf585bafb
ngocyt001 <ngocyt001@gmail.com>
2026-03-13 19:52:21 +0700

    fix: migration 149

---
+++ apps/api/plane/db/migrations/0149_merge_migrations.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("db", "0144_merge_0140_department_dept_type_0143_project_is_bank_wide"),
+        ("db", "0148_seed_default_project_views"),
+    ]
+
+    operations = []
