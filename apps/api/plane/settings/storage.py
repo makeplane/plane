44b643c8a8edb2a8d92e7922042648ce17bac219
Vikram Vaswani <2571660+vvaswani@users.noreply.github.com>
2026-03-19 02:56:42 +0530

    fix: handle server-side S3 storage initialization for generic assets
    Signed-off-by: Vikram Vaswani <2571660+vvaswani@users.noreply.github.com>

--- apps/api/plane/settings/storage.py
+++ apps/api/plane/settings/storage.py
@@ -22,7 +22,7 @@ class S3Storage(S3Boto3Storage):
 
     """S3 storage class to generate presigned URLs for S3 objects"""
 
-    def __init__(self, request=None):
+    def __init__(self, request=None, is_server=False):
         # Get the AWS credentials and bucket name from the environment
         self.aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
         # Use the AWS_SECRET_ACCESS_KEY environment variable for the secret key
@@ -42,13 +42,17 @@ class S3Storage(S3Boto3Storage):
                 endpoint_protocol = "https"
             else:
                 endpoint_protocol = request.scheme if request else "http"
+
+            endpoint_url = self.aws_s3_endpoint_url
+            if request and not is_server:
+                endpoint_url = f"{endpoint_protocol}://{request.get_host()}"
             # Create an S3 client for MinIO
             self.s3_client = boto3.client(
                 "s3",
                 aws_access_key_id=self.aws_access_key_id,
                 aws_secret_access_key=self.aws_secret_access_key,
                 region_name=self.aws_region,
-                endpoint_url=(f"{endpoint_protocol}://{request.get_host()}" if request else self.aws_s3_endpoint_url),
+                endpoint_url=endpoint_url,
                 config=boto3.session.Config(signature_version="s3v4"),
             )
         else:
