efa57b0b7d04eb5bec0bffe819267f75eb42b0e6
Aaryan Khandelwal <65252264+aaryan610@users.noreply.github.com>
2026-03-21 18:15:35 +0530

    [WEB-6689] chore: update mime types accepted by editor #6381

--- packages/editor/src/core/constants/config.ts
+++ packages/editor/src/core/constants/config.ts
@@ -24,6 +24,7 @@ export const DEFAULT_DISPLAY_CONFIG: TDisplayConfig = {
 export const ACCEPTED_IMAGE_MIME_TYPES = ["image/jpeg", "image/jpg", "image/png", "image/webp", "image/gif"];
 
 export const ACCEPTED_ATTACHMENT_MIME_TYPES = [
+  // Images
   "image/jpeg",
   "image/png",
   "image/gif",
@@ -31,6 +32,11 @@ export const ACCEPTED_ATTACHMENT_MIME_TYPES = [
   "image/webp",
   "image/tiff",
   "image/bmp",
+  // Netpbm formats
+  "image/x-portable-graymap",
+  "image/x-portable-bitmap",
+  "image/x-portable-pixmap",
+  // Documents
   "application/pdf",
   "application/msword",
   "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
@@ -40,6 +46,15 @@ export const ACCEPTED_ATTACHMENT_MIME_TYPES = [
   "application/vnd.openxmlformats-officedocument.presentationml.presentation",
   "text/plain",
   "application/rtf",
+  // ODF formats
+  "application/vnd.oasis.opendocument.spreadsheet",
+  "application/vnd.oasis.opendocument.text",
+  "application/vnd.oasis.opendocument.presentation",
+  "application/vnd.oasis.opendocument.graphics",
+  "application/vnd.oasis.opendocument.database",
+  // Microsoft Visio
+  "application/vnd.visio",
+  // Audio
   "audio/mpeg",
   "audio/wav",
   "audio/ogg",
@@ -48,6 +63,7 @@ export const ACCEPTED_ATTACHMENT_MIME_TYPES = [
   "audio/aac",
   "audio/flac",
   "audio/x-m4a",
+  // Video
   "video/mp4",
   "video/mpeg",
   "video/ogg",
@@ -55,21 +71,39 @@ export const ACCEPTED_ATTACHMENT_MIME_TYPES = [
   "video/quicktime",
   "video/x-msvideo",
   "video/x-ms-wmv",
+  // Archives
   "application/zip",
+  "application/x-rar",
   "application/x-rar-compressed",
   "application/x-tar",
   "application/gzip",
+  "application/x-zip",
+  "application/x-zip-compressed",
+  "application/x-7z-compressed",
+  "application/x-compressed",
+  "application/x-compressed-tar",
+  "application/x-compressed-tar-gz",
+  "application/x-compressed-tar-bz2",
+  "application/x-compressed-tar-zip",
+  "application/x-compressed-tar-7z",
+  "application/x-compressed-tar-rar",
+  // 3D Models
   "model/gltf-binary",
   "model/gltf+json",
   "application/octet-stream",
+  // Fonts
   "font/ttf",
   "font/otf",
   "font/woff",
   "font/woff2",
+  // Other
   "text/css",
   "text/javascript",
   "application/json",
   "text/xml",
   "text/csv",
   "application/xml",
+  "application/x-sql",
+  "application/x-gzip",
+  "text/markdown",
 ];
