0952722fca26ace071fd8cc1388445d12f7b05d6
b-saikrishnakanth <130811169+b-saikrishnakanth@users.noreply.github.com>
2026-03-30 18:44:31 +0530

    [WEB-6698] fix: fetch work item properties for peek view in home screen #6451

--- apps/web/core/components/home/peek-overviews.tsx
+++ apps/web/core/components/home/peek-overviews.tsx
@@ -12,17 +12,34 @@
  */
 
 import { observer } from "mobx-react";
+import useSWR from "swr";
 // plane imports
 import { EIssueServiceType } from "@plane/types";
 import { IssuePeekOverview } from "@/components/issues/peek-overview";
 // hooks
 import { useIssueDetail } from "@/hooks/store/use-issue-detail";
+import { useProjectIssueProperties } from "@/hooks/use-project-issue-properties";
 // plane web imports
 import { EpicPeekOverview } from "@/components/epics/peek-overview";
 
 export const HomePeekOverviewsRoot = observer(function HomePeekOverviewsRoot() {
   const { peekIssue } = useIssueDetail();
   const { peekIssue: epicPeekIssue } = useIssueDetail(EIssueServiceType.EPICS);
+
+  const { fetchAll } = useProjectIssueProperties();
+
+  const workspaceSlug = peekIssue?.workspaceSlug || epicPeekIssue?.workspaceSlug;
+  const projectId = peekIssue?.projectId || epicPeekIssue?.projectId;
+
+  // fetching all project work item properties
+  useSWR(
+    workspaceSlug && projectId ? `WORK_ITEM_PROPERTIES_${workspaceSlug}_${projectId}` : null,
+    workspaceSlug && projectId ? () => fetchAll(workspaceSlug, projectId) : null,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+    }
+  );
 
   return (
     <>
