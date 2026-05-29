a941f73b10c0328eadc5431b074624db0f714082
ngocyt001 <ngocyt001@gmail.com>
2026-05-29 19:09:41 +0700

    fix(onboarding): rebrand sidebar greeting from Plane to Shinhan Workspace

--- apps/web/ce/components/onboarding/tour/sidebar.tsx
+++ apps/web/ce/components/onboarding/tour/sidebar.tsx
@@ -53,7 +53,7 @@ export function TourSidebar({ step, setStep }: Props) {
       <h3 className="text-16 font-medium">
         Let{"'"}s get started!
         <br />
-        Get more out of Plane.
+        Get more out of Shinhan Workspace.
       </h3>
       <div className="mt-8 space-y-5">
         {sidebarOptions.map((option) => (
