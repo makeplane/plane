b53b505e0e7a03e6e02be9ad1862984f3d8aaf0e
Bluestarcodeshere <suryakirangol7@gmail.com>
2026-07-02 03:25:34 +0530

    fix: remove special characters restriction from project name

--- apps/api/plane/api/serializers/project.py
+++ apps/api/plane/api/serializers/project.py
@@ -105,11 +105,7 @@ class ProjectCreateSerializer(BaseSerializer):
         ]
 
     def validate(self, data):
-        project_name = data.get("name", None)
         project_identifier = data.get("identifier", None)
-
-        if project_name is not None and re.match(Project.FORBIDDEN_IDENTIFIER_CHARS_PATTERN, project_name):
-            raise serializers.ValidationError("Project name cannot contain special characters.")
 
         if project_identifier is not None and re.match(Project.FORBIDDEN_IDENTIFIER_CHARS_PATTERN, project_identifier):
             raise serializers.ValidationError("Project identifier cannot contain special characters.")
@@ -180,11 +176,7 @@ class ProjectUpdateSerializer(ProjectCreateSerializer):
         read_only_fields = ProjectCreateSerializer.Meta.read_only_fields
 
     def update(self, instance, validated_data):
-        project_name = validated_data.get("name", None)
         project_identifier = validated_data.get("identifier", None)
-
-        if project_name is not None and re.match(Project.FORBIDDEN_IDENTIFIER_CHARS_PATTERN, project_name):
-            raise serializers.ValidationError("Project name cannot contain special characters.")
 
         if project_identifier is not None and re.match(Project.FORBIDDEN_IDENTIFIER_CHARS_PATTERN, project_identifier):
             raise serializers.ValidationError("Project identifier cannot contain special characters.")
@@ -239,11 +231,7 @@ class ProjectSerializer(BaseSerializer):
         ]
 
     def validate(self, data):
-        project_name = data.get("name", None)
         project_identifier = data.get("identifier", None)
-
-        if project_name is not None and re.match(Project.FORBIDDEN_IDENTIFIER_CHARS_PATTERN, project_name):
-            raise serializers.ValidationError("Project name cannot contain special characters.")
 
         if project_identifier is not None and re.match(Project.FORBIDDEN_IDENTIFIER_CHARS_PATTERN, project_identifier):
             raise serializers.ValidationError("Project identifier cannot contain special characters.")
