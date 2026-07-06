f19fc1969a7c835723707c275c09ab8748639f96
FilyBox <meno117@outlook.com>
2026-07-06 16:35:21 -0600

    chore: removed some help-menu's items

--- apps/web/core/components/workspace/sidebar/help-section/root.tsx
+++ apps/web/core/components/workspace/sidebar/help-section/root.tsx
@@ -48,19 +48,19 @@ export const HelpMenuRoot = observer(function HelpMenuRoot() {
         maxHeight="lg"
         closeOnSelect
       >
-        <CustomMenu.MenuItem onClick={() => window.open("https://go.plane.so/p-docs", "_blank")}>
+        {/* <CustomMenu.MenuItem onClick={() => window.open("https://go.plane.so/p-docs", "_blank")}>
           <div className="flex items-center gap-x-2 rounded-sm text-11">
             <PageIcon className="h-3.5 w-3.5 text-secondary" height={14} width={14} />
             <span className="text-11">{t("documentation")}</span>
           </div>
-        </CustomMenu.MenuItem>
-        <CustomMenu.MenuItem onClick={() => window.open("mailto:sales@plane.so", "_blank")}>
+        </CustomMenu.MenuItem> */}
+        {/* <CustomMenu.MenuItem onClick={() => window.open("mailto:sales@plane.so", "_blank")}>
           <div className="flex items-center gap-x-2 rounded-sm text-11">
             <User className="h-3.5 w-3.5 text-secondary" size={14} />
             <span className="text-11">{t("contact_sales")}</span>
           </div>
-        </CustomMenu.MenuItem>
-        <div className="my-1 border-t border-subtle" />
+        </CustomMenu.MenuItem> */}
+        {/* <div className="my-1 border-t border-subtle" /> */}
         <CustomMenu.MenuItem>
           <button
             type="button"
@@ -70,7 +70,7 @@ export const HelpMenuRoot = observer(function HelpMenuRoot() {
             <span className="text-11">{t("keyboard_shortcuts")}</span>
           </button>
         </CustomMenu.MenuItem>
-        <CustomMenu.MenuItem>
+        {/* <CustomMenu.MenuItem>
           <button
             type="button"
             onClick={() => setProductUpdatesModalOpen(true)}
@@ -78,12 +78,12 @@ export const HelpMenuRoot = observer(function HelpMenuRoot() {
           >
             <span className="text-11">{t("whats_new")}</span>
           </button>
-        </CustomMenu.MenuItem>
-        <CustomMenu.MenuItem onClick={() => window.open("https://forum.plane.so", "_blank", "noopener,noreferrer")}>
+        </CustomMenu.MenuItem> */}
+        {/* <CustomMenu.MenuItem onClick={() => window.open("https://forum.plane.so", "_blank", "noopener,noreferrer")}>
           <div className="flex items-center gap-x-2 rounded-sm text-11">
             <span className="text-11">Forum</span>
           </div>
-        </CustomMenu.MenuItem>
+        </CustomMenu.MenuItem> */}
         <div className="mt-1 border-t border-subtle px-1 pt-2 text-11 text-secondary">
           <PlaneVersionNumber />
         </div>
